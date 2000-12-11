aa0123261fba7565e09bcfbd1438594d9c6f3404
cox <>
2000-12-11 19:18:51 +0000

    *** empty log message ***

--- src/mri_cat2D.c
+++ src/mri_cat2D.c
@@ -1,10 +1,10 @@
-#include "mrilib.h"
-
 /*****************************************************************************
-  This software is copyrighted and owned by the Medical College of Wisconsin.
-  See the file README.Copyright for details.
+   Major portions of this software are copyrighted by the Medical College
+   of Wisconsin, 1994-2000, and are released under the Gnu General Public
+   License, Version 2.  See the file README.Copyright for details.
 ******************************************************************************/
 
+#include "mrilib.h"
 
 /*** NOT 7D SAFE ***/
 
--- src/mri_cfft.c
+++ src/mri_cfft.c
@@ -1,12 +1,12 @@
+/*****************************************************************************
+   Major portions of this software are copyrighted by the Medical College
+   of Wisconsin, 1994-2000, and are released under the Gnu General Public
+   License, Version 2.  See the file README.Copyright for details.
+******************************************************************************/
+
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-/*****************************************************************************
-  This software is copyrighted and owned by the Medical College of Wisconsin.
-  See the file README.Copyright for details.
-******************************************************************************/
-
 
 #ifndef PI
 #  define PI 3.1415926536
