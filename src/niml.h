912d34b96907ff642081d14fcd2b42854607f483
rwcox <>
2002-01-28 22:53:31 +0000

    Cput

--- src/niml.h
+++ src/niml.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/*-----------------------------------------*/
+
 /*! Holds strings from the <header and=attributes> */
 
 typedef struct {
@@ -57,5 +59,92 @@ typedef struct { int num; int *ar; } intarray ;
 #define IS_DATUM_CHAR(c) ( (c) == 'b' || (c) == 's' || (c) == 'i' ||  \
                            (c) == 'f' || (c) == 'd' || (c) == 'c' ||  \
                            (c) == 'r' || (c) == 'S' || (c) == 'L'    )
+
+/*-----------------------------------------*/
+
+/*! A data element. */
+
+typedef struct {
+   char  *name ;
+   int    attr_num ;
+   char **attr_lhs ;
+   char **attr_rhs ;
+   int    vec_num ;
+   int    vec_len ;
+   int   *vec_type ;
+   void **vec ;
+} NI_element ;
+
+/*! A bunch of elements. */
+
+typedef struct {
+   int    attr_num ;
+   char **attr_lhs ;
+   char **attr_rhs ;
+
+   int    part_num ;
+   int   *part_typ ;
+   void **part ;
+} NI_group ;
+
+/* Part type codes. */
+
+#define NI_ELEMENT  0
+#define NI_GROUP    1
+
+/*! Size of NI_stream buffer. */
+
+#define NI_BUFSIZE (64*1024)
+
+/*! Data needed to process input stream. */
+
+typedef struct {
+   void *user_handle ;
+   int (*getdata)(void *, char *, int) ;
+
+   int mode ;
+   int nbuf ;
+   char buf[NI_BUFSIZE] ;
+} NI_stream_type ;
+
+/* Modes for a NI_stream_type: input or output. */
+
+#define NI_INPUT_MODE  0
+#define NI_OUTPUT_MODE 1
+
+/*! Opaque type for the C API. */
+
+typedef NI_stream_type *NI_stream ;
+
+/*-------------- prototypes ---------------*/
+
+void * NI_malloc( size_t ) ;
+void   NI_free( void * ) ;
+void * NI_realloc( void * , size_t ) ;
+
+char * NI_strncpy( char * , const char * , size_t ) ;
+
+char * NI_type_name( int ) ;
+
+int NI_read_file  ( void *, char *, int ) ;
+int NI_read_socket( void *, char *, int ) ;
+
+int NI_write_file  ( void *, char *, int ) ;
+int NI_write_socket( void *, char *, int ) ;
+
+NI_stream NI_open_for_input ( int (*getdata)(void *, char *, int) , void * ) ;
+NI_stream NI_open_for_output( int (*putdata)(void *, char *, int) , void * ) ;
+
+void NI_close( NI_stream ) ;
+
+NI_group * NI_get_group( NI_stream ) ;
+
+int NI_put_group  ( NI_stream , NI_group *   ) ;
+int NI_put_element( NI_stream , NI_element * ) ;
+
+int NI_readcheck( NI_stream ) ;
+
+void NI_free_group  ( NI_group *   ) ;
+void NI_free_element( NI_element * ) ;
 
 #endif /* _NIML_HEADER_FILE */
