ebb84decae06c6aae3c71ccd5caef70d26f055c9
rwcox <>
2002-03-01 01:36:18 +0000

    Cadd

---
+++ src/afni_niml.c
@@ -0,0 +1,154 @@
+#include "mrilib.h"
+#include "niml.h"
+
+/**************************************/
+/** global data for NIML connections **/
+/**************************************/
+
+/*---------------------------------------*/
+/*! Number of streams on which to listen */
+#define NUM_NIML   1
+
+/*--------------------------------------*/
+/*! Array of streams on which to listen */
+
+static NI_stream ns_listen[NUM_NIML] ;
+
+/*------------------------*/
+/*! Array of stream names */
+
+static char ns_name[NUM_NIML][64] ;
+
+/*-------------------------------------*/
+/*! The SUMA stream index in ns_listen */
+
+#define NS_SUMA 0
+
+/*-------------------------*/
+
+#ifndef SUMA_TCP_PORT
+#define SUMA_TCP_PORT 53211
+#endif
+
+/*-----------------------------------------------*/
+/*! Flag to tell if NIML things are initialized. */
+
+static int started = 0 ;
+
+/*---------------------*/
+/* Internal prototypes */
+
+static Boolean AFNI_niml_workproc( XtPointer ) ;
+
+/*-----------------------------------------------------------------------*/
+/*! Routine executed at AFNI exit: shutdown all open NI_stream.
+-------------------------------------------------------------------------*/
+
+static void AFNI_niml_exit( void )
+{
+   int cc ;
+   for( cc=0 ; cc < NUM_NIML ; cc++ )        /* close any open sockets */
+      NI_stream_close( ns_listen[cc] ) ;
+   return ;
+}
+
+/*-----------------------------------------------------------------------*/
+/*! Initialize NIML listening.
+-------------------------------------------------------------------------*/
+
+void AFNI_init_niml( void )
+{
+   int cc ;
+
+ENTRY("AFNI_init_niml") ;
+
+   if( started ) EXRETURN ;
+
+fprintf(stderr,"AFNI_init_niml\n") ;
+
+   PLUTO_register_workproc( AFNI_niml_workproc , NULL ) ;
+   atexit( AFNI_niml_exit ) ;
+
+   for( cc=0 ; cc < NUM_NIML ; cc++ ) ns_listen[cc] = NULL ;
+
+   sprintf(ns_name[0] , "tcp:host:%d" , SUMA_TCP_PORT ) ;
+
+   started = 1 ; EXRETURN ;
+}
+
+/*-----------------------------------------------------------------------*/
+/*! NIML workprocess.
+    - Listen for new incoming connections on any non-open connections.
+    - Read and process any new data from open connections.
+
+  (If the return is True, that means don't call this workproc again.
+   If the return is False, that means call this workproc again.......)
+-------------------------------------------------------------------------*/
+
+static Boolean AFNI_niml_workproc( XtPointer elvis )
+{
+   int cc , nn ;
+   void *nini ;
+   NI_stream ns_err ;
+
+   /** loop over input NIML streams **/
+
+   for( cc=0 ; cc < NUM_NIML ; cc++ ){
+
+     /* open streams that aren't open */
+
+     if( ns_listen[cc] == NULL ){
+fprintf(stderr,"AFNI_niml_workproc: opening %s\n",ns_name[cc]) ;
+       ns_listen[cc] = NI_stream_open( ns_name[cc] , "r" ) ;
+     }
+
+     /* check if stream has gone bad */
+
+     nn = NI_stream_goodcheck( ns_listen[cc] , 1 ) ;
+
+     if( nn < 0 ){                          /* is bad */
+       NI_stream_close( ns_listen[cc] ) ;
+       ns_listen[cc] = NULL ;
+       continue ;              /* skip to next stream */
+     }
+
+     if( nn == 0 ) continue ;  /* waiting: skip to next stream */
+
+     /* if here, stream is good;
+        see if there is any data to be read */
+
+     nn = NI_stream_readcheck( ns_listen[cc] , 1 ) ;
+
+     if( nn > 0 ){                                   /* has data */
+       nini = NI_read_element( ns_listen[cc] , 1 ) ;  /* read it */
+
+       switch( NI_element_type(nini) ){            /* process it */
+         default: break ;                     /* NULL or unknown */
+
+         case NI_ELEMENT_TYPE:{                  /* data element */
+           NI_element *nel = (NI_element *) nini ;
+           ns_err = NI_stream_open( "fd:2" , "w" ) ;
+           if( ns_err != NULL ){
+             NI_write_element( ns_err , nel , NI_TEXT_MODE ) ;
+             NI_stream_close( ns_err ) ;
+           }
+           NI_free_element( nel ) ;
+         }
+         break ;
+
+         case NI_GROUP_TYPE:{                   /* group element */
+           NI_element *ngr = (NI_element *) nini ;
+           ns_err = NI_stream_open( "fd:2" , "w" ) ;
+           if( ns_err != NULL ){
+             NI_write_element( ns_err , ngr , NI_TEXT_MODE ) ;
+             NI_stream_close( ns_err ) ;
+           }
+           NI_free_element( ngr ) ;
+         }
+         break ;
+       }
+     }
+   }
+
+   return False ;
+}
---
+++ src/suma_niml.c
@@ -0,0 +1,118 @@
+#include "niml.h"
+#include "suma_types.h"
+
+/*------------------------------------------------------------------*/
+/*! Make a NIML data element from an array of SUMA_ixyz structs.
+    Return value is NULL if you input stupid values.  Otherwise,
+    will return a populated element. Example:
+     - int    num_ixyz=999 ;
+     - SUMA_ixyz *ixyz=malloc(sizeof(SUMA_ixyz)*num_ixyz) ;
+     - NI_element *nel ;
+     - NI_stream *ns ;
+     - ** fill ixyz[0..998].stuff somehow **
+     - nel = SUMA_ixyz_to_NIML( num_ixyz , ixyz ) ;
+     - NI_write_element( ns , nel , NI_TEXT_MODE ) ;
+     - NI_free_element( nel ) ;
+--------------------------------------------------------------------*/
+
+NI_element * SUMA_ixyz_to_NIML( int num_ixyz , SUMA_ixyz *ixyz )
+{
+   NI_element *nel ;
+   int ii , *ic ;
+   float *xc,*yc,*zc ;
+
+   /* check inputs for sanity */
+
+   if( num_ixyz < 1 || ixyz == NULL ) return NULL ;  /* stupid caller */
+
+   /* make a new data element, to be filled by columns */
+
+   nel = NI_new_data_element( "SUMA_ixyz" , num_ixyz ) ;
+
+   /* make the temp columns to be put in the element */
+
+   ic = (int *)   NI_malloc( sizeof(int)   * num_ixyz ) ;
+   xc = (float *) NI_malloc( sizeof(float) * num_ixyz ) ;
+   yc = (float *) NI_malloc( sizeof(float) * num_ixyz ) ;
+   zc = (float *) NI_malloc( sizeof(float) * num_ixyz ) ;
+
+   /* load these columns from the struct array */
+
+   for( ii=0 ; ii < num_ixyz ; ii++ ){
+      ic[ii] = ixyz[ii].id ;
+      xc[ii] = ixyz[ii].x ;
+      yc[ii] = ixyz[ii].y ;
+      zc[ii] = ixyz[ii].z ;
+   }
+
+   /* copy columns into NI_element, freeing them when done */
+
+   NI_add_column( nel , NI_INT   , ic ) ; free(ic) ;
+   NI_add_column( nel , NI_FLOAT , xc ) ; free(xc) ;
+   NI_add_column( nel , NI_FLOAT , yc ) ; free(yc) ;
+   NI_add_column( nel , NI_FLOAT , zc ) ; free(zc) ;
+
+   return nel ;
+}
+
+/*------------------------------------------------------------------*/
+/*! Unload a <SUMA_ixyz> NI_element into an array of newly
+    malloc()-ed SUMA_ixyz structs.
+    Return value is number of structs (will be 0 inputs are bad).
+    *ixyz will point to the output array if the number of structs is
+    positive.  This array will be newly malloc()-ed.  Example:
+     - SUMA_ixyz *ixyz ;
+     - int    num_ixyz ;
+     - NI_element *nel ;  ** get this from somewhere **
+     - num_ixyz = NIML_to_SUMA_ixyz( nel , &ixyz ) ;
+     - if( num_ixyz == 0 ){ ** error error error ** }
+     - else               { ** good good good ** }
+     - NI_free_element(nel) ;
+--------------------------------------------------------------------*/
+
+int NIML_to_SUMA_ixyz( NI_element *nel , SUMA_ixyz **ixyz )
+{
+   int   num_ixyz ;       /* return value */
+   SUMA_ixyz *myixyz ;    /* output array of structs */
+   int   *ic , ii ;
+   float *xc, *yc, *zc ;
+
+   /* check element for correctness */
+
+   if( nel             == 0        ||   /* no data element?          */
+       nel->vec_len    <  1        ||   /* empty element?             */
+       nel->vec_filled <  1        ||   /* no data was filled in?      */
+       nel->vec_num    <  4        ||   /* less than 4 columns?         */
+       nel->vec_typ[0] != NI_INT   ||   /* must be int,float,float,float */
+       nel->vec_typ[1] != NI_FLOAT ||
+       nel->vec_typ[2] != NI_FLOAT ||
+       nel->vec_typ[3] != NI_FLOAT   ) return 0 ;  /* bad bad bad */
+
+   /* number of structs is number of completely filled rows */
+
+   num_ixyz = nel->vec_filled ;
+
+   /* make space for the new structs */
+
+   myixyz = (SUMA_ixyz *) NI_malloc( sizeof(SUMA_ixyz) * num_ixyz ) ;
+
+   /* pointers to the data columns in the NI_element */
+
+   ic = (int *)   nel->vec[0] ;
+   xc = (float *) nel->vec[1] ;
+   yc = (float *) nel->vec[2] ;
+   zc = (float *) nel->vec[3] ;
+
+   /* load the values from the element */
+
+   for( ii=0 ; ii < num_ixyz ; ii++ ){
+      myixyz[ii].id = ic[ii] ;
+      myixyz[ii].x  = xc[ii] ;
+      myixyz[ii].y  = yc[ii] ;
+      myixyz[ii].z  = zc[ii] ;
+   }
+
+   /* we is done */
+
+   *ixyz = myixyz ; return num_ixyz ;
+}
