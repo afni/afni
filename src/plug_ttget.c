fae5cf1f53579ae2f63a2eb6a186539f44e19386
rwcox <>
2002-04-05 18:18:21 +0000

    Cput

--- src/plug_ttget.c
+++ src/plug_ttget.c
@@ -15,49 +15,49 @@ static float coronal_yy[] = {
     40,  45,  50,  55,  60,  65
 } ;
 static char * coronal_ff[] = {
- "tt_corm99.ppm.gz" , "tt_corm95.ppm.gz" , "tt_corm90.ppm.gz" , "tt_corm85.ppm.gz" ,
- "tt_corm80.ppm.gz" , "tt_corm75.ppm.gz" , "tt_corm70.ppm.gz" , "tt_corm65.ppm.gz" ,
- "tt_corm60.ppm.gz" , "tt_corm55.ppm.gz" , "tt_corm50.ppm.gz" , "tt_corm45.ppm.gz" ,
- "tt_corm40.ppm.gz" , "tt_corm35.ppm.gz" , "tt_corm32.ppm.gz" , "tt_corm28.ppm.gz" ,
- "tt_corm24.ppm.gz" , "tt_corm20.ppm.gz" , "tt_corm16.ppm.gz" , "tt_corm12.ppm.gz" ,
- "tt_corm08.ppm.gz" , "tt_corm04.ppm.gz" , "tt_corp00.ppm.gz" , "tt_corp04.ppm.gz" ,
- "tt_corp08.ppm.gz" , "tt_corp12.ppm.gz" , "tt_corp16.ppm.gz" , "tt_corp20.ppm.gz" ,
- "tt_corp24.ppm.gz" , "tt_corp28.ppm.gz" , "tt_corp32.ppm.gz" , "tt_corp35.ppm.gz" ,
- "tt_corp40.ppm.gz" , "tt_corp45.ppm.gz" , "tt_corp50.ppm.gz" , "tt_corp55.ppm.gz" ,
- "tt_corp60.ppm.gz" , "tt_corp65.ppm.gz"
+ "tt_corm99.ppm.gz", "tt_corm95.ppm.gz", "tt_corm90.ppm.gz", "tt_corm85.ppm.gz",
+ "tt_corm80.ppm.gz", "tt_corm75.ppm.gz", "tt_corm70.ppm.gz", "tt_corm65.ppm.gz",
+ "tt_corm60.ppm.gz", "tt_corm55.ppm.gz", "tt_corm50.ppm.gz", "tt_corm45.ppm.gz",
+ "tt_corm40.ppm.gz", "tt_corm35.ppm.gz", "tt_corm32.ppm.gz", "tt_corm28.ppm.gz",
+ "tt_corm24.ppm.gz", "tt_corm20.ppm.gz", "tt_corm16.ppm.gz", "tt_corm12.ppm.gz",
+ "tt_corm08.ppm.gz", "tt_corm04.ppm.gz", "tt_corp00.ppm.gz", "tt_corp04.ppm.gz",
+ "tt_corp08.ppm.gz", "tt_corp12.ppm.gz", "tt_corp16.ppm.gz", "tt_corp20.ppm.gz",
+ "tt_corp24.ppm.gz", "tt_corp28.ppm.gz", "tt_corp32.ppm.gz", "tt_corp35.ppm.gz",
+ "tt_corp40.ppm.gz", "tt_corp45.ppm.gz", "tt_corp50.ppm.gz", "tt_corp55.ppm.gz",
+ "tt_corp60.ppm.gz", "tt_corp65.ppm.gz"
 } ;
 
 /*------------------------------------------------------------------------------*/
 
 #define SAGITTAL_NUM 18
 static float sagittal_xx[] = {
-    0,  3,  5,  9, 13, 17, 21, 25, 29, 33 ,
+    0,  3,  5,  9, 13, 17, 21, 25, 29, 33,
    37, 41, 43, 47, 51, 55, 59, 61
 } ;
 static char * sagittal_ff[] = {
- "tt_sag00g.ppm.gz", "tt_sag03.ppm.gz" , "tt_sag05.ppm.gz" , "tt_sag09.ppm.gz" ,
- "tt_sag13.ppm.gz" , "tt_sag17.ppm.gz" , "tt_sag21.ppm.gz" , "tt_sag25.ppm.gz" ,
- "tt_sag29.ppm.gz" , "tt_sag33.ppm.gz" , "tt_sag37.ppm.gz" , "tt_sag41.ppm.gz" ,
- "tt_sag43.ppm.gz" , "tt_sag47.ppm.gz" , "tt_sag51.ppm.gz" , "tt_sag55.ppm.gz" ,
- "tt_sag59.ppm.gz" , "tt_sag61.ppm.gz"
+ "tt_sag00g.ppm.gz", "tt_sag03.ppm.gz", "tt_sag05.ppm.gz", "tt_sag09.ppm.gz",
+ "tt_sag13.ppm.gz", "tt_sag17.ppm.gz", "tt_sag21.ppm.gz", "tt_sag25.ppm.gz",
+ "tt_sag29.ppm.gz", "tt_sag33.ppm.gz", "tt_sag37.ppm.gz", "tt_sag41.ppm.gz",
+ "tt_sag43.ppm.gz", "tt_sag47.ppm.gz", "tt_sag51.ppm.gz", "tt_sag55.ppm.gz",
+ "tt_sag59.ppm.gz", "tt_sag61.ppm.gz"
 } ;
 
 /*------------------------------------------------------------------------------*/
 
 #define AXIAL_NUM 27
 static float axial_zz[] = {
-   -40 , -36 , -32 , -28 , -24 , -20 , -16 , -12 , -8 , -4 ,
-    -1 ,   1 ,   4 ,   8 ,  12 ,  16 ,  20 ,  24 , 28 , 32 , 35 ,
-    40 ,  45 ,  50 ,  55 ,  60 ,  65
+   -40, -36, -32, -28, -24, -20, -16, -12, -8, -4,
+    -1,   1,   4,   8,  12,  16,  20,  24, 28, 32, 35,
+    40,  45,  50,  55,  60,  65
 } ;
 static char * axial_ff[] = {
- "tt_horm40.ppm.gz" , "tt_horm36.ppm.gz" , "tt_horm32.ppm.gz" , "tt_horm28.ppm.gz" ,
- "tt_horm24.ppm.gz" , "tt_horm20.ppm.gz" , "tt_horm16.ppm.gz" , "tt_horm12.ppm.gz" ,
- "tt_horm08.ppm.gz" , "tt_horm04.ppm.gz" , "tt_horm01.ppm.gz" , "tt_horp01.ppm.gz" ,
- "tt_horp04.ppm.gz" , "tt_horp08.ppm.gz" , "tt_horp12.ppm.gz" , "tt_horp16.ppm.gz" ,
- "tt_horp20.ppm.gz" , "tt_horp24.ppm.gz" , "tt_horp28.ppm.gz" , "tt_horp32.ppm.gz" ,
- "tt_horp35.ppm.gz" , "tt_horp40.ppm.gz" , "tt_horp45.ppm.gz" , "tt_horp50.ppm.gz" ,
- "tt_horp55.ppm.gz" , "tt_horp60.ppm.gz" , "tt_horp65.ppm.gz"
+ "tt_horm40.ppm.gz", "tt_horm36.ppm.gz", "tt_horm32.ppm.gz", "tt_horm28.ppm.gz",
+ "tt_horm24.ppm.gz", "tt_horm20.ppm.gz", "tt_horm16.ppm.gz", "tt_horm12.ppm.gz",
+ "tt_horm08.ppm.gz", "tt_horm04.ppm.gz", "tt_horm01.ppm.gz", "tt_horp01.ppm.gz",
+ "tt_horp04.ppm.gz", "tt_horp08.ppm.gz", "tt_horp12.ppm.gz", "tt_horp16.ppm.gz",
+ "tt_horp20.ppm.gz", "tt_horp24.ppm.gz", "tt_horp28.ppm.gz", "tt_horp32.ppm.gz",
+ "tt_horp35.ppm.gz", "tt_horp40.ppm.gz", "tt_horp45.ppm.gz", "tt_horp50.ppm.gz",
+ "tt_horp55.ppm.gz", "tt_horp60.ppm.gz", "tt_horp65.ppm.gz"
 } ;
 
 #define TTAHOME "http://128.231.212.175/TTA/"
@@ -66,7 +66,7 @@ static char * axial_ff[] = {
 /*! (xx,yy,zz) in RAI (Dicom) coords; code is (0=axial,1=sag,2=cor).            */
 /*------------------------------------------------------------------------------*/
 
-static char * TTget_URL( int code , float xx , float yy , float zz )
+static char * TTget_URL( int code, float xx, float yy, float zz )
 {
    static char nbuf[256] ;
    char *ttahome ;
@@ -85,7 +85,7 @@ static char * TTget_URL( int code , float xx , float yy , float zz )
          for( ii=1 ; ii < SAGITTAL_NUM && xx > sagittal_xx[ii] ; ii++ ) ; /* nada */
          if( fabs(xx-sagittal_xx[ii-1]) < fabs(xx-sagittal_xx[ii]) ) ii-- ;
        }
-       sprintf(nbuf,"%s%s", ttahome , sagittal_ff[ii] ) ;
+       sprintf(nbuf,"%s%s", ttahome, sagittal_ff[ii] ) ;
      break ;
 
      case 2:                       /* coronal */
@@ -98,7 +98,7 @@ static char * TTget_URL( int code , float xx , float yy , float zz )
           for( jj=1 ; jj < CORONAL_NUM && yy > coronal_yy[jj] ; jj++ ) ; /* nada */
           if( fabs(yy-coronal_yy[jj-1]) < fabs(yy-coronal_yy[jj]) ) jj-- ;
        }
-       sprintf(nbuf,"%s%s", ttahome , coronal_ff[jj] ) ;
+       sprintf(nbuf,"%s%s", ttahome, coronal_ff[jj] ) ;
      break ;
 
      default:
@@ -111,7 +111,7 @@ static char * TTget_URL( int code , float xx , float yy , float zz )
           for( kk=1 ; kk < AXIAL_NUM && zz > axial_zz[kk] ; kk++ ) ; /* nada */
           if( fabs(zz-axial_zz[kk-1]) < fabs(zz-axial_zz[kk]) ) kk-- ;
        }
-       sprintf(nbuf,"%s%s", ttahome , axial_ff[kk] ) ;
+       sprintf(nbuf,"%s%s", ttahome, axial_ff[kk] ) ;
      break ;
    }
 
@@ -122,21 +122,17 @@ static char * TTget_URL( int code , float xx , float yy , float zz )
 
 static MRI_IMAGE * TTget_ppm( char *url )
 {
-   int ii,nn , ch,nch, nx,ny,maxval , id ;
+   int ii,nn, ch,nch, nx,ny,maxval, id ;
    char buf[32] ;
-   byte *ndata=NULL , *bar ;
+   byte *ndata=NULL, *bar ;
    MRI_IMAGE *im ;
 
-   /* get data */
-
-   nn = NI_read_URL( url , (char **)&ndata ) ;
+   nn = NI_read_URL( url, (char **)&ndata ) ;
 
    if( nn < 40960 || ndata == NULL || ndata[0] != 'P' || ndata[1] != '6' ){
       if( ndata != NULL ) free(ndata) ;
       return NULL ;
    }
-
-   /* convert to image */
 
    id = 2 ; ch = ndata[id] ;  /* start scan after "P6" */
 
@@ -148,7 +144,7 @@ static MRI_IMAGE * TTget_ppm( char *url )
      while( id<nn-1 && !isdigit(ch) ){ch = ndata[++id]; SKIPCOM; }                   \
      for( nch=0 ; id<nn-1 && isdigit(ch) ; nch++,ch=ndata[++id] ) {buf[nch] = ch ;}  \
      buf[nch]='\0';                                                                  \
-     var = strtol( buf , NULL , 10 ) ; }
+     var = strtol( buf, NULL, 10 ) ; }
 
     NUMSCAN(nx) ; if( nx <= 2 || id >= nn-1 ){ free(ndata); return NULL; }
     NUMSCAN(ny) ; if( ny <= 2 || id >= nn-1 ){ free(ndata); return NULL; }
@@ -158,9 +154,9 @@ static MRI_IMAGE * TTget_ppm( char *url )
     id++ ;
     if( nn-id < 3*nx*ny ){ free(ndata); return NULL; }
 
-    im = mri_new( nx , ny , MRI_rgb ) ;
+    im = mri_new( nx, ny, MRI_rgb ) ;
     bar = MRI_RGB_PTR(im) ;
-    memcpy( bar , ndata+id , 3*nx*ny ) ;
+    memcpy( bar, ndata+id, 3*nx*ny ) ;
     free(ndata) ;
 
     if( maxval < 255 ){
@@ -190,11 +186,10 @@ static MRI_IMAGE * TTget_ppm( char *url )
 static char * xxx_name_to_inet( char * host )
 {
    struct hostent * hostp ;
-   char * iname = NULL , * str ;
+   char * iname = NULL, * str ;
    int ll ;
 
    if( host == NULL || host[0] == '\0' ) return NULL ;
-
    hostp = gethostbyname(host) ; if( hostp == NULL ) return NULL ;
 
    str = inet_ntoa(*((struct in_addr *)(hostp->h_addr))) ;
@@ -243,7 +238,7 @@ static int is_nih_host( void )
 /*-----------------------------------------------------------------*/
 
 #define NUM_ORIENT_STRINGS 4
-static char *orient_strings[4] = { "None" , "Axial" , "Sagittal", "Coronal" } ;
+static char *orient_strings[4] = { "None", "Axial", "Sagittal", "Coronal" } ;
 static int orient = 0 ;
 
 static char * TTget_main( PLUGIN_interface * ) ;
@@ -256,12 +251,12 @@ PLUGIN_interface * PLUGIN_init( int ncall )
 
    if( ncall > 0 || !is_nih_host() ) return NULL ;
 
-   plint = PLUTO_new_interface( "TT Atlas" ,
-                                "TT Atlas display" ,
-                                NULL ,
-                                PLUGIN_CALL_VIA_MENU , TTget_main  ) ;
+   plint = PLUTO_new_interface( "TT Atlas",
+                                "TT Atlas display",
+                                NULL,
+                                PLUGIN_CALL_VIA_MENU, TTget_main  ) ;
 
-   PLUTO_add_option( plint , "Mode" , "MODE" , TRUE ) ;
+   PLUTO_add_option( plint, "Mode", "MODE", TRUE ) ;
 
    PLUTO_add_string( plint, "Orient", NUM_ORIENT_STRINGS, orient_strings, orient ) ;
 
@@ -274,7 +269,7 @@ static int recv_key = -1 ;
 static Three_D_View *old_im3d = NULL ;
 static char old_url[256] = "\0" ;
 
-static void TTget_recv( int why , int np , int * ijk , void * junk ) ;
+static void TTget_recv( int why, int np, int * ijk, void * junk ) ;
 
 /*-----------------------------------------------------------------*/
 
@@ -314,7 +309,7 @@ static char * TTget_main( PLUGIN_interface *plint )
 
 static void *impop = NULL ;
 
-static void TTget_recv( int why , int np , int * ijk , void * junk )
+static void TTget_recv( int why, int np, int * ijk, void * junk )
 {
    if( orient == 0          ||
        !IM3D_OPEN(old_im3d) ||
@@ -326,15 +321,15 @@ static void TTget_recv( int why , int np , int * ijk , void * junk )
         MRI_IMAGE *im ;
         char *url ;
 
-        url = TTget_URL( orient-1 , old_im3d->vinfo->xi ,
-                                    old_im3d->vinfo->yj , old_im3d->vinfo->zk ) ;
+        url = TTget_URL( orient-1, old_im3d->vinfo->xi,
+                                    old_im3d->vinfo->yj, old_im3d->vinfo->zk ) ;
 
         if( strcmp(url,old_url) == 0 ) return ;
         strcpy(old_url,url) ;
 
         im = TTget_ppm( url ) ; if( im == NULL ) return ;
 
-        impop = PLUTO_popup_image( impop , im ) ;
+        impop = PLUTO_popup_image( impop, im ) ;
         mri_free(im) ;
       }
       break ;
