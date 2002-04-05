a4001b5f2ef0d1f15f971f90488bda230faac75f
rwcox <>
2002-04-05 18:49:00 +0000

    Cput

--- src/afni.h
+++ src/afni.h
@@ -131,12 +131,12 @@ static char * SHOWFUNC_typestr[] = { "Func=Intensity" , "Func=Threshold" } ;
 /** this should always be exactly 5 characters! **/
 /**             "12345" **/
 
-#define VERSION "2.45f"
+#define VERSION "2.45g"
 
 /** this should always be exactly 17 characters! **/
 /**             "12345678901234567" **/
 
-#define RELEASE "29 Mar 2002      "
+#define RELEASE "05 Apr 2002      "
 
 #ifdef MAIN
 #define AFNI_about \
