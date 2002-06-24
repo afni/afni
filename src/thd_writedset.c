a95d133a21adc4bcfb2a6d02e9e8b7046335fde5
rwcox <>
2002-06-24 15:38:51 +0000

    Cput

--- src/thd_writedset.c
+++ src/thd_writedset.c
@@ -39,8 +39,9 @@ Boolean THD_write_3dim_dataset( char * new_sessname , char * new_prefixname ,
        ! ISVALID_DISKPTR(dset->dblk->diskptr) ) return False ;
 
    if( DSET_IS_MINC(dset)     ) return False ;  /* 29 Oct 2001 */
-   if( DSET_IS_VOLUMES(dset)  ) return False ;  /* 20 Jun 2002 */
    if( DSET_IS_MASTERED(dset) ) return False ;  /* 11 Jan 1999 */
+
+   if( DSET_IS_VOLUMES(dset) && write_brick ) return False ;  /* 20 Jun 2002 */
 
    blk = dset->dblk ; daxes = dset->daxes ;  /* always used fixed daxes */
 
