927e0e4fcc296c20ac5030a528378e46a28361a7
rwcox <>
2002-07-15 21:10:59 +0000

    Cput

--- src/dicom_hdr.c
+++ src/dicom_hdr.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-extern char *mri_dicom_header( char * ) ;
+extern char *mri_dicom_header( char * ) ;  /* cf. mri_dicom_hdr.[ch] */
 
 int main(int argc, char **argv)
 {
    char *ppp ;
 
    if( argc < 2 ){
-     printf("Usage: mri_dicom fname\n"); exit(0);
+     printf("Usage: dicom_hdr fname\n"
+            "Prints information from the DICOM file 'fname' to stdout.\n"
+	   );
+     exit(0);
    }
 
    ppp = mri_dicom_header( argv[1] ) ;
