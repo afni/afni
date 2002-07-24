6a9b39dfc7ed5de4dcffb8037106e978cac34402
rwcox <>
2002-07-24 20:23:39 +0000

    Cput

--- src/thd_floatscan.c
+++ src/thd_floatscan.c
@@ -3,41 +3,14 @@
    of Wisconsin, 1994-2000, and are released under the Gnu General Public
    License, Version 2.  See the file README.Copyright for details.
 ******************************************************************************/
-   
+
 #include <math.h>
 #include <stdlib.h>
 
-#if defined(isnan) && defined(isfinite)
-# define IS_NAN(x)    isnan(x)
-# define IS_FINITE(x) isfinite(x)
-
+#if 1
+#  define IS_GOOD_FLOAT(x) finite(x)
 #else
-
-# if defined(SUN) || defined(SOLARIS) || defined(SGI)
-#  include <ieeefp.h>
-#  define USE_ISNANF
-# endif
-
-# if defined(HP) || defined(OSF1) || defined(LINUX)
-#  define USE_ISNANF
-#  define USE_FINITEF
-# endif
-#endif
-
-#ifndef IS_NAN
-# ifdef USE_ISNANF
-#  define IS_NAN(x) isnanf(x)
-# else
-#  define IS_NAN(x) isnan(x)
-# endif
-#endif
-
-#ifndef IS_FINITE
-# ifdef USE_FINITEF
-#  define IS_FINITE(x) finitef(x)
-# else
-#  define IS_FINITE(x) finite(x)
-# endif
+#  define IS_GOOD_FLOAT(x) isnan(x)
 #endif
 
 /*---------------------------------------------------------------------
@@ -52,7 +25,7 @@ int thd_floatscan( int nbuf , float * fbuf )
    if( nbuf <= 0 || fbuf == NULL ) return 0 ;
 
    for( nerr=ii=0 ; ii < nbuf ; ii++ ){
-      if( ! IS_FINITE(fbuf[ii]) ){ fbuf[ii] = 0.0 ; nerr++ ; }
+      if( !IS_GOOD_FLOAT(fbuf[ii]) ){ fbuf[ii] = 0.0 ; nerr++ ; }
    }
 
    return nerr ;
@@ -67,8 +40,8 @@ int thd_complexscan( int nbuf , complex * cbuf )
    if( nbuf <= 0 || cbuf == NULL ) return 0 ;
 
    for( nerr=ii=0 ; ii < nbuf ; ii++ ){
-      if( ! IS_FINITE(cbuf[ii].r) ){ cbuf[ii].r = 0.0 ; nerr++ ; }
-      if( ! IS_FINITE(cbuf[ii].i) ){ cbuf[ii].i = 0.0 ; nerr++ ; }
+      if( !IS_GOOD_FLOAT(cbuf[ii].r) ){ cbuf[ii].r = 0.0 ; nerr++ ; }
+      if( !IS_GOOD_FLOAT(cbuf[ii].i) ){ cbuf[ii].i = 0.0 ; nerr++ ; }
    }
 
    return nerr ;
