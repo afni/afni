4360579fb45796ed4fa67075c637d4a526896762
rwcox <>
2003-02-13 14:46:35 +0000

    Cput

--- src/niml/niml_do.c
+++ src/niml/niml_do.c
@@ -2,15 +2,26 @@
 
 /**** Tables for ni_do callbacks *****/
 
-static int       doer_num  = 0    ;
-static char    **doer_verb = NULL ;
-static NI_doer **doer_func = NULL ;
+static int           doer_num  = 0    ;
+static char        **doer_verb = NULL ;
+static NI_voidfunc **doer_func = NULL ;
 
 /*---------------------------------------------------------------------------*/
 /*! Register a callback for a "ni_do" verb.  [12 Feb 2003]
+
+    The function will be called like so
+      - func( char *object , NI_stream_type *ns , NI_element *nel ) ;
+      - object = RHS of ni_object attribute (may be NULL)
+      - ns = stream that sent the message (so you can reply, if you like)
+      - nel = the data element that contained the message (if you need it)
+
+    Calling with the same verb will replace the function - you can't have
+    two callbacks for the same verb.  If func is input as NULL, then this
+    will remove a callback, if it was defined earlier; if func is NULL and
+    verb was not previously defined, then nothing happens.
 -----------------------------------------------------------------------------*/
 
-void NI_register_doer( char *verb , NI_doer *func )
+void NI_register_doer( char *verb , NI_voidfunc *func )
 {
    int ii ;
 
@@ -21,22 +32,24 @@ void NI_register_doer( char *verb , NI_doer *func )
    for( ii=0 ; ii < doer_num ; ii++ )
      if( strcmp(verb,doer_verb[ii]) == 0 ) break ;
 
-   /* if was in table, replace func */
+   /* if was in table, replace func (may be NULL) */
 
    if( ii < doer_num ){
      doer_func[ii] = func ; return ;
    }
 
+   /* defining a new verb */
+
    if( func == NULL ) return ;   /* quit if no func */
 
-   /* expand table */
+   /* expand tables of verbs and funcs */
 
-   ii = doer_num ; doer_num++ ;
+   ii = doer_num++ ;
 
    doer_verb = NI_realloc( doer_verb , sizeof(char *)*doer_num ) ;
    doer_verb[ii] = NI_strdup(verb) ;
 
-   doer_func = NI_realloc( doer_func , sizeof(NI_doer *)*doer_num ) ;
+   doer_func = NI_realloc( doer_func , sizeof(NI_voidfunc *)*doer_num ) ;
    doer_func[ii] = func ;
    return ;
 }
@@ -47,6 +60,8 @@ void NI_register_doer( char *verb , NI_doer *func )
 
    - ni_verb='reopen_this' => open the stream anew and replace it [23 Aug 2002]
    - ni_verb='close_this'  => close this stream down              [20 Dec 2002]
+   - ni_verb='typedef'     => define a NI_rowtype                 [12 Feb 2003]
+   - user-defined verbs can be added using NI_register_doer()     [12 Feb 2003]
 
     Return value is -1 if an error occurs, 0 if things are cool.
 -----------------------------------------------------------------------------*/
@@ -68,9 +83,9 @@ int NI_do( NI_stream_type *ns , NI_element *nel )
    if( verb == NULL || verb[0] == '\0' ) return -1 ;        /* need a verb;  */
                                                            /* but not always */
                                                           /* need an object  */
-   /***********************************/
-   /*---- check for various verbs ----*/
-   /***********************************/
+   /*******************************************/
+   /*---- check for various builtin verbs ----*/
+   /*******************************************/
 
    if( strcmp(verb,"reopen_this") == 0 ){  /****----- reopen stream ------****/
 
@@ -95,7 +110,7 @@ int NI_do( NI_stream_type *ns , NI_element *nel )
    } /****------------------------ end close_this ------------------------****/
 
    if( strcmp(verb,"typedef") == 0 ){    /****---- define a NIML type ----****/
-
+                                         /****     [12 Feb 2003]          ****/
      char tnam[256] , tdef[8200] ;
      int tt ;
 
@@ -108,11 +123,12 @@ int NI_do( NI_stream_type *ns , NI_element *nel )
 
    } /****------------------------ end typedef ---------------------------****/
 
-   /**** Here, check for user-defined callbacks ****/
+   /**************************************************************/
+   /**** Here, check for user-defined callbacks [12 Feb 2003] ****/
 
    for( ii=0 ; ii < doer_num ; ii++ ){
      if( strcmp(verb,doer_verb[ii]) == 0 ){
-       if( doer_func[ii] != NULL ) doer_func[ii]( nel ) ;
+       if( doer_func[ii] != NULL ) doer_func[ii]( object , ns , nel ) ;
        return 0 ;
      }
    }
