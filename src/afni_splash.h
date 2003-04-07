13be52bd4b1b93033899388b1cd10c06ac9a21c9
rwcox <>
2003-04-07 17:30:06 +0000

    Cput

--- src/afni_splash.h
+++ src/afni_splash.h
@@ -28,12 +28,38 @@ static int    lover[NOVER]={NLINE_cox1,NLINE_cox2,NLINE_cox3,NLINE_cox4};
 #undef  IXOVER
 #undef  JYOVER
 #define IXOVER 290
-#define JYOVER 175
+#define JYOVER 169
 
 #undef  MAX_XOVER
 #undef  MAX_YOVER
 #define MAX_XOVER 128
 #define MAX_YOVER 128
+
+/*----------------------------------------*/
+/* Face title */
+
+#define NX_facetitle 64
+#define NY_facetitle 14
+#define NC_facetitle 10
+#define NLINE_facetitle 15
+static byte RMAP_facetitle[] = {255,142,0,199,170,85,114,29,227,57};
+static char *BAR_facetitle[] = {
+   "9E9E9E9E9E9E7E",
+   "3E6A9A9A9A9A9A7A",
+   "9A5ABCD3AE4CEFCG3ACFAFC5ACFAAFC9A4A",
+   "9A8AHCF3AECGEEIFCCDAACFAFC5ACFAAFC9AA",
+   "9A9AADCHCIAAECE3AFCCJAACFAFC5ACFAAFCAABFGAA",
+   "ADFFBAADFBB9AAAFCECBAAECE3AFCBCDACFAFC5ACFAAFCABCJ",
+   "CGAGCJFCHAECCB9AAICJAHHAAE4CEFCAHHACFAFC5ACFAAFCA",
+   "FC3DICFAAICBECB9AAABCHJCCEAECGEEIFCADCBCFAFC5ACFAA",
+   "FCAIHCHEE6CFECE9AAAHH3FCJAECE3AFCAAFHCFAFC5AC",
+   "JAAFC3ADHCDCF4EDECE9AAECG3AFCIECE3AFCAAICCFAFC3A",
+   "AAGCGBCGAFCEJHAFCBEJCDECE9AAJCI3ADCGECE3AFC3ABCFAFC",
+   "6AGCCGAAIJCHEAAGCCJDAECE9A9A9A6A",
+   "9A9A9A9A9A9A7A",
+   "9A9A9A9A3A6E9E7E",
+   "9E9E9E9E6E"
+};
 
 /*----------------------------------------*/
 /* Stuff for the image overlay at the top */
@@ -47,8 +73,8 @@ static int    lover[NOVER]={NLINE_cox1,NLINE_cox2,NLINE_cox3,NLINE_cox4};
 #define NMAIN 2
 #ifdef  NMAIN
 
-#  include "splash_gang.h"  /* MCW gang [gray]  */
-#  include "splash_sscc.h"  /* NIH gang [color] */
+# include "splash_gang.h"  /* MCW gang [gray]  */
+# include "splash_sscc.h"  /* NIH gang [color] */
 
   static char **bmain[NMAIN]={BAR_sscc  , BAR_gang  };
   static int    xmain[NMAIN]={NX_sscc   , NX_gang   };
