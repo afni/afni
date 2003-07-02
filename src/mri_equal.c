dd883bdf7ce3f28b0c41ddd08b7988a8a5e47175
rwcox <>
2003-07-02 17:26:17 +0000

    Cput

--- src/mri_equal.c
+++ src/mri_equal.c
@@ -8,11 +8,11 @@ int mri_equal( MRI_IMAGE *aim , MRI_IMAGE *bim )
    char *aar , *bar ;
    int nn ;
 
-   if( aim == bim ) return 1 ;
-   if( aim == NULL || bim == NULL ) return 0 ;
+   if( aim == bim )                                return 1 ;
+   if( aim == NULL || bim == NULL )                return 0 ;
 
-   if( aim->nvox != bim->nvox ) return 0 ;
-   if( aim->kind != bim->kind ) return 0 ;
+   if( aim->nvox != bim->nvox )                    return 0 ;
+   if( aim->kind != bim->kind )                    return 0 ;
 
    aar = mri_data_pointer(aim) ; if( aar == NULL ) return 0 ;
    bar = mri_data_pointer(bim) ; if( bar == NULL ) return 0 ;
@@ -20,5 +20,5 @@ int mri_equal( MRI_IMAGE *aim , MRI_IMAGE *bim )
    nn = memcmp( aar , bar , aim->nvox * aim->pixel_size ) ;
    if( nn != 0 ) nn = 0 ;
    else          nn = 1 ;
-   return nn ;
+                                                   return nn;
 }
