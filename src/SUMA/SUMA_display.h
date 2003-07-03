76566b021a9cf78f5e9bbe926fc932640052d789
ziad <>
2003-07-03 19:44:55 +0000

    Cput

--- src/SUMA/SUMA_display.h
+++ src/SUMA/SUMA_display.h
@@ -92,6 +92,8 @@ void SUMA_resize(Widget w, XtPointer clientData, XtPointer call);
 SUMA_Boolean SUMA_X_SurfaceViewer_Create (void);
 void SUMA_ButtOpen_pushed (Widget w, XtPointer cd1, XtPointer cd2);
 void SUMA_ButtClose_pushed (Widget w, XtPointer cd1, XtPointer cd2);
+int SUMA_generateEPS(char *filename, int inColor, unsigned int width, unsigned int height);
+GLvoid *SUMA_grabPixels(int inColor, unsigned int width, unsigned int height);
 SUMA_Boolean SUMA_RenderToPixMap (SUMA_SurfaceViewer *csv, SUMA_DO* dov);
 void SUMA_context_Init(SUMA_SurfaceViewer *sv);
 SUMA_Boolean SUMA_GetSelectionLine (SUMA_SurfaceViewer *sv, int x, int y);
