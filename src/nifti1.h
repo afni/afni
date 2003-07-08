3c9935c7131076cc315b37a1a249135c2c879e4b
rwcox <>
2003-07-08 00:03:28 +0000

    Cput

--- src/nifti1.h
+++ src/nifti1.h
@@ -14,13 +14,14 @@
       ** fields have been co-opted for other purposes.  Notably,      **
       ** most of the data_history substructure has been co-opted for  **
       ** other purposes, since the ANALYZE 7.5 format describes this  **
-      ** substructure as "optional.                                   **
+      ** substructure as "optional".                                  **
       **                                                              **
       ** Neither the National Institutes of Health (NIH), the DFWG,   **
       ** nor any of the members or employees of these institutions    **
       ** imply any warranty of usefulness of this material for any    **
       ** purpose, and do not assume any liability for damages,        **
       ** incidental or otherwise, caused by any use of this document. **
+      ** If these conditions are not acceptable, do not use this!     **
 ******************************************************************************/
 
 #define NIFTI_VERSION 1
@@ -35,7 +36,7 @@
    Incorporation of components of dbh.h are by permission of the
    Mayo Foundation.
 
-   !! Of course, I hope we do get this permission - RWCox !!
+   !!!! Of course, I hope we do get this permission - RWCox !!!!
 
    The changes from the ANALYZE 7.5 file header in this file are released
    to the public domain.
@@ -49,14 +50,15 @@
 
    To flag such a struct as being conformant to the NIFTI-1 spec,
    the last 8 bytes of the header must be the C String "nifti1" or
-   "nifti1+"; in hexadecimal, the 8 bytes 6E 69 66 74 69 31 00 00 or
-   6E 69 66 74 69 31 2B 00.  Normally, such a "magic number" or flag
-   goes at the start of the file, but trying to avoid clobbering widely-
-   used ANALYZE 7.5 fields led to putting this marker last.
-   (however, recall that "the last shall be first").
+   "nifti1+"; in hexadecimal, the 8 bytes
+     6E 69 66 74 69 31 00 00   or   6E 69 66 74 69 31 2B 00.
+   Normally, such a "magic number" or flag goes at the start of the
+   file, but trying to avoid clobbering widely-used ANALYZE 7.5 fields
+   led to putting this marker last.  (However, recall that "the last
+   shall be first").
 
    "nifti1" means that the image data is stored in the ".img" file
-   corresponding to the header file.
+   corresponding to the header file (starting at file offset 0).
 
    "nifti1+" means that the image data is stored in the same file as the
    header information.  In this case, the first byte of image data is
@@ -85,9 +87,9 @@ struct nifti_1_header { /* NIFTI-1 usage         */  /* ANALYZE 7.5 field(s) */
                         /*  interpreted as from  */  /* short unused9;       */
   float statpar_2 ;     /*  a given statistical  */  /* short unused10;      */
                         /*  distribution using   */  /* short unused11;      */
-  float statpar_3 ;     /*  stat_code and up to  */  /* short unused12;      */
-                        /*  3 parameters.        */  /* short unused13;      */
-  short stat_code ;     /* NIFTI_STAT_* code.    */  /* short unused14;      */
+  float statpar_3 ;     /*  intent_code and up   */  /* short unused12;      */
+                        /*  to 3 parameters.     */  /* short unused13;      */
+  short intent_code ;   /* NIFTI_INTENT_* code.  */  /* short unused14;      */
   short datatype;       /* Defines data type!    */  /* short datatype;      */
   short bitpix;         /* Number bits/voxel.    */  /* short bitpix;        */
   short byteorder ;     /* NIFTI_ORDER_* flag.   */  /* short dim_un0;       */
@@ -117,9 +119,9 @@ struct nifti_1_header { /* NIFTI-1 usage         */  /* ANALYZE 7.5 field(s) */
   float qoffset_y ;     /*  (0,0,0) voxel.       */
   float qoffset_z ;
 
-  float srow1[4] ;      /* Rows of standardizing */
-  float srow2[4] ;      /*  affine transform.    */
-  float srow3[4] ;
+  float srow_x[4] ;     /* Rows of standardizing */
+  float srow_y[4] ;     /*  affine transform.    */
+  float srow_z[4] ;
 
   char bunused[12] ;    /* ++UNUSED++ at present */
 
@@ -232,27 +234,29 @@ struct nifti_1_header { /* NIFTI-1 usage         */  /* ANALYZE 7.5 field(s) */
 
 /*---------------------------------------------------------------------------*/
 /* INTERPRETATION OF VOXEL DATA:
-   The stat_code field can be used to indicate that the voxel data should
-   be interpreted as being drawn from some given probablity distribution.
+   The intent_code field can be used to indicate that the voxel data has
+   some particular meaning.  In particular, a large number of codes is
+   given to indicat that the the voxel data should be interpreted as
+   being drawn from a given probablity distribution.
 
-   Values 2..10 are compatible with AFNI 1.5x (which is why there is no
-   code with value==1).
+   intent_code values 2..10 are compatible with AFNI 1.5x (which is why
+   there is no code with value==1, which is obsolete in AFNI).
 
-   Conventions for distributional parameters:
-     If stat_dim == 0, then the distributional parameters are the same for
-     each voxel, and are stored in the statpar_* fields.
+   Conventions for statistical distributional parameters:
+     If stat_dim == 0, then the distributional parameters are the same
+     for each voxel, and are stored in the statpar_* fields.
 
      If stat_dim != 0, then the parameters are voxel dependent.  They are
      stored in the last dimension of the dataset (i.e., #dim[0]).
      For example, for a 3D dataset of t-statistics where the DOF parameter
      is spatially variable, we would have
-       stat_dim  = 1 (say)
-       stat_code = 3 (t-test)
-       dim[0]    = 4
-       dim[1]    = number of voxels along the x-axis
-       dim[2]    = number of voxels along the y-axis
-       dim[3]    = number of voxels along the z-axis
-       dim[4]    = 2
+       stat_dim    = 1 (say)
+       intent_code = 3 (t-test)
+       dim[0]      = 4
+       dim[1]      = number of voxels along the x-axis
+       dim[2]      = number of voxels along the y-axis
+       dim[3]      = number of voxels along the z-axis
+       dim[4]      = 2
      Then the first 3D volume stored in the voxel data file comprises the
      t-statistic at each location, and the second volume comprises the
      degrees-of-freedom at each location.  Although dim[0] = 4, this should
@@ -260,81 +264,127 @@ struct nifti_1_header { /* NIFTI-1 usage         */  /* ANALYZE 7.5 field(s) */
      expect the datatype to be a floating point type.
 -----------------------------------------------------------------------------*/
 
-                                   /* Parameters             */
-                                   /*------------------------*/
-#define NIFTI_STAT_NONE        0   /* not applicable         */
-#define NIFTI_STAT_CORREL      2   /* Samples, fits, orts    */
-#define NIFTI_STAT_TTEST       3   /* DOF                    */
-#define NIFTI_STAT_FTEST       4   /* DOF numerator, denom   */
-#define NIFTI_STAT_ZSCORE      5   /* no params              */
-#define NIFTI_STAT_CHISQ       6   /* DOF                    */
-#define NIFTI_STAT_BETA        7   /* a and b params         */
-#define NIFTI_STAT_BINOM       8   /* # trials, p per trial  */
-#define NIFTI_STAT_GAMMA       9   /* shape, scale params    */
-#define NIFTI_STAT_POISSON    10   /* mean                   */
+#define NIFTI_INTENT_NONE        0
 
-#define NIFTI_STAT_NORMAL     11   /* mean, variance         */
-#define NIFTI_STAT_FTEST_NONC 12   /* 2 DOF, noncentrality   */
-#define NIFTI_STAT_CHISQ_NONC 13   /* DOF, noncentrality     */
-#define NIFTI_STAT_LOGISTIC   14   /* location, scale        */
-#define NIFTI_STAT_LAPLACE    15   /* location, scale        */
-#define NIFTI_STAT_UNIFORM    16   /* start, end             */
-#define NIFTI_STAT_TTEST_NONC 17   /* DOF, noncentrality     */
-#define NIFTI_STAT_WEIBULL    18   /* location, scale, power */
-#define NIFTI_STAT_CHI        19   /* DOF                    */
-#define NIFTI_STAT_INVGAUSS   20   /* mu, lambda             */
-#define NIFTI_STAT_EXTVAL     21   /* location, scale        */
-#define NIFTI_STAT_PVAL       22   /* no params              */
+         /*--- these codes are for probability distributions ---*/
+                                     /*-------------------------*/
+                                     /* Distribution Parameters */
+                                     /*-------------------------*/
+#define NIFTI_INTENT_CORREL      2   /* Samples, fits, orts     */
+#define NIFTI_INTENT_TTEST       3   /* DOF                     */
+#define NIFTI_INTENT_FTEST       4   /* DOF numerator, denom    */
+#define NIFTI_INTENT_ZSCORE      5   /* no params               */
+#define NIFTI_INTENT_CHISQ       6   /* DOF                     */
+#define NIFTI_INTENT_BETA        7   /* a and b params          */
+#define NIFTI_INTENT_BINOM       8   /* # trials, p per trial   */
+#define NIFTI_INTENT_GAMMA       9   /* shape, scale params     */
+#define NIFTI_INTENT_POISSON    10   /* mean                    */
 
-#define NIFTI_STAT_INTERVAL   23   /* width (about center)   */
+#define NIFTI_INTENT_NORMAL     11   /* mean, variance          */
+#define NIFTI_INTENT_FTEST_NONC 12   /* 2 DOF, noncentrality    */
+#define NIFTI_INTENT_CHISQ_NONC 13   /* DOF, noncentrality      */
+#define NIFTI_INTENT_LOGISTIC   14   /* location, scale         */
+#define NIFTI_INTENT_LAPLACE    15   /* location, scale         */
+#define NIFTI_INTENT_UNIFORM    16   /* start, end              */
+#define NIFTI_INTENT_TTEST_NONC 17   /* DOF, noncentrality      */
+#define NIFTI_INTENT_WEIBULL    18   /* location, scale, power  */
+#define NIFTI_INTENT_CHI        19   /* DOF                     */
+#define NIFTI_INTENT_INVGAUSS   20   /* mu, lambda              */
+#define NIFTI_INTENT_EXTVAL     21   /* location, scale         */
 
-                            /*--- these aren't statistics ---*/
-                            /*--- and have no statistical ---*/
-                            /*--- parameters attached     ---*/
+#define NIFTI_INTENT_PVAL       22   /* no params               */
 
-#define NIFTI_STAT_ESTIMATE  1001  /* estimate of some param */
+#define NIFTI_INTENT_INTERVAL   23   /* width (about center)    */
 
-#define NIFTI_STAT_ANATNAME  1002  /* code for anat label    */
+                               /*--- these aren't statistics ---*/
+                               /*--- and have no statistical ---*/
+                               /*--- parameters attached     ---*/
+
+#define NIFTI_INTENT_ESTIMATE  1001  /* =estimate of some param */
+
+#define NIFTI_INTENT_ANATNAME  1002  /* =code for anat label    */
+
+#define NIFTI_INTENT_LABEL     1003
 
 /*---------------------------------------------------------------------------*/
 /* 3D IMAGE (VOLUME) ORIENTATION AND LOCATION IN SPACE:
 
-   In this discussion, the (x,y,z) axes refer to the axes of 3D data grid,
-   with x varying most rapidly in the data array, y second most rapidly,
-   and z third most rapidly.  The (X,Y,Z) axes refer to a subject-based
-   global coordinate system, with
-     +X = Right  +Y = Anterior  +Z = Superior.
-   This is a right-handed coordinate system.  The (x,y,z) coordinate
-   system is aligned with the 3D image axes as stored.
+   There are 3 different methods that continuous coordinates can be
+   mapped to voxels.  The discussion below emphasizes 3D volumes, and
+   the continuous coordinates are referred to as (x,y,z).  The voxel
+   index coordinates (i.e., the array indexes) are referred to as (i,j,k),
+   with valid ranges:
+     i = 0 .. dim[1]-1
+     j = 0 .. dim[2]-1  (if dim[0] >= 2)
+     k = 0 .. dim[3]-1  (if dim[0] >= 3)
+   The (x,y,z) coordinates refer to the CENTER of a voxel.  In methods
+   2 and 3, the (x,y,z) axes refer to a subject-based coordinate system,
+   with
+     +x = Right  +y = Anterior  +z = Superior.
+   This is a right-handed coordinate system.  However, the exact
+   direction these axes point with respect to the subject depends
+   on the qform code (Method 2) and the sform code (Method 3).
 
-   The coord_code field indicates how the (X,Y,Z) coordinates can be
-   interpreted relative to some standard space - see the NIFTI_CORD_*
-   parameters #define-d below.  This code does not otherwise affect the
-   orientation/location discussion.
+   METHOD 1 (the "old" way, used when qform == 0):
+   The coordinate mapping from (i,j,k) to (x,y,z) is the ANALYZE
+   7.5 way.  This is a simple scaling relationship:
 
-   The discussion herein explains how to compute the (X,Y,Z) coordinates
-   of a voxel stored at index triple (i,j,k).  In addition, some fascinating
-   mathematical asides are included to facilitate your illumination.
+     x = pixdim[1] * i
+     y = pixdim[2] * j
+     z = pixdim[3] * k
 
-   To start with, the (x,y,z) coordinates of the (i,j,k) voxel are
-     x = pixdim[1]*i   for i=0..dim[1]-1
-     y = pixdim[2]*j   for j=0..dim[2]-1
-     z = pixdim[3]*k   for k=0..dim[3]-1
-   where these coordinates are the CENTER of the voxel.  Note that
-   negative pixdim[] values are possible and useful.  The (x,y,z) coordinate
-   system must be right-handed.  For example, if a volume has
-     i varies Right-to-Left
-     j varies Posterior-to-Anterior
-     k varies Inferior-to-Superior
-   then (i,j,k) is a left-handed coordinate system.  In such a case,
-   one way to deal with the (x,y,z) coordinates is to have
-     +x = Right  +y = Anterior  +z = Superior
-   which would imply
-     pixdim[1] < 0   pixdim[2] > 0   pixdim[3] > 0
-   That is, in this example, the +x axis points opposite to the
-   direction of increasing i index.
+   No particular spatial orientation is attached to these (x,y,z)
+   coordinates.  (NIFTI-1 does not have the ANALYZE 7.5 orient field,
+   which is not general and is often not set properly.)
 
-   The orientation of the (x,y,z) axes relative to the (X,Y,Z) axes
+   METHOD 2 (used when qform > 0):
+   The (x,y,z) coordinates are given by the pixdim[] scales, a rotation
+   matrix, and a shift.  This method is intended to represent
+   "scanner-anatomical" coordinates, which are often embedded in the
+   header files of images, and represent the nominal orientation and
+   location of the data.
+
+     [ x ]   [ R11 R12 R13 ] [ pixdim[1] * i ]   [ qoffset_x ]
+     [ y ] = [ R21 R22 R23 ] [ pixdim[2] * j ] + [ qoffset_y ]
+     [ z ]   [ R31 R32 R33 ] [ pixdim[3] * k ]   [ qoffset_z ]
+
+   The qoffset_* shifts are in the NIFTI-1 header.  The rotation matrix
+   R is calculated from the quatern_* parameters, as described below.
+
+   METHOD 3 (used when sform > 0):
+   The (x,y,z) coordinates are given by a general affine transformation
+   of the (i,j,k) indexes:
+
+     x = srow_x[0] * i + srow_x[1] * j + srow_x[2] * j + srow_x[3]
+     y = srow_y[0] * i + srow_y[1] * j + srow_y[2] * j + srow_y[3]
+     z = srow_z[0] * i + srow_z[1] * j + srow_z[2] * j + srow_z[3]
+
+   The srow_* vectors are in the NIFTI_1 header.  Note that no use
+   is made of pixdim[].
+
+   WHY 3 METHODS?
+   Method 1 is provided only for backwards compatibility.  The intention
+   is that Method 2 (qform > 0) represents the nominal voxel locations
+   as reported by the scanner.  Method 3, if present (sform > 0), is to
+   be used to give the location of the voxels in some standard space.
+   The sform code indicates which standard space is present.
+
+   In this scheme, a dataset would originally be set up so that the
+   Method 2 coordinates represent what the scanner reported.  Later,
+   a registration to some standard space can be computed and inserted
+   in the header.  Image display software can use either transform,
+   depending on its purposes and needs.
+
+   In Method 2, the origin of coordinates would generally be whatever
+   the scanner origin is; for example, in MRI, (0,0,0) is the center
+   of the gradient coil.
+
+   In Method 3, the origin of coordinates would depend on the value
+   of sform; for example, for the Talairach coordinate system, (0,0,0)
+   corresponds to the Anterior Commissure.
+
+   QUATERNION REPRESENATION OF ROTATION MATRIX (METHOD 2)
+   The orientation of the (x,y,z) axes relative to the (i,j,k) axes
    in 3D space is specified using a unit quaternion [a,b,c,d], where
    a*a+b*b+c*c+d*d=1.  The (b,c,d) values are all that is needed, since
    we require that a = sqrt(1.0-b*b+c*c+d*d) be positive.  The (b,c,d)
@@ -345,12 +395,25 @@ struct nifti_1_header { /* NIFTI-1 usage         */  /* ANALYZE 7.5 field(s) */
    corresponds to [a,b,c,d] is
 
          [ a*a+b*b-c*c-d*d   2*b*c-2*a*d       2*b*d+2*a*c     ]
-     R = [ 2*b*c+2*a*d       a*a+c*c-b*b-d*d   2*c*d-2*a*b     ]
+     Q = [ 2*b*c+2*a*d       a*a+c*c-b*b-d*d   2*c*d-2*a*b     ]
          [ 2*b*d-2*a*c       2*c*d+2*a*b       a*a+d*d-c*c-b*b ]
 
-         [ R11               R12               R13             ]
-      == [ R21               R22               R23             ]
-         [ R31               R32               R33             ]
+         [ Q11               Q12               Q13             ]
+      == [ Q21               Q22               Q23             ]
+         [ Q31               Q32               Q33             ]
+
+   However, the rotation matrix R needed in Method 2 may not be
+   "proper".  A rotation matrix is called proper if its determinant
+   is 1 and improper if its determinant is -1 (these are the only
+   possibilities for the determinant of a rotation matrix).  If
+   we consider the columns (or rows) of a rotation matrix as a set
+   of 3-vectors, then these vectors form a right-handed system if
+   the matrix is proper and a left-handed system if the matrix is
+   improper.  For example:
+
+       [ 0 1 0 ]                     [ 0 -1 0 ]
+       [ 1 0 0 ] is improper, while  [ 1  0 0 ] is proper.
+       [ 0 0 1 ]                     [ 0  0 1 ]
 
    If (p,q,r) is a unit 3-vector, then rotation of angle h about that
    direction is represented by the quaternion
