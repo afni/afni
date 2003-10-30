e89d2ae76add1ce3fc144450909869578134f551
rwcox <>
2003-10-30 21:19:06 +0000

    Cput

--- src/f2cdir/c_log.c
+++ src/f2cdir/c_log.c
@@ -5,7 +5,7 @@ extern double log(), f__cabs(), atan2();
 VOID c_log(r, z) complex *r, *z;
 #else
 #undef abs
-#include "math.h"
+#include "mathh.h"
 extern double f__cabs(double, double);
 
 void c_log(complex *r, complex *z)
