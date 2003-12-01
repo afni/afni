064b0e4b2328280c6eeee4ca6a7de6eba010bb8c
rickr <>
2003-12-01 22:21:12 +0000

    Cput

--- src/SUMA/SUMA_SurfMeasures.h
+++ src/SUMA/SUMA_SurfMeasures.h
@@ -7,6 +7,13 @@
 #define ST_USE_SHORT		2
 #define ST_USE_VERSION		3
 
+#define ST_INFO_NONE		0
+#define ST_INFO_AREA		1
+#define ST_INFO_NORMS		2
+#define ST_INFO_THICK           4
+#define ST_INFO_VOL             8
+#define ST_INFO_ALL        0xffff
+
 #define ST_DEBUG_MAX_LEVEL      5
 #define ST_DEFAULT_FALLOC      10	/* init alloc for num functions  */
 
@@ -14,7 +21,6 @@
 #define ST_SMEASURE             2	/* measure type    */
 
 #define ST_PI			3.141592653589793
-#define ST_EPSILON		0.00001
 
 #define RANGE(a,b,c) do {if ((b)<(a)) (b)=(a); if ((b)>(c)) (b)=(c);} while (0)
 
@@ -33,6 +39,8 @@ typedef enum
     E_SM_N_AREA_B,			/* give node area of second node     */
     E_SM_NODE_VOL,			/* give volume for each node         */
     E_SM_NODES,				/* print out node indices            */
+    E_SM_NORM_A,			/* give normal vector for first node */
+    E_SM_NORM_B,			/* give normal vector for second node*/
     E_SM_THICK,				/* display between surface thickness */
     E_SM_FINAL
 } smeasure_codes_e;
@@ -46,12 +54,15 @@ typedef enum
 
 typedef struct
 {
-    SUMA_SurfSpecFile     spec;         /* spec file structure         */
-    SUMA_SurfaceObject ** slist;	/* list of SO pointers         */
-    float               * narea[2];     /* list of computed node areas */
-    int                   nsurf;	/* number in list              */
-    int                   salloc;	/* number allocated for        */
-    int                   nnodes;       /* N_Nodes for each surf       */
+    SUMA_SurfSpecFile     spec;         /* spec file structure           */
+    SUMA_SurfaceObject ** slist;	/* list of SO pointers           */
+    float               * narea[2];     /* list of computed node areas   */
+    float               * nvol;         /* list of computed node volumes */
+    float               * fvol;         /* list of computed face volumes */
+    int                   nsurf;	/* number in list                */
+    int                   salloc;	/* number allocated for          */
+    int                   nnodes;       /* N_Nodes for each surf         */
+    int                   nfaces;       /* N_FaceSet for each surf       */
 } surf_t;
 
 typedef struct
@@ -68,7 +79,7 @@ typedef struct
     char   * spec_file;			/* surface specification file  */
     char   * sv_file;			/* AFNI surface volume dataset */
     char   * out_1D_file;		/* surface output filename     */
-    int      total_vol;			/* compute total volume        */
+    int      info;			/* display final information   */
     int      debug;			/* level of debug output       */
     int      dnode;			/* node watched for debug      */
 } opts_t;
@@ -86,35 +97,38 @@ typedef struct
 int	add_to_flist         ( func_t * F, char * fname );
 int	all_mappable_surfs   ( opts_t * opts, param_t * p );
 int	check_func_name      ( char * func );
+int	compute_face_vols    ( opts_t * opts, param_t * p );
 int	compute_node_areas   ( opts_t * opts, param_t * p, int sindex );
+int	compute_node_vols    ( opts_t * opts, param_t * p );
 int	disp_f3_point        ( char * info, float * d );
 int	disp_func_t          ( char * info, func_t * d );
 int	disp_opts_t          ( char * info, opts_t * d );
 int	disp_surf_t          ( char * info, surf_t * d );
-float   dist_f3mm            ( THD_fvec3 * p1, THD_fvec3 * p2 );
-float   fvec_magnitude       ( float * vec, int length );
 int	final_cleanup        ( opts_t * opts, param_t * p );
 int	get_surf_data        ( opts_t * opts, param_t * p );
 int	get_surf_measures    ( opts_t * opts, param_t * p );
 int	init_options         ( opts_t * opts, int argc, char * argv[] );
 int	init_opts_t          ( opts_t * opts );
-float   magnitude_vec_N      ( float * vec, int length );
-float	norm2seg_angle       ( THD_fvec3 * p0, THD_fvec3 * p1, float * norm );
 int	print_column_headers ( opts_t * opts, param_t * p );
 int	spec2SUMA            ( SUMA_SurfSpecFile * spec, char * spec_file,
 	                       char * sv_file, int debug );
 int	usage                ( char * prog, int use_type );
 int	validate_option_lists( opts_t * opts, param_t * p );
 int	validate_options     ( opts_t * opts, param_t * p );
-float	vector_angle         ( float * v0, float * v1 );
 int	verify_surf_t        ( opts_t * opts, param_t * p );
 int	write_output         ( opts_t * opts, param_t * p );
 
-/* testing routines */
+/* geometric routines */
 int     cross_product        ( double * res, double * u, double * v );
-float   eval_planar_points   ( float * a, float * b, float * c, float * d );
-int     test_planar_sides    ( opts_t * opts, param_t * p);
+double  dist_fn              ( int len, float * p1, float * p2 );
+double  dot_product          ( double * u, double * v );
+float   fvec_magnitude       ( float * vec, int length );
+float   magnitude_vec_N      ( float * vec, int length );
+float	norm2seg_angle       ( THD_fvec3 * p0, THD_fvec3 * p1, float * norm );
 int     surf_triangle_match  ( opts_t * opts, param_t * p);
+double  tetra_volume         ( float * p0, float * p1, float * p2, float * p3 );
+float	vector_angle         ( float * v0, float * v1 );
 
+double  lazy_det             ( int width, double * data );
 
 #endif     /* #define _SUMA_SURFMEASURES_H_ */
