917f9dec07b6de4e22a88de69cb0f93c12d1b990
ziad <>
2003-12-02 23:25:26 +0000

    Cput

--- src/SUMA/SUMA_Load_Surface_Object.c
+++ src/SUMA/SUMA_Load_Surface_Object.c
@@ -17,6 +17,8 @@
 #define STAND_ALONE
 #elif defined SUMA_SurfaceMetrics_STAND_ALONE
 #define STAND_ALONE 
+#elif defined SUMA_inspec_STAND_ALONE
+#define STAND_ALONE 
 #endif
 
 #ifdef STAND_ALONE
@@ -1351,9 +1353,9 @@ SUMA_Boolean SUMA_ShowSpecStruct (SUMA_SurfSpecFile *Spec, FILE *Out, int detail
          fprintf (Outp, "\tMappingRef: %s\n", Spec->MappingRef[i]);
          fprintf (Outp, "\tType: %s, Format: %s, EmbedDim: %d\n", Spec->SurfaceType[i], Spec->SurfaceFormat[i], Spec->EmbedDim[i]);
          fprintf (Outp, "\tState: %s, Group %s\n", Spec->State[i], Spec->Group[i]);
-         if (Spec->SureFitVolParam[i][0]) fprintf (Outp, "\tSureFitVolParam: %s\n", Spec->SureFitVolParam[i]);
-         if (Spec->VolParName[i][0])  fprintf (Outp, "\tVolParName: %s\n", Spec->VolParName[i]);
-         if (Spec->IDcode[i][0])  fprintf (Outp, "\tIDcode: %s\n", Spec->IDcode[i]);
+         if (strlen(Spec->SureFitVolParam[i])) fprintf (Outp, "\tSureFitVolParam: %s\n", Spec->SureFitVolParam[i]);
+         if (strlen(Spec->VolParName[i]))  fprintf (Outp, "\tVolParName: %s\n", Spec->VolParName[i]);
+         if (Spec->IDcode[i])  fprintf (Outp, "\tIDcode: %s\n", Spec->IDcode[i]);
       }   
    }
    
@@ -2310,6 +2312,109 @@ SUMA_Boolean SUMA_SurfaceMetrics_eng (SUMA_SurfaceObject *SO, const char *Metric
 
    SUMA_RETURN (YUP);
 }
+
+#ifdef SUMA_inspec_STAND_ALONE
+void usage_SUMA_inspec()
+{
+   printf ( "\n\33[1mUsage: \33[0m inspec <-spec specfile> [-detail d] [-h/-help]\n"
+            "Outputs information found from specfile.\n" 
+            "\t -spec specfile: specfile to be read\n"
+            "\t -detail d: level of output detail default is 1.\n"
+            "\t            Available levels are 1, 2 and 3.\n"
+            "\t -h or -help: This message here.\n" 
+            "\t\t Ziad S. Saad SSCC/NIMH/NIH ziad@nih.gov \n\t\t Dec 2 03\n\n");   
+}
+int main (int argc,char *argv[])
+{/* Main */
+   char FuncName[]={"inspec"};
+   int detail, kar;
+   char *spec_name;
+   SUMA_SurfSpecFile Spec;   
+   SUMA_Boolean brk;
+   
+	/* allocate space for CommonFields structure */
+	SUMAg_CF = SUMA_Create_CommonFields ();
+	if (SUMAg_CF == NULL) {
+		fprintf(SUMA_STDERR,"Error %s: Failed in SUMA_Create_CommonFields\n", FuncName);
+		exit(1);
+	}
+   
+   if (argc < 3)
+       {
+          usage_SUMA_inspec ();
+          exit (1);
+       }
+   
+   kar = 1;
+	brk = NOPE;
+   detail = 1;
+   spec_name = NULL;
+	while (kar < argc) { /* loop accross command ine options */
+		/*fprintf(stdout, "%s verbose: Parsing command line...\n", FuncName);*/
+		if (strcmp(argv[kar], "-h") == 0 || strcmp(argv[kar], "-help") == 0) {
+			 usage_SUMA_inspec();
+          exit (1);
+		}
+		if (!brk && (strcmp(argv[kar], "-spec") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -spec ");
+				exit (1);
+			}
+         spec_name = argv[kar];
+			if (!SUMA_filexists(spec_name)) {
+            fprintf (SUMA_STDERR, "File %s not found or not readable.\n", spec_name);
+            exit(1);
+         }
+			brk = YUP;
+		}
+      if (!brk && (strcmp(argv[kar], "-detail") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -detail ");
+				exit (1);
+			}
+			detail = atoi(argv[kar]);
+         if (detail < 1 || detail > 3) {
+            SUMA_SL_Err("detail is < 1 or > 3");
+            exit (1);
+         }
+			brk = YUP;
+		}
+      
+      if (!brk) {
+			fprintf (SUMA_STDERR,"Error %s: Option %s not understood. Try -help for usage\n", FuncName, argv[kar]);
+			exit (1);
+		} else {	
+			brk = NOPE;
+			kar ++;
+		}
+   }
+   
+   if (!spec_name) {
+      SUMA_SL_Err("-spec option must be specified.\n");
+      exit(1);
+   }
+   /* load spec file */
+   if (!SUMA_Read_SpecFile (spec_name, &Spec)) {
+      SUMA_SL_Err("Error in SUMA_Read_SpecFile\n");
+      exit(1);
+   }
+   
+   /* showme the contents */
+   if (!SUMA_ShowSpecStruct (&Spec, NULL, detail)) {
+      SUMA_SL_Err("Failed in SUMA_ShowSpecStruct\n");
+      exit(1);
+   }
+   
+   if (!SUMA_Free_CommonFields(SUMAg_CF)) {
+      SUMA_SL_Err("SUMAg_CF Cleanup Failed!");
+      exit(1);
+   }
+   
+   exit(0);
+}/* main inspec */
+#endif
 
 #ifdef SUMA_SurfaceMetrics_STAND_ALONE
 
