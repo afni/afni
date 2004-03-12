88764caa058dfd1badbb4811842c49b70ad7e375
rwcox <>
2004-03-12 18:02:27 +0000

    Cput

--- src/3dWarp.c
+++ src/3dWarp.c
@@ -38,7 +38,7 @@ void warp_dicom_out2in( float  xin , float  yin , float  zin ,
 
 int main( int argc , char * argv[] )
 {
-   THD_3dim_dataset *inset , *outset , *newgset=NULL ;
+   THD_3dim_dataset *inset , *outset=NULL , *newgset=NULL ;
    int nxin,nyin,nzin,nxyzin,nvals , ival ;
    int nxout,nyout,nzout,nxyzout ;
    char * prefix = "warped" ;
@@ -49,6 +49,8 @@ int main( int argc , char * argv[] )
    float ddd_newgrid=0.0 , fac ;
    MRI_IMAGE *inim , *outim , *wim ;
    void *newggg=NULL ; int gflag=0 ;
+
+   int tta2mni=0 , mni2tta=0 ;   /* 11 Mar 2004 */
 
    /*-- help? --*/
 
@@ -80,12 +82,18 @@ int main( int argc , char * argv[] )
             "               separated by commas.\n"
             "             * You will need the 'forward single quotes' around\n"
             "               the argument.\n"
+            "\n"
+            "  -tta2mni = Transform a dataset in Talairach-Tournoux Atlas\n"
+            "              coordinates to MNI-152 coordinates.\n"
+            "  -mni2tta = Transform a dataset in MNI-152 coordinates to\n"
+            "              Talairach-Tournoux Atlas coordinates.\n"
+            "\n"
             "-----------------------\n"
             "Other Transform Options:\n"
             "-----------------------\n"
             "  -linear     }\n"
             "  -cubic      } = Chooses spatial interpolation method.\n"
-            "  -NN         }     [default = linear]\n"
+            "  -NN         } =   [default = linear]\n"
             "  -quintic    }\n"
             "\n"
             "  -fsl_matvec   = Indicates that the matrix file 'mmm' uses FSL\n"
@@ -128,6 +136,28 @@ int main( int argc , char * argv[] )
    /*-- command line options --*/
 
    while( nopt < argc && argv[nopt][0] == '-' ){
+
+     /*----- 11 Mar 2004 -----*/
+
+     if( strcmp(argv[nopt],"-tta2mni") == 0 ){
+       if( mni2tta ){
+         fprintf(stderr,"** Can't use -tta2mni with -mni2tta!\n"); exit(1);
+       }
+       if( use_matvec ){
+         fprintf(stderr,"** Can't use -tta2mni with -matvec!\n");  exit(1);
+       }
+       tta2mni = 1 ; nopt++ ; continue ;
+     }
+
+     if( strcmp(argv[nopt],"-mni2tta") == 0 ){
+       if( tta2mni ){
+         fprintf(stderr,"** Can't use -mni2tta with -mni2tta!\n"); exit(1);
+       }
+       if( use_matvec ){
+         fprintf(stderr,"** Can't use -mni2tta with -matvec!\n");  exit(1);
+       }
+       mni2tta = 1 ; nopt++ ; continue ;
+     }
 
      /*-----*/
 
@@ -226,8 +256,14 @@ int main( int argc , char * argv[] )
      if( strncmp(argv[nopt],"-matvec_",8) == 0 ){
        MRI_IMAGE *matim ; float *matar , dm ;
 
+       if( mni2tta ){
+         fprintf(stderr,"** Can't use -matvec with -mni2tta!\n"); exit(1);
+       }
+       if( tta2mni ){
+         fprintf(stderr,"** Can't use -matvec with -tta2mni!\n"); exit(1);
+       }
        if( use_matvec ){
-         fprintf(stderr,"** Can't have two -matvec options!\n"); exit(0);
+         fprintf(stderr,"** Can't have two -matvec options!\n") ; exit(1);
        }
        if( ++nopt >= argc ){
          fprintf(stderr,"** ERROR: need an argument after -matvec!\n"); exit(1);
@@ -305,14 +341,15 @@ int main( int argc , char * argv[] )
 
    /*-- check to see if we have a warp specified --*/
 
-   if( !use_matvec ){
-     fprintf(stderr,"** Don't you want to use -matvec?\n") ;
-     exit(1) ;
+   if( !tta2mni && !mni2tta && !use_matvec ){
+     fprintf(stderr,"** No transformation on command line!?\n"); exit(1);
    }
+
+   if( tta2mni || mni2tta ) fsl = 0 ;
 
    /*-- 06 Aug 2003: if FSL matrix, flip stuff around --*/
 
-   if( fsl ){
+   if( use_matvec && fsl ){
      dicom_in2out.mm.mat[0][2] = -dicom_in2out.mm.mat[0][2] ;
      dicom_in2out.mm.mat[1][2] = -dicom_in2out.mm.mat[1][2] ;
      dicom_in2out.mm.mat[2][0] = -dicom_in2out.mm.mat[2][0] ;
@@ -344,6 +381,22 @@ int main( int argc , char * argv[] )
      fprintf(stderr,"** ERROR: can't open dataset %s\n",argv[nopt]); exit(1);
    }
 
+   /*- 11 Mar 2004: check for coherency -*/
+
+   if( tta2mni || mni2tta ){
+     if( inset->view_type != VIEW_TALAIRACH_TYPE ){
+       fprintf(stderr,"++ WARNING: input dataset not in +tlrc view!\n") ;
+     } else {
+       ATR_string *ast ;
+       ast = THD_find_string_atr( inset->dblk , "TLRC_SPACE" ) ;
+       if( ast != NULL ){
+         if( strcmp(ast->ch,"MNI-152") == 0 && tta2mni ){
+           fprintf(stderr,"++ WARNING: input dataset appears to be MNI-152 already!\n");
+         }
+       }
+     }
+   }
+
    /*-- do the heavy lifting --*/
 
    if( use_newgrid ){
@@ -352,14 +405,13 @@ int main( int argc , char * argv[] )
      newggg = newgset      ; gflag = WARP3D_NEWDSET ;
    }
 
-   if( !use_matvec ){
-     outset = THD_warp3D( inset ,
-                          warp_dicom_in2out ,
-                          warp_dicom_out2in ,
-                          newggg , prefix , zpad , gflag ) ;
-   } else {
+   if( use_matvec ){
      outset = THD_warp3D_affine( inset , dicom_out2in ,
                                  newggg , prefix , zpad , gflag ) ;
+   } else if( tta2mni ){
+     outset = THD_warp3D_tta2mni( inset , newggg , prefix,zpad,gflag ) ;
+   } else if( mni2tta ){
+     outset = THD_warp3D_mni2tta( inset , newggg , prefix,zpad,gflag ) ;
    }
 
    if( outset == NULL ){
@@ -371,6 +423,12 @@ int main( int argc , char * argv[] )
 
    tross_Copy_History( inset , outset ) ;
    tross_Make_History( "3dWarp" , argc,argv , outset ) ;
+
+   /*- 11 Mar 2004: mark output dataset as being in MNI space -*/
+
+   if( tta2mni ){
+     THD_set_string_atr( outset->dblk , "TLRC_SPACE" , "MNI-152" ) ;
+   }
 
    if( verb ) fprintf(stderr,"\n++ Writing dataset\n") ;
    DSET_delete( inset ) ;
