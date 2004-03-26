36df054008c877d92918956c877583268068d32c
rwcox <>
2004-03-26 22:57:39 +0000

    Cput

--- src/mri_get_cmass.c
+++ src/mri_get_cmass.c
@@ -41,3 +41,44 @@ ENTRY("mri_get_cmass_2D") ;
 
    *xcm = xx ; *ycm = yy ; EXRETURN ;
 }
+
+/*--------------------------------------------------------------------*/
+
+void mri_get_cmass_3D( MRI_IMAGE *im, float *xcm, float *ycm, float *zcm )
+{
+   int ii,jj,kk,koff,joff , nx,ny,nz,nxy ;
+   float xx,yy,zz , sum , val , *far ;
+   MRI_IMAGE *flim ;
+
+ENTRY("mri_get_cmass_3D") ;
+
+   if( im == NULL || xcm == NULL || ycm == NULL || zcm == NULL ) EXRETURN ;
+
+   if( im->kind != MRI_float ) flim = mri_to_float( im ) ;
+   else                        flim = im ;
+
+   far = MRI_FLOAT_PTR(flim) ;
+   nx  = im->nx ; ny = im->ny ; nz = im->nz ; nxy = nx*ny ;
+
+   sum = xx = yy = zz = 0.0 ;
+   for( kk=0 ; kk < nz ; kk++ ){
+     koff = kk * nxy ;
+     for( jj=0 ; jj < ny ; jj++ ){
+       joff = koff + jj * nx ;
+       for( ii=0 ; ii < nx ; ii++ ){
+         val = fabs(far[ii+joff]) ;
+         sum += val ;
+         xx  += val * ii ;
+         yy  += val * jj ;
+         zz  += val * kk ;
+       }
+     }
+   }
+
+   if( sum > 0.0 ){ xx /= sum ; yy /= sum ; zz /= sum ; }
+   else           { xx = 0.5*(nx-1); yy=0.5*(ny-1); zz=0.5*(nz-1); }
+
+   if( flim != im ) mri_free(flim) ;
+
+   *xcm = xx ; *ycm = yy ; *zcm = zz ; EXRETURN ;
+}
