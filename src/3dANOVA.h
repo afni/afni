cbdc56085879a5df8c718eacb29cb53f35c74b55
rwcox <>
2004-07-06 14:22:40 +0000

    Cput

--- src/3dANOVA.h
+++ src/3dANOVA.h
@@ -89,7 +89,7 @@ static char * commandline = NULL ;         /* command line for history notes */
 #endif
 
 
-#define MAX_LEVELS 100           /* max. number of factor levels */  
+#define MAX_LEVELS 300           /* max. number of factor levels */  
 #define MAX_OBSERVATIONS 100     /* max. number of observations per cell */
 #define MAX_MEANS 20             /* max. number of user requested means */
 #define MAX_DIFFS 20             /* max. number of user requested diffs. */
