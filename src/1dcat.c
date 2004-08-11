997b77bbdbe62333874b2f2909c00b4b6f253375
rwcox <>
2004-08-11 21:01:18 +0000

    Cput

--- src/1dcat.c
+++ src/1dcat.c
@@ -34,10 +34,12 @@ int main( int argc , char * argv[] )
    nim = argc-1 ;
    inim = (MRI_IMAGE **) malloc( sizeof(MRI_IMAGE *) * nim ) ;
    for( jj=0 ; jj < nim ; jj++ ){
-#if 0
-      if( AFNI_yesenv("ragged") )
-        inim[jj] = mri_read_ascii_ragged( argv[jj+1] , 1.e+38 ) ;
-      else
+#if 0                                   /** for testing only **/
+      if( AFNI_yesenv("ragged") ){
+        MRI_IMAGE *qim ;
+        qim      = mri_read_ascii_ragged( argv[jj+1] , 3.e+33 ) ;
+        inim[jj] = mri_transpose(qim) ; mri_free(qim) ;
+      } else
 #endif
       inim[jj] = mri_read_1D( argv[jj+1] ) ;
       if( inim[jj] == NULL ){
