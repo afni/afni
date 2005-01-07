f8c25c34d89c0a0fd41a5d48f276c1cbd35c8e5e
ziad <>
2005-01-07 15:31:42 +0000

    Cadd

--- src/SUMA/SUMA_BrainWrap.c
+++ src/SUMA/SUMA_BrainWrap.c
@@ -31,8 +31,13 @@ extern int SUMAg_N_DOv;
 float SUMA_LoadPrepInVol (SUMA_ISOSURFACE_OPTIONS *Opt)
 {
    static char FuncName[]={"SUMA_LoadPrepInVol"};
-   int i;
-   float vol=-1;
+   int i, nxx, nxy, cnt, *isort = NULL,iPercRangeVal[2], tind, znxy;
+   THD_fvec3 ncoord, ndicom;
+   THD_ivec3 nind3;
+   float vol=-1, mass=-1, volmass = -1, *CoordList = NULL;
+   double PercRange[2], PercRangeVal[2], *dvec_sort=NULL, *dvec=NULL;
+   SUMA_ISINSPHERE IsIn;
+   SUMA_Boolean LocalHead = YUP;
    
    SUMA_ENTRY;
    
@@ -69,13 +74,97 @@ float SUMA_LoadPrepInVol (SUMA_ISOSURFACE_OPTIONS *Opt)
                            DSET_BRICK_TYPE(Opt->in_vol,0), DSET_ARRAY(Opt->in_vol, 0) ,      /* input  */
                            MRI_double               , Opt->dvec  ) ;   /* output */
    
-   /* estimate the volume */
-   vol = 0.0;
-   for (i=0; i<Opt->nvox; ++i) { if (Opt->dvec[i]) ++vol; }
+   /* estimate the t2, t98 and tm parameters, should do them regionally by Bob's octant method */
+   if (Opt->t2 < 0) {
+      PercRange[0] = 2; PercRange[1] = 98;
+      dvec_sort = SUMA_dPercRange (Opt->dvec, NULL, Opt->nvox, PercRange, PercRangeVal, iPercRangeVal);
+      if (!dvec_sort) {
+         SUMA_SL_Err("Failed to find range");
+         SUMA_RETURN(NOPE);
+      }
+      Opt->t2 = PercRangeVal[0]; Opt->t98 = PercRangeVal[1];
+      if (!Opt->t98) {
+         SUMA_SL_Err("98 percentile is 0!\nWhat kind of dataset is this?");
+         SUMA_RETURN(NOPE);
+      }
+      if (0 && !Opt->t2) { /* No need*/
+         fprintf (SUMA_STDERR,"%s: t2 is zero, going for next non zero val\n", FuncName);
+         /* search for first non zero entry in sorted vector */
+         while (!(dvec_sort[iPercRangeVal[0]])) ++iPercRangeVal[0];
+         Opt->t2 = PercRangeVal[0] = dvec_sort[iPercRangeVal[0]];
+      } 
+      
+      Opt->t = Opt->t2 + 0.1 * (Opt->t98 - Opt->t2);
    
+      Opt->tm = -1;
+      if (LocalHead) fprintf (SUMA_STDERR,"%s: Wild shot tm = %f\n", FuncName, dvec_sort[(iPercRangeVal[0]+iPercRangeVal[1])/2]);
+      if (dvec_sort) SUMA_free(dvec_sort); dvec_sort = NULL;
+   } else {
+      /* user specified */
+   }
+
+   /* estimate the volume */
+   CoordList = (float *)SUMA_calloc(3 * Opt->nvox, sizeof(float) );
+   if (!CoordList) {
+      SUMA_SL_Err("Failed to allocate CoordList");
+      SUMA_RETURN(vol);
+   }
+   Opt->cog[0] = Opt->cog[1] = Opt->cog[2] = 0;
+   nxx = DSET_NX(Opt->in_vol);
+   nxy = DSET_NX(Opt->in_vol) * DSET_NY(Opt->in_vol);
+   vol = 0.0; volmass = 0;
+   for (i=0; i<Opt->nvox; ++i) { 
+      /* coord of point */
+         nind3.ijk[2] = (int)(i/nxy); znxy = nind3.ijk[2] * nxy;
+         nind3.ijk[1] = ( i - znxy ) / nxx;
+         nind3.ijk[0] = i - nind3.ijk[1] * nxx - znxy;
+         ncoord = THD_3dind_to_3dmm(Opt->in_vol, nind3);
+         ndicom = THD_3dmm_to_dicomm(Opt->in_vol,ncoord);
+         CoordList[3*i] = ndicom.xyz[0];
+         CoordList[3*i+1] = ndicom.xyz[1];
+         CoordList[3*i+2] = ndicom.xyz[2];
+      if (Opt->dvec[i] > Opt->t) {
+         mass = SUMA_MIN_PAIR(Opt->t98, Opt->dvec[i]);
+         Opt->cog[0] += mass * ndicom.xyz[0];
+         Opt->cog[1] += mass * ndicom.xyz[1];
+         Opt->cog[2] += mass * ndicom.xyz[2];
+         volmass += mass;
+         ++vol;
+      } 
+   }
+   Opt->cog[0] = Opt->cog[0]/volmass; Opt->cog[1] = Opt->cog[1]/volmass; Opt->cog[2] = Opt->cog[2]/volmass; 
+   if (LocalHead) {
+      fprintf (SUMA_STDERR,"%s: COG %f %f %f\n",FuncName, Opt->cog[0], Opt->cog[1], Opt->cog[2]); 
+   }
    vol *= fabs(DSET_DX(Opt->in_vol) * DSET_DY(Opt->in_vol) * DSET_DZ(Opt->in_vol) );
    
+   /* find the radius */
+   Opt->r = pow(vol*3.0/(3.14159*4.0), 1/3.0);
    
+   /* form a vector of values inside the sphere */
+   if (Opt->tm < 0) {
+      /* create a mask of the voxels inside the sphere of radius r */
+      IsIn =  SUMA_isinsphere (CoordList, Opt->nvox, Opt->cog , Opt->r , 1 );
+      dvec = (double *)SUMA_malloc(Opt->nvox * sizeof(double));
+      if (!dvec) { SUMA_SL_Crit("Failed to allocate for dvec"); SUMA_RETURN(-1); }
+      cnt = 0;
+      for (i=0; i<IsIn.nIsIn; ++i) { 
+         dvec[cnt] = Opt->dvec[IsIn.IsIn[i]]; ++cnt;
+      }
+      /* now sort dvec */
+      isort = SUMA_z_doubqsort(dvec, cnt);
+      Opt->tm = dvec[cnt/2];
+      if (LocalHead) {
+         fprintf (SUMA_STDERR,"%s: Real tm %f\n", FuncName, Opt->tm);
+      }
+      if (dvec) SUMA_free(dvec); dvec = NULL;
+      if (isort) SUMA_free(isort); isort = NULL;
+      SUMA_Free_IsInSphere(&IsIn);
+      
+   }
+   
+   
+   if (CoordList) SUMA_free(CoordList); CoordList = NULL;
    SUMA_RETURN(vol);
 }
 
@@ -99,9 +188,10 @@ float SUMA_LoadPrepInVol (SUMA_ISOSURFACE_OPTIONS *Opt)
 #define SUMA_MEAN_SEGMENT_LENGTH(SO, l) { \
    int m_cnt=0, m_j;   \
    float *m_p1, *m_p2, m_d=0.0, m_v[3];  \
+   l = 0.0; \
    for (m_j=0; m_j < SO->EL->N_EL; ++m_j) {  \
       if (SO->EL->ELps[m_j][2] >= 0) { \
-         m_p1 = &(SO->NodeList[SO->EL->EL[m_j][0]]); m_p2 = &(SO->NodeList[SO->EL->EL[m_j][1]]); \
+         m_p1 = &(SO->NodeList[3*SO->EL->EL[m_j][0]]); m_p2 = &(SO->NodeList[3*SO->EL->EL[m_j][1]]); \
          SUMA_UNIT_VEC(m_p1, m_p2, m_v, m_d);   \
          l += m_d;   \
          ++m_cnt; \
@@ -113,13 +203,51 @@ float SUMA_LoadPrepInVol (SUMA_ISOSURFACE_OPTIONS *Opt)
 /*!
    Based on BET's recipe 
 */
-int SUMA_Find_IminImax (SUMA_SurfaceObject *SO, SUMA_ISOSURFACE_OPTIONS *Opt, int ni, float t2, float tm, float *Iminp, float *Imaxp )
+int SUMA_Find_IminImax (SUMA_SurfaceObject *SO, SUMA_ISOSURFACE_OPTIONS *Opt, int ni, float *Iminp, float *Imaxp )
 {
    static char FuncName[]={"SUMA_Find_IminImax"};
-   float Imax, Imin;
-   
+   float d1, d2, travdir[3], travstp, d1t, d2t, lmin, lmax;
+   float t2 = Opt->t2, tm = Opt->tm, t = Opt->t; 
+   THD_fvec3 ncoord, ndicom;
+   THD_ivec3 nind3;
+   int nind, istep, istepmax, istep2max, nxx, nxy;
    SUMA_ENTRY;
    
+   d1 = Opt->d1; d2 = d1/2.0; 
+   travstp = 1.0;
+   
+   lmin = Opt->tm;
+   lmax = Opt->t;
+   nxx = DSET_NX(Opt->in_vol);
+   nxy = DSET_NX(Opt->in_vol) * DSET_NY(Opt->in_vol);
+   istep = 0; istepmax = (int)(ceil((double)d1/travstp)); istep2max = (int)(ceil((double)d2/travstp));
+   while (istep <= istepmax) {
+      /* calculate offset */
+      travdir[0] = - istep * travstp * SO->NodeNormList[3*ni]; travdir[1] = -istep * travstp * SO->NodeNormList[3*ni+1]; travdir[2] = -istep * travstp * SO->NodeNormList[3*ni+2]; 
+      
+      /* get 1d coord of point */
+      ndicom.xyz[0] = SO->NodeList[3*ni] + travdir[0] ; ndicom.xyz[1] = SO->NodeList[3*ni+1]+ travdir[1]; ndicom.xyz[2] = SO->NodeList[3*ni+2]+ travdir[2]; 
+      ncoord = THD_dicomm_to_3dmm(Opt->in_vol, ndicom);
+      nind3 = THD_3dmm_to_3dind(Opt->in_vol, ncoord);
+      nind = nind3.ijk[0] + nind3.ijk[1] * nxx + nind3.ijk[2] * nxy;
+      if (ni == Opt->NodeDbg) {
+         fprintf(SUMA_STDERR, "%s: Node %d\n"
+                              " nind3 = [%d %d %d] voxVal = %.3f\n", 
+                              FuncName, Opt->NodeDbg, nind3.ijk[0], nind3.ijk[1], nind3.ijk[2],
+                              Opt->dvec[nind]);
+      }
+      /* find local min */ 
+      lmin = SUMA_MIN_PAIR(lmin, Opt->dvec[nind]);
+      if (istep <= istep2max) {
+         lmax = SUMA_MAX_PAIR(lmax, Opt->dvec[nind]);  
+      }
+      
+      ++istep;
+   }
+   
+   
+   *Iminp = SUMA_MAX_PAIR(t2, lmin);
+   *Imaxp = SUMA_MIN_PAIR(tm, lmax); 
    
    SUMA_RETURN(YUP);
 }
@@ -130,29 +258,63 @@ int SUMA_Find_IminImax (SUMA_SurfaceObject *SO, SUMA_ISOSURFACE_OPTIONS *Opt, in
 int SUMA_StretchToFitLeCerveau (SUMA_SurfaceObject *SO, SUMA_ISOSURFACE_OPTIONS *Opt)
 {
    static char FuncName[]={"SUMA_StretchToFitLeCerveau"};
-   int it=0, N_it=200, in=0, ii;
+   int it=0, in=0, ii;
    float mnc[3], s[3], sn[3], st[3], dp, 
-         nsn, rmin, rmax, E, F, su1, su2, 
+         nsn, rmin, rmax, E, F, su1, su2, su3,  
          r, u1[3], u2[3], u3[3], u[3],
          tm=0.0, t2 = 0.0, t98 = 0.0, Imin, Imax, l,
-         *n=NULL;
-   
+         *n=NULL, tb = 0.0, t = 0.0;
+   float *tmpptr, *NewCoord = NULL, f3;
+   FILE *OutNodeFile = NULL;
+   SUMA_Boolean DoDbg=NOPE;
    SUMA_Boolean LocalHead = YUP;
    
    SUMA_ENTRY;
    
-   /* estimate the t2, t98 and tm parameters, should do them regionally by Bob's octant method */
-   ii = -1;
-   do {
-      ++ii;
-      if (Opt->dvec[ii]) { tm = t2 = t98 = Opt->dvec[ii]; }
-   } while (Opt->dvec[ii] && ii < Opt->nvox -1);
    
+   t2 = Opt->t2; t98 = Opt->t98; tm = Opt->tm; t = Opt->t;
    
+   if (LocalHead) fprintf(SUMA_STDERR,"%s: t2 = %f, t = %f, tm = %f, t98 = %f\n", FuncName, t2, t, tm, t98);
+   
+   NewCoord = (float *)SUMA_malloc(3*SO->N_Node*sizeof(float));
+   if (!NewCoord) {
+      SUMA_SL_Crit("Could not allocate for temp vector.");
+      SUMA_RETURN(NOPE);
+   }
    rmin = 3.33; rmax = 10.0;  E = (1/rmin + 1/rmax)/2; F = 6/(1/rmin - 1/rmax); 
    su1 = 0.5; 
-   for (it=0; it < N_it; ++it) {
+   
+   if (Opt->NodeDbg >= 0) {
+      char fname[100];
+      sprintf(fname,"dbg_%d.1D", Opt->NodeDbg);
+      OutNodeFile = fopen(fname,"w");
+      if (!OutNodeFile) {
+         SUMA_S_Err("Failed to open debug file");
+         SUMA_RETURN(NOPE);
+      }
+      fprintf(OutNodeFile, "#0 Node id (in)\n"
+                           "#1 Iteration (it)\n"
+                           "#2 Seg length (l)\n"
+                           "#3-5 Imin, Imax, tb\n"
+                           "#6-8 Node coord (n)\n"
+                           "#9 Norm . Disp (dp)\n"
+                           "#10-12 Mean Neighb Coord (mnc)\n"
+                           "#13-15 Normal direction (  )\n"
+                           "#16-18 Disp (s)\n"
+                           "#19-21 DispT (st)\n"
+                           "#22-24 DispN (sn)\n"
+                           "#25-27 constants (su1 su2 su3)\n"
+                           "#28-30 update vector (u)\n"
+                           "#31 t2\n"
+                           "#32 t\n"
+                           "#33 tm\n"
+                           "#34 t98\n"
+                           "#\n"
+                           "#t2 = %f, t = %f, tm = %f, t98 = %f\n", t2, t, tm, t98);
+   }
+   for (it=0; it < Opt->N_it; ++it) {
       SUMA_MEAN_SEGMENT_LENGTH(SO, l);
+      if (LocalHead) fprintf (SUMA_STDERR,"%s: Iteration %d, l = %f...\n", FuncName, it, l);
       for (in=0; in<SO->N_Node; ++in) {
          SUMA_MEAN_NEIGHB_COORD(SO, in, mnc);
          n = &(SO->NodeList[3*in]);
@@ -167,10 +329,68 @@ int SUMA_StretchToFitLeCerveau (SUMA_SurfaceObject *SO, SUMA_ISOSURFACE_OPTIONS 
          su2 = ( 1 + tanh(F*(1/r - E)))/2.0;
          SUMA_SCALE_VEC(sn, u2, su2, 3, float, float); /* u2 = sn * su2 */
          
-         SUMA_Find_IminImax(SO, Opt, in, t2, tm, &Imin, &Imax);         
+         SUMA_Find_IminImax(SO, Opt, in, &Imin, &Imax);  
+
+         tb = (Imax - t2) * Opt->Zt + t2; 
+         f3 = 2.0 * (Imin - tb) / (Imax - t2 );
+         
+         
+         su3 = Opt->ExpFrac * l * f3 ;
+         
+         u[0] = su1 * st[0] + su2 * sn[0] + su3 * SO->NodeNormList[3*in] ;
+         u[1] = su1 * st[1] + su2 * sn[1] + su3 * SO->NodeNormList[3*in+1] ;
+         u[2] = su1 * st[2] + su2 * sn[2] + su3 * SO->NodeNormList[3*in+2] ; 
+         if ((in == Opt->NodeDbg)) { 
+            fprintf(SUMA_STDERR, "%s: Node %d, iter %d, l %.6f\n", FuncName, in, it, l);
+            fprintf(SUMA_STDERR, " MinMaxTb = [%.6f %.6f %.6f]\n", Imin, Imax, tb);  
+            fprintf(SUMA_STDERR, " Coord(n )= [%.6f %.6f %.6f], dp = %f\n",   n[0],  n[1],  n[2], dp); 
+            fprintf(SUMA_STDERR, " Neigh(mnc)=[%.6f %.6f %.6f]\n",   mnc[0],  mnc[1],  mnc[2]); 
+            fprintf(SUMA_STDERR, " Norm (  )= [%.6f %.6f %.6f]\n",   SO->NodeNormList[3*in], SO->NodeNormList[3*in+1],SO->NodeNormList[3*in+2]); 
+            fprintf(SUMA_STDERR, " Disp (s )= [%.6f %.6f %.6f]\n",   s[0],  s[1],  s[2]); 
+            fprintf(SUMA_STDERR, "      (st)= [%.6f %.6f %.6f]\n",  st[0], st[1], st[2]); 
+            fprintf(SUMA_STDERR, "      (sn)= [%.6f %.6f %.6f]\n",  sn[0], sn[1], sn[2]); 
+            fprintf(SUMA_STDERR, "       su = [%.6f %.6f %.6f]\n",  su1, su2, su3); 
+            fprintf(SUMA_STDERR, "        u = [%.6f %.6f %.6f]\n",  u[0], u[1], u[2]); 
+            if (OutNodeFile) {
+               fprintf(OutNodeFile, " %d %d  %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f"
+                                    " %.6f %.6f %.6f %.6f"
+                                    "\n"
+                                    , in, it, l
+                                    ,Imin, Imax, tb
+                                    ,   n[0],  n[1],  n[2], dp
+                                    ,   mnc[0],  mnc[1],  mnc[2]
+                                    ,SO->NodeNormList[3*in], SO->NodeNormList[3*in+1],SO->NodeNormList[3*in+2]
+                                    ,s[0],  s[1],  s[2]
+                                    ,  st[0], st[1], st[2]
+                                    ,  sn[0], sn[1], sn[2]
+                                    ,  su1, su2, su3
+                                    ,  u[0], u[1], u[2]
+                                    , t2, t, tm, t98);
+            }
+                                 
+         }
+         NewCoord[3*in] = n[0] + u[0]; NewCoord[3*in+1] = n[1] + u[1]; NewCoord[3*in+2] = n[2] + u[2];       
+         DoDbg = NOPE;
       } /* loop over number of nodes */
+      /* swap vectors */
+         tmpptr = SO->NodeList;
+         SO->NodeList = NewCoord; 
+         NewCoord = tmpptr;
+         tmpptr = NULL;
+      /* recalculate surface normals */
+      SUMA_RECOMPUTE_NORMALS(SO);
    } /* loop over number of iterations */
-   
+   if (OutNodeFile) fclose(OutNodeFile); OutNodeFile = NULL;
+   if (NewCoord) SUMA_free(NewCoord); NewCoord = NULL;
    SUMA_RETURN(YUP);
 }
 
@@ -314,6 +534,14 @@ SUMA_ISOSURFACE_OPTIONS *SUMA_BrainWrap_ParseInput (char *argv[], int argc, SUMA
    Opt->XYZ = NULL;
    Opt->in_1D = NULL;
    Opt->N_XYZ = 0;
+   Opt->Zt = 0.5;
+   Opt->ExpFrac = 0.01;
+   Opt->N_it = 200;
+   Opt->Icold = 50;
+   Opt->NodeDbg = -1;
+   Opt->t2 = Opt->t98 = Opt->t = Opt->tm = -1;
+   Opt->r = 0;
+   Opt->d1 = 20;
 	brk = NOPE;
 	while (kar < argc) { /* loop accross command ine options */
 		/*fprintf(stdout, "%s verbose: Parsing command line...\n", FuncName);*/
@@ -350,6 +578,118 @@ SUMA_ISOSURFACE_OPTIONS *SUMA_BrainWrap_ParseInput (char *argv[], int argc, SUMA
 				exit (1);
 			}
 			Opt->in_1D = argv[kar];
+         brk = YUP;
+		}
+      
+      if (!brk && (strcmp(argv[kar], "-d1") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -d1 \n");
+				exit (1);
+			}
+			Opt->d1 = atof(argv[kar]);
+         brk = YUP;
+		}
+      
+      if (!brk && (strcmp(argv[kar], "-bf") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -bf \n");
+				exit (1);
+			}
+			Opt->Zt = atof(argv[kar]);
+         brk = YUP;
+		}
+      
+      if (!brk && (strcmp(argv[kar], "-t2") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -t2 \n");
+				exit (1);
+			}
+			Opt->t2 = atof(argv[kar]);
+         brk = YUP;
+		}
+      if (!brk && (strcmp(argv[kar], "-t98") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -t98 \n");
+				exit (1);
+			}
+			Opt->t98 = atof(argv[kar]);
+         brk = YUP;
+		}
+      if (!brk && (strcmp(argv[kar], "-t") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -t \n");
+				exit (1);
+			}
+			Opt->t = atof(argv[kar]);
+         brk = YUP;
+		}
+      if (!brk && (strcmp(argv[kar], "-tm") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -tm \n");
+				exit (1);
+			}
+			Opt->tm = atof(argv[kar]);
+         brk = YUP;
+		}
+      
+      if (!brk && (strcmp(argv[kar], "-niter") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -niter \n");
+				exit (1);
+			}
+			Opt->N_it = atoi(argv[kar]);
+         if (Opt->N_it < 0 || Opt->N_it > 100000) {
+            fprintf (SUMA_STDERR, "%d is a bad iteration number.\n", Opt->N_it);
+				exit (1);
+         }
+         brk = YUP;
+		}
+      
+      if (!brk && (strcmp(argv[kar], "-exp_frac") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -exp_frac \n");
+				exit (1);
+			}
+			Opt->ExpFrac = atof(argv[kar]);
+         if (Opt->ExpFrac < -1 || Opt->ExpFrac > 1) { /* accroding to algo, should be between 0 and 1 */
+            fprintf (SUMA_STDERR, "%f is a bad expantion fraction.\n", Opt->ExpFrac);
+				exit (1);
+         }
+         brk = YUP;
+		}
+      if (!brk && (strcmp(argv[kar], "-node_dbg") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -node_dbg \n");
+				exit (1);
+			}
+			Opt->NodeDbg = atoi(argv[kar]);
+         if (Opt->NodeDbg < 0) {
+            fprintf (SUMA_STDERR, "%d is a bad node number.\n", Opt->NodeDbg);
+				exit (1);
+         }
+         brk = YUP;
+		}
+      
+      if (!brk && (strcmp(argv[kar], "-ld") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -ld \n");
+				exit (1);
+			}
+			Opt->Icold = atoi(argv[kar]);
+         if (Opt->Icold < 0 || Opt->Icold > 300) {
+            fprintf (SUMA_STDERR, "%d is a bad iteration number.\n", Opt->Icold);
+				exit (1);
+         }
          brk = YUP;
 		}
       
@@ -498,6 +838,13 @@ SUMA_ISOSURFACE_OPTIONS *SUMA_BrainWrap_ParseInput (char *argv[], int argc, SUMA
       }
    }
    
+   if (Opt->t2 >= 0 || Opt->t98 >= 0 || Opt->tm >= 0  || Opt->t >= 0 ) {
+      if (!(Opt->t2 >= 0 && Opt->t98 > 0 && Opt->tm > 0 && Opt->t > 0)){
+         fprintf (SUMA_STDERR,"Error %s: \nAll or none of the t parameters are to be specified on command line:\nt2 %f t %f tm %f t98 %f\n", 
+            FuncName, Opt->t2, Opt->t, Opt->tm, Opt->t98);
+         exit(1);
+      }
+   }
    SUMA_RETURN(Opt);
 }
 
@@ -547,7 +894,7 @@ int main (int argc,char *argv[])
    
    
    /* Now create that little sphere that is about to expand */
-   SO = SUMA_CreateIcosahedron (pow(vol*3.14159/4.0, 1/3.0)/3.0, 50, Opt->VolCM, "n", 1);
+   SO = SUMA_CreateIcosahedron (Opt->r/2.0, Opt->Icold, Opt->cog, "n", 1);
    if (!SO) {
       SUMA_S_Err("Failed to create Icosahedron");
       exit(1);
