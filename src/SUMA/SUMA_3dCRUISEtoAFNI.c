1248fef74c83f65d4d828a7915dd5515525b416b
ziad <>
2005-04-28 20:54:02 +0000

    Cadd

---
+++ src/SUMA/SUMA_3dCRUISEtoAFNI.c
@@ -0,0 +1,114 @@
+#include "SUMA_suma.h"
+
+SUMA_SurfaceViewer *SUMAg_cSV = NULL; /*!< Global pointer to current Surface Viewer structure*/
+SUMA_SurfaceViewer *SUMAg_SVv = NULL; /*!< Global pointer to the vector containing the various Surface Viewer Structures 
+                                    SUMAg_SVv contains SUMA_MAX_SURF_VIEWERS structures */
+int SUMAg_N_SVv = 0; /*!< Number of SVs realized by X */
+SUMA_DO *SUMAg_DOv = NULL;   /*!< Global pointer to Displayable Object structure vector*/
+int SUMAg_N_DOv = 0; /*!< Number of DOs stored in DOv */
+SUMA_CommonFields *SUMAg_CF = NULL; /*!< Global pointer to structure containing info common to all viewers */
+
+void usage_3dCRUISEtoAFNI (SUMA_GENERIC_ARGV_PARSE *ps)
+{
+      static char FuncName[]={"usage_3dCRUISEtoAFNI"};
+      char * s = NULL, *sio=NULL, *st = NULL, *sts = NULL;
+      int i;
+      s = SUMA_help_basics();
+      sio  = SUMA_help_IO_Args(ps);
+      printf ( "\n"
+               "Usage: A template code for writing SUMA programs.\n"
+               " \n"
+               "%s"
+               "%s"
+               "\n", sio,  s);
+      SUMA_free(s); s = NULL; SUMA_free(st); st = NULL; SUMA_free(sio); sio = NULL;       
+      s = SUMA_New_Additions(0, 1); printf("%s\n", s);SUMA_free(s); s = NULL;
+      printf("       Ziad S. Saad SSCC/NIMH/NIH ziad@nih.gov     \n");
+      exit(0);
+}
+
+SUMA_GENERIC_PROG_OPTIONS_STRUCT *SUMA_3dCRUISEtoAFNI_ParseInput(char *argv[], int argc, SUMA_GENERIC_ARGV_PARSE *ps)
+{
+   static char FuncName[]={"SUMA_BrainWrap_ParseInput"}; 
+   SUMA_GENERIC_PROG_OPTIONS_STRUCT *Opt=NULL;
+   int kar;
+   SUMA_Boolean brk;
+   SUMA_Boolean LocalHead = NOPE;
+
+   SUMA_ENTRY;
+   
+   Opt = SUMA_Alloc_Generic_Prog_Options_Struct();
+   kar = 1;
+   brk = NOPE;
+	while (kar < argc) { /* loop accross command ine options */
+		/*fprintf(stdout, "%s verbose: Parsing command line...\n", FuncName);*/
+		if (strcmp(argv[kar], "-h") == 0 || strcmp(argv[kar], "-help") == 0) {
+			 usage_3dCRUISEtoAFNI(ps);
+          exit (0);
+		}
+		
+		SUMA_SKIP_COMMON_OPTIONS(brk, kar);
+      
+      if (!brk && (strcmp(argv[kar], "-input") == 0)) {
+         kar ++;
+			if (kar >= argc)  {
+		  		fprintf (SUMA_STDERR, "need argument after -input\n");
+				exit (1);
+			}
+         Opt->in_name = argv[kar];
+			brk = YUP;
+		}
+
+      if (!brk && !ps->arg_checked[kar]) {
+			fprintf (SUMA_STDERR,"Error %s:\nOption %s not understood. Try -help for usage\n", FuncName, argv[kar]);
+			exit (1);
+		} else {	
+			brk = NOPE;
+			kar ++;
+		}
+   }
+   
+   SUMA_RETURN(Opt);
+}
+
+
+int main (int argc,char *argv[])
+{/* Main */    
+   static char FuncName[]={"3dCRUISEtoAFNI"}; 
+   SUMA_GENERIC_PROG_OPTIONS_STRUCT *Opt;  
+   SUMA_GENERIC_ARGV_PARSE *ps=NULL;
+   SUMA_OPEN_DX_STRUCT **dx = NULL;
+   THD_3dim_dataset *dset=NULL;
+   char *sto3d = NULL;
+   SUMA_Boolean LocalHead = NOPE;
+
+	SUMA_mainENTRY;
+   SUMA_STANDALONE_INIT;
+
+   /* Allocate space for DO structure */
+	SUMAg_DOv = SUMA_Alloc_DisplayObject_Struct (SUMA_MAX_DISPLAYABLE_OBJECTS);
+   ps = SUMA_Parse_IO_Args(argc, argv, "-o;-talk;");
+   
+   if (argc < 2) {
+      usage_3dCRUISEtoAFNI(ps);
+      exit (1);
+   }
+   
+   Opt = SUMA_3dCRUISEtoAFNI_ParseInput (argv, argc, ps);
+
+   if (Opt->debug > 2) LocalHead = YUP;
+   
+   dset = EDIT_empty_copy( NULL ) ;
+   tross_Make_History( "3dCRUISEtoAFNI" , argc,argv , dset) ;
+   if (!(sto3d = SUMA_OpenDX_Read_CruiseVolHead(Opt->in_name, dset))) {
+      SUMA_SL_Err("Failed to open volume");
+      exit(1);   
+   }
+   SUMA_LH(sto3d);
+      
+   if (sto3d) SUMA_free(sto3d); sto3d = NULL;
+   if (Opt) Opt = SUMA_Free_Generic_Prog_Options_Struct(Opt);
+   if (!SUMA_Free_CommonFields(SUMAg_CF)) SUMA_error_message(FuncName,"SUMAg_CF Cleanup Failed!",1);
+   exit(0);
+   
+} 
