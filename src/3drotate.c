31871ce78132717d9c604970a15b449a7c9334f5
ziad <>
2005-05-03 17:22:24 +0000

    Cput

--- src/3drotate.c
+++ src/3drotate.c
@@ -1017,8 +1017,8 @@ fprintf(stderr,"ax1=%d ax2=%d ax3=%d\n",ax1,ax2,ax3) ;
                      ax1,th1, ax2,th2, ax3,th3, dcode,dx,dy,dz ) ;
 
        rmat = rot_to_matrix( ax1,-th1,ax2,-th2,ax3,-th3 ) ;
-       if( dcode == DELTA_BEFORE ) tvec = DMATVEC(rmat,tvec) ;
        LOAD_DFVEC3( tvec , dx,dy,dz ) ;
+       if( dcode == DELTA_BEFORE ) tvec = DMATVEC(rmat,tvec) ;
       }
      }
 
