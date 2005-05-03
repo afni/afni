146dc1e0bd31a09a77afb27431ed79dbb83eb540
rwcox <>
2005-05-03 00:00:49 +0000

    Cput

--- src/3drotate.c
+++ src/3drotate.c
@@ -900,9 +900,11 @@ fprintf(stderr,"ax1=%d ax2=%d ax3=%d\n",ax1,ax2,ax3) ;
      }
    }
 
-   /*-- read dataset, prepare to process it, write it back out (with new name) --*/
+   /*- read dataset, prepare to process it, write back out (with new name) -*/
 
    DSET_mallocize(dset) ;
+   DSET_load(dset) ;
+   dset->dblk->diskptr->storage_mode = STORAGE_BY_BRICK ; /* 14 Jan 2004 */
 
    dset->idcode = MCW_new_idcode() ;  /* 08 Jun 1999 - is a new dataset */
    EDIT_dset_items( dset ,
@@ -915,9 +917,6 @@ fprintf(stderr,"ax1=%d ax2=%d ax3=%d\n",ax1,ax2,ax3) ;
              dset->dblk->diskptr->header_name ) ;
      exit(1) ;
    }
-
-   DSET_load(dset) ;
-   dset->dblk->diskptr->storage_mode = STORAGE_BY_BRICK ; /* 14 Jan 2004 */
 
    /* old history is already in the dataset */
 
