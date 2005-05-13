73610a5589a76bf1c148c27a9829db4ec9fb1cec
rwcox <>
2005-05-13 21:02:29 +0000

    Cput

--- src/edt_fullcopy.c
+++ src/edt_fullcopy.c
@@ -10,11 +10,11 @@
    Routine to make a copy of a dataset, with data attached.
 -----------------------------------------------------------------------*/
 
-THD_3dim_dataset * EDIT_full_copy( THD_3dim_dataset * dset , char * new_prefix )
+THD_3dim_dataset * EDIT_full_copy( THD_3dim_dataset *dset , char *new_prefix )
 {
-   THD_3dim_dataset * new_dset ;
+   THD_3dim_dataset *new_dset ;
    int ival , ityp , nbytes , nvals ;
-   void * new_brick , * old_brick ;
+   void *new_brick , *old_brick ;
 
 ENTRY("EDIT_full_copy") ;
 
@@ -29,10 +29,10 @@ ENTRY("EDIT_full_copy") ;
    /*-- change its name? --*/
 
    if( new_prefix != NULL )
-      EDIT_dset_items( new_dset ,
-                          ADN_prefix , new_prefix ,
-                          ADN_label1 , new_prefix ,
-                       ADN_none ) ;
+     EDIT_dset_items( new_dset ,
+                        ADN_prefix , new_prefix ,
+                        ADN_label1 , new_prefix ,
+                      ADN_none ) ;
 
    /*-- make brick(s) for this dataset --*/
 
@@ -41,27 +41,27 @@ ENTRY("EDIT_full_copy") ;
    nvals = DSET_NVALS(dset) ;
 
    for( ival=0 ; ival < nvals ; ival++ ){
-      ityp      = DSET_BRICK_TYPE(new_dset,ival) ;   /* type of data */
-      nbytes    = DSET_BRICK_BYTES(new_dset,ival) ;  /* how much data */
-      new_brick = malloc( nbytes ) ;                 /* make room */
+     ityp      = DSET_BRICK_TYPE(new_dset,ival) ;   /* type of data */
+     nbytes    = DSET_BRICK_BYTES(new_dset,ival) ;  /* how much data */
+     new_brick = malloc( nbytes ) ;                 /* make room */
 
-      if( new_brick == NULL ){
-        THD_delete_3dim_dataset( new_dset , False ) ;
-        RETURN(NULL) ;
-      }
+     if( new_brick == NULL ){
+       THD_delete_3dim_dataset( new_dset , False ) ;
+       RETURN(NULL) ;
+     }
 
-      EDIT_substitute_brick( new_dset , ival , ityp , new_brick ) ;
+     EDIT_substitute_brick( new_dset , ival , ityp , new_brick ) ;
 
-      /*-- copy data from old brick to new brick --*/
+     /*-- copy data from old brick to new brick --*/
 
-      old_brick = DSET_BRICK_ARRAY(dset,ival) ;
+     old_brick = DSET_BRICK_ARRAY(dset,ival) ;
 
-      if( old_brick == NULL ){
-         THD_delete_3dim_dataset( new_dset , False ) ;
-         RETURN(NULL) ;
-      }
+     if( old_brick == NULL ){
+       THD_delete_3dim_dataset( new_dset , False ) ;
+       RETURN(NULL) ;
+     }
 
-      memcpy( new_brick , old_brick , nbytes ) ;
+     memcpy( new_brick , old_brick , nbytes ) ;
    }
 
    RETURN( new_dset );
