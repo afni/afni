e7b224f5b952c71b53227b793d4035d2af6e5a00
ziad <>
2005-05-18 20:29:43 +0000

    Cadd

---
+++ src/SUMA/SUMA_SurfaceToSurface.c
@@ -0,0 +1,12 @@
+#include "SUMA_suma.h"
+#include "../thd_brainormalize.h"
+
+#undef STAND_ALONE
+
+extern SUMA_CommonFields *SUMAg_CF;
+extern SUMA_DO *SUMAg_DOv;
+extern SUMA_SurfaceViewer *SUMAg_SVv;
+extern int SUMAg_N_SVv; 
+extern int SUMAg_N_DOv;  
+
+
