f44c36bce0fb119634080c9aa0c5d176e26e6bbe
ziad <>
2006-01-04 21:16:44 +0000

    Cput

--- src/matlab/TellAfni_Commands.m
+++ src/matlab/TellAfni_Commands.m
@@ -1,22 +1,24 @@
-function [l] = TellAfni_Commands (f)
+function [l, sh] = TellAfni_Commands (f, fpart )
 %
-%   [l] = TellAfni_Commands (f)
+%   [l, sh] = TellAfni_Commands (f, field)
 %
 %Purpose:
-%   Extract, all the commands from README_driver
+%   Extract, all the commands from README.driver
 %   No guarantee that you'll get all the commands extracted, 
-%   Use this as a guide but read the entire README_driver for the full picture
+%   Use this as a guide but read the entire README.driver for the full picture
 %   
 %Input Parameters:
 %   f: Full path and name of README.driver file
 %      pass no parameters if you want the function to try and
 %      locate the file automatically. 
 %      Use '' if you want to look for the file interactively.
-%     
+%   field: Particular command of interest. 
+%          If specified, the help section from README.driver 
+%          is returned in sh.  
 %   
 %Output Parameters:
 %   l a cell string of the possible parameters
-%   
+%   sh: help string corresponding to field
 %   
 %      
 %Key Terms:
@@ -43,6 +45,12 @@ err = 1;
 
 if (nargin == 0), %try to find README.driver
    f = '';
+   fpart = '';
+elseif (nargin == 1),
+   fpart = '';
+end
+
+if (isempty(f)),
    [s,w] = unix('locate README.driver');
    if (~isempty(w)), %got a hit
       i = find(isspace(w));
@@ -53,7 +61,7 @@ end
 if (~isempty(f) & exist(f,'file') == 2),
    fprintf(1,'Using file %s for info.\n', f);
 else 
-   f = ''
+   f = '';
 end
 
 if (isempty(f)),
@@ -79,6 +87,7 @@ ninl = length(inl);
 ncm = 50;
 cnt = 1;
 S = char(zeros(ninl, ncm));
+sl = [];
 if (ninl),
    for (i=1:1:ninl-1),
       [err,w] = GetWord(s(inl(i):min(inl(i)+ncm, ns)),1);
@@ -88,6 +97,8 @@ if (ninl),
          if ( (sum(abs(double(upper(w))-double(w))) == 0) & sum(isletter(w)) > nw/2),
             % fprintf(2,'>%s<\t',w); pause
             S(cnt,1:nw) = w;
+            %store location in s
+            sl(cnt) = inl(i);
             cnt = cnt + 1;
          end
       end
@@ -102,7 +113,30 @@ if (cnt < 2),
    return;
 end
 
-l = unique(cellstr (S(1:cnt-1,:)));
+[l, ilu] = unique(cellstr (S(1:cnt-1,:)));
+ils = sl(ilu);
+ 
+%do they want particular info ?
+sh = '';
+if (~isempty(fpart)),
+   fpart = upper(zdeblank(fpart));
+   cnt = 1;
+   fnd = 0;
+   while (cnt <= length(l)),
+      if (~isempty(findstr(char(l(cnt)),fpart))), %(strncmp(char(l(cnt)),fpart,length(fpart))),
+         fnd = cnt;
+         %find location of next command
+         dils = (ils - ils(cnt)); dils(find(dils<=0)) = max(dils + 1); [jj,imin] = min(dils); 
+         %here is the string
+         sh = [sh s(ils(cnt)+1:min(ils(imin), length(s)))];
+      end
+      cnt = cnt + 1;
+   end
+   if (~fnd),
+      fprintf(2,'Failed to find help for %s.\n', fpart);
+      return;
+   end   
+end
 
 return;
 
