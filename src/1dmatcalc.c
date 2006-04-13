b9b7211e0eee99a7683511a582ae7c93b7aa2bf9
rwcox <>
2006-04-13 20:22:21 +0000

    Cput

--- src/1dmatcalc.c
+++ src/1dmatcalc.c
@@ -3,18 +3,22 @@
 int main( int argc , char *argv[] )
 {
    char *expr ;
-   int ii , ll ;
+   int ii , ll , ebas=1 ;
 
    if( argc < 2 || strcmp(argv[1],"-help") == 0 ){
-     printf("Usage: 1dmatcalc expression\n\n") ;
+     printf("Usage: 1dmatcalc [-verb] expression\n\n") ;
      printf("%s\n",mri_matrix_evalrpn_help()) ;
      exit(1) ;
    }
 
+   if( strcmp(argv[1],"-verb") == 0 ){
+     mri_matrix_evalrpn_verb(1) ; ebas++ ;
+   }
+
    ll=16 ;
-   for( ii=1 ; ii < argc ; ii++ ) ll += strlen(argv[ii])+1 ;
+   for( ii=ebas ; ii < argc ; ii++ ) ll += strlen(argv[ii])+1 ;
    expr = calloc(1,ll) ;
-   for( ii=1 ; ii < argc ; ii++ ){
+   for( ii=ebas ; ii < argc ; ii++ ){
      strcat(expr,argv[ii]) ; strcat(expr," ") ;
    }
 
--- src/mri_fromstring.c
+++ src/mri_fromstring.c
@@ -2,6 +2,7 @@
 
 /*-----------------------------------------------------------------------------*/
 /*! Produce a 1D (float) image from a string of the form "20@1,10@0,5@1".
+    13 Apr 2006: make '\' be a column separator.
 -------------------------------------------------------------------------------*/
 
 MRI_IMAGE * mri_1D_fromstring( char *str )
@@ -11,6 +12,7 @@ MRI_IMAGE * mri_1D_fromstring( char *str )
    NI_str_array *sar ;
    char sep ;
    MRI_IMAGE *flim ;
+   int col_num , *col_len ;
 
 ENTRY("mri_1D_fromstring") ;
 
@@ -18,7 +20,8 @@ ENTRY("mri_1D_fromstring") ;
    if( sar == NULL ) RETURN(NULL) ;
    if( sar->num == 0 ){ NI_delete_str_array(sar); RETURN(NULL); }
 
-   far = (float *) malloc(sizeof(float)) ;
+   col_num = 1 ; col_len = (int *)calloc(1,sizeof(int)) ;
+   far = (float *)malloc(sizeof(float)) ;
    for( ii=0 ; ii < sar->num ; ii++ ){
 
      if( strstr(sar->str[ii],"@") != NULL ||    /* if has one of the    */
@@ -27,22 +30,42 @@ ENTRY("mri_1D_fromstring") ;
          strstr(sar->str[ii],"*") != NULL   ){  /* scan for count@value */
 
         nnn = sscanf( sar->str[ii] , "%d%c%f" , &count , &sep , &value ) ;
-        if( nnn != 3 || count < 1 ){ free(far); RETURN(NULL); }
+        if( nnn != 3 || count < 1 ){ free(col_len); free(far); RETURN(NULL); }
+
+     } else if( strcmp(sar->str[ii],"\\") == 0 ){ /* a col separator */
+       col_num++ ;
+       col_len = (int *)realloc( col_len , sizeof(int)*col_num ) ;
+       col_len[col_num-1] = 0 ;
+       count = 0 ;
 
      } else {                                 /* just scan for value */
         count = 1 ;
         nnn   = sscanf( sar->str[ii] , "%f" , &value ) ;
-        if( nnn != 1 ){ free(far); RETURN(NULL); }
+        if( nnn != 1 ){ free(col_len); free(far); RETURN(NULL); }
      }
 
-     far = (float *) realloc( far , sizeof(float)*(ntot+count) ) ;
-     for( nnn=0 ; nnn < count ; nnn++ ) far[nnn+ntot] = value ;
-     ntot += count ;
+     if( count > 0 ){
+       far = (float *) realloc( far , sizeof(float)*(ntot+count) ) ;
+       for( nnn=0 ; nnn < count ; nnn++ ) far[nnn+ntot] = value ;
+       ntot += count ; col_len[col_num-1] += count ;
+     }
 
    }
 
    NI_delete_str_array(sar) ;
-   flim = mri_new_vol_empty( ntot,1,1 , MRI_float ) ;
-   mri_fix_data_pointer( far , flim ) ;
-   RETURN(flim) ;
+   if( col_num == 1 ){
+     flim = mri_new_vol_empty( ntot,1,1 , MRI_float ) ;
+     mri_fix_data_pointer( far , flim ) ;
+   } else {
+#undef  FL
+#define FL(i,j) flar[(i)+(j)*nnn]
+     float *flar ; int jj,kk ;
+     for( nnn=ii=0 ; ii < col_num ; ii++ ) nnn = MAX(nnn,col_len[ii]) ;
+     flim = mri_new( nnn, col_num, MRI_float ) ; flar = MRI_FLOAT_PTR(flim) ;
+     for( kk=jj=0 ; jj < col_num ; kk+=col_len[jj],jj++ ){
+       for( ii=0 ; ii < col_len[jj] ; ii++ ) FL(ii,jj) = far[kk+ii] ;
+     }
+   }
+
+   free(col_len) ; RETURN(flim) ;
 }
