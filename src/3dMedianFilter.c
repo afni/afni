10f83d16652f8da15543fa7dc79de773d1162c04
rwcox <>
2006-07-13 21:03:19 +0000

    Cput

--- src/3dMedianFilter.c
+++ src/3dMedianFilter.c
@@ -7,9 +7,9 @@ int main( int argc , char * argv[] )
    float irad=1.5 ;
    int nrep=1 ;
    char *prefix = "MedianFilter" ;
-   int iarg , verb=0 , do_mask=0 ;
+   int iarg , verb=0 , do_mask=0 , niter=1,pit ;
    THD_3dim_dataset *inset , *outset ;
-   MRI_IMAGE *imout ;
+   MRI_IMAGE *imout , *imin ;
    byte *mask=NULL ;
 
    if( argc < 2 || strcmp(argv[1],"-help") == 0 ){
@@ -19,6 +19,7 @@ int main( int argc , char * argv[] )
              "\n"
              "Options:\n"
              "  -irad x    = Radius in voxels of spherical regions\n"
+             "  -iter n    = Iterate 'n' times [default=1]\n"
              "  -verb      = Be verbose during run\n"
              "  -prefix pp = Use 'pp' for prefix of output dataset\n"
              "  -automask  = Create a mask (a la 3dAutomask)\n"
@@ -39,6 +40,13 @@ int main( int argc , char * argv[] )
    iarg = 1 ;
    while( iarg < argc && argv[iarg][0] == '-' ){
 
+      if( strcmp(argv[iarg],"-iter") == 0 ){
+        niter = (int)strtol( argv[++iarg], NULL , 10 ) ;
+        if( niter < 1 )
+          ERROR_exit("Illegal value after -iter!") ;
+        iarg++ ; continue ;
+      }
+
       if( strncmp(argv[iarg],"-automask",5) == 0 ){
         do_mask++ ; iarg++ ; continue ;
       }
@@ -48,41 +56,35 @@ int main( int argc , char * argv[] )
       }
 
       if( strcmp(argv[iarg],"-irad") == 0 ){
-         irad = strtod( argv[++iarg] , NULL ) ;
-         if( irad < 1.0 ){
-            fprintf(stderr,"*** Illegal value after -irad!\n"); exit(1);
-         }
-         iarg++ ; continue ;
+        irad = strtod( argv[++iarg] , NULL ) ;
+        if( irad < 1.0f )
+          ERROR_exit("Illegal value after -irad!\n");
+        iarg++ ; continue ;
       }
 
       if( strcmp(argv[iarg],"-prefix") == 0 ){
-         prefix = argv[++iarg] ;
-         if( !THD_filename_ok(prefix) ){
-            fprintf(stderr,"*** Illegal value after -prefix!\n"); exit(1);
-         }
-         iarg++ ; continue ;
+        prefix = argv[++iarg] ;
+        if( !THD_filename_ok(prefix) )
+          ERROR_exit("Illegal value after -prefix!\n");
+        iarg++ ; continue ;
       }
 
-      fprintf(stderr,"*** Unknown option: %s\n",argv[iarg]); exit(1) ;
+      ERROR_exit("Unknown option: %s\n",argv[iarg]);
    }
 
-   if( iarg >= argc ){
-      fprintf(stderr,"*** No dataset name on command line?\n"); exit(1);
-   }
+   if( iarg >= argc )
+     ERROR_exit("No dataset name on command line?\n");
 
    /*-- read input --*/
 
    inset = THD_open_dataset( argv[iarg] ) ;
-   if( inset == NULL ){
-     fprintf(stderr,"**  ERROR: Can't open dataset %s\n",argv[iarg]); exit(1);
-   }
+   if( inset == NULL )
+     ERROR_exit("Can't open dataset %s\n",argv[iarg]);
    DSET_load( inset ) ;
-   if( !DSET_LOADED(inset) ){
-     fprintf(stderr,"** ERROR: can't load dataset %s\n",argv[iarg]); exit(1);
-   }
-   if( DSET_NVALS(inset) > 1 ){
-     fprintf(stderr,"++ WARNING: only processing sub-brick #0\n") ;
-   }
+   if( !DSET_LOADED(inset) )
+     ERROR_exit("Can't load dataset %s\n",argv[iarg]);
+   if( DSET_NVALS(inset) > 1 )
+     WARNING_message("Only processing sub-brick #0\n");
 
    if( do_mask ){
      THD_automask_verbose( verb ) ;
@@ -90,8 +92,14 @@ int main( int argc , char * argv[] )
      mask = THD_automask( inset ) ;
    }
 
-   imout = mri_medianfilter( DSET_BRICK(inset,0) , irad , mask , verb ) ;
-   free((void *)mask) ;
+   imin = mri_to_float( DSET_BRICK(inset,0) ) ;
+   if( imin == NULL ) ERROR_exit("Can't copy input dataset brick") ;
+   for( pit=0 ; pit < niter ; pit++ ){
+     imout = mri_medianfilter( imin , irad , mask , verb ) ;
+     mri_free(imin) ; imin = imout ;
+   }
+
+   if( mask != NULL ) free((void *)mask) ;
 
    outset = EDIT_empty_copy( inset )  ;
    EDIT_dset_items( outset ,
@@ -103,6 +111,6 @@ int main( int argc , char * argv[] )
    tross_Copy_History( inset , outset ) ;
    tross_Make_History( "3dMedianFilter" , argc,argv , outset ) ;
    DSET_write(outset) ;
-   fprintf(stderr,"++ output dataset: %s\n",DSET_BRIKNAME(outset)) ;
+   WROTE_DSET(outset) ;
    exit(0) ;
 }
--- src/mri_genalign.c
+++ src/mri_genalign.c
@@ -21,6 +21,7 @@ static int find_relprime_fixed( int n )
    for( dj=n5 ; gcd(n,dj) > 1 ; dj++ ) ; /*nada*/
    return dj ;
 }
+#if 0
 static int find_relprime_random( int n )
 {
    int dj , n5=n/5 , n2=n/2 ;
@@ -28,6 +29,7 @@ static int find_relprime_random( int n )
    do{ dj = n5 + lrand48()%n2 ; } while( gcd(n,dj) > 1 ) ;
    return dj ;
 }
+#endif
 /*---------------------------------------------------------------------------*/
 
 static MRI_IMAGE * GA_smooth( MRI_IMAGE *im , int meth , float rad )
@@ -62,6 +64,9 @@ static MRI_IMAGE * GA_smooth( MRI_IMAGE *im , int meth , float rad )
 #undef  FAR
 #define FAR(i,j,k) far[(i)+(j)*nx+(k)*nxy]
 
+#undef  CLIP
+#define CLIP(mm,nn) if(mm < 0)mm=0; else if(mm > nn)mm=nn
+
 static void GA_interp_NN( MRI_IMAGE *fim ,
                           int npp, float *ip, float *jp, float *kp, float *vv )
 {
@@ -82,19 +87,301 @@ static void GA_interp_NN( MRI_IMAGE *fim ,
 
 /*---------------------------------------------------------------------------*/
 
+static void GA_interp_linear( MRI_IMAGE *fim ,
+                              int npp, float *ip, float *jp, float *kp, float *vv )
+{
+   int nx=fim->nx , ny=fim->ny , nz=fim->nz , nxy=nx*ny , pp ;
+   float nxh=nx-0.501f , nyh=ny-0.501f , nzh=nz-0.501f , xx,yy,zz ;
+   float fx,fy,fz ;
+   float *far = MRI_FLOAT_PTR(fim) ;
+   int nx1=nx-1,ny1=ny-1,nz1=nz-1, ix,jy,kz ;
+   int ix_00,ix_p1 ;         /* interpolation indices */
+   int jy_00,jy_p1 ;
+   int kz_00,kz_p1 ;
+   float wt_00,wt_p1 ;       /* interpolation weights */
+   float f_j00_k00, f_jp1_k00, f_j00_kp1, f_jp1_kp1, f_k00, f_kp1 ;
+
+   for( pp=0 ; pp < npp ; pp++ ){
+     xx = ip[pp] ; if( xx < -0.499f || xx > nxh ){ vv[pp]=0.0f; continue; }
+     yy = jp[pp] ; if( yy < -0.499f || yy > nyh ){ vv[pp]=0.0f; continue; }
+     zz = kp[pp] ; if( zz < -0.499f || zz > nzh ){ vv[pp]=0.0f; continue; }
+
+     ix = floorf(xx) ;  fx = xx - ix ;   /* integer and       */
+     jy = floorf(yy) ;  fy = yy - jy ;   /* fractional coords */
+     kz = floorf(zz) ;  fz = zz - kz ;
+
+     ix_00 = ix ; ix_p1 = ix+1 ; CLIP(ix_00,nx1) ; CLIP(ix_p1,nx1) ;
+     jy_00 = jy ; jy_p1 = jy+1 ; CLIP(jy_00,ny1) ; CLIP(jy_p1,ny1) ;
+     kz_00 = kz ; kz_p1 = kz+1 ; CLIP(kz_00,nz1) ; CLIP(kz_p1,nz1) ;
+
+     wt_00 = 1.0f-fx ; wt_p1 = fx ;  /* weights for ix_00 and ix_p1 points */
+
+#undef  XINT
+#define XINT(j,k) wt_00*FAR(ix_00,j,k)+wt_p1*FAR(ix_p1,j,k)
+
+     /* interpolate to location ix+fx at each jy,kz level */
+
+     f_j00_k00 = XINT(jy_00,kz_00) ; f_jp1_k00 = XINT(jy_p1,kz_00) ;
+     f_j00_kp1 = XINT(jy_00,kz_p1) ; f_jp1_kp1 = XINT(jy_p1,kz_p1) ;
+
+     /* interpolate to jy+fy at each kz level */
+
+     wt_00 = 1.0f-fy ; wt_p1 = fy ;
+     f_k00 =  wt_00 * f_j00_k00 + wt_p1 * f_jp1_k00 ;
+     f_kp1 =  wt_00 * f_j00_kp1 + wt_p1 * f_jp1_kp1 ;
+
+     /* interpolate to kz+fz to get output */
+
+     vv[pp] = (1.0f-fz) * f_k00 + fz * f_kp1 ;
+   }
+   return ;
+}
+
+/*---------------------------------------------------------------------------*/
+
+/* define Lagrange cubic interpolation polynomials */
+
+#undef  P_M1
+#undef  P_00
+#undef  P_P1
+#undef  P_P2
+#undef  P_FACTOR
+#define P_M1(x)  (-(x)*((x)-1)*((x)-2))
+#define P_00(x)  (3*((x)+1)*((x)-1)*((x)-2))
+#define P_P1(x)  (-3*(x)*((x)+1)*((x)-2))
+#define P_P2(x)  ((x)*((x)+1)*((x)-1))
+#define P_FACTOR 4.62962963e-3            /* 1/216 = final scaling factor */
+
+static void GA_interp_cubic( MRI_IMAGE *fim ,
+                             int npp, float *ip, float *jp, float *kp, float *vv )
+{
+   int nx=fim->nx , ny=fim->ny , nz=fim->nz , nxy=nx*ny , pp ;
+   float nxh=nx-0.501f , nyh=ny-0.501f , nzh=nz-0.501f , xx,yy,zz ;
+   float fx,fy,fz ;
+   float *far = MRI_FLOAT_PTR(fim) ;
+   int nx1=nx-1,ny1=ny-1,nz1=nz-1, ix,jy,kz ;
+
+   int ix_m1,ix_00,ix_p1,ix_p2 ;     /* interpolation indices */
+   int jy_m1,jy_00,jy_p1,jy_p2 ;
+   int kz_m1,kz_00,kz_p1,kz_p2 ;
+   float wt_m1,wt_00,wt_p1,wt_p2 ;   /* interpolation weights */
+   float f_jm1_km1, f_j00_km1, f_jp1_km1, f_jp2_km1, /* interpolants */
+         f_jm1_k00, f_j00_k00, f_jp1_k00, f_jp2_k00,
+         f_jm1_kp1, f_j00_kp1, f_jp1_kp1, f_jp2_kp1,
+         f_jm1_kp2, f_j00_kp2, f_jp1_kp2, f_jp2_kp2,
+         f_km1    , f_k00    , f_kp1    , f_kp2     ;
+
+   for( pp=0 ; pp < npp ; pp++ ){
+     xx = ip[pp] ; if( xx < -0.499f || xx > nxh ){ vv[pp]=0.0f; continue; }
+     yy = jp[pp] ; if( yy < -0.499f || yy > nyh ){ vv[pp]=0.0f; continue; }
+     zz = kp[pp] ; if( zz < -0.499f || zz > nzh ){ vv[pp]=0.0f; continue; }
+
+     ix = floorf(xx) ;  fx = xx - ix ;   /* integer and       */
+     jy = floorf(yy) ;  fy = yy - jy ;   /* fractional coords */
+     kz = floorf(zz) ;  fz = zz - kz ;
+
+     ix_m1 = ix-1    ; ix_00 = ix      ; ix_p1 = ix+1    ; ix_p2 = ix+2    ;
+     CLIP(ix_m1,nx1) ; CLIP(ix_00,nx1) ; CLIP(ix_p1,nx1) ; CLIP(ix_p2,nx1) ;
+
+     jy_m1 = jy-1    ; jy_00 = jy      ; jy_p1 = jy+1    ; jy_p2 = jy+2    ;
+     CLIP(jy_m1,ny1) ; CLIP(jy_00,ny1) ; CLIP(jy_p1,ny1) ; CLIP(jy_p2,ny1) ;
+
+     kz_m1 = kz-1    ; kz_00 = kz      ; kz_p1 = kz+1    ; kz_p2 = kz+2    ;
+     CLIP(kz_m1,nz1) ; CLIP(kz_00,nz1) ; CLIP(kz_p1,nz1) ; CLIP(kz_p2,nz1) ;
+
+     wt_m1 = P_M1(fx) ; wt_00 = P_00(fx) ;  /* interpolation weights */
+     wt_p1 = P_P1(fx) ; wt_p2 = P_P2(fx) ;
+
+#undef  XINT
+#define XINT(j,k) wt_m1*FAR(ix_m1,j,k)+wt_00*FAR(ix_00,j,k)  \
+                 +wt_p1*FAR(ix_p1,j,k)+wt_p2*FAR(ix_p2,j,k)
+
+     /* interpolate to location ix+fx at each jy,kz level */
+
+     f_jm1_km1 = XINT(jy_m1,kz_m1) ; f_j00_km1 = XINT(jy_00,kz_m1) ;
+     f_jp1_km1 = XINT(jy_p1,kz_m1) ; f_jp2_km1 = XINT(jy_p2,kz_m1) ;
+     f_jm1_k00 = XINT(jy_m1,kz_00) ; f_j00_k00 = XINT(jy_00,kz_00) ;
+     f_jp1_k00 = XINT(jy_p1,kz_00) ; f_jp2_k00 = XINT(jy_p2,kz_00) ;
+     f_jm1_kp1 = XINT(jy_m1,kz_p1) ; f_j00_kp1 = XINT(jy_00,kz_p1) ;
+     f_jp1_kp1 = XINT(jy_p1,kz_p1) ; f_jp2_kp1 = XINT(jy_p2,kz_p1) ;
+     f_jm1_kp2 = XINT(jy_m1,kz_p2) ; f_j00_kp2 = XINT(jy_00,kz_p2) ;
+     f_jp1_kp2 = XINT(jy_p1,kz_p2) ; f_jp2_kp2 = XINT(jy_p2,kz_p2) ;
+
+     /* interpolate to jy+fy at each kz level */
+
+     wt_m1 = P_M1(fy) ; wt_00 = P_00(fy) ;
+     wt_p1 = P_P1(fy) ; wt_p2 = P_P2(fy) ;
+
+     f_km1 =  wt_m1 * f_jm1_km1 + wt_00 * f_j00_km1
+            + wt_p1 * f_jp1_km1 + wt_p2 * f_jp2_km1 ;
+     f_k00 =  wt_m1 * f_jm1_k00 + wt_00 * f_j00_k00
+            + wt_p1 * f_jp1_k00 + wt_p2 * f_jp2_k00 ;
+     f_kp1 =  wt_m1 * f_jm1_kp1 + wt_00 * f_j00_kp1
+            + wt_p1 * f_jp1_kp1 + wt_p2 * f_jp2_kp1 ;
+     f_kp2 =  wt_m1 * f_jm1_kp2 + wt_00 * f_j00_kp2
+            + wt_p1 * f_jp1_kp2 + wt_p2 * f_jp2_kp2 ;
+
+     /* interpolate to kz+fz to get output */
+
+     wt_m1 = P_M1(fz) ; wt_00 = P_00(fz) ;
+     wt_p1 = P_P1(fz) ; wt_p2 = P_P2(fz) ;
+
+     vv[pp] = P_FACTOR * (  wt_m1 * f_km1 + wt_00 * f_k00
+                          + wt_p1 * f_kp1 + wt_p2 * f_kp2 ) ;
+   }
+   return ;
+}
+
+
+/*---------------------------------------------------------------------------*/
+
+/* define quintic interpolation polynomials (Lagrange) */
+
+#undef  Q_M2
+#undef  Q_M1
+#undef  Q_00
+#undef  Q_P1
+#undef  Q_P2
+#undef  Q_P3
+#define Q_M2(x)  (x*(x*x-1.0)*(2.0-x)*(x-3.0)*0.008333333)
+#define Q_M1(x)  (x*(x*x-4.0)*(x-1.0)*(x-3.0)*0.041666667)
+#define Q_00(x)  ((x*x-4.0)*(x*x-1.0)*(3.0-x)*0.083333333)
+#define Q_P1(x)  (x*(x*x-4.0)*(x+1.0)*(x-3.0)*0.083333333)
+#define Q_P2(x)  (x*(x*x-1.0)*(x+2.0)*(3.0-x)*0.041666667)
+#define Q_P3(x)  (x*(x*x-1.0)*(x*x-4.0)*0.008333333)
+
+static void GA_interp_quintic( MRI_IMAGE *fim ,
+                               int npp, float *ip, float *jp, float *kp, float *vv )
+{
+   int nx=fim->nx , ny=fim->ny , nz=fim->nz , nxy=nx*ny , pp ;
+   float nxh=nx-0.501f , nyh=ny-0.501f , nzh=nz-0.501f , xx,yy,zz ;
+   float fx,fy,fz ;
+   float *far = MRI_FLOAT_PTR(fim) ;
+   int nx1=nx-1,ny1=ny-1,nz1=nz-1, ix,jy,kz ;
+   int ix_m2,ix_m1,ix_00,ix_p1,ix_p2,ix_p3 ; /* interpolation indices */
+   int jy_m2,jy_m1,jy_00,jy_p1,jy_p2,jy_p3 ; /* (input image) */
+   int kz_m2,kz_m1,kz_00,kz_p1,kz_p2,kz_p3 ;
+
+   float wt_m2,wt_m1,wt_00,wt_p1,wt_p2,wt_p3 ; /* interpolation weights */
+
+   float f_jm2_km2, f_jm1_km2, f_j00_km2, f_jp1_km2, f_jp2_km2, f_jp3_km2,
+         f_jm2_km1, f_jm1_km1, f_j00_km1, f_jp1_km1, f_jp2_km1, f_jp3_km1,
+         f_jm2_k00, f_jm1_k00, f_j00_k00, f_jp1_k00, f_jp2_k00, f_jp3_k00,
+         f_jm2_kp1, f_jm1_kp1, f_j00_kp1, f_jp1_kp1, f_jp2_kp1, f_jp3_kp1,
+         f_jm2_kp2, f_jm1_kp2, f_j00_kp2, f_jp1_kp2, f_jp2_kp2, f_jp3_kp2,
+         f_jm2_kp3, f_jm1_kp3, f_j00_kp3, f_jp1_kp3, f_jp2_kp3, f_jp3_kp3,
+         f_km2    , f_km1    , f_k00    , f_kp1    , f_kp2    , f_kp3     ;
+
+   for( pp=0 ; pp < npp ; pp++ ){
+     xx = ip[pp] ; if( xx < -0.499f || xx > nxh ){ vv[pp]=0.0f; continue; }
+     yy = jp[pp] ; if( yy < -0.499f || yy > nyh ){ vv[pp]=0.0f; continue; }
+     zz = kp[pp] ; if( zz < -0.499f || zz > nzh ){ vv[pp]=0.0f; continue; }
+
+     ix = floorf(xx) ;  fx = xx - ix ;   /* integer and       */
+     jy = floorf(yy) ;  fy = yy - jy ;   /* fractional coords */
+     kz = floorf(zz) ;  fz = zz - kz ;
+
+     /* compute indexes from which to interpolate (-2,-1,0,+1,+2,+3),
+        but clipped to lie inside input image volume                 */
+
+     ix_m1 = ix-1    ; ix_00 = ix      ; ix_p1 = ix+1    ; ix_p2 = ix+2    ;
+     CLIP(ix_m1,nx1) ; CLIP(ix_00,nx1) ; CLIP(ix_p1,nx1) ; CLIP(ix_p2,nx1) ;
+     ix_m2 = ix-2    ; ix_p3 = ix+3 ;
+     CLIP(ix_m2,nx1) ; CLIP(ix_p3,nx1) ;
+
+     jy_m1 = jy-1    ; jy_00 = jy      ; jy_p1 = jy+1    ; jy_p2 = jy+2    ;
+     CLIP(jy_m1,ny1) ; CLIP(jy_00,ny1) ; CLIP(jy_p1,ny1) ; CLIP(jy_p2,ny1) ;
+     jy_m2 = jy-2    ; jy_p3 = jy+3 ;
+     CLIP(jy_m2,ny1) ; CLIP(jy_p3,ny1) ;
+
+     kz_m1 = kz-1    ; kz_00 = kz      ; kz_p1 = kz+1    ; kz_p2 = kz+2    ;
+     CLIP(kz_m1,nz1) ; CLIP(kz_00,nz1) ; CLIP(kz_p1,nz1) ; CLIP(kz_p2,nz1) ;
+     kz_m2 = kz-2    ; kz_p3 = kz+3 ;
+     CLIP(kz_m2,nz1) ; CLIP(kz_p3,nz1) ;
+
+     wt_m1 = Q_M1(fx) ; wt_00 = Q_00(fx) ;  /* interpolation weights */
+     wt_p1 = Q_P1(fx) ; wt_p2 = Q_P2(fx) ;  /* in x-direction        */
+     wt_m2 = Q_M2(fx) ; wt_p3 = Q_P3(fx) ;
+
+#undef  XINT
+#define XINT(j,k) wt_m2*FAR(ix_m2,j,k)+wt_m1*FAR(ix_m1,j,k) \
+                 +wt_00*FAR(ix_00,j,k)+wt_p1*FAR(ix_p1,j,k) \
+                 +wt_p2*FAR(ix_p2,j,k)+wt_p3*FAR(ix_p3,j,k)
+
+     /* interpolate to location ix+fx at each jy,kz level */
+
+     f_jm2_km2 = XINT(jy_m2,kz_m2) ; f_jm1_km2 = XINT(jy_m1,kz_m2) ;
+     f_j00_km2 = XINT(jy_00,kz_m2) ; f_jp1_km2 = XINT(jy_p1,kz_m2) ;
+     f_jp2_km2 = XINT(jy_p2,kz_m2) ; f_jp3_km2 = XINT(jy_p3,kz_m2) ;
+
+     f_jm2_km1 = XINT(jy_m2,kz_m1) ; f_jm1_km1 = XINT(jy_m1,kz_m1) ;
+     f_j00_km1 = XINT(jy_00,kz_m1) ; f_jp1_km1 = XINT(jy_p1,kz_m1) ;
+     f_jp2_km1 = XINT(jy_p2,kz_m1) ; f_jp3_km1 = XINT(jy_p3,kz_m1) ;
+
+     f_jm2_k00 = XINT(jy_m2,kz_00) ; f_jm1_k00 = XINT(jy_m1,kz_00) ;
+     f_j00_k00 = XINT(jy_00,kz_00) ; f_jp1_k00 = XINT(jy_p1,kz_00) ;
+     f_jp2_k00 = XINT(jy_p2,kz_00) ; f_jp3_k00 = XINT(jy_p3,kz_00) ;
+
+     f_jm2_kp1 = XINT(jy_m2,kz_p1) ; f_jm1_kp1 = XINT(jy_m1,kz_p1) ;
+     f_j00_kp1 = XINT(jy_00,kz_p1) ; f_jp1_kp1 = XINT(jy_p1,kz_p1) ;
+     f_jp2_kp1 = XINT(jy_p2,kz_p1) ; f_jp3_kp1 = XINT(jy_p3,kz_p1) ;
+
+     f_jm2_kp2 = XINT(jy_m2,kz_p2) ; f_jm1_kp2 = XINT(jy_m1,kz_p2) ;
+     f_j00_kp2 = XINT(jy_00,kz_p2) ; f_jp1_kp2 = XINT(jy_p1,kz_p2) ;
+     f_jp2_kp2 = XINT(jy_p2,kz_p2) ; f_jp3_kp2 = XINT(jy_p3,kz_p2) ;
+
+     f_jm2_kp3 = XINT(jy_m2,kz_p3) ; f_jm1_kp3 = XINT(jy_m1,kz_p3) ;
+     f_j00_kp3 = XINT(jy_00,kz_p3) ; f_jp1_kp3 = XINT(jy_p1,kz_p3) ;
+     f_jp2_kp3 = XINT(jy_p2,kz_p3) ; f_jp3_kp3 = XINT(jy_p3,kz_p3) ;
+
+     /* interpolate to jy+fy at each kz level */
+
+     wt_m1 = Q_M1(fy) ; wt_00 = Q_00(fy) ; wt_p1 = Q_P1(fy) ;
+     wt_p2 = Q_P2(fy) ; wt_m2 = Q_M2(fy) ; wt_p3 = Q_P3(fy) ;
+
+     f_km2 =  wt_m2 * f_jm2_km2 + wt_m1 * f_jm1_km2 + wt_00 * f_j00_km2
+            + wt_p1 * f_jp1_km2 + wt_p2 * f_jp2_km2 + wt_p3 * f_jp3_km2 ;
+
+     f_km1 =  wt_m2 * f_jm2_km1 + wt_m1 * f_jm1_km1 + wt_00 * f_j00_km1
+            + wt_p1 * f_jp1_km1 + wt_p2 * f_jp2_km1 + wt_p3 * f_jp3_km1 ;
+
+     f_k00 =  wt_m2 * f_jm2_k00 + wt_m1 * f_jm1_k00 + wt_00 * f_j00_k00
+            + wt_p1 * f_jp1_k00 + wt_p2 * f_jp2_k00 + wt_p3 * f_jp3_k00 ;
+
+     f_kp1 =  wt_m2 * f_jm2_kp1 + wt_m1 * f_jm1_kp1 + wt_00 * f_j00_kp1
+            + wt_p1 * f_jp1_kp1 + wt_p2 * f_jp2_kp1 + wt_p3 * f_jp3_kp1 ;
+
+     f_kp2 =  wt_m2 * f_jm2_kp2 + wt_m1 * f_jm1_kp2 + wt_00 * f_j00_kp2
+            + wt_p1 * f_jp1_kp2 + wt_p2 * f_jp2_kp2 + wt_p3 * f_jp3_kp2 ;
+
+     f_kp3 =  wt_m2 * f_jm2_kp3 + wt_m1 * f_jm1_kp3 + wt_00 * f_j00_kp3
+            + wt_p1 * f_jp1_kp3 + wt_p2 * f_jp2_kp3 + wt_p3 * f_jp3_kp3 ;
+
+     /* interpolate to kz+fz to get output */
+
+     wt_m1 = Q_M1(fz) ; wt_00 = Q_00(fz) ; wt_p1 = Q_P1(fz) ;
+     wt_p2 = Q_P2(fz) ; wt_m2 = Q_M2(fz) ; wt_p3 = Q_P3(fz) ;
+
+     vv[pp] =  wt_m2 * f_km2 + wt_m1 * f_km1 + wt_00 * f_k00
+             + wt_p1 * f_kp1 + wt_p2 * f_kp2 + wt_p3 * f_kp3 ;
+   }
+   return ;
+}
+
+/*---------------------------------------------------------------------------*/
+
 #undef  NPER
 #define NPER 1024
 
 static void GA_get_warped_values( int nmpar , double *mpar , float *avm )
 {
-   int    npar , nfree , ii,jj,kk,qq,pp,npp,mm,nx,ny,nxy , *pma ;
+   int    npar , nfree , ii,jj,kk,qq,pp,npp,mm,nx,ny,nxy , clip=0 ;
    float *wpar ;
    float *imf , *jmf , *kmf ;
    float *imw , *jmw , *kmw ;
 
    npar  = stup->wfunc_numpar ;
    nfree = stup->wfunc_numfree ;
-   pma   = stup->wfunc_pma ;
    wpar  = (float *)malloc(sizeof(float)*npar) ;
 
    for( ii=pp=0 ; ii < npar ; ii++ ){
@@ -132,14 +419,22 @@ static void GA_get_warped_values( int nmpar , double *mpar , float *avm )
 
      switch( stup->interp_code ){
        case MRI_NN:
-         GA_interp_NN    ( stup->ajim , npp , imw,jmw,kmw , avm+pp ) ;
+         GA_interp_NN( stup->ajim , npp , imw,jmw,kmw , avm+pp ) ;
        break ;
+
        case MRI_LINEAR:
          GA_interp_linear( stup->ajim , npp , imw,jmw,kmw , avm+pp ) ;
        break ;
-       default:
-       case MRI_HEPTIC:
-         GA_interp_heptic( stup->ajim , npp , imw,jmw,kmw , avm+pp ) ;
+
+       case MRI_CUBIC:
+         clip = 1 ;
+         GA_interp_cubic( stup->ajim , npp , imw,jmw,kmw , avm+pp ) ;
+       break ;
+
+       default:        /* for higher order methods not implemented herein */
+       case MRI_QUINTIC:
+         clip = 1 ;
+         GA_interp_quintic( stup->ajim , npp , imw,jmw,kmw , avm+pp ) ;
        break ;
      }
    }
@@ -149,6 +444,13 @@ static void GA_get_warped_values( int nmpar , double *mpar , float *avm )
      free((void *)kmf); free((void *)jmf); free((void *)imf);
    }
    free((void *)wpar) ;
+
+   if( clip ){
+     float bb=stup->ajbot , tt=stup->ajtop ;
+     for( pp=0 ; pp < stup->npt_match ; pp++ )
+            if( avm[pp] < bb ) avm[pp] = bb ;
+       else if( avm[pp] > tt ) avm[pp] = tt ;
+   }
 
    return ;
 }
@@ -182,6 +484,7 @@ double GA_scalar_fitter( int npar , double *mpar )
     break ;
   }
 
+  free((void *)avm) ;
   return (double)val ;
 }
 
@@ -201,6 +504,7 @@ MRI_IMAGE * mri_genalign_scalar( MRI_IMAGE *basim  ,
    int nspad , qq , rr , nx,ny,nz,nxy , mm,ii,jj,kk ;
    int use_all=0 ;
    float *bsar ;
+   double *wpar ;
 
 ENTRY("mri_genalign_scalar") ;
 
@@ -275,6 +579,8 @@ ENTRY("mri_genalign_scalar") ;
      qim = GA_smooth( stup->ajim , parm->smooth_code , rad ) ;
      if( qim != NULL ){ mri_free(stup->ajim) ; stup->ajim = qim ; }
    }
+   stup->ajbot = (float)mri_min(stup->ajim) ;
+   stup->ajtop = (float)mri_max(stup->ajim) ;
 
    /** load mask array **/
 
@@ -295,7 +601,7 @@ ENTRY("mri_genalign_scalar") ;
 
    /*-- extract matching points from base image --*/
 
-   if( stup->npt_match <= 9 ){
+   if( stup->npt_match <= 9 || stup->npt_match > stup->bsim->nvox ){
      stup->npt_match = stup->bsim->nvox ; use_all = 1 ;
    }
    if( stup->nmask > 0 && stup->npt_match > stup->nmask ){
@@ -358,6 +664,21 @@ ENTRY("mri_genalign_scalar") ;
    if( stup->match_code == GA_MATCH_SPEARMAN_SCALAR )
      stup->bvstat = spearman_rank_prepare( stup->npt_match , stup->bvm ) ;
 
+   /**----- can now optimize warping parameters -----**/
+
+   wpar = (double *)malloc(sizeof(double)*stup->wfunc_numfree) ;
+   for( ii=qq=0 ; qq < stup->wfunc_numpar ; qq++ )
+     if( !stup->wfunc_param[qq].fixed )
+       wpar[ii++] = stup->wfunc_param[ii].val_init ;
+
+   powell_newuoa( stup->wfunc_numfree , wpar ,
+                  1.0 , 0.05 , 6666 , GA_scalar_fitter ) ;
+
+   for( ii=qq=0 ; qq < stup->wfunc_numpar ; qq++ )
+     if( stup->wfunc_param[qq].fixed )
+       parm->wfunc_param[qq].val_out = parm->wfunc_param[qq].val_fixed ;
+     else
+       parm->wfunc_param[qq].val_out = wpar[ii++] ;
 
    RETURN(NULL) ;
 }
