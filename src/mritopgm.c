d3daab8d964df25632169dd5d5ec1bd6037483a0
rwcox <>
2006-08-11 21:02:39 +0000

    Cput

--- src/mritopgm.c
+++ src/mritopgm.c
@@ -41,36 +41,21 @@ int main( int argc , char *argv[] )
      }
      narg++ ;
    }
-   im   = mri_read_just_one( argv[narg] ) ;
+   im = mri_read_just_one( argv[narg] ) ;
    if( im == NULL ) exit(1) ;
+
+   if( im->kind != MRI_float ){
+     imb = mri_to_float(im) ; mri_free(im) ; im = imb ;
+   }
 
    if( perc > 0.0 ){
      float val = mri_quantile( im , perc ) ;
+     float *ar = mri_data_pointer(im) ;
      int ii ;
-     switch( im->kind ){
-        case MRI_short:{
-          short *ar = mri_data_pointer(im) ;
-          for( ii=0 ; ii < im->nvox ; ii++ ) if( ar[ii] > val ) ar[ii] = val ;
-        }
-        break ;
-        case MRI_byte:{
-          byte *ar = mri_data_pointer(im) ;
-          for( ii=0 ; ii < im->nvox ; ii++ ) if( ar[ii] > val ) ar[ii] = val ;
-        }
-        break ;
-        case MRI_float:{
-          float *ar = mri_data_pointer(im) ;
-          for( ii=0 ; ii < im->nvox ; ii++ ) if( ar[ii] > val ) ar[ii] = val ;
-        }
-        break ;
-        default:
-          fprintf(stderr,"** Don't know how to percent clip this type of image!\n") ;
-        break ;
-     }
+     for( ii=0 ; ii < im->nvox ; ii++ ) if( ar[ii] > val ) ar[ii] = val ;
    }
 
-   imb = mri_to_byte( im ) ;
-   mri_free( im ) ;
+   imb = mri_to_byte( im ) ; mri_free( im ) ;
 
    printf( "P5 %d %d 255\n" , imb->nx , imb->ny ) ;
    fwrite( imb->im.byte_data , imb->pixel_size , imb->nx * imb->ny , stdout ) ;
