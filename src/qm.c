bcd4fd3010166c5b1318212824530bedde095820
rwcox <>
2006-08-16 20:49:47 +0000

    Cput

--- src/qm.c
+++ src/qm.c
@@ -10,20 +10,31 @@ int main( int argc , char *argv[] )
    GA_setup stup ;
    float xxx,yyy,zzz ; int ii , iarg=1 ;
    int meth=GA_MATCH_PEARSON_SCALAR ;
-   float rad=0.0f ; int sm=GA_SMOOTH_GAUSSIAN , mask=0 , twopass=0 ;
+   float rad=0.0f ; int sm=GA_SMOOTH_GAUSSIAN , mask=0 , twopass=0 , verb=1 ;
    char *fout=NULL ;
    MRI_IMAGE *maskim=NULL ; byte *maskar=NULL ; int nx,ny,nz , nrand,nmask=0 ;
+   int npmax = 999999999 ;
 
    if( argc < 3 ){
-     printf("Usage: qm [-mask] [-rank] [-rad r] [-out f] [-med] base targ\n"); exit(0);
+     printf("Usage: qm [-n nnn] [-q] [-mask] [-rank] [-mi] [-rad r] [-out f] [-med] base targ\n");
+     exit(0);
    }
 
    mainENTRY("qm") ;
    srand48((long)time(NULL)+(long)getpid()) ;
 
    while( iarg < argc && argv[iarg][0] == '-' ){
+     if( strcmp(argv[iarg],"-n") == 0 ){
+       npmax = (int)strtod(argv[++iarg],NULL); iarg++; continue ;
+     }
+     if( strcmp(argv[iarg],"-q") == 0 ){
+       verb = 0 ; iarg++ ; continue ;
+     }
      if( strcmp(argv[iarg],"-rank") == 0 ){
        meth = GA_MATCH_SPEARMAN_SCALAR ; iarg++ ; continue ;
+     }
+     if( strcmp(argv[iarg],"-mi") == 0 ){
+       meth = GA_MATCH_KULLBACK_SCALAR ; iarg++ ; continue ;
      }
      if( strcmp(argv[iarg],"-rad") == 0 ){
        rad = (float)strtod(argv[++iarg],NULL) ; iarg++ ; continue ;
@@ -56,7 +67,7 @@ int main( int argc , char *argv[] )
        THD_mask_fillin_completely( nx,ny,nz , maskar, 3 ) ;
      }
      nmask = THD_countmask(nx*ny*nz,maskar) ;
-     INFO_message("%d/%d voxels in mask/image",nmask,nx*ny*nz) ;
+     if(verb)INFO_message("%d/%d voxels in mask/image",nmask,nx*ny*nz) ;
      maskim = mri_new_vol_empty(nx,ny,nz,MRI_byte) ;
      mri_fix_data_pointer(maskar,maskim) ;
    } else {
@@ -74,6 +85,7 @@ int main( int argc , char *argv[] )
    stup.smooth_radius = rad ;
    stup.interp_code   = MRI_NN ;
    stup.npt_match     = nmask / 16 ;
+   if( stup.npt_match > npmax ) stup.npt_match = npmax ;
 
    stup.wfunc         = mri_genalign_affine ;
    stup.wfunc_numpar  = 6 ;
@@ -90,10 +102,10 @@ int main( int argc , char *argv[] )
      stup.wfunc_param[p].fixed = 0 ;              \
  } while(0)
 
-   xxx = 0.5*fabs(ima->nx*ima->dx) ;
-   yyy = 0.5*fabs(ima->ny*ima->dy) ;
-   zzz = 0.5*fabs(ima->nz*ima->dz) ;
-   INFO_message("Max displacement allowed = (%.1f,%.1f,%.1f) mm",xxx,yyy,zzz) ;
+   xxx = 0.444*fabs(ima->nx*ima->dx) ;
+   yyy = 0.444*fabs(ima->ny*ima->dy) ;
+   zzz = 0.444*fabs(ima->nz*ima->dz) ;
+   if(verb)INFO_message("Max displacement allowed = (%.1f,%.1f,%.1f) mm",xxx,yyy,zzz) ;
 
    DEFPAR( 0, "x-shift" , -xxx , xxx , 0.0 , 0.0 , 0.0 ) ;
    DEFPAR( 1, "y-shift" , -yyy , yyy , 0.0 , 0.0 , 0.0 ) ;
@@ -108,44 +120,48 @@ int main( int argc , char *argv[] )
      stup.wfunc_param[4].fixed = 1 ;
      stup.wfunc_param[5].fixed = 1 ;
      nrand = 77 ;
+     if(verb)INFO_message("2D files") ;
    } else {
      nrand = 21 ;
+     if(verb)INFO_message("3D files") ;
    }
 
    mri_genalign_scalar_setup( ima , maskim , imb , &stup ) ;
 
-INFO_message("Start random setup") ;
+   if(verb)INFO_message("Start random setup") ;
    mri_genalign_scalar_ransetup( &stup , nrand ) ;
-   INFO_message("val_init: %.2f %.2f %.2f  %.2f %.2f %.2f\n",
+   if(verb)INFO_message("val_init: %.2f %.2f %.2f  %.2f %.2f %.2f\n",
                 stup.wfunc_param[0].val_init ,
                 stup.wfunc_param[1].val_init ,
                 stup.wfunc_param[2].val_init ,
                 stup.wfunc_param[3].val_init ,
                 stup.wfunc_param[4].val_init ,
                 stup.wfunc_param[5].val_init  );
-   INFO_message("vbest = %g",stup.vbest) ;
+   if(verb)INFO_message("vbest = %g",stup.vbest) ;
 
-INFO_message("Start optimization") ;
+   if(verb)INFO_message("Start optimization") ;
    stup.npt_match   = nmask / 8 ;
+   if( stup.npt_match > npmax ) stup.npt_match = npmax ;
    stup.interp_code = MRI_LINEAR ;
    mri_genalign_scalar_setup( ima , maskim , imb , &stup ) ;
-   ii = mri_genalign_scalar_optim( &stup , 0.01 , 0.0001 , 6666 ) ;
-   INFO_message("val_out:: %.2f %.2f %.2f  %.2f %.2f %.2f\n",
+   ii = mri_genalign_scalar_optim( &stup , 0.04 , 0.0001 , 6666 ) ;
+   if(verb)INFO_message("val_out:: %.2f %.2f %.2f  %.2f %.2f %.2f\n",
                 stup.wfunc_param[0].val_out ,
                 stup.wfunc_param[1].val_out ,
                 stup.wfunc_param[2].val_out ,
                 stup.wfunc_param[3].val_out ,
                 stup.wfunc_param[4].val_out ,
                 stup.wfunc_param[5].val_out  );
-   INFO_message("nfunc = %d",ii) ;
-   INFO_message("vbest = %g",stup.vbest) ;
+   if(verb)INFO_message("nfunc = %d",ii) ;
+   if(verb)INFO_message("vbest = %g",stup.vbest) ;
 
    if( twopass ){
-INFO_message("Restart optimization") ;
+   if(verb)INFO_message("Restart optimization") ;
      stup.smooth_code   = 0  ;
      stup.smooth_radius = 0.0f ;
      stup.interp_code   = MRI_CUBIC ;
      stup.npt_match     = nmask / 4 ;
+     if( stup.npt_match > npmax ) stup.npt_match = npmax ;
      mri_genalign_scalar_setup( NULL,NULL,NULL, &stup ) ;
      stup.wfunc_param[0].val_init = stup.wfunc_param[0].val_out ;
      stup.wfunc_param[1].val_init = stup.wfunc_param[1].val_out ;
@@ -153,7 +169,7 @@ INFO_message("Restart optimization") ;
      stup.wfunc_param[3].val_init = stup.wfunc_param[3].val_out ;
      stup.wfunc_param[4].val_init = stup.wfunc_param[4].val_out ;
      stup.wfunc_param[5].val_init = stup.wfunc_param[5].val_out ;
-     ii = mri_genalign_scalar_optim( &stup , 0.005 , 0.0001 , 6666 ) ;
+     ii = mri_genalign_scalar_optim( &stup , 0.01 , 0.0001 , 6666 ) ;
      INFO_message("val_fin:: %.2f %.2f %.2f  %.2f %.2f %.2f\n",
                   stup.wfunc_param[0].val_out ,
                   stup.wfunc_param[1].val_out ,
@@ -161,8 +177,8 @@ INFO_message("Restart optimization") ;
                   stup.wfunc_param[3].val_out ,
                   stup.wfunc_param[4].val_out ,
                   stup.wfunc_param[5].val_out  );
-     INFO_message("nfunc = %d",ii) ;
-     INFO_message("vbest = %g",stup.vbest) ;
+     if(verb)INFO_message("nfunc = %d",ii) ;
+     if(verb)INFO_message("vbest = %g",stup.vbest) ;
    }
 
    if( fout != NULL ){
@@ -172,8 +188,10 @@ INFO_message("Restart optimization") ;
          MRI_IMAGE *qim = mri_to_mri( ima->kind , wim ) ;
          mri_free(wim) ; wim = qim ;
        }
+       if(verb)INFO_message("Writing %d X %d X %d image of %s",
+                wim->nx , wim->ny , wim->nz , MRI_TYPE_name[wim->kind] ) ;
        mri_write( fout , wim ) ; mri_free(wim) ;
-       INFO_message("Output %s written",fout) ;
+       if(verb)INFO_message("Output %s written",fout) ;
      }
    }
 
