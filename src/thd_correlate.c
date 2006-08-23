926ca46ed3f21fd13eae18389b03b1dce4684793
rwcox <>
2006-08-23 20:53:40 +0000

    Cput

--- src/thd_correlate.c
+++ src/thd_correlate.c
@@ -1,5 +1,15 @@
 #include "mrilib.h"
 
+#ifdef SOLARIS
+# define sqrtf sqrt
+# define logf  log
+#endif
+
+#if defined(SOLARIS) || defined(SGI)
+# define cbrtf  cbrt
+#endif
+
+
 /*==============================================================================*/
 /*========== The following functions were moved from afni_fimfunc.c -===========*/
 /*==============================================================================*/
@@ -153,6 +163,19 @@ float THD_spearman_corr( int n , float *x , float *y )
    return spearman_rank_corr( n,y,xv,x ) ;
 }
 
+/*--------------------------------------------------------------------------*/
+/*! Spearman rank-order correlation of x[] and y[] (nondestructive).  */
+
+float THD_spearman_corr_nd( int n , float *x , float *y )
+{
+   float *qx, *qy , cv=0.0f ;
+   qx = (float *)malloc(sizeof(float)*n); memcpy(qx,x,sizeof(float)*n);
+   qy = (float *)malloc(sizeof(float)*n); memcpy(qy,y,sizeof(float)*n);
+   cv = THD_spearman_corr(n,qx,qy) ;
+   free((void *)qy); free((void *)qx); 
+   return cv ;
+}
+
 /*--------------------------------------------------------------*/
 /*! Quadrant correlation of x[] and y[] (x and y are modified). */
 
@@ -161,6 +184,19 @@ float THD_quadrant_corr( int n , float *x , float *y )
    float xv = quadrant_corr_prepare(n,x) ;
    if( xv <= 0.0 ) return 0.0 ;
    return quadrant_corr( n,y,xv,x ) ;
+}
+
+/*--------------------------------------------------------------------------*/
+/*! Quadrant rank-order correlation of x[] and y[] (nondestructive).  */
+
+float THD_quadrant_corr_nd( int n , float *x , float *y )
+{
+   float *qx, *qy , cv=0.0f ;
+   qx = (float *)malloc(sizeof(float)*n); memcpy(qx,x,sizeof(float)*n);
+   qy = (float *)malloc(sizeof(float)*n); memcpy(qy,y,sizeof(float)*n);
+   cv = THD_quadrant_corr(n,qx,qy) ;
+   free((void *)qy); free((void *)qx); 
+   return cv ;
 }
 
 /*----------------------------------------------------------------*/
@@ -200,11 +236,12 @@ float mri_spearman_corr( MRI_IMAGE *im , MRI_IMAGE *jm )
 /*! Compute the mutual info between two vectors, sort of.  [16 Aug 2006]
 ----------------------------------------------------------------------------*/
 
-float THD_mutual_info( int n , float xbot,float xtop,float *x ,
-                               float ybot,float ytop,float *y  )
+float THD_mutual_info_scl( int n , float xbot,float xtop,float *x ,
+                                   float ybot,float ytop,float *y  )
 {
    int nbin,nbp , ii,jj,kk ;
-   float xb,xi , yb,yi , xx,yy , x1,y1 , nbm , val ;
+   float xb,xi , yb,yi , xx,yy , x1,y1 , nbb , val ;
+
    static int n_old=-1 , nbin_old=255 ;
    static float *xc=NULL , *yc=NULL , *xyc=NULL ;
 
@@ -212,6 +249,7 @@ float THD_mutual_info( int n , float xbot,float xtop,float *x ,
      if( xc  != NULL ){ free((void *)xc ); xc  = NULL; }
      if( yc  != NULL ){ free((void *)yc ); yc  = NULL; }
      if( xyc != NULL ){ free((void *)xyc); xyc = NULL; }
+     return 0.0f ;
    }
 
    if( xbot >= xtop ){
@@ -230,16 +268,17 @@ float THD_mutual_info( int n , float xbot,float xtop,float *x ,
      if( ybot >= ytop ) return 0.0f ;
    }
 
-   if( n = n_old ){
+   if( n == n_old ){
      nbin = nbin_old ;
    } else {
-     nbin = (int)sqrtf((float)n); if( nbin > 255 ) nbin = 255 ;
+     nbin = (int)cbrtf((float)n);
+     if( nbin > 255 ) nbin = 255; else if( nbin < 3 ) nbin = 3;
      nbin_old = nbin ; n_old = n ;
      if( xc  != NULL ){ free((void *)xc ); xc  = NULL; }
      if( yc  != NULL ){ free((void *)yc ); yc  = NULL; }
      if( xyc != NULL ){ free((void *)xyc); xyc = NULL; }
    }
-   nbp = nbin+1 ; nbm = nbin-1 ;
+   nbp = nbin+1 ; nbb = nbin-0.001f ;
 
    if( xc  == NULL ) xc  = (float *)malloc(sizeof(float)*nbp) ;
    if( yc  == NULL ) yc  = (float *)malloc(sizeof(float)*nbp) ;
@@ -248,20 +287,21 @@ float THD_mutual_info( int n , float xbot,float xtop,float *x ,
 #undef  XYC
 #define XYC(p,q) xyc[(p)+(q)*nbp]
 
-   memset( xc  , 0 , sizeof(float)*nbin ) ;
-   memset( yc  , 0 , sizeof(float)*nbin ) ;
-   memset( xyc , 0 , sizeof(float)*nbin*nbin ) ;
+   memset( xc  , 0 , sizeof(float)*nbp ) ;
+   memset( yc  , 0 , sizeof(float)*nbp ) ;
+   memset( xyc , 0 , sizeof(float)*nbp*nbp ) ;
 
-   xb = xbot ; xi = (nbin-0.0001)/(xtop-xbot) ;
-   yb = ybot ; yi = (nbin-0.0001)/(ytop-xbot) ;
+   xb = xbot ; xi = nbb/(xtop-xbot) ;
+   yb = ybot ; yi = nbb/(ytop-xbot) ;
    for( ii=0 ; ii < n ; ii++ ){
      xx = (x[ii]-xb)*xi ;
-     if( xx < 0.0f ) xx = 0.0f ; else if( xx > nbm ) xx = nbm ;
+     if( xx < 0.0f ) xx = 0.0f ; else if( xx > nbb ) xx = nbb ;
      jj = (int)xx ; xx = xx - jj ; x1 = 1.0f-xx ;
      yy = (y[ii]-yb)*yi ;
-     if( yy < 0.0f ) yy = 0.0f ; else if( yy > nbm ) yy = nbm ;
+     if( yy < 0.0f ) yy = 0.0f ; else if( yy > nbb ) yy = nbb ;
      kk = (int)yy ; yy = yy - kk ; y1 = 1.0f-yy ;
 
+#if 1
      xc[jj] += x1 ; xc[jj+1] += xx ;
      yc[kk] += y1 ; yc[kk+1] += yy ;
 
@@ -269,14 +309,118 @@ float THD_mutual_info( int n , float xbot,float xtop,float *x ,
      XYC(jj+1,kk  ) += xx*y1 ;
      XYC(jj  ,kk+1) += x1*yy ;
      XYC(jj+1,kk+1) += xx*yy ;
+#else
+     xc[jj]++ ; yc[kk]++ ; XYC(jj,kk)++ ;
+#endif
    }
 
-   val = 0.0f ;
+   val = 0.0f ; nbb = n ;
    for( ii=0 ; ii < nbp ; ii++ ){
      for( jj=0 ; jj < nbp ; jj++ ){
        if( XYC(ii,jj) > 0.0f )
-         val += XYC(ii,jj) * logf( XYC(ii,jj)/(xc[ii]*yc[jj]) ) ;
+         val += XYC(ii,jj) * logf( nbb*XYC(ii,jj)/(xc[ii]*yc[jj]) ) ;
      }
    }
-   return val ;
+   return (1.4427*val/nbb) ;  /* units are bits, just for fun */
 }
+
+/*--------------------------------------------------------------------------*/
+
+float THD_mutual_info( int n , float *x , float *y )
+{
+   return THD_mutual_info_scl( n , 1.0f,-1.0f , x, 1.0f,-1.0f , y ) ;
+}
+
+#if 0
+/*--------------------------------------------------------------------------*/
+/*! Compute the correlation ratio between two vectors, sort of.  [23 Aug 2006]
+----------------------------------------------------------------------------*/
+
+float THD_corr_ratio_scl( int n , float xbot,float xtop,float *x ,
+                                  float ybot,float ytop,float *y  )
+{
+   int nbin,nbp , ii,jj,kk ;
+   float xb,xi , yb,yi , xx,yy , x1,y1 , nbb , val ;
+
+   static int n_old=-1 , nbin_old=255 ;
+   static float *xc=NULL , *yc=NULL , *xyc=NULL ;
+
+   if( n <= 1 || x == NULL || y == NULL ){
+     if( xc  != NULL ){ free((void *)xc ); xc  = NULL; }
+     if( yc  != NULL ){ free((void *)yc ); yc  = NULL; }
+     if( xyc != NULL ){ free((void *)xyc); xyc = NULL; }
+     return 0.0f ;
+   }
+
+   if( xbot >= xtop ){
+     xbot = ybot = x[0] ;
+     for( ii=0 ; ii < n ; ii++ )
+            if( x[ii] > xtop ) xtop = x[ii] ;
+       else if( x[ii] < xbot ) xbot = x[ii] ;
+     if( xbot >= xtop ) return 0.0f ;
+   }
+
+   if( ybot >= ytop ){
+     ybot = ybot = y[0] ;
+     for( ii=0 ; ii < n ; ii++ )
+            if( y[ii] > ytop ) ytop = y[ii] ;
+       else if( y[ii] < ybot ) ybot = y[ii] ;
+     if( ybot >= ytop ) return 0.0f ;
+   }
+
+   if( n == n_old ){
+     nbin = nbin_old ;
+   } else {
+     nbin = (int)cbrtf((float)n);
+     if( nbin > 255 ) nbin = 255; else if( nbin < 3 ) nbin = 3;
+     nbin_old = nbin ; n_old = n ;
+     if( xc  != NULL ){ free((void *)xc ); xc  = NULL; }
+     if( yc  != NULL ){ free((void *)yc ); yc  = NULL; }
+     if( xyc != NULL ){ free((void *)xyc); xyc = NULL; }
+   }
+   nbp = nbin+1 ; nbb = nbin-0.001f ;
+
+   if( xc  == NULL ) xc  = (float *)malloc(sizeof(float)*nbp) ;
+   if( yc  == NULL ) yc  = (float *)malloc(sizeof(float)*nbp) ;
+   if( xyc == NULL ) xyc = (float *)malloc(sizeof(float)*nbp*nbp) ;
+
+#undef  XYC
+#define XYC(p,q) xyc[(p)+(q)*nbp]
+
+   memset( xc  , 0 , sizeof(float)*nbp ) ;
+   memset( yc  , 0 , sizeof(float)*nbp ) ;
+   memset( xyc , 0 , sizeof(float)*nbp*nbp ) ;
+
+   xb = xbot ; xi = nbb/(xtop-xbot) ;
+   yb = ybot ; yi = nbb/(ytop-xbot) ;
+   for( ii=0 ; ii < n ; ii++ ){
+     xx = (x[ii]-xb)*xi ;
+     if( xx < 0.0f ) xx = 0.0f ; else if( xx > nbb ) xx = nbb ;
+     jj = (int)xx ; xx = xx - jj ; x1 = 1.0f-xx ;
+     yy = (y[ii]-yb)*yi ;
+     if( yy < 0.0f ) yy = 0.0f ; else if( yy > nbb ) yy = nbb ;
+     kk = (int)yy ; yy = yy - kk ; y1 = 1.0f-yy ;
+
+#if 1
+     xc[jj] += x1 ; xc[jj+1] += xx ;
+     yc[kk] += y1 ; yc[kk+1] += yy ;
+
+     XYC(jj  ,kk  ) += x1*y1 ;
+     XYC(jj+1,kk  ) += xx*y1 ;
+     XYC(jj  ,kk+1) += x1*yy ;
+     XYC(jj+1,kk+1) += xx*yy ;
+#else
+     xc[jj]++ ; yc[kk]++ ; XYC(jj,kk)++ ;
+#endif
+   }
+
+   return 0.0f ;
+}
+
+/*--------------------------------------------------------------------------*/
+
+float THD_corr_ratio( int n , float *x , float *y )
+{
+   return THD_corr_ratio_scl( n , 1.0f,-1.0f , x, 1.0f,-1.0f , y ) ;
+}
+#endif
