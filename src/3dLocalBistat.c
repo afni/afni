7fefcd22bcaf8fc3bd55d7e40c34083995762e13
rwcox <>
2006-10-26 20:47:12 +0000

    Cadd

---
+++ src/3dLocalBistat.c
@@ -0,0 +1,294 @@
+#include "mrilib.h"
+
+#define MAX_NCODE 666
+
+#define NTYPE_SPHERE 1
+#define NTYPE_RECT   2
+
+int main( int argc , char *argv[] )
+{
+   THD_3dim_dataset *inset=NULL , *jnset=NULL , *outset ;
+   int ncode=0 , code[MAX_NCODE] , iarg=1 , ii ;
+   MCW_cluster *nbhd=NULL ;
+   byte *mask=NULL ; int mask_nx,mask_ny,mask_nz , automask=0 ;
+   char *prefix="./localstat" ;
+   int ntype=0 ; float na=0.0f,nb=0.0f,nc=0.0f ;
+
+   /*---- for the clueless who wish to become clued-in ----*/
+
+   if( argc < 2 || strcmp(argv[1],"-help") == 0 ){
+     printf(
+      "Usage: 3dLocalBistat [options] dataset1 dataset2\n"
+      "\n"
+      "This program computes statistics between 2 datasets,\n"
+      "at each voxel, based on a local neighborhood of that voxel.\n"
+      " - The neighborhood is defined by the '-nbhd' option.\n"
+      " - Statistics to be calculated are defined by the '-stat' option(s).\n"
+      "\n"
+      "OPTIONS\n"
+      "-------\n"
+      " -nbhd 'nnn' = The string 'nnn' defines the region around each\n"
+      "               voxel that will be extracted for the statistics\n"
+      "               calculation.  The format of the 'nnn' string are:\n"
+      "               * 'SPHERE(r)' where 'r' is the radius in mm;\n"
+      "                 the neighborhood is all voxels whose center-to-\n"
+      "                 center distance is less than or equal to 'r'.\n"
+      "                 ** A negative value for 'r' means that the region\n"
+      "                    is calculated using voxel indexes rather than\n"
+      "                    voxel dimensions; that is, the neighborhood\n"
+      "                    region is a \"sphere\" in voxel indexes of\n"
+      "                    \"radius\" abs(r).\n"
+      "               * 'RECT(a,b,c)' is a rectangular block which\n"
+      "                 proceeds plus-or-minus 'a' mm in the x-direction,\n"
+      "                 'b' mm in the y-direction, and 'c' mm in the\n"
+      "                 z-direction.  The correspondence between the\n"
+      "                 dataset xyz axes and the actual spatial orientation\n"
+      "                 can be determined by using program 3dinfo.\n"
+      "                 ** A negative value for 'a' means that the region\n"
+      "                    extends plus-and-minus abs(a) voxels in the\n"
+      "                    x-direction, rather than plus-and-minus a mm.\n"
+      "                    Mutatis mutandum for negative 'b' and/or 'c'.\n"
+      "\n"
+      " -stat sss   = Compute the statistic named 'sss' on the values\n"
+      "               extracted from the region around each voxel:\n"
+      "               * pearson  = Pearson correlation coefficient\n"
+      "               * spearman = Spearman correlation coefficient\n"
+      "               * quadrant = Quadrant correlation coefficient\n"
+      "               * mutinfo  = Mutual Information\n"
+      "               * normuti  = Normalized Mutual Information\n"
+      "               * jointent = Joint entropy\n"
+      "               * hellinger= Hellinger metric\n"
+      "               * crU      = Correlation ratio (unsymmetric)\n"
+      "               * crM      = Correlation ratio (symmetrized by multiplication)\n"
+      "               * crA      = Correlation ratio (symmetrized by addition)\n"
+      "               * num    = number of the values in the region:\n"
+      "                          with the use of -mask or -automask,\n"
+      "                          the size of the region around any given\n"
+      "                          voxel will vary; this option lets you\n"
+      "                          map that size.\n"
+      "               * ALL    = all of the above, in that order\n"
+      "               More than one '-stat' option can be used.\n"
+      "\n"
+      " -mask mset  = Read in dataset 'mset' and use the nonzero voxels\n"
+      "               therein as a mask.  Voxels NOT in the mask will\n"
+      "               not be used in the neighborhood of any voxel. Also,\n"
+      "               a voxel NOT in the mask will have its statistic(s)\n"
+      "               computed as zero (0).\n"
+      " -automask   = Compute the mask as in program 3dAutomask.\n"
+      "               -mask and -automask are mutually exclusive: that is,\n"
+      "               you can only specify one mask.\n"
+      "\n"
+      " -prefix ppp = Use string 'ppp' as the prefix for the output dataset.\n"
+      "               The output dataset is always stored as floats.\n"
+      "\n"
+      "Author: RWCox - October 2006.\n"
+     ) ;
+     exit(0) ;
+   }
+
+   /*---- official startup ---*/
+
+   PRINT_VERSION("3dLocalstat"); mainENTRY("3dLocalstat main"); machdep();
+
+   /*---- loop over options ----*/
+
+   while( iarg < argc && argv[iarg][0] == '-' ){
+
+     if( strcmp(argv[iarg],"-prefix") == 0 ){
+       if( ++iarg >= argc ) ERROR_exit("Need argument after '-prefix'") ;
+       prefix = strdup(argv[iarg]) ;
+       if( !THD_filename_ok(prefix) ) ERROR_exit("Bad name after '-prefix'") ;
+       iarg++ ; continue ;
+     }
+
+     if( strcmp(argv[iarg],"-mask") == 0 ){
+       THD_3dim_dataset *mset ; int mmm ;
+       if( ++iarg >= argc ) ERROR_exit("Need argument after '-mask'") ;
+       if( mask != NULL || automask ) ERROR_exit("Can't have two mask inputs") ;
+       mset = THD_open_dataset( argv[iarg] ) ;
+       if( mset == NULL ) ERROR_exit("Can't open dataset '%s'",argv[iarg]) ;
+       DSET_load(mset) ;
+       if( !DSET_LOADED(mset) ) ERROR_exit("Can't load dataset '%s'",argv[iarg]) ;
+       mask_nx = DSET_NX(mset); mask_ny = DSET_NY(mset); mask_nz = DSET_NZ(mset);
+       mask = THD_makemask( mset , 0 , 0.5f, 0.0f ) ; DSET_delete(mset) ;
+       if( mask == NULL ) ERROR_exit("Can't make mask from dataset '%s'",argv[iarg]) ;
+       mmm = THD_countmask( mask_nx*mask_ny*mask_nz , mask ) ;
+       INFO_message("Number of voxels in mask = %d",mmm) ;
+       if( mmm < 2 ) ERROR_exit("Mask is too small to process") ;
+       iarg++ ; continue ;
+     }
+
+     if( strcmp(argv[iarg],"-automask") == 0 ){
+       if( mask != NULL ) ERROR_exit("Can't have -automask and -mask") ;
+       automask = 1 ;
+       iarg++ ; continue ;
+     }
+
+     if( strcmp(argv[iarg],"-stat") == 0 ){
+       char *cpt ;
+       if( ++iarg >= argc ) ERROR_exit("Need argument after '-stat'") ;
+       cpt = argv[iarg] ; if( *cpt == '-' ) cpt++ ;
+            if( strcasecmp(cpt,"pearson")  == 0 ) code[ncode++] = NBISTAT_PEARSON_CORR ;
+       else if( strcasecmp(cpt,"spearman") == 0 ) code[ncode++] = NBISTAT_SPEARMAN_CORR;
+       else if( strcasecmp(cpt,"quadrant") == 0 ) code[ncode++] = NBISTAT_QUADRANT_CORR;
+       else if( strcasecmp(cpt,"mutinfo")  == 0 ) code[ncode++] = NBISTAT_MUTUAL_INFO  ;
+       else if( strcasecmp(cpt,"normuti")  == 0 ) code[ncode++] = NBISTAT_NORMUT_INFO  ;
+       else if( strcasecmp(cpt,"jointent") == 0 ) code[ncode++] = NBISTAT_JOINT_ENTROPY;
+       else if( strcasecmp(cpt,"hellinger")== 0 ) code[ncode++] = NBISTAT_HELLINGER    ;
+       else if( strcasecmp(cpt,"crU")      == 0 ) code[ncode++] = NBISTAT_CORR_RATIO_U ;
+       else if( strcasecmp(cpt,"crM")      == 0 ) code[ncode++] = NBISTAT_CORR_RATIO_M ;
+       else if( strcasecmp(cpt,"crA")      == 0 ) code[ncode++] = NBISTAT_CORR_RATIO_A ;
+       else if( strcasecmp(cpt,"num")      == 0 ) code[ncode++] = NBISTAT_NUM          ;
+       else if( strcasecmp(cpt,"ALL")      == 0 ){
+          code[ncode++] = NBISTAT_PEARSON_CORR ; code[ncode++] = NBISTAT_SPEARMAN_CORR;
+          code[ncode++] = NBISTAT_QUADRANT_CORR; code[ncode++] = NBISTAT_MUTUAL_INFO  ;
+          code[ncode++] = NBISTAT_NORMUT_INFO  ; code[ncode++] = NBISTAT_JOINT_ENTROPY;
+          code[ncode++] = NBISTAT_HELLINGER    ; code[ncode++] = NBISTAT_CORR_RATIO_U ;
+          code[ncode++] = NBISTAT_CORR_RATIO_M ; code[ncode++] = NBISTAT_CORR_RATIO_A ;
+          code[ncode++] = NBISTAT_NUM          ;
+       }
+       else
+         ERROR_exit("-stat '%s' is an unknown statistic type",argv[iarg]) ;
+
+       iarg++ ; continue ;
+     }
+
+     if( strcmp(argv[iarg],"-nbhd") == 0 ){
+       char *cpt ;
+       if( ntype  >  0    ) ERROR_exit("Can't have 2 '-nbhd' options") ;
+       if( ++iarg >= argc ) ERROR_exit("Need argument after '-nbhd'") ;
+
+       cpt = argv[iarg] ;
+       if( strncasecmp(cpt,"SPHERE",6) == 0 ){
+         sscanf( cpt+7 , "%f" , &na ) ;
+         if( na == 0.0f ) ERROR_exit("Can't have a SPHERE of radius 0") ;
+         ntype = NTYPE_SPHERE ;
+       } else if( strncasecmp(cpt,"RECT",4) == 0 ){
+         sscanf( cpt+5 , "%f,%f,%f" , &na,&nb,&nc ) ;
+         if( na == 0.0f && nb == 0.0f && nc == 0.0f )
+           ERROR_exit("'RECT(0,0,0)' is not a legal neighborhood") ;
+         ntype = NTYPE_RECT ;
+       } else {
+           ERROR_exit("Unknown -nbhd shape: '%s'",cpt) ;
+       }
+       iarg++ ; continue ;
+     }
+
+     ERROR_exit("Uknown option '%s'",argv[iarg]) ;
+
+   } /*--- end of loop over options ---*/
+
+   /*---- check for stupid user inputs ----*/
+
+   if( ncode <= 0 ) ERROR_exit("No '-stat' options given?") ;
+   if( ntype <= 0 ) ERROR_exit("No '-nbhd' option given?!") ;
+
+   /*---- deal with input datasets ----*/
+
+   if( iarg >= argc-1 ) ERROR_exit("No input dataset on command line?") ;
+   inset = THD_open_dataset( argv[iarg] ) ;
+   if( inset == NULL  ) ERROR_exit("Can't open dataset '%s'",argv[iarg]) ;
+   iarg++ ;
+   jnset = THD_open_dataset( argv[iarg] ) ;
+   if( jnset == NULL  ) ERROR_exit("Can't open dataset '%s'",argv[iarg]) ;
+   if( DSET_NVOX(jnset)  != DSET_NVOX(inset) )
+     ERROR_exit("Input datasets have different numbers of voxels!?");
+   if( DSET_NVALS(jnset) != DSET_NVALS(inset)  )
+     ERROR_exit("Input datasets have different numbers of sub-bricks!?");
+
+   DSET_load(inset) ;
+   if( !DSET_LOADED(inset) )
+     ERROR_exit("Can't load input dataset '%s'",DSET_BRIKNAME(inset)) ;
+   DSET_load(jnset) ;
+   if( !DSET_LOADED(jnset) )
+     ERROR_exit("Can't load input dataset '%s'",DSET_BRIKNAME(jnset)) ;
+
+   if( mask != NULL ){
+     if( mask_nx != DSET_NX(inset) ||
+         mask_ny != DSET_NY(inset) ||
+         mask_nz != DSET_NZ(inset)   )
+       ERROR_exit("-mask dataset grid doesn't match input dataset") ;
+
+   } else if( automask ){
+     int mmm , nvox ; byte *jask ;
+     mask = THD_automask( inset ) ;
+     if( mask == NULL )
+       ERROR_message("Can't create -automask from input dataset #1?") ;
+     jask = THD_automask( jnset ) ;
+     if( jask == NULL )
+       ERROR_message("Can't create -automask from input dataset #2?") ;
+     nvox = DSET_NVOX(inset) ;
+     for( ii=0 ; ii < nvox ; ii++ ) mask[ii] = (mask[ii] || jask[ii]) ;
+     free(jask) ;
+     mmm = THD_countmask( nvox , mask ) ;
+     INFO_message("Number of voxels in automask = %d",mmm) ;
+     if( mmm < 11 ) ERROR_exit("Automask is too small to process") ;
+   }
+
+   /*---- create neighborhood -----*/
+
+   switch( ntype ){
+     default:
+       ERROR_exit("WTF?  ntype=%d",ntype) ;
+
+     case NTYPE_SPHERE:{
+       float dx , dy , dz ;
+       if( na < 0.0f ){ dx = dy = dz = 1.0f ; na = -na ; }
+       else           { dx = fabsf(DSET_DX(inset)) ;
+                        dy = fabsf(DSET_DY(inset)) ;
+                        dz = fabsf(DSET_DZ(inset)) ; }
+       nbhd = MCW_spheremask( dx,dy,dz , na ) ;
+     }
+     break ;
+
+     case NTYPE_RECT:{
+       float dx , dy , dz ;
+       if( na < 0.0f ){ dx = 1.0f; na = -na; } else dx = fabsf(DSET_DX(inset));
+       if( nb < 0.0f ){ dy = 1.0f; nb = -nb; } else dy = fabsf(DSET_DY(inset));
+       if( nc < 0.0f ){ dz = 1.0f; nc = -nc; } else dz = fabsf(DSET_DZ(inset));
+       nbhd = MCW_rectmask( dx,dy,dz , na,nb,nc ) ;
+     }
+     break ;
+   }
+
+   INFO_message("Neighborhood comprises %d voxels",nbhd->num_pt) ;
+
+   /*---- actually do some work for a change ----*/
+
+   THD_localbistat_verb(1) ;
+   outset = THD_localbistat( inset,jnset , mask , nbhd , ncode , code ) ;
+
+   DSET_unload(inset) ; DSET_unload(jnset) ;
+
+   if( outset == NULL ) ERROR_exit("Function THD_localbistat() fails?!") ;
+
+   EDIT_dset_items( outset , ADN_prefix,prefix , ADN_none ) ;
+
+   tross_Copy_History( inset , outset ) ;
+   tross_Make_History( "3dLocalBistat" , argc,argv , outset ) ;
+
+#if 0
+   { char *lcode[66] , lll[66] ;
+     lcode[NSTAT_MEAN]   = "MEAN" ; lcode[NSTAT_SIGMA]  = "SIGMA"  ;
+     lcode[NSTAT_CVAR]   = "CVAR" ; lcode[NSTAT_MEDIAN] = "MEDIAN" ;
+     lcode[NSTAT_MAD]    = "MAD"  ; lcode[NSTAT_MAX]    = "MAX"    ;
+     lcode[NSTAT_MIN]    = "MIN"  ; lcode[NSTAT_ABSMAX] = "ABSMAX" ;
+     lcode[NSTAT_VAR]    = "VAR"  ; lcode[NSTAT_NUM]    = "NUM"    ;
+     if( DSET_NVALS(inset) == 1 ){
+       for( ii=0 ; ii < DSET_NVALS(outset) ; ii++ )
+         EDIT_dset_items( outset ,
+                            ADN_brick_label_one+ii , lcode[code[ii%ncode]] ,
+                          ADN_none ) ;
+     } else {
+       for( ii=0 ; ii < DSET_NVALS(outset) ; ii++ ){
+         sprintf(lll,"%s[%d]",lcode[code[ii%ncode]],(ii/ncode)) ;
+         EDIT_dset_items( outset , ADN_brick_label_one+ii,lll, ADN_none ) ;
+       }
+     }
+   }
+#endif
+
+   DSET_write( outset ) ;
+   WROTE_DSET( outset ) ;
+   exit(0) ;
+}
