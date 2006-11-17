42fd6c84a52dc046a3b8fd30424c4d82e95a71e9
rickr <>
2006-11-17 21:43:29 +0000

    Cadd

---
+++ src/python_scripts/afni_util.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import sys, os, string, options
+
+# this file contains various afni utilities   17 Nov 2006 [rickr]
+
+
+# given a path (leading directory or not) swap the trailing
+# filename with the passed prefix and suffix
+def change_path_basename(orig, prefix, suffix):
+    if not orig or not prefix: return
+    (head, tail) = os.path.split(orig)
+    if head == '': return prefix
+    return "%s/%s%s" % (head, prefix, suffix)
+
+
+# read a simple 1D file into a float matrix, and return the matrix
+def read_1D_file(filename, nlines = -1, verb = 0):
+    """skip leading '#', return a 2D array of floats"""
+    try:
+        fp = open(filename, 'r')
+    except:
+        print "failed to open 1D file %s" % filename
+        return None
+
+    if verb: print "+d opened file %s" % filename
+
+    retmat = []
+    lnum   = 0
+    data = fp.read()
+    fp.close()
+    for line in data.splitlines():
+        if 0 <= nlines <= lnum: break   # then stop early
+        if line[0] == '#' or line[0] == '\0':
+            if verb: print "skipping comment line: %s" % line
+            continue
+        retmat.append([])
+        tokens = line.split()
+        for tok in tokens:
+            retmat[lnum].append(float(tok))
+
+        if verb > 1: print "+d line %d, length %d" % (lnum, len(retmat[lnum]))
+
+        lnum += 1
+
+    return retmat
+
+# transpose a 2D matrix, returning the new one
+def transpose(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    newmat = []
+    for c in range(cols):
+        newrow = []
+        for r in range(rows):
+            newrow.append(matrix[r][c])
+        newmat.append(newrow)
+    return newmat
+
+
---
+++ src/python_scripts/make.stim.times.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import sys, os, string, options, afni_util
+
+# Convert a set of binary stim files to a set of stim_times files.
+#
+# Each input stim file can have a set of columns of stim classes,
+#      and multiple input files can be used.  Each column of an
+#      input file is expected to have one row per TR, and a total
+#      of num_TRs * num_runs rows.
+#
+# Options: -files file1.1D file2.1D ...  : specify stim files
+#          -prefix PREFIX                : output prefix for files
+#          -tr TR                        : TR time, in seconds
+#          -nruns NRUNS                  : number of runs
+#          -verb LEVEL                   : provide verbose output
+#
+
+def get_opts():
+    okopts = options.OptionList('for input')
+    okopts.add_opt('-files', -1, [], req=True)
+    okopts.add_opt('-prefix', 1, [], req=True)
+    okopts.add_opt('-tr', 1, [], req=True)
+    okopts.add_opt('-nruns', 1, [], req=True)
+    okopts.add_opt('-verb', 1, [])
+    okopts.trailers = True
+
+    return options.read_options(sys.argv, okopts)
+
+def proc_mats(uopts):
+    if uopts == None: return
+
+    opt = uopts.find_opt('-verb')
+    if opt: verb = int(opt.parlist[0])
+    else:   verb = 0
+
+    opt    = uopts.find_opt('-files')
+    files  = opt.parlist
+    opt    = uopts.find_opt('-nruns')
+    nruns  = int(opt.parlist[0])
+    opt    = uopts.find_opt('-prefix')
+    prefix = opt.parlist[0]
+    opt    = uopts.find_opt('-tr')
+    tr     = float(opt.parlist[0])
+    
+    for file in files:
+        mat = read_1D_file(file, -1, verb=verb)
+        mat = transpose(mat)
+        if not mat:
+            print "read_1D_file failed for file: %s" % file
+            return
+
+        nt = len(mat[0])/nruns
+        if verb: print "file %s, nt = %d, nruns = %d" % (file, nt, nruns)
+
+        newfile_index = 1
+        for row in mat:
+
+            newp = "%s.%02d" % (prefix,newfile_index)
+            newfile = change_path_basename(file, newp, ".1D")
+            if newfile == None: return
+            fp = open(newfile, 'w')
+            for run in range(nruns):
+                rindex = run * nt   # point to start of run
+
+                if not 1 in row[rindex:rindex+nt]:  # no stim in this run
+                    fp.write('*\n')
+                    continue
+                time = 0        # in this run
+                for lcol in range(nt):
+                    if row[rindex+lcol]: fp.write('%f '%time)
+                    time += tr
+                fp.write('\n')
+
+            fp.close()
+            newfile_index += 1
+
+if __name__ == "__main__":
+    proc_mats(get_opts())
+
