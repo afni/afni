228d97393a3cad6e44fdb487e44e6250cdbe1062
rwcox <>
2006-12-15 19:58:19 +0000

    Cput

--- src/afni_graph.c
+++ src/afni_graph.c
@@ -5892,8 +5892,8 @@ ENTRY("GRA_file_pixmap") ;
    tim = XImage_to_mri( grapher->dc , xim , 0 ) ;
    if( tim == NULL ){ MCW_kill_XImage( xim ) ; EXRETURN ; }
 
+   INFO_message("Writing grapher image to '%s'",fname) ;
    mri_write_pnm( fname , tim ) ;
-   INFO_message("Writing grapher image to file %s\n",fname) ;
    mri_free( tim ) ;
    MCW_kill_XImage( xim ) ;  /* 10 Mar 1999 */
    EXRETURN ;
