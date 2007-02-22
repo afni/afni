0e499312c2116d18e28cbf94e2fff767c24c0c63
ziad <>
2007-02-22 19:02:21 +0000

    Cadd

---
+++ src/matlab/ExamineXmat.m
@@ -0,0 +1,43 @@
+function ExamineXmat(fname, polort, dt)
+% a function to examine the design matrix produced by AFNI's 3dDeconvolve
+% parameters not specified in function call 
+if (nargin < 1 | isempty(fname)),
+   fname = uigetfile('*.1D','Pick an Xmat');
+end
+if (nargin < 2 | polort == -1),
+   polort = [];
+   while(isempty(polort) | ~isnumeric(polort) | polort < 0),
+      polort = input('Enter polort: ');
+   end
+end
+if (nargin < 3 | dt < 0.0),   
+   dt = [];
+   while(isempty(dt) | ~isnumeric(dt) | dt < 0.0),
+      dt = input('Enter dt');
+   end
+end
+
+[e,Xabi] = Read_1D(fname);
+
+s = 'ddd';
+
+%assuming motion is last, remove last 6 regressors
+trimmed = Xabi(:,[1+(polort+1)*8:size(Xabi,2)-6]);
+CondFull = cond(Xabi);
+CondNoMotion = cond(Xabi(:, [1:size(Xabi,2)-6]));
+CondNoMotionNoBase = cond(Xabi(:,[1+(polort+1)*8:size(Xabi,2)-6]));
+
+v = [1:1:size(trimmed,2)];
+t = 0:dt:(size(trimmed,1)-1)*dt;
+while (~isempty(s) & ~isempty(v)),
+   figure(1); clf
+   for (i=1:1:length(v)),
+      subplot (length(v), 1, i);
+      plot (t, trimmed(:, v(i))); ylabel(sprintf('R%d', v(i)));
+      if (i==1) title(sprintf('Condition #: Full %d\t NoMot %g\t NoMotNoBase %g\t Viewed %g\n',...
+                               CondFull, CondNoMotion, CondNoMotionNoBase, cond(trimmed(:, [v])))); end
+   end
+   xlabel('time (sec)');
+   s = input ('Enter what you want see: ', 's');
+   eval(sprintf('v=%s;', s));
+end
