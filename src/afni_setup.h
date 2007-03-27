7c1399807f735337ffeb21b46f238011b1bf16c0
rwcox <>
2007-03-27 00:38:07 +0000

    Cput

--- src/afni_setup.h
+++ src/afni_setup.h
@@ -93,6 +93,10 @@ extern void AFNI_palette_tran_CB( MCW_arrowval * , XtPointer ) ;
 extern void AFNI_thr_EV( Widget , XtPointer , XEvent * , Boolean * ) ;
 extern void AFNI_clu_CB( Widget , XtPointer , XtPointer ) ;
 
+extern void set_vedit_label( Three_D_View *im3d , int ll ) ;  /* 26 Mar 2007 */
+#define VEDIT_clear_label(iq) set_vedit_label(iq,0)
+#define VEDIT_clust_label(iq) set_vedit_label(iq,1)
+
 #ifdef  __cplusplus
 }
 #endif
