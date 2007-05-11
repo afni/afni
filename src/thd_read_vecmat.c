6c2a48f4d6f806d0c169562801bc9fd3da5d2df0
ziad <>
2007-05-11 20:02:26 +0000

    Cput

--- src/thd_read_vecmat.c
+++ src/thd_read_vecmat.c
@@ -43,109 +43,113 @@ ENTRY("THD_read_dvecmat") ;
        dvm.vv = tvec ; dvm.mm = tmat ; free(dname) ; RETURN(dvm) ;
      }
 
-     ss = strstr(cc,"[") ;
-     if( ss != NULL ){
-       *ss = '\0'; ss++; iss = (int)strtol(ss,NULL,10); if( iss < 0 ) iss = 0;
-     }
-     atr = THD_find_float_atr( dset->dblk , cc ) ;
-     if( atr == NULL ){
-       ERROR_message("THD_read_dvecmat: can't find attribute %s in dataset %s\n",
-                     cc,dname) ;
-       dvm.vv = tvec ; dvm.mm = tmat ; free(dname) ; RETURN(dvm) ;
-     }
+     if (strstr(cc,"IJK_TO_CARD_DICOM")) {  /* return the matrix that turns i,j,k to mm RAI ZSS May 07*/
+        THD_dicom_card_xform(dset, &tmat, &tvec);
+     } else { 
+        ss = strstr(cc,"[") ;
+        if( ss != NULL ){
+          *ss = '\0'; ss++; iss = (int)strtol(ss,NULL,10); if( iss < 0 ) iss = 0;
+        }
+        atr = THD_find_float_atr( dset->dblk , cc ) ;
+        if( atr == NULL ){
+          ERROR_message("THD_read_dvecmat: can't find attribute %s in dataset %s\n",
+                        cc,dname) ;
+          dvm.vv = tvec ; dvm.mm = tmat ; free(dname) ; RETURN(dvm) ;
+        }
 
-     if( strcmp(cc,"WARP_DATA") == 0 ){  /* 10 Aug 2005 */
+        if( strcmp(cc,"WARP_DATA") == 0 ){  /* 10 Aug 2005 */
 
-       LOAD_DMAT(tmat,atr->fl[0],atr->fl[1],atr->fl[2],
-                      atr->fl[3],atr->fl[4],atr->fl[5],
-                      atr->fl[6],atr->fl[7],atr->fl[8] ) ;
-       LOAD_DFVEC3(tvec,-atr->fl[18],-atr->fl[19],-atr->fl[20]) ;
-       loaded = 1 ;
-    }
-
-    if( !loaded ){
-      switch( atr->nfl ){
-
-        default:
-          ERROR_message(
-                  "THD_read_dvecmat: can't read matrix+vector from %s::%s\n",
-                   dname,cc) ;
-           dvm.vv = tvec ; dvm.mm = tmat ; free(dname) ; RETURN(dvm) ;
-         break ; /* unreachable */
-
-         case 12: LOAD_DMAT(tmat,atr->fl[0],atr->fl[1],atr->fl[2],
-                                 atr->fl[4],atr->fl[5],atr->fl[6],
-                                 atr->fl[8],atr->fl[9],atr->fl[10] ) ;
-                  LOAD_DFVEC3(tvec,atr->fl[3],atr->fl[7],atr->fl[11]) ;
-         break ;
-
-         case 9:  LOAD_DMAT(tmat,atr->fl[0],atr->fl[1],atr->fl[2],
-                                 atr->fl[3],atr->fl[4],atr->fl[5],
-                                 atr->fl[6],atr->fl[7],atr->fl[8] ) ;
-                  LOAD_DFVEC3(tvec,0,0,0) ;
-         break ;
+          LOAD_DMAT(tmat,atr->fl[0],atr->fl[1],atr->fl[2],
+                         atr->fl[3],atr->fl[4],atr->fl[5],
+                         atr->fl[6],atr->fl[7],atr->fl[8] ) ;
+          LOAD_DFVEC3(tvec,-atr->fl[18],-atr->fl[19],-atr->fl[20]) ;
+          loaded = 1 ;
        }
-    }
 
-   {
-      /* ZSS: Dec 27, the year of our Lord when the War on Christmas was raging */
-      /* Need to include VOLREG_CENTER_OLD, VOLREG_CENTER_BASE, 
-                         ROTATE_CENTER_OLD, ROTATE_CENTER_BASE, if applicable. */
-      /* Do we have a ROTATE or VOLREG attribute ?*/
-      incl = 0;
-      if (strstr(cc,"VOLREG_MATVEC")) {
-         atrc = THD_find_float_atr( dset->dblk , "VOLREG_CENTER_OLD");
-         if ( atrc ) {
-            LOAD_DFVEC3(tvec_co,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
-            incl = 1;
-         } else {
-            LOAD_DFVEC3(tvec_co,0,0,0) ;
+       if( !loaded ){
+         switch( atr->nfl ){
+
+           default:
+             ERROR_message(
+                     "THD_read_dvecmat: can't read matrix+vector from %s::%s\n",
+                      dname,cc) ;
+              dvm.vv = tvec ; dvm.mm = tmat ; free(dname) ; RETURN(dvm) ;
+            break ; /* unreachable */
+
+            case 12: LOAD_DMAT(tmat,atr->fl[0],atr->fl[1],atr->fl[2],
+                                    atr->fl[4],atr->fl[5],atr->fl[6],
+                                    atr->fl[8],atr->fl[9],atr->fl[10] ) ;
+                     LOAD_DFVEC3(tvec,atr->fl[3],atr->fl[7],atr->fl[11]) ;
+            break ;
+
+            case 9:  LOAD_DMAT(tmat,atr->fl[0],atr->fl[1],atr->fl[2],
+                                    atr->fl[3],atr->fl[4],atr->fl[5],
+                                    atr->fl[6],atr->fl[7],atr->fl[8] ) ;
+                     LOAD_DFVEC3(tvec,0,0,0) ;
+            break ;
+          }
+       }
+
+      {
+         /* ZSS: Dec 27, the year of our Lord when the War on Christmas was raging */
+         /* Need to include VOLREG_CENTER_OLD, VOLREG_CENTER_BASE, 
+                            ROTATE_CENTER_OLD, ROTATE_CENTER_BASE, if applicable. */
+         /* Do we have a ROTATE or VOLREG attribute ?*/
+         incl = 0;
+         if (strstr(cc,"VOLREG_MATVEC")) {
+            atrc = THD_find_float_atr( dset->dblk , "VOLREG_CENTER_OLD");
+            if ( atrc ) {
+               LOAD_DFVEC3(tvec_co,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
+               incl = 1;
+            } else {
+               LOAD_DFVEC3(tvec_co,0,0,0) ;
+            }
+
+            atrc = THD_find_float_atr( dset->dblk , "VOLREG_CENTER_BASE");
+            if ( atrc ) {
+               LOAD_DFVEC3(tvec_cb,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
+               incl = 1;
+            } else {
+               LOAD_DFVEC3(tvec_cb,0,0,0) ;
+            }
+
+            if (incl == 1) INFO_message("THD_read_dvecmat:\n"
+                                        "   Including VOLREG_CENTER_BASE and VOLREG_CENTER_OLD\n"
+                                        "   attributes in final transform\n");
+            else INFO_message("THD_read_dvecmat:\n"
+                                        "   No VOLREG_CENTER_BASE or VOLREG_CENTER_OLD\n"
+                                        "   attributes found with VOLREG_MATVEC\n");
+         } else if (strstr(cc,"ROTATE_MATVEC")) {
+            atrc = THD_find_float_atr( dset->dblk , "ROTATE_CENTER_OLD");
+            if ( atrc ) {
+               LOAD_DFVEC3(tvec_co,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
+               incl = 1;
+            } else {
+               LOAD_DFVEC3(tvec_co,0,0,0) ;
+            }
+
+            atrc = THD_find_float_atr( dset->dblk , "ROTATE_CENTER_BASE");
+            if ( atrc ) {
+               LOAD_DFVEC3(tvec_cb,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
+               incl = 1;
+            } else {
+               LOAD_DFVEC3(tvec_cb,0,0,0) ;
+            }
+
+            if (incl == 1) INFO_message("THD_read_dvecmat:\n"
+                                        "   Including ROTATE_CENTER_BASE and ROTATE_CENTER_OLD\n"
+                                        "   attributes in final transform\n");
+            else INFO_message("THD_read_dvecmat:\n"
+                                        "   No ROTATE_CENTER_BASE or ROTATE_CENTER_OLD\n"
+                                        "   attributes found with ROTATE_MATVEC\n");
          }
-         
-         atrc = THD_find_float_atr( dset->dblk , "VOLREG_CENTER_BASE");
-         if ( atrc ) {
-            LOAD_DFVEC3(tvec_cb,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
-            incl = 1;
-         } else {
-            LOAD_DFVEC3(tvec_cb,0,0,0) ;
+         if (incl == 1) {
+            tvec_co = DMATVEC(tmat, tvec_co);
+            NEGATE_DFVEC3(tvec_co);
+            tvec.xyz[0] += tvec_cb.xyz[0] + tvec_co.xyz[0];
+            tvec.xyz[1] += tvec_cb.xyz[1] + tvec_co.xyz[1];
+            tvec.xyz[2] += tvec_cb.xyz[2] + tvec_co.xyz[2];
          }
-         
-         if (incl == 1) INFO_message("THD_read_dvecmat:\n"
-                                     "   Including VOLREG_CENTER_BASE and VOLREG_CENTER_OLD\n"
-                                     "   attributes in final transform\n");
-         else INFO_message("THD_read_dvecmat:\n"
-                                     "   No VOLREG_CENTER_BASE or VOLREG_CENTER_OLD\n"
-                                     "   attributes found with VOLREG_MATVEC\n");
-      } else if (strstr(cc,"ROTATE_MATVEC")) {
-         atrc = THD_find_float_atr( dset->dblk , "ROTATE_CENTER_OLD");
-         if ( atrc ) {
-            LOAD_DFVEC3(tvec_co,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
-            incl = 1;
-         } else {
-            LOAD_DFVEC3(tvec_co,0,0,0) ;
-         }
-         
-         atrc = THD_find_float_atr( dset->dblk , "ROTATE_CENTER_BASE");
-         if ( atrc ) {
-            LOAD_DFVEC3(tvec_cb,atrc->fl[0],atrc->fl[1],atrc->fl[2]) ;
-            incl = 1;
-         } else {
-            LOAD_DFVEC3(tvec_cb,0,0,0) ;
-         }
-                  
-         if (incl == 1) INFO_message("THD_read_dvecmat:\n"
-                                     "   Including ROTATE_CENTER_BASE and ROTATE_CENTER_OLD\n"
-                                     "   attributes in final transform\n");
-         else INFO_message("THD_read_dvecmat:\n"
-                                     "   No ROTATE_CENTER_BASE or ROTATE_CENTER_OLD\n"
-                                     "   attributes found with ROTATE_MATVEC\n");
-      }
-      if (incl == 1) {
-         tvec_co = DMATVEC(tmat, tvec_co);
-         NEGATE_DFVEC3(tvec_co);
-         tvec.xyz[0] += tvec_cb.xyz[0] + tvec_co.xyz[0];
-         tvec.xyz[1] += tvec_cb.xyz[1] + tvec_co.xyz[1];
-         tvec.xyz[2] += tvec_cb.xyz[2] + tvec_co.xyz[2];
       }
    }
     free(dname) ; DSET_delete(dset) ;
