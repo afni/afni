aa3174ffa141d2ce22f1b5697ace12d0b416eaa7
dglen <>
2007-08-02 18:50:04 +0000

    Cput

--- src/thd_ttatlas_CA_EZ.h
+++ src/thd_ttatlas_CA_EZ.h
@@ -1,8 +1,8 @@
 /*! Data for atlases from Eickhoff's SPM toolbox.
-Automatically compiled from: AllAreas_v13c_MPM.mat
- located at: /var/automount/Volumes/elrond0/home4/users/ziad/Programs/matlab/spm2/toolbox/Anatomy_13c
- by function /Users/ziad/AFNI/src/matlab/CA_EZ_Prep.m
-Date: 08-May-2006*/
+Automatically compiled from: AllAreas_v15_MPM.mat
+ located at: /Users/dglen/spm5/toolbox/Anatomy_v15
+ by function /Users/dglen/afni/src/matlab/CA_EZ_Prep.m
+Date: 31-Jul-2007*/
 
 /* ----------- Macro Labels --------------------- */
 /* ----------- Based on: Macro.mat -------------*/
@@ -22,7 +22,7 @@ extern int LR_EZ_labeled ;
 extern int LR_EZ_current ;
 
 /* -----------     MPM      --------------------- */
-/* ----------- Based on: AllAreas_v13c_MPM.mat --------------*/
+/* ----------- Based on: AllAreas_v15_MPM.mat --------------*/
 #define CA_EZ_COUNT   29
 #define CA_EZ_MPM_MIN 100  /*!< minimum meaningful value in MPM atlas */
 extern ATLAS_point CA_EZ_list[CA_EZ_COUNT] ;
@@ -32,5 +32,5 @@ extern int CA_EZ_current ;
 
 /* -----------     Refs      --------------------- */
 /* ----------- Based on se_note.m --------------*/
-extern char CA_EZ_REF_STR[27][141];
-extern char CA_EZ_VERSION_STR[27];
+extern char CA_EZ_REF_STR[128][256];
+extern char CA_EZ_VERSION_STR[128];
