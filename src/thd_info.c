09ce130e762660f890546b4a103625af8e39ebe9
rwcox <>
2007-10-10 20:53:17 +0000

    Cput

--- src/thd_info.c
+++ src/thd_info.c
@@ -260,7 +260,7 @@ ENTRY("THD_dataset_info") ;
    else                            nval_per = 1 ;                 /* 12 Feb 2002 */
 #else
    nval_per = dset->dblk->nvals ;
-   if( verbose < 0 && nval_per > 1 ) nval_per = 1 ;
+   if( verbose < 0 && nval_per > 3 ) nval_per = 3 ;
 #endif
 
    /* print out stuff for each sub-brick */
