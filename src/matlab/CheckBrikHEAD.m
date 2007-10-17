cd8bc8049e03199811c2ac4e45982ae6fa0c5e2e
ziad <>
2007-10-17 17:54:29 +0000

    Cput

--- src/matlab/CheckBrikHEAD.m
+++ src/matlab/CheckBrikHEAD.m
@@ -190,10 +190,13 @@ end
 			end
 		end
 
-		if (isfield(Info, 'BRICK_LABS') & (WordCount(Info.BRICK_LABS, '~')-1 ~= Info.DATASET_RANK(2))),
-			%err = 1; ErrMessage = sprintf('Error %s: There has to be exactly %d ~ separated labels in BRICK_LABS', FuncName, Info.DATASET_RANK(2)); errordlg(ErrMessage); return;	
-         if (WordCount(Info.BRICK_LABS, '~') > 1),
-            fprintf(2, 'Warning %s: You have %d sub-bricks but %d ~ separated labels.\n', FuncName, Info.DATASET_RANK(2), WordCount(Info.BRICK_LABS, '~')); 
+      if (isfield(Info, 'BRICK_LABS')),
+		   ntilda = length(find(Info.BRICK_LABS == '~'));
+         if (ntilda ~= Info.DATASET_RANK(2)),
+			   %err = 1; ErrMessage = sprintf('Error %s: There has to be exactly %d ~ separated labels in BRICK_LABS', FuncName, Info.DATASET_RANK(2)); errordlg(ErrMessage); return;	
+            if (ntilda > 1),
+               fprintf(2, 'Warning %s: You have %d sub-bricks but %d ~ separated labels.\n', FuncName, Info.DATASET_RANK(2), ntilda); 
+            end
          end	
       end
 
