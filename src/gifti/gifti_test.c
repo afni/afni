2d3438b26b130078cedf38a09b6a34a893662332
rickr <>
2007-12-28 21:29:16 +0000

    Cput

--- src/gifti/gifti_test.c
+++ src/gifti/gifti_test.c
@@ -28,41 +28,21 @@ int show_help()
     "            gifti_test -infile time_series.gii -gfile ts3.gii  \\\n"
     "                       -slist 3 0 4 5\n"
     "\n"
-    "        4. create .asc surfaces dataset (surf.asc)\n"
-    "\n"
-    "            gifti_test -infile pial.gii -prefix surf\n"
-    "\n"
-    "        5. create .1D time series surface dataset (surf.1D)\n"
-    "\n"
-    "            gifti_test -infile time_series.gii -prefix surf\n"
-    "\n"
     "    options:\n"
     "       -help           : show this help\n"
+    "       -gifti_hist     : show giftilib history\n"
+    "       -gifti_ver      : show giftilib version\n"
     "\n"
-    "       -b64_check TYPE : set method for checking base64 errors\n"
-    "                  TYPE = NONE       : no checks - assume all is well\n"
-    "                  TYPE = DETECT     : report whether errors were found\n"
-    "                  TYPE = COUNT      : count the number of bad chars\n"
-    "                  TYPE = SKIP       : ignore any bad characters\n"
-    "                  TYPE = SKIPnCOUNT : ignore but count bad characters\n"
-    "\n"
-    "       -buf_size       : set buffer size\n"
-    "                         e.g. -buf_size 1024\n"
     "       -encoding  TYPE : set the data encoding for any output file\n"
     "                  TYPE = ASCII      : ASCII encoding\n"
     "                  TYPE = BASE64     : base64 binary\n"
     "                  TYPE = BASE64GZIP : base64 compressed binary\n"
     "       -gfile   OUTPUT : write out dataset as gifti image\n"
-    "       -gifti_hist     : show giftilib history\n"
-    "       -gifti_ver      : show giftilib version\n"
-    "       -infile  INPUT  : write out dataset as gifti image\n"
+    "       -infile   INPUT : specify INPUT as the GIFTI dataset to read\n"
     "       -no_data        : do not write out data\n"
-    "       -prefix  OUTPUT : write out dataset(s) as surf images\n"
-    "                         (these are either .asc or .1D files)\n"
     "       -show           : show final gifti image\n"
     "       -slist LEN s0...: restrict output to list of length LEN\n"
     "       -verb    VERB   : set verbose level\n"
-    "       -zlevel  LEVEL  : set compression level (-1 or 0..9)\n"
     "------------------------------------------------------------\n"
     );
     return 0;
@@ -71,9 +51,9 @@ int show_help()
 int main( int argc, char * argv[] )
 {
     gifti_image * gim;
-    char        * infile = NULL, * prefix = NULL, * gfile = NULL;
+    char        * infile = NULL, * gfile = NULL;
     int         * slist = NULL, slen = 0;
-    int           c, ac, show = 0, data = 1, zlevel;
+    int           c, ac, show = 0, data = 1;
     int           encoding = 0; /* no change, else 1,2,3  */
 
     if( argc <= 1 ) {
@@ -83,28 +63,7 @@ int main( int argc, char * argv[] )
 
     for( ac = 1; ac < argc; ac++ )
     {
-        if( !strcmp(argv[ac], "-buf_size") ) {
-            ac++;
-            CHECK_NEXT_OPT(ac, argc, "-buf_size");
-            if( gifti_set_xml_buf_size(atoi(argv[ac])) ) return 1;
-        } else if( !strcmp(argv[ac], "-b64_check") ) {
-            ac++;
-            CHECK_NEXT_OPT(ac, argc, "-b64_check");
-            if     ( !strcmp(argv[ac], "NONE" ) )
-                        gifti_set_b64_check(GIFTI_B64_CHECK_NONE);
-            else if( !strcmp(argv[ac], "DETECT") )
-                        gifti_set_b64_check(GIFTI_B64_CHECK_DETECT);
-            else if( !strcmp(argv[ac], "COUNT") )
-                        gifti_set_b64_check(GIFTI_B64_CHECK_COUNT);
-            else if( !strcmp(argv[ac], "SKIP" ) )
-                        gifti_set_b64_check(GIFTI_B64_CHECK_SKIP);
-            else if( !strcmp(argv[ac], "SKIPnCOUNT" ) )
-                        gifti_set_b64_check(GIFTI_B64_CHECK_SKIPNCOUNT);
-            else {
-                fprintf(stderr,"** invalid parm to -b64_check: %s\n",argv[ac]);
-                return 1;
-            }
-        } else if( !strcmp(argv[ac], "-encoding") ) {
+        if( !strcmp(argv[ac], "-encoding") ) {
             ac++;
             CHECK_NEXT_OPT(ac, argc, "-encoding");
             if     ( !strcmp(argv[ac], "ASCII" ) )
@@ -136,10 +95,6 @@ int main( int argc, char * argv[] )
             infile = argv[ac];
         } else if( !strcmp(argv[ac], "-no_data") ) {
             data = 0;
-        } else if( !strcmp(argv[ac], "-prefix") ) {
-            ac++;
-            CHECK_NEXT_OPT(ac, argc, "-prefix");
-            prefix = argv[ac];
         } else if( !strcmp(argv[ac], "-show") ) {
             show = 1;
         } else if( !strcmp(argv[ac], "-slist") ) {
@@ -174,11 +129,6 @@ int main( int argc, char * argv[] )
             ac++;
             CHECK_NEXT_OPT(ac, argc, "-verb");
             gifti_set_verb( atoi(argv[ac]) );
-        } else if( !strcmp(argv[ac], "-zlevel") ) {
-            ac++;
-            CHECK_NEXT_OPT(ac, argc, "-zlevel");
-            zlevel = atoi(argv[ac]);
-            if( gifti_set_zlevel(zlevel) ) return 1;
         } else {
             fprintf(stderr,"** unknown option: '%s'\n",argv[ac]);
             return 1;
@@ -192,7 +142,7 @@ int main( int argc, char * argv[] )
     }
 
     /* actually read the dataset */
-    gim = gifti_read_da_list(infile, 0, slist, slen);
+    gim = gifti_read_da_list(infile, 1, slist, slen);
     if( !gim ) {
         fprintf(stderr,"** failed gifti_read_da_list()\n");
         return 1;
@@ -206,129 +156,11 @@ int main( int argc, char * argv[] )
             if( gim->darray[c]->encoding ) gim->darray[c]->encoding = encoding;
 
     if( gfile ) gifti_write_image(gim, gfile, data);
-    if( prefix ) write_as_ascii(gim, prefix);
 
     /* clean up */
     gifti_free_image(gim);  gim = NULL;
 
     if( slist ){ free(slist); slist = NULL; slen = 0; }
-
-    return 0;
-}
-
-int write_as_ascii(gifti_image * gim, char * prefix)
-{
-    giiDataArray  * dac; /* coords */
-    giiDataArray  * dat; /* triangles */
-    giiDataArray ** da_list; /* time series? */
-    int             len;
-
-    fprintf(stderr,"-- trying to write data with prefix '%s'\n", prefix);
-
-    /* write surface file, *.1D */
-    if( (dac = gifti_find_DA(gim, GIFTI_CAT_COORDINATES, 0)) &&
-        (dat = gifti_find_DA(gim, GIFTI_CAT_TOPO_TRI, 0))    )
-        (void) write_surf_file(dac, dat, prefix, 1);
-    else
-        fprintf(stderr,"** failed to find coordinate struct\n");
-
-    if( gifti_find_DA_list(gim, GIFTI_CAT_FUNCTIONAL, &da_list, &len) ) {
-        fprintf(stderr,"** failed to find functional list\n");
-        return 1;
-    }
-
-    /* write time series file, *.1D */
-    if( len > 0 ) {
-        (void) write_1D_file(da_list, len, prefix, 1); 
-        free(da_list);
-    }
-    else
-        fprintf(stderr,"** no functionals in gifti_image\n");
-
-    return 0;
-}
-
-
-/* if dlist contains 1 element, write out as 2-D list,
-   else each DA must have only 1 dimension */
-int write_1D_file(giiDataArray ** dlist, int len, char * prefix, int add_suf)
-{
-    giiDataArray * da;
-    FILE         * fp;
-    char         * name = prefix;
-    char         * nbuf = NULL;
-    int            rows, cols, c;
-
-    if( add_suf ) {     /* create a new name */
-        nbuf = (char *)malloc(strlen(prefix) + strlen(".1D") + 1);
-        strcpy(nbuf, prefix);
-        strcat(nbuf, ".1D");
-        name = nbuf;
-    }
-
-    if( len == 1 ){     /* write out as 2D list */
-        /* note the number of rows and columns */
-        fprintf(stderr,"++ writing 1D '%s' from single DA\n", name);
-        da = dlist[0];
-        if( gifti_DA_rows_cols(da, &rows, &cols) ) {
-            if( nbuf ) free(nbuf);
-            return 1;
-        }
-
-        if( !(fp = fopen(name, "w")) ) {
-            fprintf(stderr,"** failed to open '%s' for 'w'\n",name);
-            if( nbuf ) free(nbuf);
-            return 1;
-        }
-
-        fprintf(stderr,"++ 1D write, RxC = %d x %d\n", rows, cols);
-        if( da->ind_ord == GIFTI_IND_ORD_COL_MAJOR ) {
-            fprintf(stderr,"-- writing data rows in reverse order\n");
-            for(c = rows-1; c >= 0; c-- )
-                ewrite_data_line(da->data, da->datatype, c, cols, 0, 1, fp);
-        } else {
-            fprintf(stderr,"-- writing data rows in normal order\n");
-            for(c = 0; c < rows; c++ )
-                ewrite_data_line(da->data, da->datatype, c, cols, 0, 1, fp);
-        }
-    } else {            /* write da->nvals lines of 'num values */
-        void ** vlist = (void **)malloc(len * sizeof(void *));
-
-        fprintf(stderr,"++ writing 1D '%s' from DA list (%d)\n", name, len);
-
-        /* set data pointers */
-        for( c = 0; c < len; c++ ) {
-            vlist[c] = dlist[c]->data;
-            if( dlist[c]->nvals != dlist[0]->nvals ) {
-                fprintf(stderr,"** d[%d] has %u vals, but d[0] has %u\n",
-                        c,(unsigned)dlist[c]->nvals,(unsigned)dlist[0]->nvals);
-                free(vlist);
-                return 1;
-            } else if (dlist[c]->datatype != dlist[0]->datatype) {
-                fprintf(stderr,"** d[%d] has type %d, but d[0] has %d\n",
-                        c, dlist[c]->datatype, dlist[0]->datatype);
-                free(vlist);
-                return 1;
-            }
-        }
-
-        /* good to go */
-        if( !(fp = fopen(name, "w")) ) {
-            fprintf(stderr,"** failed to open '%s' for 'w'\n",name);
-            if( nbuf ) free(nbuf);
-            return 1;
-        }
-
-        fprintf(stderr,"++ 1D write, RxC = %u x %d\n",
-                (unsigned)dlist[0]->nvals, len);
-        ewrite_many_lines(vlist, dlist[0]->datatype,len, dlist[0]->nvals, 0,fp);
-                          
-        free(vlist);
-    }
-
-    fprintf(stderr,"++ 1D write, apparent success\n");
-
-    fclose(fp);
 
     return 0;
 }
@@ -341,7 +173,7 @@ int write_surf_file(giiDataArray * dc, giiDataArray * dt, char * prefix,
     FILE         * fp;
     char         * name = prefix;
     char         * nbuf = NULL;
-    int            crows, ccols, trows, tcols, rows, cols, c;
+    long long      crows, ccols, trows, tcols, rows, cols, c;
 
     if( add_suf ) {     /* create a new name */
         nbuf = (char *)malloc(strlen(prefix) + strlen(".asc") + 1);
@@ -368,7 +200,7 @@ int write_surf_file(giiDataArray * dc, giiDataArray * dt, char * prefix,
     }
 
     fprintf(fp, "#!ascii version of surface\n"
-                "%d %d\n", crows, trows);
+                "%lld %lld\n", crows, trows);
 
     /* write out the coordinates */
 
