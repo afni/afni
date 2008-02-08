69f39e003b08cc28f0c833b6935f285c2c62f750
rwcox <>
2008-02-08 14:28:02 +0000

    Cadd

---
+++ src/edt_floatize.c
@@ -0,0 +1,55 @@
+#include "mrilib.h"
+
+/*-----------------------------------------------------------------*/
+/*! Convert this dataset to float format, in place. */
+
+void EDIT_floatize_dataset( THD_3dim_dataset *dset )
+{
+   int nvals , iv ;
+   MRI_IMAGE *bim , *qim ;
+   float      bfac, *qar ;
+
+ENTRY("EDIT_floatize_dataset") ;
+
+   if( !ISVALID_DSET(dset) ) EXRETURN ;
+
+   DSET_mallocize(dset) ;
+   DSET_load(dset) ;
+   if( !DSET_LOADED(dset) ){
+     ERROR_message("Can't load dataset '%s' for floatize",DSET_BRIKNAME(dset));
+     EXRETURN ;
+   }
+
+   nvals = DSET_NVALS(dset) ;
+   for( iv=0 ; iv < nvals ; iv++ ){
+     bim  = DSET_BRICK(dset,iv) ;
+     if( bim->kind == MRI_float ) continue ; /* already floatized */
+     bfac = DSET_BRICK_FACTOR(dset,iv) ;
+     qim  = mri_scale_to_float( bfac , bim ) ;
+     qar  = MRI_FLOAT_PTR(qim) ;
+     EDIT_substitute_brick( dset , iv , MRI_float , qar ) ;
+     mri_clear_data_pointer(qim); mri_free(qim) ;
+   }
+   EDIT_dset_items( dset , ADN_brick_fac,NULL , ADN_none ) ;
+
+   EXRETURN ;
+}
+
+/*-----------------------------------------------------------------*/
+/*! Return type of all bricks, if they are the same, or -1 if not. */
+
+int DSET_pure_type( THD_3dim_dataset *dset )
+{
+   int nvals , iv , typ ;
+
+ENTRY("DSET_pure_type") ;
+
+   if( !ISVALID_DSET(dset) ) RETURN(-1) ;
+
+   nvals = DSET_NVALS(dset) ;
+   typ   = (int)DSET_BRICK_TYPE(dset,0) ;
+   for( iv=1 ; iv < nvals ; iv++ )
+     if( (int)DSET_BRICK_TYPE(dset,iv) != typ ) RETURN(-1) ;
+
+   RETURN(typ) ;
+}
