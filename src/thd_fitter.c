4a13433c5266c581e11c64af3cd5f519093055c3
rwcox <>
2008-02-19 16:15:24 +0000

    Cput

--- src/thd_fitter.c
+++ src/thd_fitter.c
@@ -1,15 +1,50 @@
 #include "mrilib.h"
 
+/*------------------------------------------------------------------*/
+
+static float * new_lsqfit( int npt  , float *far   ,
+                           int nref , float *ref[] , float *ccon )
+{
+  int  jj ;
+  MRI_IMAGE *rmat,*pmat,*smat ;
+  float *rar;
+
+  rmat = mri_new(npt,nref,MRI_float ); rar = MRI_FLOAT_PTR(rmat);
+  for( jj=0 ; jj < nref ; jj++ )
+    memcpy( rar+jj*npt , ref[jj] , sizeof(float)*npt ) ;
+  pmat = mri_matrix_psinv(rmat,NULL,0.0f) ;
+  mri_free(rmat) ;
+  if( pmat == NULL ) return NULL ;
+
+  rmat = mri_new_vol_empty( npt , 1 , 1 , MRI_float ) ;
+  mri_fix_data_pointer( far , rmat ) ;
+  smat = mri_matrix_mult( pmat , rmat ) ;
+  mri_free(pmat); mri_clear_data_pointer(rmat); mri_free(rmat);
+  if( smat == NULL ) return NULL ;
+
+  rar = MRI_FLOAT_PTR(smat);
+  mri_clear_data_pointer(smat); mri_free(smat);
+  return rar ;
+}
+
+/*------------------------------------------------------------------*/
+
 #undef  ERREX
 #define ERREX(s) do{ ERROR_message(s); return NULL; } while(0)
 
+/* Fit the npt-long vector far[] to the nref vectors in ref[].
+   meth=1 ==> L1 fit
+   meth=2 ==> L2 fit
+   ccon != NULL ==> con[i] is constraint on coef #i
+*/
+
 floatvec * THD_fitter( int npt , float *far  ,
-                       int nref, float *ref[], int meth, int flags )
+                       int nref, float *ref[],
+                       int meth, float *ccon  )
 {
    int jj ;
    float *qfit=NULL, val ;
    floatvec *fv=NULL ;
-   int cony = ((flags&1) != 0) ;
 
    /* check inputs */
 
@@ -22,15 +57,24 @@ floatvec * THD_fitter( int npt , float *far  ,
    switch( meth ){
 
      default: ERREX("THD_fitter: bad meth code") ;
-  
-     case 2: 
-       qfit = lsqfit( npt, far, NULL, nref, ref ) ;
+
+     case 2:
+       qfit = new_lsqfit( npt, far, nref, ref , NULL ) ;
      break ;
 
      case 1:
        qfit = (float *)malloc(sizeof(float)*nref) ;
-       val = cl1_solve( npt, nref, far, ref, qfit, cony ) ;
-       if( val < 0.0f ){ free(qfit); qfit = NULL; }
+       if( ccon != NULL ) memcpy(qfit,ccon,sizeof(float)*nref) ;
+       val = cl1_solve( npt, nref, far, ref, qfit, (ccon!=NULL) ) ;
+       if( val < 0.0f ){ free(qfit); qfit = NULL; } /* error */
+#if 0
+       if( ccon != NULL && qfit != NULL ){
+         for( jj=0 ; jj < nref ; jj++ )
+           if( ccon[jj]*qfit[jj] < 0.0f )
+             ERROR_message("cl1 constraint failure: ccon[%d]=%f qfit=%f",
+                           jj,ccon[jj],qfit[jj]) ;
+       }
+#endif
      break ;
    }
 
@@ -38,6 +82,5 @@ floatvec * THD_fitter( int npt , float *far  ,
 
    MAKE_floatvec(fv,nref) ;
    memcpy( fv->ar, qfit, sizeof(float)*nref ) ;
-   free(qfit) ;
-   return fv ;
+   free(qfit) ; return fv ;
 }
