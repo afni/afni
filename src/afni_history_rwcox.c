67a738e8fd9950e53458f15797763a0a6588a41b
rwcox <>
2008-02-26 16:52:28 +0000

    Cadd

---
+++ src/afni_history_rwcox.c
@@ -0,0 +1,38 @@
+
+/** cf. afni_history.h **/
+
+g_afni_history_struct rwcox_history[] = {
+
+ { 16 , FEB , 2008 , RWC , "3dTfitter" , MAJOR ,
+   "new program = linear fits to voxel time series" ,
+   "Uses L1 or L2 regression, with optional constraints to fit each voxel\n"
+   "time series as a sum of basis time series, which can be 1D files or\n"
+   "3D+time datasets.  Basis time series that are 1D time series are\n"
+   "the same for all input voxels.  Basis time series that are 3D+time\n"
+   "datasets are different for each voxel.\n"
+   "Differences from 3dDeconvolve:\n"
+   "* Basis time series can vary across voxels.\n"
+   "* Fit coefficients can be found with L1 or L2 error functions, and\n"
+   "  can be constrained to be positive or negative.\n"
+   "* 3dTfitter does not compute goodness-of-fit statistics.\n" } ,
+
+ { 20 , FEB , 2008 , RWC , "1deval" , MINOR ,
+   "add '-1D:' option, to write output that is usable on the command line" ,
+   "Sample usage:
+   " 1dplot `1deval -1D: -num 71 -expr 'cos(t/2)*exp(-t/19)'`
+   "The backquotes `...` capture the output of the command and put this string\n"
+   "on the command line.  The '-1D:' option formats the 1deval output so that\n"
+   "it is ready to be used in this way.\n" } ,
+
+ { 22 , FEB , 2008 , RWC , "3dpc" , MINOR ,
+   "add '-eigonly' and '-reduce' options; also, output eigenvalues to a 1D file" ,
+   "'-eigonly' causes 3dpc to print the eigenvalues to stdout and stop there.\n"
+   "'-reduce n pp' outputs a reduced dataset, using only the largest 'n'\n"
+   "eigenvalues.\n" } ,
+
+ { 25 , FEB , 2008 , RWC , "1dsvd" , MINOR ,
+   "add '-vmean' and '-vnorm' options, to mirror capabilities in 3dpc" ,
+   NULL } ,
+
+ { 99,99,99, NULL,NULL, 99, NULL,NULL}  /** the end */
+} ;
