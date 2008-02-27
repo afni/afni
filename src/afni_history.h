c7f7458c928b4e95280ea25487d7a1671c3ae943
rickr <>
2008-02-27 18:24:03 +0000

    Cput

--- src/afni_history.h
+++ src/afni_history.h
@@ -36,24 +36,31 @@
 #define major  3
 #define super  4
 
+#define TYPE_NOT_SET    0
+#define TYPE_NEW_OPT    1
+#define TYPE_NEW_PROG   2
+#define TYPE_FIX        3
+
 
 typedef struct {
-  short dd;          /* Day: 1..31 */
-  short mm;          /* Month: 1..12 (use a macro) */
-  short yyyy;        /* Year: 2006..9999 */
-  char *author;      /* use a macro */
-  char *program;     /* will be checked against allowed names */
-  short rank;        /* use a macro */
-  char *desc;        /* one line description */
-  char *verbtext;    /* can be NULL, but shouldn't be */
+  short   dd;          /* Day: 1..31 */
+  short   mm;          /* Month: 1..12 (use a macro) */
+  short   yyyy;        /* Year: 2001..9999 */
+  char  * author;      /* use a macro */
+  char  * program;     /* will be checked against allowed names */
+  short   rank;        /* use a macro */
+  char  * desc;        /* one line description */
+  char  * verbtext;    /* can be NULL, but shouldn't be */
 } afni_history_struct;
 
 
 #define MAX_LINE_CHARS  73  /* max description chars without newline   */
                             /* 80 - ( 3 spaces + " + \n" )             */
 #define NUM_HIST_USERS  20  /* increase this if we go beyond 20 people */
+#define FIRST_YEAR    2001  /* start of SSCC, just in case (Ziad...)   */
 
 
+/* refereces to the structures in each history file */
 extern afni_history_struct bpittman_history[];
 extern afni_history_struct christip_history[];
 extern afni_history_struct dglen_history[];
@@ -62,5 +69,61 @@ extern afni_history_struct rickr_history[];
 extern afni_history_struct rwcox_history[];
 extern afni_history_struct ziad_history[];
 
+
+/*----------------------------------------------------------------------*/
+/*-- specific to afni_history.c ----------------------------------------*/
+
+typedef struct {
+    afni_history_struct * hlist;
+    char                * author;
+} histpair;
+
+typedef struct {
+    /* user options */
+    char     * author;
+    char     * program;
+    int        html;
+    int        level;
+    int        min_level;
+    int        past_days, past_months, past_years;
+    int        verb;
+
+    /* assigned variables */
+    int        plen;
+    histpair * histpairs;
+} global_data;
+
+#undef INT_IN_RANGE
+#define INT_IN_RANGE(val,min,max) (((val)<(min)) ? 0 : ((val)>(max)) ? 0 : 1)
+
+#undef CHECK_NULL_STR
+#define CHECK_NULL_STR(str) (str ? str : "NULL")
+
+#undef CHECK_NEXT_OPT
+#define CHECK_NEXT_OPT(n,m,str)                                     \
+   do { if ( (n) >= (m) ) {                                          \
+           fprintf(stderr,"** option '%s': missing parameter\n",str); \
+           fprintf(stderr,"   consider: '-help' option\n");            \
+           return 1;      }                                             \
+      } while(0)
+
+#undef CHECK_NEXT_OPT2
+#define CHECK_NEXT_OPT2(n,m,s1,s2)                                        \
+   do { if ( (n) >= (m) ) {                                                \
+           fprintf(stderr,"** option '%s': missing parameter '%s'\n",s1,s2);\
+           fprintf(stderr,"   consider: '-help' option\n");                  \
+           return 1;      }                                                   \
+      } while(0)
+
+int histlists_are_valid (histpair * hpairs, int plen);
+int hlist_is_sorted     (afni_history_struct * hlist);
+int hlist_len           (afni_history_struct * hlist);
+int init_histlist       (global_data * gd);
+int process_options     (int argc, char * argv[], global_data * gd);
+int show_help           (void);
+int valid_dstring       (char * str, int max_line_len);
+int valid_histlist      (afni_history_struct * hlist, char * author);
+int valid_histstruct    (afni_history_struct * hstr, char * author);
+int valid_program       (char * prog);
 
 #endif /* _AFNI_HISTORY_HEADER_ */
--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -38,6 +38,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 27, FEB, 2008, RCR, "afni_proc.py", MINOR,
+   "fixed -regress_use_stim_files typo (was -regress_use_stim_times)", 
+   NULL
+ } ,
+
  { 26, FEB, 2008, RCR, "afni_history", MICRO,
    "checked in initial afni_history files", 
    NULL
