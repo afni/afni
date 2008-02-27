9c98066a4441bff89748369e625771b8aac2ba16
rickr <>
2008-02-27 00:06:34 +0000

    Cput

--- src/afni_history_rwcox.c
+++ src/afni_history_rwcox.c
@@ -1,7 +1,42 @@
 
 /** cf. afni_history.h **/
 
-g_afni_history_struct rwcox_history[] = {
+#include "afni_history.h"
+
+/*  basic format: 3-field date, user, program_name, impact_level,
+                  short description without newline
+                  (optional) long descrption with intermediate newlines
+  
+    copy entire section: { ... } ,
+  
+    Notes: - months are JAN ... DEC (see .h file)
+
+           - levels are :
+                    MICRO   - users don't see
+                    MINOR   - small affect on users
+                    MAJOR   - large affect on users
+                    SUPER   - we expect users to know
+
+           - these will probably replace AFNI.changes.*
+
+           - PLEASE, stick to what fits on an 80 column screen
+
+           - it may be nice to put the newest entires at the top
+
+           - leave the last entry as it is
+
+ -- example --
+
+ { 26 , FEB , 2008 , RCR , "my_program" , MAJOR ,
+   "short description of change" ,
+   "(optional) detailed description, or where to get more information\n"
+   "   - with newlines, if you babble for multiple lines\n"
+   "   (but none at the end)"
+ } ,
+
+*/
+
+afni_history_struct rwcox_history[] = {
 
  { 16 , FEB , 2008 , RWC , "3dTfitter" , MAJOR ,
    "new program = linear fits to voxel time series" ,
@@ -18,8 +53,8 @@ g_afni_history_struct rwcox_history[] = {
 
  { 20 , FEB , 2008 , RWC , "1deval" , MINOR ,
    "add '-1D:' option, to write output that is usable on the command line" ,
-   "Sample usage:
-   " 1dplot `1deval -1D: -num 71 -expr 'cos(t/2)*exp(-t/19)'`
+   "Sample usage:\n"
+   " 1dplot `1deval -1D: -num 71 -expr 'cos(t/2)*exp(-t/19)'`\n"
    "The backquotes `...` capture command's output and put this string on\n"
    "the command line.  The '-1D:' option formats the 1deval output so that\n"
    "it is ready to be used in this way.\n" } ,
