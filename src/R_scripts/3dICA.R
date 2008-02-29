97a2a54ba5458a03ad7e7c1a8653ce6791ca544b
gangc <>
2008-02-29 20:40:16 +0000

    Cadd

---
+++ src/R_scripts/3dICA.R
@@ -0,0 +1,79 @@
+#!/usr/bin/env afni_run_R
+#Welcome to 3dICA.R, an AFNI IVA Package!
+#-----------------------------------------------------------
+#Version 0.0.1, Feb. 29, 2007
+#Author: Gang Chen (gangchen@mail.nih.gov)
+#Website: http://afni.nimh.nih.gov/sscc/gangc/lme.html
+#SSCC/NIMH, National Institutes of Health, Bethesda MD 20892
+#-----------------------------------------------------------
+
+# Commannd line to run this script: 3dIVA.R Output (Output is a file
+# in which the running progress including error message will be store)
+
+source(file.path(Sys.getenv("AFNI_R_DIR"), "AFNIio.R"))
+#source(file.path(Sys.getenv("LME"), "AFNIio.R"))
+library(fastICA)
+
+meth <- "C"         # or "R"
+
+
+# Line 1: data type - volume or surface
+InFile <- unlist(strsplit(unlist(scan(file="par.txt", what= list(""), 
+   skip=0, nline=1)), "\\:"))[2]
+
+#  Line 2: Output filename
+#how to check output filename?
+Out <- unlist(strsplit(unlist(scan(file="par.txt", what= list(""), 
+   skip=1, nline=1)), "\\:"))[2]
+OutFile <- paste(Out, "+orig", sep="")
+
+#  Line 3: Number of components
+NoComp <- as.integer(unlist(strsplit(unlist(scan(file="par.txt", 
+   what= list(""), skip=2, nline=1)), "\\:"))[2])
+	
+#  Line 4: function for approximation to neg-antropy
+Func <- unlist(strsplit(unlist(scan(file="par.txt", 
+   what= list(""), skip=3, nline=1)), "\\:"))[2]
+	
+#  Line 5: extraction method
+Type <- unlist(strsplit(unlist(scan(file="par.txt", 
+   what= list(""), skip=4, nline=1)), "\\:"))[2]
+		
+
+Data <- read.AFNI(InFile)
+dimx <- Data$dim[1]
+dimy <- Data$dim[2]
+dimz <- Data$dim[3]
+tp   <- Data$dim[4]
+
+NData <- array(data=NA, dim=c(dimx, dimy, dimz, tp))
+NData <- Data$ttt
+dim(NData) <- c(prod(dimx, dimy, dimz), tp)
+
+#ww <- apply(NData, 4, rbind)
+
+
+# If the number of components are estimated to be very high (like 100 components for 200 images) 
+# then around 20 to 30 components should give a reasonable answer.
+
+# 10500 voxels, 38 times points
+ica <- fastICA(NData, NoComp, alg.typ = Type, fun = "logcosh", alpha = 1,
+                  method = meth, row.norm = FALSE, maxit = 200, 
+                  tol = 0.0001, verbose = TRUE)
+
+MData <- ica$S 
+dim(MData) <- c(dimx, dimy, dimz, NoComp)
+
+MyLabel <- rep("component", NoComp)
+
+write.AFNI(OutFile, MData, MyLabel, note=Data$header$HISTORY_NOTE, origin=Data$origin, delta=Data$delta, idcode="whatever")
+statpar <- "3drefit"
+statpar <- paste(statpar, " -view tlrc -newid ", OutFile)
+system(statpar)
+
+print(sprintf("Congratulations! You've got output %s+tlrc.*", Out))
+
+# set save defaults using option:
+options(save.defaults=list(ascii=TRUE, safe=FALSE))
+save.image()
+unlink(".RData")
