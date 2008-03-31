db7e5b9adcff47fa0f8ed9ed33b6afbb500c9a40
rickr <>
2008-03-31 21:04:56 +0000

    Cput

--- src/3dANOVA.h
+++ src/3dANOVA.h
@@ -107,9 +107,9 @@ static char * commandline = NULL ;         /* command line for history notes */
 
 #define MAX_LEVELS 300           /* max. number of factor levels */  
 #define MAX_OBSERVATIONS 300     /* max. number of observations per cell */
-#define MAX_MEANS 20             /* max. number of user requested means */
-#define MAX_DIFFS 20             /* max. number of user requested diffs. */
-#define MAX_CONTR 40             /* max. number of user requested contrasts */
+#define MAX_MEANS 50             /* max. number of user requested means */
+#define MAX_DIFFS 50             /* max. number of user requested diffs. */
+#define MAX_CONTR 50             /* max. number of user requested contrasts */
 #define MAX_NAME_LENGTH THD_MAX_NAME  /* max. string length for file names */
 
 
