e3a7cb7d1dafcde3080f27bbbcf4b1432ad25e75
rwcox <>
2008-04-08 13:58:28 +0000

    Cput

--- src/afni_history_rwcox.c
+++ src/afni_history_rwcox.c
@@ -44,6 +44,10 @@
 afni_history_struct rwcox_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+ { 8 , APR , 2008 , RWC , "3dUpsample" , MINOR , TYPE_NEW_PROG ,
+   "Upsample a 3D+time dataset (in the time direction)" ,
+   NULL } ,
+
  { 7 , APR , 2008 , RWC , "1dUpsample" , MINOR , TYPE_NEW_PROG ,
    "Upsample a 1D time series" ,
    NULL } ,
