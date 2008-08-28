896fef3ff6a1ffcc222f42e9e1cda28f764400c3
rwcox <>
2008-08-28 19:54:28 +0000

    Cput

--- src/thd_dsetinsess.c
+++ src/thd_dsetinsess.c
@@ -21,6 +21,8 @@ THD_slist_find THD_dset_in_session( int find_type , void *target ,
 
    /*-- sanity check --*/
 
+   ZZME(find) ;
+
    if( ! ISVALID_SESSION(sess) || target == NULL ){
       BADFIND(find) ; return find ;
    }
