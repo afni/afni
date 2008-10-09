ec12e887b1a18c87fa997116ea263deb921bfd05
ziad <>
2008-10-09 13:28:37 +0000

    Cadd

---
+++ src/SUMA/SUMA_volume_render.h
@@ -0,0 +1,19 @@
+#ifndef SUMA_VOLUME_INCLUDED
+#define SUMA_VOLUME_INCLUDED
+
+
+SUMA_Boolean SUMA_Init3DTextureNIDOnel(  NI_element *nel, 
+                                    SUMA_SurfaceObject *SO, 
+                                    SUMA_DO_CoordUnits default_coord_type,
+                                    float *default_txcol, 
+                                    void *default_font,
+                                    SUMA_SurfaceViewer *sv) ;
+
+SUMA_Boolean SUMA_Load3DTextureNIDOnel (NI_element *nel);
+SUMA_EnablingRecord SUMA_RecordEnablingState(void);
+void SUMA_RestoreEnablingState(SUMA_EnablingRecord SER);
+void SUMA_ShowEnablingState(SUMA_EnablingRecord SER, FILE *out, char *preamble);
+char *SUMA_EnablingState_Info(SUMA_EnablingRecord SER);
+
+
+#endif
