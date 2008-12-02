8e0ddc2355cfbbadb71e9b28a121de64c5d47fbe
ziad <>
2008-12-02 15:47:58 +0000

    Cput

--- src/afni_environ.h
+++ src/afni_environ.h
@@ -15,6 +15,7 @@ extern int AFNI_process_environ( char * ) ;
 extern char * AFNI_suck_file( char * ) ;
 extern char * my_getenv( char * ) ;
 extern void AFNI_mark_environ_done(void) ;  /* 16 Apr 2000 */
+extern void AFNI_mark_environ_undone(void); /* 26 Nov 2008 */
 extern int AFNI_yesenv( char * ) ;          /* 21 Jun 2000 */
 extern int AFNI_noenv ( char * ) ;          /* 21 Jun 2000 */
 extern double AFNI_numenv( char * ) ;       /* 23 Aug 2003 */
