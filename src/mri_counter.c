266d7c3aec8a5bd82d107aede2c648223ab38f09
rwcox <>
2008-12-03 21:40:53 +0000

    Cput

--- src/mri_counter.c
+++ src/mri_counter.c
@@ -1,8 +1,10 @@
 #include "mrilib.h"
 
+/*! Count number of values between bot and top (inclusive) */
+
 int mri_counter( MRI_IMAGE *im , float bot , float top )
 {
-   float *far ; int ii,nv,cc ;
+   register float *far ; register int ii,nv,cc ;
    if( im == NULL || im->kind != MRI_float || bot > top ) return -1 ;
    far = MRI_FLOAT_PTR(im) ; if( far == NULL ) return -1 ;
    nv  = im->nvox ;
