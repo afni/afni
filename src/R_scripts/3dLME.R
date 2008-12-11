342c5d21deb54b46beb784b48f1de067f75fe375
gangc <>
2008-12-11 17:56:52 +0000

    Cput

--- src/R_scripts/3dLME.R
+++ src/R_scripts/3dLME.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env afni_run_R
 #Welcome to 3dLME.R, an AFNI Group Analysis Package!
 #-----------------------------------------------------------
-#Version 1.0.2,  June 4, 2008
+#Version 1.0.3,  Dec 11, 2008
 #Author: Gang Chen (gangchen@mail.nih.gov)
 #Website: http://afni.nimh.nih.gov/sscc/gangc/lme.html
 #SSCC/NIMH, National Institutes of Health, Bethesda MD 20892
@@ -11,16 +11,27 @@
 # (Output is a file in which the running progress including 
 # error messages will be stored)
 
+libLoad <- function(myLib) {
+   sucLoad <- FALSE
+   sucCheck <- FALSE
+   try(sucLoad <- library(myLib, character.only = TRUE, logical.return = TRUE))
+   if (sucLoad) {print(sprintf("Package %s successfully loaded!", myLib)); sucCheck <- TRUE} else {
+	  	try(install.packages(myLib))
+      try(sucLoad <- library(myLib, character.only = TRUE, logical.return = TRUE))
+      if (sucLoad) print(sprintf("Package %s successfully loaded...", myLib)) 
+   	}
+}
+
 system("rm -f .RData")
 source(file.path(Sys.getenv("AFNI_R_DIR"), "AFNIio.R"))
 #source(file.path(Sys.getenv("LME"), "AFNIio.R"))
-library(nlme)
-library(contrast)
+libLoad("nlme")
+libLoad("contrast")
 
 # Line 1: data type - volume or surface
 datatype <- unlist(strsplit(unlist(scan(file="model.txt", what= list(""), 
    skip=0, strip.white=TRUE, nline=1)), "\\:"))[2]
-
+   
 #  Line 2: Output filename
 #how to check output filename?
 Out <-unlist(strsplit(unlist(scan(file="model.txt", what= list(""), 
@@ -58,8 +69,20 @@ for (n in 1:nCov) {
 }
 
 # Line 6: Random effect - lme or gls?
-RanEff <- as.logical(unlist(strsplit(unlist(scan(file="model.txt", what= list(""), 
-   skip=5, strip.white=TRUE, nline=1)), "\\:"))[2])
+ranEff <- vector("list", 2)
+ranEff[[1]] <- unlist(strsplit(unlist(strsplit(unlist(scan(file="model.txt", what= list(""), 
+   skip=5, strip.white=TRUE, nline=1)), "\\:"))[2], "\\,"))
+if (ranEff[[1]][1]) ranEff[[1]][1] <- 1  # simple for backward compatibility
+
+
+nRand <- length(ranEff[[1]])
+if (nRand==1 & as.numeric(ranEff[[1]][1])==1) ranEff[[2]] <- as.formula(~1|Subj)
+if (nRand>1) {
+	ranForm <- vector("list", nRand)		
+	ranEff[[2]][[1]] <- ~1; 
+	for (ii in 2:nRand) ranEff[[2]][[ii]] <- as.formula(sprintf("~%s-1",ranEff[[1]][ii]))
+	ranEff[[2]] <- list(Subj=pdBlocked(lapply(ranEff[[2]], pdIdent)))
+}	
 
 # Line 7: Variance structure for modeling dependence among within-subject errors
 VarStr <- unlist(strsplit(unlist(scan(file="model.txt", what= list(""), skip=6, 
@@ -81,7 +104,13 @@ CorForm <- as.formula(paste("~", unlist(CorTmp)[3]))
 Ftype <- unlist(strsplit(unlist(scan(file="model.txt", what= list(""), skip=8, 
    strip.white=TRUE, nline=1)), "\\:"))[2]
 
+clusPos<-grep("Clusters", readLines("model.txt"))
+if (length(clusPos)>0) nNodes<- as.integer(unlist(strsplit(unlist(scan(file="model.txt", what= list(""), 
+   skip=clusPos-1, strip.white=TRUE, nline=1)), "\\:"))[2]) else nNodes<-1
+
+
 # Line 10 and the next few: pair-wise contrasts
+
 
 # header position (hp) defined by column name InputFile
 hp <- grep("InputFile", readLines("model.txt")) 
@@ -92,12 +121,12 @@ Model$InputFile <-  as.character(Model$InputFile)
 
 # number of contrasts (pair-wise only right now)
 ncontr <- (hp-10)%/%2
+clist      <- vector('list', ncontr)
 
 if (ncontr > 0) {
 contrLabel <- array(data=NA, dim=ncontr)
 contr      <- array(data=NA, dim=ncontr)
 cc         <- array(data=NA, dim=c(ncontr, 2, nfixed))
-clist      <- vector('list', ncontr)
 
 # CAREFUL: assume contrasts at the same covariate value for multiple groups, 
 # but is this desirable???
@@ -148,21 +177,18 @@ NoFile <- dim(Model[1])[1]
 if (datatype == "volume" | datatype == "Volume") {
 
 # Read in the 1st input file so that we have the dimension information
-Data <- read.AFNI(Model[1, FileCol])
-dimx <- Data$dim[1]
-dimy <- Data$dim[2]
-dimz <- Data$dim[3]
+inData <- read.AFNI(Model[1, FileCol])
+dimx <- inData$dim[1]
+dimy <- inData$dim[2]
+dimz <- inData$dim[3]
+myHist <- inData$header$HISTORY_NOTE; myOrig <- inData$origin; myDelta <- inData$delta
 
-if (is.na(mask)) { Mask <- array(1, c(dimx, dimy, dimz, 1)) 
-   } else Mask <- read.AFNI(mask)$ttt
+inData <- unlist(lapply(lapply(Model[,FileCol], read.AFNI), '[[', 1))
+dim(inData) <- c(dimx, dimy, dimz, NoFile)
 
-# initialization
-IData <- array(data=NA, dim=c(dimx, dimy, dimz, NoFile))
-
-IData[,,,1] <- Data$ttt
-# Read in the rest input files (beta)
-for (m in 2:NoFile) {
-   IData[,,,m] <- read.AFNI(Model[m, FileCol])$ttt
+if (!is.na(mask)) {
+	Mask <- read.AFNI(mask)$ttt[,,,1]
+	inData <- array(apply(inData, 4, function(x) x*read.AFNI(mask)$ttt[,,,1]), dim=c(dimx,dimy,dimz,NoFile))
 }
 
 # try out a few voxels and see if the model is OK, and find out the number of F tests and DF's 
@@ -173,33 +199,34 @@ if (ncontr > 0) contrDF <- array(data=NA, dim=ncontr)
 tag<-1
 while (tag == 1) {
    tag<-0
-	Model$Beta<-IData[ii, jj, kk,]
-	if (RanEff) {
+	Model$Beta<-inData[ii, jj, kk,]
+	if (as.numeric(ranEff[[1]][1])!=0) {
 		if (CorStr == 0) { if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, weights=varIdent(VarForm), Model), tag <- 1) } }
+		   try(fm <- lme(ModelForm, random = ranEff[[2]], Model), tag <- 1) } else {
+			try(fm <- lme(ModelForm, random = ranEff[[2]], weights=varIdent(VarForm), Model), tag <- 1) } }
 			
 		if (CorStr == 1) { if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, 
+		   try(fm <- lme(ModelForm, random = ranEff[[2]], 
 		   correlation=corAR1(0.3, form=CorForm), Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, correlation=corAR1(0.3, form=CorForm),
+			try(fm <- lme(ModelForm, random = ranEff[[2]], correlation=corAR1(0.3, form=CorForm),
 		   weights=varIdent(VarForm), Model), tag <- 1) } }
 			
 		if (CorStr == 2) { if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, 
+		   try(fm <- lme(ModelForm, random = ranEff[[2]], 
 		   correlation=corARMA(c(0.3,0.3), p=2, form=CorForm), Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, 
+			try(fm <- lme(ModelForm, random = ranEff[[2]], 
 			correlation=corARMA(c(0.3,0.3), p=2, form=CorForm),
 			weights=varIdent(VarForm), Model), tag <- 1) } }
 			
 		if (CorStr == 3) { if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, 
+		   try(fm <- lme(ModelForm, random = ranEff[[2]], 
 		   correlation=corARMA(c(0.3,0.3), p=1, q=1, form=CorForm), Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, 
+			try(fm <- lme(ModelForm, random = ranEff[[2]], 
 		   correlation=corARMA(c(0.3,0.3), p=1, q=1, form=CorForm),
 			weights=varIdent(VarForm), Model), tag <- 1) } }
 	}	
-	else try(fm <- gls(ModelForm, Model), tag <- 1)
+	if (nRand==1 & as.numeric(ranEff[[1]][1])==0) try(fm <- gls(ModelForm, Model), tag <- 1)
+				
 	if (ncontr > 0) try(for (n in 1:ncontr) { contrDF[n] <- 
 	   contrast(fm, clist[[n]][[1]], clist[[n]][[2]], type="average")$df }, tag <- 1)
    if (ii<dimx) ii<-ii+1 else {
@@ -216,7 +243,6 @@ if (tag == 0)  {
       print(sprintf("Insoluble model! Formula %s on the 4th line in model.txt", ModelShape))
 		print("might be inappropriate, or there are incorrect specifications in model.txt")
 		print("such as contrasts, factor levels, or other obscure problems.")
-		print("Meditate on model.txt for 3 minutes, and try to pinpoint the problem...")
       break; next # won't run the whole brain analysis if the test fails
 }
 
@@ -227,7 +253,7 @@ if (NoConst) FArr <- 1:NoF else FArr <- 2:(NoF+1)
 # Number of sub-bricks
 NoBrick <- NoF + 2*ncontr + 2*nCov
 if (NoConst) {
-   if (RanEff) NoCoef <- length(fm$coefficients$fixed) else {
+   if (as.numeric(ranEff[[1]][1])) NoCoef <- length(fm$coefficients$fixed) else {
 	   NoCoef <- length(fm$coefficients)
 	}
 	NoBrick <- NoBrick + 2*NoCoef	
@@ -243,73 +269,86 @@ Stat <- array(0, dim=c(dimx, dimy, dimz, NoBrick))
 #time1 <- format(Sys.time(), "%D %H:%M:%OS3")
 
 print(format(Sys.time(), "%D %H:%M:%OS3"))
-print(sprintf("Start to compute %s planes along X axis. You can monitor the progress", dimx))
+print(sprintf("Start to compute %s slices along Z axis. You can monitor the progress", dimz))
 print("and estimate the total run time by opening this file from time to time.")
 
-tag <- 0
-for (i in 1:dimx) {
-for (j in 1:dimy) {
-for (k in 1:dimz) {
-   if ((Mask[i, j, k,] != 0) & !all(IData[i, j, k,] == 0)) {	
-	Model$Beta<-IData[i, j, k,]
-	if (RanEff) {
-		if (CorStr == 0) if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, weights=varIdent(VarForm), Model), tag <- 1) }
-			
-		if (CorStr == 1) if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, 
-		   correlation=corAR1(0.3, form=CorForm), Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, correlation=corAR1(0.3, form=CorForm),
-		   weights=varIdent(VarForm), Model), tag <- 1) }
-			
-		if (CorStr == 2) if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, 
-		   correlation=corARMA(c(0.3,0.3), p=2, form=CorForm), Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, 
-			correlation=corARMA(c(0.3,0.3), p=2, form=CorForm),
-			weights=varIdent(VarForm), Model), tag <- 1) }
-			
-		if (CorStr == 3) if (VarStr == 0) {
-		   try(fm <- lme(ModelForm, random = ~1|Subj, 
-		   correlation=corARMA(c(0.3,0.3), p=1, q=1, form=CorForm), Model), tag <- 1) } else {
-			try(fm <- lme(ModelForm, random = ~1|Subj, 
-		   correlation=corARMA(c(0.3,0.3), p=1, q=1, form=CorForm),
-			weights=varIdent(VarForm), Model), tag <- 1) }
-	}	
-   else try(fm <- gls(ModelForm, Model), tag <- 1) 
+myRand <- vector("list", 6)
+myRand[[1]]<-ranEff[[1]]; myRand[[2]]<-ranEff[[2]]; myRand[[3]]<-CorStr; myRand[[4]]<-CorForm; myRand[[5]]<-VarStr; myRand[[6]]<-VarForm
+myStuff <- vector("list", 10)
+myStuff[[1]]<-NoBrick; myStuff[[2]]<-BrickCnt; myStuff[[3]]<-NoCoef; myStuff[[4]]<-ncontr
+myStuff[[5]]<-clist; myStuff[[6]]<-nCov; myStuff[[7]]<-Ftype; myStuff[[8]]<-FArr; myStuff[[9]]<-NoF; myStuff[[10]]<-NoConst
+
+runAna <- function(inData, dataframe, ModelForm, myRand, myStuff, tag) {
+	Stat <- vector(mode="numeric", length=myStuff[[1]])
+	if (!all(inData == 0)) {	
+	   dataframe$Beta<-inData
+	if (as.numeric(myRand[[1]][1])!=0) {
+		if (myRand[[3]] == 0) { if (myRand[[5]] == 0) {
+		   try(fm <- lme(ModelForm, random = myRand[[2]], dataframe), tag <- 1) } else {
+			try(fm <- lme(ModelForm, random = myRand[[2]], weights=varIdent(myRand[[6]]), dataframe), tag <- 1) } }			
+		if (myRand[[3]] == 1) { if (myRand[[5]] == 0) {
+		   try(fm <- lme(ModelForm, random = myRand[[2]], 
+		   correlation=corAR1(0.3, form=myRand[[4]]), dataframe), tag <- 1) } else {
+			try(fm <- FUNC(ModelForm, random = myRand[[2]], correlation=corAR1(0.3, form=myRand[[4]]),
+		   weights=varIdent(myRand[[6]]), dataframe), tag <- 1) } }			
+		if (myRand[[3]] == 2) { if (myRand[[5]] == 0) {
+		   try(fm <- lme(ModelForm, random = myRand[[2]], 
+		   correlation=corARMA(c(0.3,0.3), p=2, form=myRand[[4]]), dataframe), tag <- 1) } else {
+			try(fm <- lme(ModelForm, random = myRand[[2]], 
+			correlation=corARMA(c(0.3,0.3), p=2, form=myRand[[4]]),
+			weights=varIdent(myRand[[6]]), dataframe), tag <- 1) } }			
+		if (myRand[[3]] == 3) { if (myRand[[5]] == 0) {
+		   try(fm <- lme(ModelForm, random = myRand[[2]], 
+		   correlation=corARMA(c(0.3,0.3), p=1, q=1, form=myRand[[4]]), dataframe), tag <- 1) } else {
+			try(fm <- lme(ModelForm, random = myRand[[2]], 
+		   correlation=corARMA(c(0.3,0.3), p=1, q=1, form=myRand[[4]]),
+			weights=varIdent(myRand[[6]]), dataframe), tag <- 1) } }
+	}
+	if (length(myRand[[1]])==1 & as.numeric(myRand[[1]][1])==0) try(fm <- gls(ModelForm, dataframe), tag <- 1)    
 	if (tag != 1) {
-	   Stat[i, j, k, 1:NoF] <- anova(fm, type=Ftype)$F[FArr]
-		if (NoConst) 
-		   if (RanEff) {
+	   if (myStuff[[9]] > 0) Stat[1:myStuff[[9]]] <- anova(fm, type=myStuff[[7]])$F[myStuff[[8]]]
+		if (myStuff[[10]]) 
+		   if (as.numeric(myRand[[1]][1])) {
 			   # or unname(summary(fm)$tTable[, "Value"])
-				Stat[i, j, k, NoF+2*0.5:NoCoef] <- unname(fm$coefficients$fixed) 
-			   Stat[i, j, k, NoF+2*1:NoCoef] <- unname(summary(fm)$tTable[,"t-value"])
+				Stat[myStuff[[9]]+2*0.5:myStuff[[3]]] <- unname(fm$coefficients$fixed) 
+			   Stat[myStuff[[9]]+2*1:myStuff[[3]]] <- unname(summary(fm)$tTable[,"t-value"])
 			} else {
-			   Stat[i, j, k, NoF+2*0.5:NoCoef] <- unname(fm$coefficients)
-				Stat[i, j, k, NoF+2*1:NoCoef] <- unname(summary(fm)$tTable[,"t-value"])
+			   Stat[myStuff[[9]]+2*0.5:myStuff[[3]]] <- unname(fm$coefficients)
+				Stat[myStuff[[9]]+2*1:myStuff[[3]]] <- unname(summary(fm)$tTable[,"t-value"])
 		   }
 
-		if (ncontr > 0) {
-		   for (n in 1:ncontr) { 
-		   con <- contrast(fm, clist[[n]][[1]], clist[[n]][[2]], type="average") 
-		   Stat[i, j, k, (BrickCnt+2*n-1):(BrickCnt+2*n)] <- c(con$Contrast, con$testStat)
+		if (myStuff[[4]] > 0) {
+		   for (n in 1:myStuff[[4]]) { 
+		   con <- contrast(fm, myStuff[[5]][[n]][[1]], myStuff[[5]][[n]][[2]], type="average") 
+		   Stat[(myStuff[[2]]+2*n-1):(myStuff[[2]]+2*n)] <- c(con$Contrast, con$testStat)
 	   }
 		}
-		if (nCov > 0) {
-		   for (n in 1:nCov) {
-			   Stat[i, j, k, (BrickCnt+2*ncontr+2*n-1):(BrickCnt+2*ncontr+2*n)] <- 
+		if (myStuff[[6]] > 0) {
+		   for (n in 1:myStuff[[6]]) {
+			   Stat[(myStuff[[2]]+2*myStuff[[4]]+2*n-1):(myStuff[[2]]+2*myStuff[[4]]+2*n)] <- 
 				c(summary(fm)$tTable[Cov[n], "Value"], summary(fm)$tTable[Cov[n], "t-value"])
 			}
 		}
 	}
-	tag <- 0
 	}
+	return(Stat)	
 }
-}
-print(sprintf("Finished dimX = %s", i))
-print(format(Sys.time(), "%D %H:%M:%OS3"))
-#gc()
+
+if (nNodes==1) for (kk in 1:dimz) {
+#   library(nlme); library(contrast)
+	Stat[,,kk,] <-aperm(apply(inData[,,kk,], c(1,2), runAna, dataframe=Model, ModelForm=ModelForm, myRand=myRand, myStuff=myStuff, tag=0), c(2,3,1))
+   cat("Z slice #", kk, "done: ", format(Sys.time(), "%D %H:%M:%OS3"), "\n")
+} 
+
+if (nNodes>1)	 {
+   library(snow)
+   cl <- makeCluster(nNodes, type = "SOCK")
+	clusterEvalQ(cl, library(nlme)); clusterEvalQ(cl, library(contrast))
+   for (kk in 1:dimz) {
+      Stat[,,kk,] <-aperm(parApply(cl, inData[,,kk,], c(1,2), runAna, dataframe=Model, ModelForm=ModelForm, myRand=myRand, myStuff=myStuff, tag=0), c(2,3,1))
+      cat("Z slice #", kk, "done: ", format(Sys.time(), "%D %H:%M:%OS3"), "\n")
+   } 
+   stopCluster(cl)
 }
 
 MyLabel <- paste(rownames(anova(fm))[FArr], " F")
@@ -332,22 +371,21 @@ for (n in 1:nCov) {
 }
 }
 
-write.AFNI(OutFile, Stat, MyLabel, note=Data$header$HISTORY_NOTE, origin=Data$origin, 
-   delta=Data$delta, idcode="whatever")
+write.AFNI(OutFile, Stat, MyLabel, note=myHist, origin=myOrig, delta=myDelta, idcode="whatever")
 statpar <- "3drefit"
-if (!RanEff) glsDF <- as.integer(gsub(" \n", "", gsub("Denom. DF: ", "", attr(anova(fm), "label"))))
+if (!as.numeric(ranEff[[1]][1])) glsDF <- as.integer(gsub(" \n", "", gsub("Denom. DF: ", "", attr(anova(fm), "label"))))
 
 #Index adjustment when no intercept 
 IdxAdj <- as.integer(NoConst)
 for (i in (2-IdxAdj):(NoF+1-IdxAdj)) {  # has an intercept or not
    # DFs are acquired from the last solvable voxel 
-	if (RanEff) statpar <- paste(statpar, " -substatpar ", i-2+IdxAdj, 
+	if (as.numeric(ranEff[[1]][1])) statpar <- paste(statpar, " -substatpar ", i-2+IdxAdj, 
 	   " fift ", anova(fm)$numDF[i], " ", anova(fm)$denDF[i])
 	else statpar <- paste(statpar, " -substatpar ", i-2+IdxAdj, " fift ", 1, " ", glsDF)
 }  # from 0 to NoF-1
 
 if (NoConst) {
-   if (RanEff) { 
+   if (as.numeric(ranEff[[1]][1])) { 
 		   for (n in 1:dim(summary(fm)$tTable)[1]) {
 		      statpar <- paste(statpar, " -substatpar ", NoF+2*n-1, " fitt ", summary(fm)$tTable[n,"DF"])
 			}
@@ -358,7 +396,7 @@ if (NoConst) {
 
 if (ncontr > 0) for (n in 1:ncontr) statpar <- paste(statpar, " -substatpar ", BrickCnt+2*n-1, " fitt ", contrDF[n])
 if (nCov > 0) {
-   if (RanEff) for (n in 1:nCov) statpar <- paste(statpar, " -substatpar ", BrickCnt+2*ncontr+2*n-1, " fitt ",
+   if (as.numeric(ranEff[[1]][1])) for (n in 1:nCov) statpar <- paste(statpar, " -substatpar ", BrickCnt+2*ncontr+2*n-1, " fitt ",
 	   summary(fm)$tTable[Cov[n], "DF"]) else
 	   for (n in 1:nCov) statpar <- paste(statpar, " -substatpar ", BrickCnt+2*ncontr+2*n-2, " fitt ", glsDF)
 }
@@ -390,7 +428,7 @@ contrDF <- array(data=NA, dim=ncontr)
 tag<-1
 while (tag == 1) {
    tag<-0
-   if (RanEff) try(fm <- lme(ModelForm, random = ~1|Subj, Model), tag <- 1)
+   if (as.numeric(ranEff[[1]][1])) try(fm <- lme(ModelForm, random = ranEff[[2]], Model), tag <- 1)
    else try(fm <- gls(ModelForm, Model), tag <- 1)
 	if (ncontr != 0) try(for (n in 1:ncontr) { contrDF[n] <- contrast(fm, clist[[n]][[1]], clist[[n]][[2]], type="average")$df }, tag <- 1)
    if (ii<NoNode%/%3+10) ii<-ii+1 else {
@@ -421,7 +459,7 @@ tag <- 0
 for (i in 1:NoNode) {
    if (!all(IData[i,] == 0)) {	
 	Model$Beta<-IData[i,]
-	if (RanEff) try(fm <- lme(ModelForm, random = ~1|Subj, Model), tag <- 1)
+	if (as.numeric(ranEff[[1]][1])) try(fm <- lme(ModelForm, random = ranEff[[2]], Model), tag <- 1)
    else try(fm <- gls(ModelForm, Model), tag <- 1)
 	if (tag != 1) {
 	   Stat[i,1:NoF] <- anova(fm, type=Ftype)$F[FArr]
@@ -453,7 +491,7 @@ print("Degrees of freedom for the statistics in the output are:")
 print("========================================================")
 print("F-statistic  numDF  denumDF")
 print("---------------------------")
-if (RanEff) for (ii in FArr) print(sprintf("%s F     %d    %d", rownames(anova(fm))[ii], anova(fm)$numDF[ii], anova(fm)$denDF[ii]))
+if (as.numeric(ranEff[[1]][1])) for (ii in FArr) print(sprintf("%s F     %d    %d", rownames(anova(fm))[ii], anova(fm)$numDF[ii], anova(fm)$denDF[ii]))
 else for (ii in FArr) print(sprintf("%s F     %d    %d", rownames(anova(fm))[ii], anova(fm)$numDf[ii], fm$df.residual))
 print("---------------------------")
 print("t-statistic  DF")
