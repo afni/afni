568ab13305c18792653b3f9eb18522283a4fe0dd
rwcox <>
2008-12-11 16:37:12 +0000

    Cput

--- src/matrix_f.h
+++ src/matrix_f.h
@@ -370,7 +370,7 @@ double * matrix_singvals( matrix X ) ; /* 14 Jul 2004 */
 
 void matrix_psinv( matrix X , matrix *XtXinv , matrix *XtXinvXt ) ;  /* 19 Jul 2004 */
 
-void matrix_qrr( matrix X , matrix *R ) ;  /* 03 Jul 2008 */
+int  matrix_qrr( matrix X , matrix *R ) ;  /* 03 Jul 2008 */
 void vector_rr_solve( matrix R , vector b , vector *x ) ;
 void vector_rrtran_solve( matrix R , vector b , vector *x ) ;
 
