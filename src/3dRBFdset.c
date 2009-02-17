eb5e8751b1351361efa8a69eb34c0297c19b6a89
rwcox <>
2009-02-17 22:05:15 +0000

    Cput

--- src/3dRBFdset.c
+++ src/3dRBFdset.c
@@ -240,6 +240,8 @@ int main( int argc , char * argv[] )
 
    /*----- setup for RBF interpolation at the given set of points -----*/
 
+   RBF_set_verbosity( 1 ) ;
+
    rbk = RBF_setup_knots( kfnx , rad,uselin ,
                           kfar+0*kfnx , kfar+1*kfnx , kfar+2*kfnx ) ;
    if( rbk == NULL )
