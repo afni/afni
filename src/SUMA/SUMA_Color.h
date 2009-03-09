9ec26fc81b6264c144ff35f03bed0caea906d055
rickr <>
2009-03-09 18:55:06 +0000

    Cput

--- src/SUMA/SUMA_Color.h
+++ src/SUMA/SUMA_Color.h
@@ -46,8 +46,6 @@
 int SUMA_a_good_col(char *name, int i, float *acol);
 SUMA_COLOR_MAP * SUMA_MakeColorMap (float **Fiducials, int Nfid, int Ncols, SUMA_Boolean SkipLast, char *Name);
 void SUMA_Free_ColorMap (SUMA_COLOR_MAP* SM);
-int r_ulong_size ( unsigned long l );
-int r_sprintf_long_to_hex (char  * dest, unsigned long  lsrc,	int bytes, int	pad);
 SUMA_SCALE_TO_MAP_OPT * SUMA_ScaleToMapOptInit(void);
 void SUMA_Free_ColorScaledVect (SUMA_COLOR_SCALED_VECT * S);
 SUMA_COLOR_SCALED_VECT * SUMA_Create_ColorScaledVect(int N_Node);
