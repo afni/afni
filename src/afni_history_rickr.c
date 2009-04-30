5ecf039eed310ae7284dd88317ad610ff8277ac1
rickr <>
2009-04-30 18:54:44 +0000

    Cput

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -48,6 +48,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 30, APR, 2009, RCR, "thd_niftiread", MINOR, TYPE_MODIFY,
+   "be sure to warn users when nifti is converted to float (w/dglen)",
+   NULL
+ } ,
+
  { 30, APR, 2009, RCR, "vol2surf", MINOR, TYPE_NEW_OPT,
    "return a node v2s time series when afni changes xhair position",
    NULL
