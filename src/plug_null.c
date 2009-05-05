ad4c4ea9c7940ad40d1d03b5d955afb2bb98045b
rickr <>
2009-05-05 17:22:46 +0000

    Cadd

---
+++ src/plug_null.c
@@ -0,0 +1,19 @@
+/*****************************************************************************
+   Major portions of this software are copyrighted by the Medical College
+   of Wisconsin, 1994-2000, and are released under the Gnu General Public
+   License, Version 2.  See the file README.Copyright for details.
+******************************************************************************/
+
+#include "afni.h"
+
+#ifndef ALLOW_PLUGINS
+#  error "Plugins not properly set up -- see machdep.h"
+#endif
+
+DEFINE_PLUGIN_PROTOTYPE
+
+PLUGIN_interface * PLUGIN_init(int ncall)
+{
+   return NULL;
+}
+
