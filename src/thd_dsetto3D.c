e9a106b338f2e8616016c3498247fcf6dcbb0ee0
rwcox <>
2009-07-16 20:51:47 +0000

    Cput

--- src/thd_dsetto3D.c
+++ src/thd_dsetto3D.c
@@ -6,18 +6,18 @@
   -- RWCox - 10 Aug 2001
 -----------------------------------------------------------------*/
 
-MRI_IMAGE * THD_extract_float_brick( int iv , THD_3dim_dataset * dset )
+MRI_IMAGE * THD_extract_float_brick( int iv , THD_3dim_dataset *dset )
 {
-   MRI_IMAGE * im ;
-   float * var ;
+   MRI_IMAGE *im ;
+   register float *var , fac ;
    register int ii , nvox ;
 
 ENTRY("THD_extract_float_brick") ;
 
    if( iv < 0 || !ISVALID_DSET(dset) || iv >= DSET_NVALS(dset) ) RETURN(NULL);
 
-   im = mri_new_conforming( DSET_BRICK(dset,iv) , MRI_float ) ;
-   var = MRI_FLOAT_PTR(im) ;
+   im   = mri_new_conforming( DSET_BRICK(dset,iv) , MRI_float ) ;
+   var  = MRI_FLOAT_PTR(im) ;
    nvox = DSET_NVOX(dset) ;
 
    /*-- extract/scale brick into var --*/
@@ -25,65 +25,58 @@ ENTRY("THD_extract_float_brick") ;
    switch( DSET_BRICK_TYPE(dset,iv) ){
 
       default:
-         fprintf(stderr,"Can't handle sub-bricks of type %s\n",
-                 MRI_TYPE_name[DSET_BRICK_TYPE(dset,iv)]) ;
-         mri_free(im) ;
+        mri_free(im) ;
+        ERROR_message("Can't handle sub-bricks of type %s\n",
+                      MRI_TYPE_name[DSET_BRICK_TYPE(dset,iv)] ) ;
       RETURN(NULL) ;
 
       case MRI_short:{
-         register short *dar = DSET_ARRAY(dset,iv) ;
-         register float fac  = DSET_BRICK_FACTOR(dset,iv) ;
-         if( fac == 0.0 ) fac = 1.0 ;
-         for( ii=0 ; ii < nvox ; ii++ ) var[ii] = fac*dar[ii] ;
+        register short *dar = DSET_ARRAY(dset,iv) ;
+        for( ii=0 ; ii < nvox ; ii++ ) var[ii] = dar[ii] ;
       }
       break ;
 
       case MRI_byte:{
-         register byte  *dar = DSET_ARRAY(dset,iv) ;
-         register float fac  = DSET_BRICK_FACTOR(dset,iv) ;
-         if( fac == 0.0 ) fac = 1.0 ;
-         for( ii=0 ; ii < nvox ; ii++ ) var[ii] = fac*dar[ii] ;
+        register byte  *dar = DSET_ARRAY(dset,iv) ;
+        for( ii=0 ; ii < nvox ; ii++ ) var[ii] = dar[ii] ;
       }
       break ;
 
       case MRI_float:{
-         register float *dar = DSET_ARRAY(dset,iv) ;
-         register float fac  = DSET_BRICK_FACTOR(dset,iv) ;
-         if( fac == 0.0 ) fac = 1.0 ;
-         for( ii=0 ; ii < nvox ; ii++ ) var[ii] = fac*dar[ii] ;
+        register float *dar = DSET_ARRAY(dset,iv) ;
+        for( ii=0 ; ii < nvox ; ii++ ) var[ii] = dar[ii] ;
       }
       break ;
 
       case MRI_complex:{
-         register complex *dar = DSET_ARRAY(dset,iv) ;
-         register float fac    = DSET_BRICK_FACTOR(dset,iv) ;
-         if( fac == 0.0 ) fac = 1.0 ;
-         for( ii=0 ; ii < nvox ; ii++ ) var[ii] = fac*CABS(dar[ii]) ;
+        register complex *dar = DSET_ARRAY(dset,iv) ;
+        for( ii=0 ; ii < nvox ; ii++ ) var[ii] = CABS(dar[ii]) ;
       }
       break ;
 
+#if 0
       case MRI_int:{
-         register int   *dar = DSET_ARRAY(dset,iv) ;
-         register float fac  = DSET_BRICK_FACTOR(dset,iv) ;
-         if( fac == 0.0 ) fac = 1.0 ;
-         for( ii=0 ; ii < nvox ; ii++ ) var[ii] = fac*dar[ii] ;
+        register int   *dar = DSET_ARRAY(dset,iv) ;
+        for( ii=0 ; ii < nvox ; ii++ ) var[ii] = dar[ii] ;
       }
       break ;
 
       case MRI_double:{
-         register double *dar = DSET_ARRAY(dset,iv) ;
-         register float fac   = DSET_BRICK_FACTOR(dset,iv) ;
-         if( fac == 0.0 ) fac = 1.0 ;
-         for( ii=0 ; ii < nvox ; ii++ ) var[ii] = fac*dar[ii] ;
+        register double *dar = DSET_ARRAY(dset,iv) ;
+        for( ii=0 ; ii < nvox ; ii++ ) var[ii] = dar[ii] ;
       }
       break ;
+#endif
 
       case MRI_rgb:{
-         register byte *dar = DSET_ARRAY(dset,iv) ;
-         for( ii=0 ; ii < nvox ; ii++ )
-            var[ii] = 0.299*dar[3*ii] + 0.587*dar[3*ii+1] + 0.114*dar[3*ii+2] ;
+        register byte *dar = DSET_ARRAY(dset,iv) ;
+        for( ii=0 ; ii < nvox ; ii++ )
+          var[ii] = 0.299*dar[3*ii] + 0.587*dar[3*ii+1] + 0.114*dar[3*ii+2] ;
       }
    }
+
+   fac = DSET_BRICK_FACTOR(dset,iv) ; if( fac == 0.0f ) fac = 1.0f ;
+   if( fac != 1.0f ){ for( ii=0 ; ii < nvox ; ii++ ) var[ii] *= fac ; }
 
    RETURN(im) ;
 }
