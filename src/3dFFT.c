f9a05438b53927997f6a73ac569655b031960798
rwcox <>
2009-08-25 20:19:12 +0000

    Cput

--- src/3dFFT.c
+++ src/3dFFT.c
@@ -8,7 +8,7 @@
 int main( int argc , char *argv[] )
 {
    THD_3dim_dataset *dset_in , *dset_out ;
-   int Lxx=-1 , Lyy=-1 , Lzz=-1 , Mode=FFT_ABS ;
+   int Lxx=-1 , Lyy=-1 , Lzz=-1 , Mode=FFT_ABS , Sign=-1 ;
    char *prefix = "FFT" ;
    int iarg ;
 
@@ -23,23 +23,25 @@ int main( int argc , char *argv[] )
        " -abs       = Outputs the magnitude of the FFT [default]\n"
        " -phase     = Outputs the phase of the FFT (-PI..PI)\n"
        " -complex   = Outputs the complex FFT\n"
+       " -inverse   = Does the inverse FFT\n"
        " -Lx xx     = Use FFT of length 'xx' in the x-direction\n"
        " -Ly yy     = Use FFT of length 'yy' in the y-direction\n"
        " -Lz zz     = Use FFT of length 'zz' in the z-direction\n"
-       "              * Set a length to 0 to skip\n"
-       "                the FFT in that direction\n"
+       "              * Set a length to 0 to skip the FFT in that direction\n"
        " -prefix pp = Use 'pp' for the output dataset prefix.\n"
        "\n"
        "Notes\n"
        "=====\n"
        " * The program can only do FFT lengths that are factorable\n"
-       "    into powers of 2, 3, and 5.\n"
+       "    into a product of powers of 2, 3, and 5.\n"
        " * For -abs and -phase, the output dataset is in float format.\n"
+       " * Forward FFT = sum_k[ exp(-2*PI*i*k/N) * data(k) ]\n"
+       " * Inverse FFT = sum_k[ exp(+2*PI*i*k/N) * data(k) ] / N\n"
      ) ;
      PRINT_COMPILE_DATE ; exit(0) ;
    }
 
-   mainENTRY("3dFFT main") ; machdep() ;
+   mainENTRY("3dFFT main") ; machdep() ; AUTHOR("RW Cox") ;
 
    /*--- scan args ---*/
 
@@ -47,6 +49,9 @@ int main( int argc , char *argv[] )
 
    while( iarg < argc && argv[iarg][0] == '-' ){
 
+     if( strcmp(argv[iarg],"-inverse") == 0 ){
+       Sign = +1 ; iarg++ ; continue ;
+     }
      if( strcmp(argv[iarg],"-abs") == 0 ){
        Mode = FFT_ABS ; iarg++ ; continue ;
      }
@@ -60,54 +65,57 @@ int main( int argc , char *argv[] )
      if( strlen(argv[iarg]) == 3 && strncmp(argv[iarg],"-L",2) == 0 ){
        int lll=-1 ; char *ept ;
        iarg++ ;
-       if( iarg >= argc ){
-         fprintf(stderr,"** ERROR: need an argument after %s\n",argv[iarg-1]) ;
-         exit(1) ;
-       }
+       if( iarg >= argc )
+         ERROR_exit("need an argument after option %s",argv[iarg-1]) ;
+
        lll = strtol( argv[iarg] , &ept , 10 ) ;
        if( *ept != '\0' ){
-         fprintf(stderr,"** ERROR: bad argument after %s\n",argv[iarg-1]) ;
+         ERROR_exit("bad argument after option %s",argv[iarg-1]) ;
          exit(1) ;
        }
+       if( lll < 0 ) ERROR_exit("negative argument after option %s",argv[iarg-1]) ;
+       if( lll > 0 && csfft_nextup(lll) != lll )
+         ERROR_exit("argument after '%s' is not a legal FFT length in this program!",argv[iarg-1]) ;
        switch( argv[iarg-1][2] ){
          case 'x': Lxx = lll ; break ;
          case 'y': Lyy = lll ; break ;
          case 'z': Lzz = lll ; break ;
-         default:
-           fprintf(stderr,"** ERROR: unknown option '%s'\n",argv[iarg-1]) ;
-           exit(1) ;
+         default:  ERROR_exit("unknown option '%s'",argv[iarg-1]) ;
        }
        iarg++ ; continue ;
      }
 
      if( strcmp(argv[iarg],"-prefix") == 0 ){
        iarg++ ;
-       if( iarg >= argc ){
-         fprintf(stderr,"** ERROR: need an argument after %s\n",argv[iarg-1]) ;
-         exit(1) ;
-       }
+       if( iarg >= argc )
+         ERROR_exit("need an argument after %s\n",argv[iarg-1]) ;
        prefix = strdup( argv[iarg] ) ;
-       if( !THD_filename_ok(prefix) ){
-         fprintf(stderr,"** ERROR: bad argument after %s\n",argv[iarg-1]) ;
-         exit(1) ;
-       }
+       if( !THD_filename_ok(prefix) )
+         ERROR_exit("bad argument after %s\n",argv[iarg-1]) ;
        iarg++ ; continue ;
      }
 
-     fprintf(stderr,"** ERROR: unknown option '%s'\n",argv[iarg]) ;
-     exit(1) ;
+     ERROR_exit("unknown option '%s'\n",argv[iarg]) ;
    }
 
-   if( iarg >= argc ){
-     fprintf(stderr,"** ERROR: no input dataset on command line?!\n") ;
-     exit(1) ;
-   }
-   if( Lxx == 0 && Lyy == 0 && Lzz == 0 ){
-     fprintf(stderr,"** ERROR: -Lx, -Ly, -Lz all given as zero?!\n") ;
-     exit(1) ;
-   }
+   if( iarg >= argc ) ERROR_exit("no input dataset on command line?!\n") ;
+
+   if( Lxx == 0 && Lyy == 0 && Lzz == 0 )
+     ERROR_exit("-Lx, -Ly, -Lz all given as zero?!\n") ;
 
    /* open input dataset */
 
    dset_in = THD_open_dataset(argv[iarg]); CHECK_OPEN_ERROR(dset_in,argv[iarg]);
+
+   nx = DSET_NX(dset_in) ; ny = DSET_NY(dset_in) ; nz = DSET_NZ(dset_in) ;
+
+   if( Lxx < 0 ) Lxx = csfft_nextup(nxx) ;
+   if( Lyy < 0 ) Lxx = csfft_nextup(nyy) ;
+   if( Lzz < 0 ) Lxx = csfft_nextup(nzz) ;
+   INFO_message("x-axis length = %d ;  FFT length = %d",nx,Lxx) ;
+   INFO_message("y-axis length = %d ;  FFT length = %d",ny,Lyy) ;
+   INFO_message("z-axis length = %d ;  FFT length = %d",nz,Lzz) ;
+   if( Lxx > 0 && Lxx < nx ) ERROR_exit("x-axis length mismatch!") ;
+   if( Lyy > 0 && Lyy < ny ) ERROR_exit("y-axis length mismatch!") ;
+   if( Lzz > 0 && Lzz < nz ) ERROR_exit("z-axis length mismatch!") ;
 }
