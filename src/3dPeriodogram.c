ddb51169da7529f626eb98aaaed9e7d90b5f880d
rwcox <>
2009-10-08 20:04:37 +0000

    Cput

--- src/3dPeriodogram.c
+++ src/3dPeriodogram.c
@@ -3,6 +3,7 @@
 static char prefix[THD_MAX_PREFIX] = "pgram" ;
 
 static float taper = 0.1f ;
+static int   gfft  = 0 ;
 
 static void PGRAM_tsfunc( double tzero , double tdelta ,
                          int npts , float ts[] , double ts_mean ,
@@ -11,7 +12,7 @@ static void PGRAM_tsfunc( double tzero , double tdelta ,
 int main( int argc , char *argv[] )
 {
    THD_3dim_dataset *old_dset , *new_dset ;  /* input and output datasets */
-   int nopt, nvals , nfft , nbin ;
+   int nopt, nvals , nbin ;
 
    /*----- Help the pitiful user? -----*/
 
@@ -24,17 +25,27 @@ int main( int argc , char *argv[] )
              " -prefix p = use string 'p' for the prefix of the\n"
              "               output dataset [DEFAULT = 'pgram']\n"
              " -taper    = fraction of data to taper [DEFAULT = 0.1]\n"
+             " -nfft L   = set FFT length to 'L' points\n"
+             "               (longer than the data ==> zero padding)\n"
+             "               (shorter than the data ==> data pruning)\n"
              "\n"
              "Notes:\n"
-             "* Output is in float format.\n"
-             "* FFT length is chosen by the program.\n"
+             "* Output is in float format; number of sub-bricks will be\n"
+             "   half the FFT length; sub-brick #0 = FFT bin #1, etc.\n"
              "* There is no '-mask' option.  The hyper-clever user could\n"
              "   use something like\n"
              "     '3dcalc( -a dset+orig -b mask+orig -expr a*b )'\n"
              "   to apply a binary mask on the command line.\n"
-             "* Data is not scaled to be exactly like the Power plugin.\n"
+             "* Data is not scaled exactly as in the AFNI Power plugin.\n"
+             "* FFT length defaults to be the next legal length >= input dataset.\n"
+             "* The program can only do FFT lengths that are factorable\n"
+             "   into a product of powers of 2, 3, and 5, and are even.\n"
+             "  + The largest power of 3 that is allowed is 3^3 = 27.\n"
+             "  + The largest power of 5 that is allowed is 5^3 = 125.\n"
+             "  + e.g., FFT of length 3*5*8=120 is possible.\n"
+             "  + e.g., FFT of length 4*31 =124 is not possible.\n"
              "* This is a really quick hack for DH and PB and SfN.\n"
-             "* Author = RWCox -- who wants his bribe now (should I say that?)\n"
+             "* Author = RWCox -- who doesn't want any bribe at all for this!\n"
            ) ;
       PRINT_COMPILE_DATE ; exit(0) ;
    }
@@ -49,9 +60,18 @@ int main( int argc , char *argv[] )
    nopt = 1 ;
    while( nopt < argc && argv[nopt][0] == '-' ){
 
-      /*-- prefix --*/
+      if( strcasecmp(argv[nopt],"-nfft") == 0 ){
+         int mmm ;
+         if( ++nopt >= argc ) ERROR_exit("%s needs an argument!",argv[nopt-1]);
+         gfft = (int)strtod(argv[nopt],NULL) ;
+         if( gfft > 0 && gfft != (mmm=csfft_nextup_even(gfft)) )
+           ERROR_exit(
+            "%d is not a legal FFT length here: next largest legal value = %d" ,
+            gfft , mmm ) ;
+         nopt++ ; continue ;
+      }
 
-      if( strcmp(argv[nopt],"-prefix") == 0 ){
+      if( strcasecmp(argv[nopt],"-prefix") == 0 ){
          if( ++nopt >= argc ) ERROR_exit("-prefix needs an argument!");
          MCW_strncpy(prefix,argv[nopt],THD_MAX_PREFIX) ;
          if( !THD_filename_ok(prefix) )
@@ -59,7 +79,7 @@ int main( int argc , char *argv[] )
          nopt++ ; continue ;
       }
 
-      if( strcmp(argv[nopt],"-taper") == 0 ){
+      if( strcasecmp(argv[nopt],"-taper") == 0 ){
          if( ++nopt >= argc ) ERROR_exit("-taper needs an argument!");
          taper = (float)strtod(argv[nopt],NULL) ;
          if( taper < 0.0f || taper > 1.0f ) ERROR_exit("-taper value out of range 0..1");
@@ -90,9 +110,10 @@ int main( int argc , char *argv[] )
    if( nopt < argc )
      WARNING_message("Trailing inputs on command line ignored: %s ...",argv[nopt]) ;
 
-   nfft = csfft_nextup_even(nvals) ;
-   nbin = nfft / 2 ;
-   INFO_message("Lengths: Input=%d  FFT=%d  Output=%d",nvals,nfft,nbin) ;
+   if( gfft <= 0 ) gfft = csfft_nextup_even(nvals) ;
+   nbin = gfft / 2 ;
+   INFO_message("Lengths: Data=%d%s FFT=%d  Output=%d" ,
+                nvals, (nvals>gfft)?"* ":" ", gfft, nbin) ;
 
    /*------------- ready to compute new dataset -----------*/
 
@@ -113,7 +134,7 @@ int main( int argc , char *argv[] )
      if( DSET_TIMEUNITS(old_dset) == UNITS_MSEC_TYPE ) tr *= 0.001f ;
 
      if( tr <= 0.0f ) tr = 1.0f ;
-     df = 1.0f / (nfft*tr) ;
+     df = 1.0f / (gfft*tr) ;
 
      tross_Copy_History( old_dset , new_dset ) ;
      tross_Make_History( "3dPeriodogram" , argc,argv , new_dset ) ;
@@ -145,7 +166,7 @@ static void PGRAM_tsfunc( double tzero, double tdelta ,
    static complex *cxar=NULL ;
    static int     first=1 ;
 
-   int nbin=nbrik , nfft=nbin*2 ;
+   int nbin=nbrik , nfft=gfft ;
    float pfact ;
 
    register int kk ;
@@ -159,7 +180,6 @@ ENTRY("PGRAM_tsfunc") ;
 
       } else {  /* the "end notification" */
 
-INFO_message("free-ing") ;
         if( tar  != NULL ){ free(tar) ; tar  = NULL; }
         if( cxar != NULL ){ free(cxar); cxar = NULL; }
         first = 1 ;
@@ -168,17 +188,17 @@ INFO_message("free-ing") ;
       EXRETURN ;
    }
 
+   if( npts > nfft ) npts = nfft ;  /* truncate data */
+
    if( first ){
      int ntaper , ktbot ; float phi ;
 
-INFO_message("malloc-ing: nfft=%d",nfft) ;
      cxar = (complex *)malloc(sizeof(complex)*nfft) ;
      tar  = (float *  )malloc(sizeof(float  )*nfft) ;
      ntaper = (int)(0.5 * taper * npts + 0.49f) ; /* will taper data over */
      phi    = PI / MAX(ntaper,1) ;                /* kk=0..ntaper-1 on left */
      ktbot  = npts - ntaper ;                     /* kk=ktbot..npts1 on right */
      pfact  = 0.0f ;                              /* sum of taper**2 */
-INFO_message("taper=%g npts=%d ntaper=%d phi=%g",taper,npts,ntaper,phi) ;
 
      for( kk=0 ; kk < npts ; kk++ ){                        /* Hamming-ize */
        if( kk < ntaper )
@@ -191,8 +211,9 @@ INFO_message("taper=%g npts=%d ntaper=%d phi=%g",taper,npts,ntaper,phi) ;
        pfact += tar[kk]*tar[kk] ;
      }
      pfact = 1.0f / pfact ;
-INFO_message("pfact=%g",pfact) ;
      first = 0 ;
+     if( ntaper > 0 )
+       ININFO_message("Taper length at each end of data = %d",ntaper) ;
    }
 
    /** do the work **/
