d76704405320c37a0278a11314520cc0080dfd7b
ziad <>
2009-11-19 14:36:21 +0000

    Cput

--- src/matlab/AFNI_XYZcontinuous2Index.m
+++ src/matlab/AFNI_XYZcontinuous2Index.m
@@ -1,6 +1,6 @@
-function [err,Indx] = AFNI_XYZcontinuous2Index (XYZmm, Info, CoordCode, IndexDim)
+function [err,Indx] = AFNI_XYZcontinuous2Index (XYZmm, Info, CoordCode, IndexDim, retNaNoutside)
 %
-%   [err,Indx] = AFNI_XYZcontinuous2Index (XYZmm, Info, [CoordCode], [IndexDim])
+%   [err,Indx] = AFNI_XYZcontinuous2Index (XYZmm, Info, [CoordCode], [IndexDim], [retNaNoutside)
 %
 %Purpose:
 %   Change from voxel XYZ in mm to XYZindex (called Voxel Coords in AFNI) 
@@ -14,13 +14,24 @@ function [err,Indx] = AFNI_XYZcontinuous2Index (XYZmm, Info, CoordCode, IndexDim
 %       The coordnate system is assumed to be RAI (DICOM) 
 %       unless otherwise specified by CoordCode
 %   Info is the output of BrikInfo
-%   CoordCode is an optional parameter used to specify the coordinates system of XYZmm
-%      if empty or not specified, the default is 'RAI'. The code can be either a string or a vector 
-%      of numbers (see AFNI_CoordChange for more on that)
-%   IndexDim (3 or 1) is an optional parameter used to specify if Indx is Mx3 or Mx1 vector
+%   CoordCode is an optional parameter used to specify the 
+%            coordinates system of XYZmm. If empty or not specified, 
+%            the default is 'RAI'. The code can be either a string or a vector 
+%            of numbers (see AFNI_CoordChange for more on that)
+%   IndexDim (3 or 1) is an optional parameter used to specify if Indx 
+%                     is Mx3 or Mx1 vector
 %      (see AfniIndex2AfniXYZ for more info)
-%   	The default is 3 . If you choose to specify IndexDim, you must specify CoordCode 
+%   	The default is 3 . If you choose to specify IndexDim, you must 
+%     specify CoordCode 
 %      (you could use an empty string to leave CoordCode to the default)
+%
+%   [NNO Nov 2009 added <n.oosterhof@bangor.ac.uk>]
+% 
+%   retNaNoutside (true or false) specifies if NaN should be returned if
+%   a coordinate is outsize the volume specified by Info. The default is
+%   false and yields valid indices in Indx of the nearest voxel. If set to 
+%   true, then this yields NaN values in Indx for coordinates outside the
+%   volume
 %   
 %Output Parameters:
 %   err : 0 No Problem
@@ -59,15 +70,26 @@ if (nargin > 2)
 end
 
 ChangeDim = 0;
-if (nargin == 4),
+if (nargin >= 4), %NNO was '=='
 	if (~isempty(IndexDim)),
 		ChangeDim = 1;
 	end
 end
 
+
 %initailize return variables
 err = 1;
 Indx = [];
+
+% NNO added
+if nargin < 5 %check last input argument
+    retNaNoutside=false; % default: use old behaviour, do not return NaNs
+elseif ~islogical(retNaNoutside) || numel(retNaNoutside) ~= 1
+    fprintf(2,'%s: Illegal value for retNaNoutside\n\n', FuncName);
+    return
+end
+    
+
 
 
 Indx = XYZmm;
@@ -100,27 +122,40 @@ end
 		%Indx(:,maploc(3)) = round( ( XYZmm(:, 3) - Info.ORIGIN(maploc(3)) ) ./ Info.DELTA(maploc(3)) );
 		
       %Feb 18 04
-      Indx(:,1) = round( ( XYZmm(:, 1) - Info.ORIGIN(1) ) ./ Info.DELTA(1) );
-		Indx(:,2) = round( ( XYZmm(:, 2) - Info.ORIGIN(2) ) ./ Info.DELTA(2) );
-		Indx(:,3) = round( ( XYZmm(:, 3) - Info.ORIGIN(3) ) ./ Info.DELTA(3) );
-		
-      ineg = find(Indx < 0);
-		if (~isempty(ineg)), Indx(ineg) = 0; end
-		
-		[iover] = find(Indx(:,1) >  Info.DATASET_DIMENSIONS(1)-1);
-		if (~isempty(iover)), Indx(iover, 1) = Info.DATASET_DIMENSIONS(1)-1;	end
-		
-		[iover] = find(Indx(:,2) >  Info.DATASET_DIMENSIONS(2)-1);
-		if (~isempty(iover)), Indx(iover, 2) = Info.DATASET_DIMENSIONS(2)-1;	end
-		
-		[iover] = find(Indx(:,3) >  Info.DATASET_DIMENSIONS(3)-1);
-		if (~isempty(iover)), Indx(iover, 3) = Info.DATASET_DIMENSIONS(3)-1;	end
-		
-			
-	%Now, if needed, change the Index dimension from Mx3 to Mx1
-	if (ChangeDim &	IndexDim == 1),
-		 [err, Indx] = AfniXYZ2AfniIndex (Indx, Info.DATASET_DIMENSIONS(1), Info.DATASET_DIMENSIONS(2));  
-	end
+    
+    % NNO added
+    IndxCount=size(Indx,1); % number of indices
+    settoNaNmask=false(IndxCount,1); % values to be set to NaN
+    outsidevalues=[0 0 0;Info.DATASET_DIMENSIONS(1:3)-1]; % if not set to NaN, values outside the volume. 
+
+    % for x, y, z dimension see which indices fall outside the volume
+    for dim=1:3
+        IndxDim=round( ( XYZmm(:, dim) - Info.ORIGIN(dim) ) ./ Info.DELTA(dim) ); % transform to voxel (ijk) space
+        
+        mask1=IndxDim<0; % values too low
+        mask2=IndxDim>Info.DATASET_DIMENSIONS(dim); % values too high
+
+        if retNaNoutside
+            settoNaNmask=settoNaNmask | mask1 | mask2; % add to mask
+        else
+            IndxDim(mask1)=outsidevalues(1,dim); % } set outside
+            IndxDim(mask2)=outsidevalues(2,dim); % } value
+        end
+        
+        Indx(:,dim)=IndxDim;
+    end
+
+    %Now, if needed, change the Index dimension from Mx3 to Mx1
+    if ChangeDim && IndexDim == 1
+        Indx1=zeros(IndxCount,1);
+        [err, Indx1(~settoNaNmask)] = AfniXYZ2AfniIndex (Indx(~settoNaNmask,:), Info.DATASET_DIMENSIONS(1), Info.DATASET_DIMENSIONS(2));  
+        Indx=Indx1; 
+    end
+    
+    % set to NaN if requested
+    if retNaNoutside
+        Indx(settoNaNmask,:)=NaN;
+    end
 	
 err = 0;
 return;
