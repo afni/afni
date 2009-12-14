af37ea858265c3448f304856bc65542a04d08e05
rwcox <>
2009-12-14 18:35:37 +0000

    Cput

--- src/afni_history_rwcox.c
+++ src/afni_history_rwcox.c
@@ -44,6 +44,10 @@
 afni_history_struct rwcox_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+ { 14 , DEC , 2009 , RWC , "afni" , MICRO , TYPE_MODIFY ,
+   "New environment variable AFNI_RECENTER_VIEWING" ,
+   NULL } ,
+
  { 4 , DEC , 2009 , RWC , "3dLocalPV" , MICRO , TYPE_MODIFY ,
    "OpenMP changes.  Speedup about factor of 3 using 6 CPUs." ,
    NULL } ,
