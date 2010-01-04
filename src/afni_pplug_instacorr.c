ca5fd16e446446b923967be41b882fe888f20301
rwcox <>
2010-01-04 17:30:22 +0000

    Cput

--- src/afni_pplug_instacorr.c
+++ src/afni_pplug_instacorr.c
@@ -730,7 +730,7 @@ void GICOR_process_dataset( NI_element *nel , int ct_start )
      AFNI_set_fim_index(im3d,0) ;
      AFNI_set_thr_index(im3d,1) ;
 #if 1
-     sprintf(cmd,"SET_FUNC_RANGE %c 0.5"   ,cpt[1]) ; AFNI_driver(cmd) ;
+     sprintf(cmd,"SET_FUNC_RANGE %c 0.2"   ,cpt[1]) ; AFNI_driver(cmd) ;
      sprintf(cmd,"SET_THRESHNEW %c 0.01 *p",cpt[1]) ; AFNI_driver(cmd) ;
 #endif
    }
