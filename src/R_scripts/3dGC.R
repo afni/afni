374991d9d96065fa17211a0e4821d59cfb71b1e4
gangc <>
2010-02-01 21:50:47 +0000

    Cput

--- src/R_scripts/3dGC.R
+++ src/R_scripts/3dGC.R
@@ -1,8 +1,10 @@
+#!/usr/bin/env AFNI_Batch_R
+
 print("#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("          ================== Welcome to 3dGC.R ==================          ")
 print("AFNI Bivariate Auto-Regressive Modeling Package!")
 print("#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-print("Version 0.0.6,  Feb. 26, 2009")
+print("Version 0.0.7,  Feb. 1, 2010")
 print("Author: Gang Chen (gangchen@mail.nih.gov)")
 print("Website: http://afni.nimh.nih.gov/sscc/gangc/3dGC.html")
 print("SSCC/NIMH, National Institutes of Health, Bethesda MD 20892")
@@ -19,167 +21,15 @@ libLoad <- function(myLib) {
    	}
 }
 
+first.in.path <- function(file) {
+   ff <- paste(strsplit(Sys.getenv('PATH'),':')[[1]],'/', file, sep='')
+   ff<-ff[lapply(ff,file.exists)==TRUE];
+   #cat('Using ', ff[1],'\n');
+   return(gsub('//','/',ff[1], fixed=TRUE)) 
+}
+source(first.in.path('AFNIio.R'))
+
 #=============
-
-read.AFNI <- function(filename) {
-  fileparts <- strsplit(filename,"\\.")[[1]]
-  ext <- tolower(fileparts[length(fileparts)])
-
-  if (ext == "head") {
-    filename.head <- filename
-    filename.brik <- paste(c(fileparts[-length(fileparts)],"BRIK"),collapse=".")
-  } else if (ext == "brik") {
-    filename.head <- paste(c(fileparts[-length(fileparts)],"HEAD"),collapse=".")
-    filename.brik <- filename
-  } else {
-    filename.head <- paste(filename,".HEAD",sep="")
-    filename.brik <- paste(filename,".BRIK",sep="")
-  }
-  
-  conhead <- file(filename.head,"r")
-  header <- readLines(conhead)
-  close(conhead)
-
-  types <- NULL
-  args <- NULL
-  counts <- NULL
-  values <- NULL
-  
-  for (i in 1:length(header)) {
-    if (regexpr("^type *= *", header[i]) != -1) {
-      tmptype <- strsplit(header[i]," *= *")[[1]][2]
-      types <- c(types,tmptype)
-      args <- c(args,strsplit(header[i+1]," *= *")[[1]][2])
-      tmpcounts <- as.numeric(strsplit(header[i+2]," *= *")[[1]][2])
-      counts <- c(counts,tmpcounts)
-      i <- i+3
-      tmpvalue <- ""
-      while ((regexpr("^$", header[i]) == -1) && (i <= length(header))) {
-        tmpvalue <- paste(tmpvalue,header[i])
-        i <- i+1
-      }
-      tmpvalue <- sub("^ +","",tmpvalue)
-      if ((tmptype == "integer-attribute") || (tmptype == "float-attribute")) {
-        tmpvalue <- as.numeric(strsplit(tmpvalue," +")[[1]])
-      }
-      values <- c(values,list(value=tmpvalue))
-    }        
-  }
-
-  names(values) <- args
-
-  dx <- values$DATASET_DIMENSIONS[1]
-  dy <- values$DATASET_DIMENSIONS[2]
-  dz <- values$DATASET_DIMENSIONS[3]
-  dt <- values$DATASET_RANK[2]
-  scale <- values$BRICK_FLOAT_FACS
-  size <- file.info(filename.brik)$size/(dx*dy*dz*dt)
-
-  if (regexpr("MSB",values$BYTEORDER_STRING[1]) != -1) {
-    endian <- "big"
-  } else {
-    endian <- "little"
-  }
-
-  if (min(abs(values$DELTA)) != 0) {
-    weights <-
-      abs(values$DELTA/min(abs(values$DELTA)))
-  } else {
-    weights <- NULL
-  }
-#  browser()
-  if (as.integer(size) == size) {
-    conbrik <- file(filename.brik,"rb")
-  # modified below by GC 12/2/2008
-  if (all(values$BRICK_TYPES==0) | all(values$BRICK_TYPES==1)) myttt<- readBin(conbrik, "int", n=dx*dy*dz*dt, size=size, signed=TRUE, endian=endian) # unsigned charater or short
-  if (all(values$BRICK_TYPES==3)) myttt<- readBin(conbrik, "numeric", n=dx*dy*dz*dt, size=size, signed=TRUE, endian=endian) # float        
-    close(conbrik)
-    dim(myttt) <- c(dx,dy,dz,dt)
-#    for (k in 1:dt) {
-#      if (scale[k] != 0) {
-#        cat("scale",k,"with",scale[k],"\n")
-#        cat(range(myttt[,,,k]),"\n")
-#        myttt[,,,k] <- scale[k] * myttt[,,,k]
-#        cat(range(myttt[,,,k]),"\n")
-#      }
-#    }
-    for (k in 1:dt) if (scale[k] != 0) myttt[,,,k] <- scale[k] * myttt[,,,k]
-
-  mask <- array(TRUE,c(dx,dy,dz))
-  mask[myttt[,,,1] < quantile(myttt[,,,1],0.75)] <- FALSE
-    z <-
-      list(ttt=myttt,format="HEAD/BRIK",delta=values$DELTA,origin=values$ORIGIN,orient=values$ORIENT_SPECIFIC,dim=c(dx,dy,dz,dt),weights=weights, header=values,mask=mask)
-#      list(ttt=writeBin(as.numeric(myttt),raw(),4),format="HEAD/BRIK",delta=values$DELTA,origin=values$ORIGIN,orient=values$ORIENT_SPECIFIC,dim=c(dx,dy,dz,dt),weights=weights, header=values,mask=mask)
-
-  } else {
-    warning("Error reading file: Could not detect size per voxel\n")
-    z <- list(ttt=NULL,format="HEAD/BRIK",delta=NULL,origin=NULL,orient=NULL,dim=NULL,weights=NULL,header=values,mask=NULL)    
-  }
-
-  class(z) <- "fmridata"
-  attr(z,"file") <- paste(filename,".HEAD/BRIK",sep="")
-  invisible(z)
-}
-
-write.AFNI <- function(filename, ttt, label, note="", origin=c(0,0,0), delta=c(4,4,4), idcode="WIAS_noid") {
-  ## TODO:
-  ## 
-  ## create object oriented way!!!!
-  
-  AFNIheaderpart <- function(type, name, value) {
-    a <- "\n"
-    a <- paste(a, "type = ", type, "\n", sep="")
-    a <- paste(a, "name = ", name, "\n", sep="")
-    if (regexpr("string",type) == 1) {
-      value <- paste("'", value, "~", sep="")
-      a <- paste(a, "count = ", nchar(value) - 1, "\n", sep ="")
-      a <- paste(a, value, "\n", sep="")
-    } else {
-      a <- paste(a, "count = ", length(value), "\n", sep ="")
-      j <- 0
-      while (j<length(value)) {
-        left <- length(value) - j
-        if (left>4) left <- 5
-        a <- paste(a, paste(value[(j+1):(j+left)],collapse="  "), "\n", sep="  ")
-        j <- j+5
-      }
-    }
-    a
-  }
-  
-  conhead <- file(paste(filename, ".HEAD", sep=""), "w")
-  writeChar(AFNIheaderpart("string-attribute","HISTORY_NOTE",note),conhead,eos=NULL)
-  writeChar(AFNIheaderpart("string-attribute","TYPESTRING","3DIM_HEAD_FUNC"),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("string-attribute","IDCODE_STRING",idcode),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("string-attribute","IDCODE_DATE",date()),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("integer-attribute","SCENE_DATA",c(0,11,1,-999,-999,-999,-999,-999)),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("integer-attribute","ORIENT_SPECIFIC",c(0,3,4)),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("float-attribute","ORIGIN",origin),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("float-attribute","DELTA",delta),conhead,eos=NULL)  
-  minmax <- function(y) {r <- NULL;for (k in 1:dim(y)[4]) {r <- c(r,min(y[,,,k]),max(y[,,,k]))}; r}
-  mm <- minmax(ttt)
-  writeChar(AFNIheaderpart("float-attribute","BRICK_STATS",mm),conhead,eos=NULL)
-  writeChar(AFNIheaderpart("integer-attribute","DATASET_RANK",c(3,dim(ttt)[4],0,0,0,0,0,0)),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("integer-attribute","DATASET_DIMENSIONS",c(dim(ttt)[1:3],0,0)),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("integer-attribute","BRICK_TYPES",rep(1,dim(ttt)[4])),conhead,eos=NULL)  
-
-  scale <- rep(0,dim(ttt)[4])
-  for (k in 1:dim(ttt)[4]) {
-    scale[k] <- max(abs(mm[2*k-1]),abs(mm[2*k]))/32767
-    ttt[,,,k] <- ttt[,,,k] / scale[k]
-  }
-
-  writeChar(AFNIheaderpart("float-attribute","BRICK_FLOAT_FACS",scale),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("string-attribute","BRICK_LABS",paste(label,collapse="~")),conhead,eos=NULL)  
-  writeChar(AFNIheaderpart("string-attribute","BYTEORDER_STRING","MSB_FIRST"),conhead,eos=NULL)  
-  close(conhead)
-
-  conbrik <- file(paste(filename, ".BRIK", sep=""), "wb")
-  dim(ttt) <- NULL
-  writeBin(as.integer(ttt), conbrik,size=2, endian="big")
-  close(conbrik)
-}
-
 
 #libLoad('tcltk')    # for graphics
 
@@ -240,8 +90,12 @@ if (masked) {maskFN <- readline("Mask file name (suffix unnecessary, e.g., mask+
 
 inDataTS <- read.AFNI(inFN)
 myNote=inDataTS$header$HISTORY_NOTE; myOrig=inDataTS$origin; myDelta=inDataTS$delta
+
+dataOrient <- system(sprintf("@GetAfniOrient %s", inFN), intern = TRUE)
+
 inDataTS <- inDataTS$ttt
 dimx <- dim(inDataTS)[1]; dimy <- dim(inDataTS)[2]; dimz <- dim(inDataTS)[3]; nT <- dim(inDataTS)[4]
+
 
 print("#++++++++++++++++++++++++++++++++++++++++++++")
 print("If there are n consecutive chunks of data, enter n-1 here.")
@@ -385,8 +239,9 @@ while (tag == 1) {
 		tag<-0
 		tmpData <- cbind(inDataTS[ii, jj, kk,], sdTS)
 		try(fm <- VAR(tmpData, p=nLags, type="none", exogen=exMatMod), tag<-1)
+      if(tag == 0) try(summary(fm), tag<-1) # in case of running out of DFs
 		if (ii<dimx) ii<-ii+1 else break
-	}	
+	} else if(ii<dimx) ii<-ii+1 else break	
 }
 
 if (tag == 0)  {
@@ -466,8 +321,8 @@ statpar <- "3drefit"
 	   statpar <- paste(statpar, " -substatpar ", 6*(ii-1)+2*jj-1, 
 	   " fitt ", summary(fm)$varresult[[1]]$df[2])
 	}
-if (outView=="tlrc") statpar <- paste(statpar, " -view tlrc -addFDR -newid ", outFN)
-if (outView=="orig") statpar <- paste(statpar, "-addFDR -newid ", outFN)
+if (outView=="tlrc") statpar <- paste(statpar, " -view tlrc -addFDR -newid -orient ", dataOrient, outFN)
+if (outView=="orig") statpar <- paste(statpar, "-addFDR -newid -orient ", dataOrient, outFN)
 system(statpar)
 
 
