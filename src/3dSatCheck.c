403eb84ca670c237bb62c9d339a3fe77fb11b571
rwcox <>
2010-02-08 17:42:59 +0000

    Cput

--- src/3dSatCheck.c
+++ src/3dSatCheck.c
@@ -6,15 +6,18 @@
 
 int main( int argc , char *argv[] )
 {
-   THD_3dim_dataset *dset ;
+   THD_3dim_dataset *dset ; int aa ;
    float val ;
 
    if( argc < 2 ){
      printf("Usage: 3dSatCheck dataset\n") ; exit(0) ;
    }
-   dset = THD_open_dataset( argv[1] ) ; CHECK_OPEN_ERROR(dset,argv[1]) ;
-   DSET_load(dset) ; CHECK_LOAD_ERROR(dset) ;
-   val = THD_saturation_check( dset , NULL ) ;
-   INFO_message("satcheck = %.3f",val) ;
+   for( aa=1 ; aa < argc ; aa++ ){
+     dset = THD_open_dataset( argv[aa] ) ; CHECK_OPEN_ERROR(dset,argv[aa]) ;
+     DSET_load(dset) ; CHECK_LOAD_ERROR(dset) ;
+     val = THD_saturation_check( dset , NULL ) ;
+     INFO_message("satcheck = %.3f",val) ;
+     DSET_delete(dset) ;
+   }
    exit(0) ;
 }
