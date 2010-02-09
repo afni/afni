b20f235043ab80351fbad971a627d66f3324679d
rwcox <>
2010-02-09 14:28:57 +0000

    Cput

--- src/3dSatCheck.c
+++ src/3dSatCheck.c
@@ -6,18 +6,26 @@
 
 int main( int argc , char *argv[] )
 {
-   THD_3dim_dataset *dset ; int aa ;
-   float val ;
+   THD_3dim_dataset *dset ; int aa,ll ; char *cpt ; float val ;
 
    if( argc < 2 ){
-     printf("Usage: 3dSatCheck dataset [...]\n") ; exit(0) ;
+     printf("Usage: 3dSatCheck dataset [...]\n"
+            "\n"
+            "Prints the 'raw' initial transient (saturation) check\n"
+            "value for each dataset on the command line.  Round this\n"
+            "number to the nearest integer to get an estimate of\n"
+            "how many non-saturated time points start a dataset.\n"
+           ) ;
+     exit(0) ;
    }
    for( aa=1 ; aa < argc ; aa++ ){
-     dset = THD_open_dataset( argv[aa] ) ; CHECK_OPEN_ERROR(dset,argv[aa]) ;
+     dset = THD_open_dataset( argv[aa] ) ; if( !ISVALID_DSET(dset) ) continue ;
      if( DSET_NVALS(dset) < 9 ) continue ;
-     DSET_load(dset) ; CHECK_LOAD_ERROR(dset) ;
+     DSET_load(dset) ; if( !DSET_LOADED(dset) ) continue ;
      val = THD_saturation_check( dset , NULL ) ;
-     INFO_message("%40.40s  satcheck = %.3f",argv[aa],val) ;
+     ll = strlen(argv[aa]) ;
+     cpt = (ll <= 50) ? argv[aa] : argv[aa]+(ll-50) ;
+     INFO_message("%-50.50s = %.3f",cpt,val) ;
      DSET_delete(dset) ;
    }
    exit(0) ;
