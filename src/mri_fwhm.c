5ab4ab73e948cc7470c543ae47a805d4e5f6ddcb
rwcox <>
2010-03-24 21:22:29 +0000

    Cput

--- src/mri_fwhm.c
+++ src/mri_fwhm.c
@@ -53,7 +53,7 @@ THD_fvec3 mriarr_estimate_FWHM_1dif( MRI_IMARR *imar, byte *mask, int unif )
        (e.g., FWHM(z) == -1.0 when nz == 1).
      - Adapted from original 3dFWHM.c by BD Ward.
      - 31 Oct 2006 -- BOO!
-     - A sister function is in SUMA_GeomComp.c if changes are made to this 
+     - A sister function is in SUMA_GeomComp.c if changes are made to this
        one, please notify the half-gloved man.
 -----------------------------------------------------------------------------*/
 
@@ -389,6 +389,9 @@ THD_fvec3 mri_estimate_FWHM_12dif_MAD( MRI_IMAGE *im , byte *mask )
   qmedmad_float( countx , dx2ar , NULL , &vx2 ) ; vx2 = vx2*vx2 ;
   qmedmad_float( county , dy2ar , NULL , &vy2 ) ; vy2 = vy2*vy2 ;
   qmedmad_float( countz , dz2ar , NULL , &vz2 ) ; vz2 = vz2*vz2 ;
+
+  free(dx1ar) ; free(dy1ar) ; free(dz1ar) ;
+  free(dx2ar) ; free(dy2ar) ; free(dz2ar) ;
 
   /*----- now estimate the FWHMs -----*/
 
