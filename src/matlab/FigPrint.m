e875c9533bdd5bcc7a61bd8fad54564d4be58221
ziad <>
2010-05-04 20:16:23 +0000

    Cput

--- src/matlab/FigPrint.m
+++ src/matlab/FigPrint.m
@@ -5,8 +5,10 @@ function [err] = FigPrint (Opt, colopt)
 %   mode 2:
 %   [err] = FigPrint (h, [col])
 %Purpose:
-%   Prints the figure
-%   
+%   Prints the figure, or save to a file
+%   Printing only works if you add the printer setup on your machine
+%   in this file. I recommend you make your own copy of this function
+%   should you choose to modify it.
 %   
 %Input Parameters:
 %   Usage mode 1:
@@ -29,8 +31,17 @@ function [err] = FigPrint (Opt, colopt)
 %               of the figure when printed on the page like
 %               [0.25 0.25 4 6] (for [left bottom width height])
 %               the units are inches
+%Output options:
+%    Paper:
+%      .prndriver: One of MATLAB's built in drivers, like '-dpsc2', 
+%                  See help print
+%      .prnname: Name of printer, something like '-P_128.231.212.205'
+%                prndriver and prnname are set automatically 
+%                for recognized machines
+%    TreeHugging:
 %      .as: 'jpg', or 'pdf' , prints electronic instead of paper version
 %      .prefix: prefix for electronic printout.
+%
 %    Usage mode 2:
 %       h is the figure handle (h can be a vector of handles)
 %       col is either 'b' for bw or 'c' for color
@@ -66,10 +77,12 @@ OptDef.PrintOpt = '';
 OptDef.PaperPosition = [];
 OptDef.as = [];
 OptDef.prefix = 'FigPrint';
+OptDef.prndriver = [];
+OptDef.prnname = [];
 
 %if nothing was passed
 if (nargin == 0),
-	Opt = OptDef;
+   Opt = OptDef;
 else
    if (~isstruct(Opt)),
       h = Opt; Opt = []; Opt.Handle = h;
@@ -77,119 +90,134 @@ else
          Opt.Color = colopt;
       end
    end
-	if (~isfield(Opt,'Handle') | isempty (Opt.Handle)),	Opt.Handle = OptDef.Handle;	end
-	if (~isfield(Opt,'Color') | isempty (Opt.Color)),	Opt.Color = OptDef.Color;	end
-	if (~isfield(Opt,'Layout') | isempty (Opt.Layout)),	Opt.Layout = OptDef.Layout;	end
-	if (~isfield(Opt,'GUI') | isempty (Opt.GUI)),	Opt.GUI = OptDef.GUI;	end
-	if (~isfield(Opt,'Prompt') | isempty (Opt.Prompt)),	Opt.Prompt = OptDef.Prompt;	end
-	if (~isfield(Opt,'PrintOpt') | isempty (Opt.PrintOpt)),	Opt.PrintOpt = OptDef.PrintOpt;	end
-	if (~isfield(Opt,'PaperPosition') | isempty (Opt.PaperPosition)),	Opt.PaperPosition = []; end
-	if (~isfield(Opt,'as') | isempty (Opt.as)),	Opt.as = []; end
-	if (~isfield(Opt,'prefix') | isempty (Opt.prefix)),	Opt.prefix = 'FigPrint'; end
+   if (~isfield(Opt,'Handle') | isempty (Opt.Handle)),   Opt.Handle = OptDef.Handle;   end
+   if (~isfield(Opt,'Color') | isempty (Opt.Color)),   Opt.Color = OptDef.Color;   end
+   if (~isfield(Opt,'Layout') | isempty (Opt.Layout)),   Opt.Layout = OptDef.Layout;   end
+   if (~isfield(Opt,'GUI') | isempty (Opt.GUI)),   Opt.GUI = OptDef.GUI;   end
+   if (~isfield(Opt,'Prompt') | isempty (Opt.Prompt)),   Opt.Prompt = OptDef.Prompt;   end
+   if (~isfield(Opt,'PrintOpt') | isempty (Opt.PrintOpt)),   Opt.PrintOpt = OptDef.PrintOpt;   end
+   if (~isfield(Opt,'PaperPosition') | isempty (Opt.PaperPosition)),   Opt.PaperPosition = []; end
+   if (~isfield(Opt,'as') | isempty (Opt.as)),   Opt.as = []; end
+   if (~isfield(Opt,'prefix') | isempty (Opt.prefix)),   
+      Opt.prefix = 'FigPrint';    
+   end
+   if (~isfield(Opt,'prnname')),   Opt.prnname = []; end
+   if (~isfield(Opt,'prndriver')),   Opt.prndriver = []; end
 end
-	
+   
 
 for (kk=1:1:length(Opt.Handle)),
    figh = Opt.Handle(kk);
    
    if (Opt.Prompt == 1),
-	   figure (figh);
-	   s = input ('Print figure (y/n) ?','s');
-	   if (isempty(s) | s ~= 'y'), err = 0; return;	end
+      figure (figh);
+      s = input ('Print figure (y/n) ?','s');
+      if (isempty(s) | s ~= 'y'), err = 0; return;   end
    elseif (Opt.Prompt == 2),
-	   figure (figh);
-	   Butt = questdlg(sprintf('Print figure %d?', figh), 'Print Figure', 'Yes', 'No', 'No');
-	   switch Butt,
-		   case 'No',
-			   err = 0; 
-			   return;
-	   end
+      figure (figh);
+      Butt = questdlg(sprintf('Print figure %d?', figh), 'Print Figure', 'Yes', 'No', 'No');
+      switch Butt,
+         case 'No',
+            err = 0; 
+            return;
+      end
    end
 
    %Now form the command line
-	   %The handle
-	   Hndl = sprintf('-f%g',figh);
+      %The handle
+      Hndl = sprintf('-f%g',figh);
 
-	   %The layout
-	   if (Opt.Layout == 'p'),
-	     orient(figh, 'portrait');
-	   elseif (Opt.Layout == 'l'),
-	     orient(figh, 'landscape');
-	   elseif (Opt.Layout == 't'),
-	     orient(figh, 'tall');
-	   else 
-	     fprintf (2,'%s: Wrong layout specification\n\a', mfilename);
-	     err = 1;
-	     return;
-	   end
+      %The layout
+      if (Opt.Layout == 'p'),
+        orient(figh, 'portrait');
+      elseif (Opt.Layout == 'l'),
+        orient(figh, 'landscape');
+      elseif (Opt.Layout == 't'),
+        orient(figh, 'tall');
+      else 
+        fprintf (2,'%s: Wrong layout specification\n\a', mfilename);
+        err = 1;
+        return;
+      end
 
-	   %The colors
-	   [e,mc] = unix('hostname -s'); ilt = find(isletter(mc)); mc = mc(ilt);
-	   switch mc,
-	   case 'rhodium',
-		   if (Opt.Color == 'b') 
-		     prntype = '-Plp';
-		     prndevice = '-dps2';
-		   elseif (Opt.Color == 'c')
-		     prntype = '-Plpc';
-		     prndevice = '-dpsc2';
-		   else 
-		     fprintf (2,'%s: Wrong printer (Color) specification\n\a', mfilename);
-		     err = 1;
-		     return;
-		   end
-	   case 'eomer',
-		   if (Opt.Color == 'b') 
-		     prntype = '-P_128.231.212.205';
-		     prndevice = '-dps2';
-		   elseif (Opt.Color == 'c')
-		     prntype = '-PPhaser_8550DT__00_00_aa_9b_d2_6b_';
-		     prndevice = '-dpsc2';
-		   else 
-		     fprintf (2,'%s: Wrong printer (Color) specification\n\a', mfilename);
-		     err = 1;
-		     return;
-		   end
-	   case 'iodine',
-		   if (Opt.Color == 'b') 
-		     prntype = '-PHP8100DN';
-		     prndevice = '-dps2';
-		   elseif (Opt.Color == 'c')
-		     prntype = '-PTek';
-		     prndevice = '-dpsc2';
-		   else 
-		     fprintf (2,'%s: Wrong printer (Color) specification\n\a', mfilename);
-		     err = 1;
-		     return;
-		   end	
-	   otherwise,
-		   fprintf (2,'%s: Machine %s not recognized.\n\a',mfilename , mc);
-		   err = 1;
-		   return
-	   end
-	   %The GUI settings
-	   if (Opt.GUI),
-		   set(figh,'PaperPositionMode','auto'); 
-		   set(figh,'InvertHardcopy','off');
-	   end
+      %The colors
+      [e,mc] = unix('hostname -s'); ilt = find(isletter(mc)); mc = mc(ilt);
+      if (isempty(Opt.prnname) | isempty(Opt.prndriver)),
+         switch mc,
+         case 'rhodium',
+            if (Opt.Color == 'b') 
+              prnname = '-Plp';
+              prndriver = '-dps2';
+            elseif (Opt.Color == 'c')
+              prnname = '-Plpc';
+              prndriver = '-dpsc2';
+            else 
+              fprintf (2,'%s: Wrong printer (Color) specification\n\a', mfilename);
+              err = 1;
+              return;
+            end
+         case 'eomer',
+            if (Opt.Color == 'b') 
+              prnname = '-P_128.231.212.205';
+              prndriver = '-dps2';
+            elseif (Opt.Color == 'c')
+              prnname = '-PPhaser_8550DT__00_00_aa_9b_d2_6b_';
+              prndriver = '-dpsc2';
+            else 
+              fprintf (2,'%s: Wrong printer (Color) specification\n\a', mfilename);
+              err = 1;
+              return;
+            end
+         case 'iodine',
+            if (Opt.Color == 'b') 
+              prnname = '-PHP8100DN';
+              prndriver = '-dps2';
+            elseif (Opt.Color == 'c')
+              prnname = '-PTek';
+              prndriver = '-dpsc2';
+            else 
+              fprintf (2,'%s: Wrong printer (Color) specification\n\a', mfilename);
+              err = 1;
+              return;
+            end   
+         otherwise,
+            fprintf (2,'%s: Machine %s not recognized.\n\a',mfilename , mc);
+            err = 1;
+            return
+         end
+      else
+         prnname = Opt.prnname;
+         prndriver = Opt.prndriver;
+      end
+      %The GUI settings
+      if (Opt.GUI),
+         set(figh,'PaperPositionMode','auto'); 
+         set(figh,'InvertHardcopy','off');
+      end
 
-	   %the size settings
-	   if (~isempty(Opt.PaperPosition)),
-		   set(figh,'PaperUnits','inches');
-		   set(figh,'PaperPosition', Opt.PaperPosition);
-	   end
+      %the size settings
+      if (~isempty(Opt.PaperPosition)),
+         set(figh,'PaperUnits','inches');
+         set(figh,'PaperPosition', Opt.PaperPosition);
+      end
 
-	   if (isempty(Opt.as)),
-            CmndLine = sprintf ('print %s %s %s %s',prndevice, Opt.PrintOpt, prntype, Hndl);
+      if (isempty(Opt.as)),
+            CmndLine = sprintf ('print %s %s %s %s',prndriver, Opt.PrintOpt, prnname, Hndl);
 
        else
-           CmndLine = sprintf ('saveas(%d, ''%s'' ,''%s'')', figh, sprintf('%s.%02d.%s', Opt.prefix, kk, Opt.as), Opt.as);
+         if (length(Opt.Handle) > 1),
+            CmndLine = sprintf ('saveas(%d, ''%s'' ,''%s'')', ...
+               figh, sprintf('%s.%02d.%s', Opt.prefix, kk, Opt.as), Opt.as);
+         else,
+            CmndLine = sprintf ('saveas(%d, ''%s'' ,''%s'')', ...
+               figh, sprintf('%s.%s', Opt.prefix, Opt.as), Opt.as);
+         end
        end
-                   fprintf (1,'%s verbose : Executing %s ...\n',FuncName,CmndLine);
+       fprintf (1,'%s verbose : Executing %s ...\n',FuncName,CmndLine);
 
-            eval([CmndLine]);
-	end
+       eval([CmndLine]);
+   end
    
-	err = 0;
-	return;
+   err = 0;
+   return;
 
