54a4a0df015d34ba28f185718db25ffb075d07d5
rwcox <>
2010-07-16 21:00:48 +0000

    Cput

--- src/3dAutoTcorrelate.c
+++ src/3dAutoTcorrelate.c
@@ -9,19 +9,91 @@
 #define PEARSON  3
 #define ETA2     4
 
+#ifdef USE_OMP
+#define pearson_func my_THD_pearson_corr
+#define etasqrd_func my_THD_eta_squared
+/*----------------------------------------------------------------*/
+/**** Include these here for potential optimization for OpenMP ****/
+/*----------------------------------------------------------------*/
+/*! Pearson correlation of x[] and y[] (x and y are NOT modified. */
+
+float my_THD_pearson_corr( int n, float *x , float *y )
+{
+   float xv,yv,xy , vv,ww , xm,ym ;
+   register int ii ;
+
+   xm = ym = 0.0f ;
+   for( ii=0 ; ii < n ; ii++ ){ xm += x[ii] ; ym += y[ii] ; }
+   xm /= n ; ym /= n ;
+   xv = yv = xy = 0.0f ;
+   for( ii=0 ; ii < n ; ii++ ){
+     vv = x[ii]-xm ; ww = y[ii]-ym ; xv += vv*vv ; yv += ww*ww ; xy += vv*ww ;
+   }
+
+   if( xv <= 0.0f || yv <= 0.0f ) return 0.0f ;
+   return xy/sqrtf(xv*yv) ;
+}
+
+/*----------------------------------------------------------------*/
+/*! eta^2 (Cohen, NeuroImage 2008)              25 Jun 2010 [rickr]
+ *
+ *  eta^2 = 1 -  SUM[ (a_i - m_i)^2 + (b_i - m_i)^2 ]
+ *               ------------------------------------
+ *               SUM[ (a_i - M  )^2 + (b_i - M  )^2 ]
+ *
+ *  where  o  a_i and b_i are the vector elements
+ *         o  m_i = (a_i + b_i)/2
+ *         o  M = mean across both vectors
+ -----------------------------------------------------------------*/
+
+float my_THD_eta_squared( int n, float *x , float *y )
+{
+   float num , denom , gm , lm, vv, ww;
+   register int ii ;
+
+   gm = 0.0f ;
+   for( ii=0 ; ii < n ; ii++ ){ gm += x[ii] + y[ii] ; }
+   gm /= (2.0f*n) ;
+
+   num = denom = 0.0f ;
+   for( ii=0 ; ii < n ; ii++ ){
+     lm = 0.5f * ( x[ii] + y[ii] ) ;
+     vv = (x[ii]-lm); ww = (y[ii]-lm); num   += ( vv*vv + ww*ww );
+     vv = (x[ii]-gm); ww = (y[ii]-gm); denom += ( vv*vv + ww*ww );
+   }
+
+   if( num < 0.0f || denom <= 0.0f || num >= denom ) return 0.0f ;
+   return (1.0f - num/denom) ;
+}
+#else
+#define pearson_func THD_pearson_corr  /* in thd_correlate.c */
+#define etasqrd_func THD_eta_squared
+#endif /* USE_OMP */
+
+/*----------------------------------------------------------------------------*/
+
+static void vstep_print(void)
+{
+   static int nn=0 ;
+   static char xx[10] = "0123456789" ;
+   fprintf(stderr , "%c" , xx[nn%10] ) ;
+   if( nn%10 == 9) fprintf(stderr,".") ;
+   nn++ ;
+}
+
+/*----------------------------------------------------------------------------*/
+
 int main( int argc , char *argv[] )
 {
    THD_3dim_dataset *xset , *cset, *mset=NULL ;
    int nopt=1 , method=PEARSON , do_autoclip=0 ;
    int nvox , nvals , ii,jj,kout , polort=1 , ix,jy,kz ;
-   MRI_IMAGE *xsim , *ysim ;
-   float     *xsar , *ysar ;
-   short     *car ;
    char * prefix = "ATcorr" ;
    byte * mmm=NULL ;
    int   nmask , abuc=1 ;
    int   all_source=0;          /* output all source voxels  25 Jun 2010 [rickr] */
    char str[32] ;
+   int *imap ; MRI_vectim *xvectim ;
 
    /*----*/
 
@@ -36,11 +108,15 @@ int main( int argc , char *argv[] )
              "Options:\n"
              "  -pearson  = Correlation is the normal Pearson (product moment)\n"
              "                correlation coefficient [default].\n"
+             "  -eta2     = Output is eta^2 measure from Cohen, NeuroImage, 2008.\n"
+             "                Note: -polort -1 is recommended with this option.\n"
+#if 0
              "  -spearman = Correlation is the Spearman (rank) correlation\n"
              "                coefficient.\n"
              "  -quadrant = Correlation is the quadrant correlation coefficient.\n"
-             "  -eta2     = Output is eta^2 measure from Cohen, NeuroImage, 2008.\n"
-             "                Note: -polort -1 is recommended with this option.\n"
+#else
+             "  -spearman AND -quadrant are disabled at this time :-(\n"
+#endif
              "\n"
              "  -polort m = Remove polynomical trend of order 'm', for m=-1..3.\n"
              "                [default is m=1; removal is by least squares].\n"
@@ -81,14 +157,19 @@ int main( int argc , char *argv[] )
              " * The output dataset is anatomical bucket type of shorts.\n"
              " * The output file might be gigantic and you might run out\n"
              "    of memory running this program.  Use at your own risk!\n"
+             "    * If you get an error message like\n"
+             "        *** malloc error for dataset sub-brick\n"
+             "      this means that the program ran out of memory when making\n"
+             "      the output dataset.\n"
              " * The program prints out an estimate of its memory usage\n"
              "    when it starts.  It also prints out a progress 'meter'\n"
-             "    of 1 dot per 10 output sub-bricks.\n"
+             "    to keep you pacified.\n"
              " * This is a quick hack for Peter Bandettini. Now pay up.\n"
+             " * OpenMP-ized for Hang Joon Jo.  Where's my soju?\n"
              "\n"
-             "-- RWCox - Jan 31 2002\n"
+             "-- RWCox - 31 Jan 2002 and 16 Jul 2010\n"
             ) ;
-      /** PRINT_AFNI_OMP_USAGE("3dAutoTcorrelate",NULL) ; **/
+      PRINT_AFNI_OMP_USAGE("3dAutoTcorrelate",NULL) ;
       PRINT_COMPILE_DATE ; exit(0) ;
    }
 
@@ -123,6 +204,7 @@ int main( int argc , char *argv[] )
          method = PEARSON ; nopt++ ; continue ;
       }
 
+#if 0
       if( strcmp(argv[nopt],"-spearman") == 0 ){
          method = SPEARMAN ; nopt++ ; continue ;
       }
@@ -130,6 +212,7 @@ int main( int argc , char *argv[] )
       if( strcmp(argv[nopt],"-quadrant") == 0 ){
          method = QUADRANT ; nopt++ ; continue ;
       }
+#endif
 
       if( strcmp(argv[nopt],"-eta2") == 0 ){
          method = ETA2 ; nopt++ ; continue ;
@@ -189,6 +272,18 @@ int main( int argc , char *argv[] )
    if( method == ETA2 && polort >= 0 )
       WARNING_message("Polort for -eta2 should probably be -1...");
 
+   /*-- create vectim from input dataset --*/
+
+   INFO_message("vectim-izing input dataset") ;
+   xvectim = THD_dset_to_vectim( xset , NULL , 0 ) ;
+   if( xvectim == NULL ) ERROR_exit("Can't create vectim?!") ;
+   DSET_unload(xset) ;
+   if( polort > 0 ){
+     for( ii=0 ; ii < nvox ; ii++ ){  /* remove polynomial trend */
+       DETREND_polort(polort,nvals,VECTIM_PTR(xvectim,ii)) ;
+     }
+   }
+
    /*-- create output dataset --*/
 
    cset = EDIT_empty_copy( xset ) ;
@@ -220,84 +315,103 @@ int main( int argc , char *argv[] )
      nb += (double)(nmask) * (double)(nvox) * sizeof(short) ;
      nb /= (1024.0*1024.0) ;
      INFO_message(
-       "++ Memory required = %.1f Mbytes for %d output sub-bricks",nb,nmask);
-     if( nb > 2000.0 )
-       ERROR_exit("Can't use more than 2000 Mbytes of memory!");
+       "Memory required = %.1f Mbytes for %d output sub-bricks",nb,nmask);
    }
 
    tross_Make_History( "3dAutoTcorrelate" , argc,argv , cset ) ;
 
-   /* loop over voxels, correlate */
+   /*--- make output dataset first ---*/
 
-   kout = 0 ;
-   for( ii=0 ; ii < nvox ; ii++ ){  /* voxel to correlate with */
+   imap = (int *)calloc(sizeof(int),nmask) ;
 
+   for( kout=ii=0 ; ii < nvox ; ii++ ){
       if( mmm != NULL && mmm[ii] == 0 ) continue ; /* skip it */
 
-      if( kout > 0 && kout%10 == 0 )
-         fprintf(stderr, (kout%1000==0) ? "!"
-                        :(kout%100 ==0) ? ":" : "." ) ;  /* progress meter */
+      EDIT_substitute_brick( cset,kout, MRI_short, NULL ) ; /* make array   */
+      EDIT_BRICK_TO_NOSTAT(cset,kout) ;                     /* stat params  */
+      EDIT_BRICK_FACTOR(cset,kout,0.0001) ;                 /* scale factor */
 
-      /* create and modify output brick */
-
-      EDIT_substitute_brick( cset,kout, MRI_short, NULL ) ; /* make array */
-      car = DSET_ARRAY(cset,kout) ;                         /* get array  */
-
-      EDIT_BRICK_TO_FICO(cset,kout,nvals,1,polort+1) ;  /* stat params  */
-      EDIT_BRICK_FACTOR(cset,kout,0.0001) ;             /* scale factor */
-
-      ix = DSET_index_to_ix(cset,ii) ;                  /* brick label  */
+      ix = DSET_index_to_ix(cset,ii) ;                      /* brick label  */
       jy = DSET_index_to_jy(cset,ii) ;
       kz = DSET_index_to_kz(cset,ii) ;
       sprintf(str,"%03d_%03d_%03d",ix,jy,kz) ;
       EDIT_BRICK_LABEL(cset,kout,str) ;
+      imap[kout] = ii ; kout++ ;
+   }
+
+   /* loop over mask voxels, correlate */
+
+#pragma omp parallel if( nmask > 999 )
+{
+   int ii,jj,kout , ithr,nthr , vstep,vii ; float *xsar , *ysar ; short *car ;
+
+AFNI_OMP_START ;
+
+#ifdef USE_OMP
+   ithr = omp_get_thread_num() ;
+   nthr = omp_get_num_threads() ;
+   if( ithr == 0 ) INFO_message("%d OpenMP threads started",nthr) ;
+#else
+   ithr = 0 ; nthr = 1 ;
+#endif
+
+   vstep = (int)( nmask / (nthr*50.0f) + 0.901f ) ; vii = 0 ;
+   if( ithr == 0 ) fprintf(stderr,"Looping:") ;
+
+#pragma omp for
+   for( kout=0 ; kout < nmask ; kout++ ){
+
+      ii = imap[kout] ;  /* we know that ii is in the mask */
+
+      if( ithr == 0 ){ vii++ ; if( vii%vstep == vstep/2 ) vstep_print(); }
 
       /* get ref time series from this voxel */
 
-      xsim = THD_extract_series(ii,xset,0) ; xsar = MRI_FLOAT_PTR(xsim) ;
-
-      DETREND_polort(polort,nvals,xsar) ;  /* remove polynomial trend */
+      xsar = VECTIM_PTR(xvectim,ii) ;
+      car  = DSET_ARRAY(cset,kout) ;
 
       for( jj=0 ; jj < nvox ; jj++ ){  /* loop over voxels, correlate w/ref */
 
-         /* skip unmasked voxels, unless we want results from all source voxels */
-         if( mmm != NULL && mmm[jj] == 0 && ! all_source ){  /* the easy case */
-            car[jj] = 0 ; continue ;
+         /* skip unmasked voxels, unless want correlations from all source voxels */
+
+         if( !all_source && mmm != NULL && mmm[jj] == 0 ){  /* the easy case */
+           car[jj] = 0 ; continue ;
          }
 
-         ysim = THD_extract_series(jj,xset,0) ; ysar = MRI_FLOAT_PTR(ysim) ;
-         DETREND_polort(polort,nvals,ysar) ;
+         ysar = VECTIM_PTR(xvectim,jj) ;
 
          switch( method ){                    /* correlate */
             default:
             case PEARSON:
-              car[jj] = rint(10000.0*THD_pearson_corr (nvals,xsar,ysar));
+              car[jj] = (short)(10000.49f*pearson_func(nvals,xsar,ysar));
             break;
+            case ETA2:
+              car[jj] = (short)(10000.49f*etasqrd_func(nvals,xsar,ysar));
+            break;
+#if 0
             case SPEARMAN:
-              car[jj] = rint(10000.0*THD_spearman_corr(nvals,xsar,ysar));
+              car[jj] = (short)(10000.49*THD_spearman_corr(nvals,xsar,ysar));
             break;
             case QUADRANT:
-              car[jj] = rint(10000.0*THD_quadrant_corr(nvals,xsar,ysar));
-            case ETA2:
-              car[jj] = rint(10000.0*THD_eta_squared(nvals,xsar,ysar));
-            break;
+              car[jj] = (short)(10000.49*THD_quadrant_corr(nvals,xsar,ysar));
+            break ;
+#endif
          }
-
-         mri_free(ysim) ;
-
       } /* end of loop over voxels in this sub-brick */
-
-      mri_free(xsim) ;
-      kout++ ;         /* move to next output brick */
 
    } /* end of loop over ref voxels */
 
+AFNI_OMP_END ;
+}
+   fprintf(stderr," Done!\n") ;
+
    /* toss the other trash */
 
-   DSET_unload(xset); if( mmm != NULL ) free(mmm);
+   free(imap); VECTIM_destroy(xvectim); if( mmm != NULL ) free(mmm);
 
    /* finito */
 
+   INFO_message("Writing output dataset to disk") ;
    DSET_write(cset) ;
    WROTE_DSET(cset) ;
    exit(0) ;
