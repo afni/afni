6e9848af32a1f3ffc24e1560163b9bdc64b86d4d
rwcox <>
2010-08-04 18:02:52 +0000

    Cput

--- src/afni_pplug_env.c
+++ src/afni_pplug_env.c
@@ -476,6 +476,11 @@ PLUGIN_interface * ENV_init(void)
                     "Max number rows in Clusterize Rpt table" ,
                     10,99,0,15 , NULL ) ;
 
+   /* 04 Aug 2010 [RWCox] */
+   ENV_add_numeric( "AFNI_CLUSTER_WAMIMAX" ,
+                    "Max clusters sent to 'whereami'" ,
+                    1,99,0,20 , NULL ) ;
+
    /* 03 Jun 2008 [RWCox] */
    ENV_add_yesno( "AFNI_IMAGE_TICK_DIV_IN_MM" ,
                   "Image tick divisions count is in mm?" ) ;
