90da1e8a26e8727539e8b433532b760d6b1d8954
rickr <>
2010-09-08 21:29:53 +0000

    Cput

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ {  8, SEP, 2010, RCR, "afni_util.py", MICRO, TYPE_ENHANCE,
+   "added wildcard construction functions",
+   NULL
+ } ,
+
  {  1, SEP, 2010, RCR, "afni_proc.py", MICRO, TYPE_MODIFY,
    "tiny changes to help output (e.g. 3dAllineate options)",
    NULL
