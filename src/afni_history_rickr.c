1c6afb91b86045ee3c93a6bcc5e46191da39c0d0
rickr <>
2010-09-10 13:00:12 +0000

    Cput

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 10, SEP, 2010, RCR, "@SUMA_Make_Spec_FS", MICRO, TYPE_BUG_FIX,
+   "removed extra endif's in case of $label_dir",
+   NULL
+ } ,
+
  {  8, SEP, 2010, RCR, "afni_util.py", MICRO, TYPE_ENHANCE,
    "added wildcard construction functions",
    NULL
