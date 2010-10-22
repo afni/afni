4acfafff601d9794daa1489d1cf684b0238ac9ad
rwcox <>
2010-10-22 13:51:15 +0000

    Cput

--- src/afni_friends.c
+++ src/afni_friends.c
@@ -140,7 +140,7 @@ typedef struct { int mon,day; char *label; } mday ;
 /*-----------------------------------------*/
 /*! max # trivia elements allowed per date */
 
-#define NTMAX 9
+#define NTMAX 31
 
 char * AFNI_get_date_trivia(void) ;
 
@@ -750,7 +750,7 @@ static mday holiday[] = {
    {DEC,31,"George Marshall's birthday"                              } ,
 
    /* cf. http://www.theonlinerecipebox.com/national-food-holidays/ */
-  
+
    {JAN, 1, "Celebrate National Soup Month" } ,
    {JAN, 2, "National Cream Puff Day" } ,
    {JAN, 3, "National Chocolate Covered Cherry Day" } ,
@@ -1121,6 +1121,46 @@ static mday holiday[] = {
 
  {0,0,NULL} } ;  /* the last element, a flag to stop searching */
 
+/*---------------- Dates of Easter thru the years ---------------*/
+
+typedef struct { int yy,mm,dd; } yymmdd ;
+
+static yymmdd EasterDate[] = {
+   {2011,4,24} , {2012,4,8}  , {2013,3,31} , {2014,4,20} , {2015,4,5}  ,
+   {2016,3,27} , {2017,4,16} , {2018,4,1}  , {2019,4,21} , {2020,4,12} ,
+   {2021,4,4}  , {2022,4,17} , {2023,4,9}  , {2024,3,31} , {2025,4,20} ,
+   {2026,4,5}  , {2027,3,28} , {2028,4,16} , {2029,4,1}  , {2030,4,21} ,
+   {2031,4,13} , {2032,3,28} , {2033,4,17} , {2034,4,9}  , {2035,3,25} ,
+   {2036,4,13} , {2037,4,5}  , {2038,4,25} , {2039,4,10} , {2040,4,1}  ,
+   {2041,4,21} , {2042,4,6}  , {2043,3,29} , {2044,4,17} , {2045,4,9}  ,
+   {2046,3,25} , {2047,4,14} , {2048,4,5}  , {2049,4,18} , {2050,4,10} ,
+   {2051,4,2}  , {2052,4,21} , {2053,4,6}  , {2054,3,29} , {2055,4,18} ,
+   {2056,4,2}  , {2057,4,22} , {2058,4,14} , {2059,3,30} , {2060,4,18} ,
+   {2061,4,10} , {2062,3,26} , {2063,4,15} , {2064,4,6}  , {2065,3,29} ,
+   {2066,4,11} , {2067,4,3}  , {2068,4,22} , {2069,4,14} , {2070,3,30} ,
+   {2071,4,19} , {2072,4,10} , {2073,3,26} , {2074,4,15} , {2075,4,7}  ,
+   {2076,4,19} , {2077,4,11} , {2078,4,3}  , {2079,4,23} , {2080,4,7}  ,
+   {2081,3,30} , {2082,4,19} , {2083,4,4}  , {2084,3,26} , {2085,4,15} ,
+   {2086,3,31} , {2087,4,20} , {2088,4,11} , {2089,4,3}  , {2090,4,16} ,
+   {2091,4,8}  , {2092,3,30} , {2093,4,12} , {2094,4,4}  , {2095,4,24} ,
+   {2096,4,15} , {2097,3,31} , {2098,4,20} , {2099,4,12} , {2100,3,28} ,
+   {2101,4,17} , {2102,4,9}  , {2103,3,25} , {2104,4,13} , {2105,4,5}  ,
+   {2106,4,18} , {2107,4,10} , {2108,4,1}  , {2109,4,21} , {2110,4,6}  ,
+   {2111,3,29} , {2112,4,17} , {2113,4,2}  , {2114,4,22} , {2115,4,14} ,
+   {2116,3,29} , {2117,4,18} , {2118,4,10} , {2119,3,26} , {2120,4,14} ,
+   {2121,4,6}  , {2122,3,29} , {2123,4,11}
+} ;
+
+#define EASTER_FIRST 2011
+#define EASTER_LAST  2123  /* if AFNI last this long -- wow! */
+
+int AFNI_is_Easter( int yy , int mm , int dd )
+{
+   if( yy < EASTER_FIRST || yy > EASTER_LAST ) return 0 ;
+   yy = yy - EASTER_FIRST ;
+   return ( mm == EasterDate[yy].mm && dd == EasterDate[yy].dd ) ;
+}
+
 /*------------------------------------------------------------------------------*/
 /*! Return one of today's date trivia string. */
 
@@ -1158,7 +1198,7 @@ char * AFNI_get_date_trivia(void)
    /* 4th Thursday in November */
 
    if( ntar < NTMAX && lt->tm_mon+1 == NOV && lt->tm_wday == 4 &&
-                       lt->tm_mday <= 28 && lt->tm_mday >= 22 )
+                       lt->tm_mday <= 28   && lt->tm_mday >= 22  )
       tar[ntar++] = "Thanksgiving (USA)" ;
 
    /* 1st Monday in October */
@@ -1171,6 +1211,11 @@ char * AFNI_get_date_trivia(void)
    if( ntar < NTMAX && lt->tm_mon+1 == NOV && lt->tm_wday == 2 &&
                        lt->tm_mday >= 2    && lt->tm_mday <= 8   )
       tar[ntar++] = "Election Day (USA)" ;
+
+   /* Easter? */
+
+   if( ntar < NTMAX && AFNI_is_Easter(lt->tm_year+1900,lt->tm_mon+1,lt->tm_mday) )
+      tar[ntar++] = "Easter (Western rite)" ;
 
    ncall++ ;
    if( ntar == 0 || (ntar < NTMAX && ncall > 3) ){  /* 04 Oct 2007 */
