1eee5b98c22b36d72d98cdcfbea858e6114d7c7f
ziad <>
2010-11-01 17:26:47 +0000

    Cadd

---
+++ src/R_scripts/Signatures.R
@@ -0,0 +1,99 @@
+#------------------------------------------------------------------
+# Functions to support 3dSignatures.R
+#------------------------------------------------------------------
+
+keylabel.labeltable <- function (key=NULL, ltfile=NULL) {
+   com <- sprintf(
+           '@MakeLabelTable -quiet_death -labeltable %s -klabel %d',
+                  ltfile, key)
+   lab <- system(com, ignore.stderr = TRUE, intern=TRUE)
+   if (length(lab) == 0) {
+      err.AFNI(paste("Failed to get key label from labeltable.\n",
+                     "Command ",com,"Failed"));
+      return(NULL);
+   }
+   return(lab);
+}
+
+labels.labeltable  <- function(ltfile=NULL) {
+   com <- paste('@MakeLabelTable -labeltable ', ltfile,
+                '-all_labels',
+                collapse = ' ');
+   labs <- system(com, ignore.stderr = TRUE, intern=TRUE)
+   if (length(labs) == 0) {
+      err.AFNI(paste("Failed to get key label from labeltable.\n",
+                     "Command ",com,"Failed"));
+      return(NULL);
+   }
+   return(labs)   
+}
+
+
+dice.vollabels  <- function(basevol=NULL,
+                        labeltable=NULL,
+                        prefix=NULL, labelvol=NULL,
+                        diceout=NULL, maskbybase = TRUE) {
+   if (is.null(diceout)) {
+      diceout <- sprintf('%s.1D', prefix)
+   }
+   if (is.null(labelvol)) {
+      err.AFNI("Need labelvol")
+      return(NULL)
+   }
+   if (maskbybase) mm <- '-mask_by_base'
+   else mm <- ''
+   
+   com <- paste('@DiceMetric -base ', basevol,
+                '-save_match -save_diff ', mm,
+                '-prefix ', prefix,
+                '-labeltable ',labeltable ,
+                '-dsets ',  labelvol,
+                '> ', diceout,
+                collapse = ' ')
+   
+   note.AFNI(com)
+   system(com, ignore.stderr = TRUE, intern=TRUE)
+   system(sprintf("echo '#Command: %s' >> %s", com, diceout));
+   dd <- read.AFNI.matrix(diceout)
+   ll <- list(command=com, coef=dd[,2])
+   names(ll$coef) <- labels.labeltable(labeltable)
+   
+   return(ll)
+}
+
+group.vollabels <- function(labelvol=NULL, pvol=NULL, 
+                            grptable=NULL, 
+                            grplabelpref=NULL, grpppref=NULL) {
+   if (!is.null(pvol)) {
+      com <- paste('@RegroupLabels -grouped_labeltable ',
+             grptable, '-labdset ',labelvol ,
+             '-grpd_labprefix ', grplabelpref, 
+             '-pdset', pvol,
+             '-grpd_pprefix', grpppref,
+             collapse = '')
+   } else {
+      com <- paste('@RegroupLabels -grouped_labeltable ',
+             grptable, '-labdset ',labelvol ,
+             '-grpd_labprefix ', grplabelpref, 
+             collapse = '')
+   }
+   note.AFNI(com)
+   mm <- system(com, ignore.stderr = FALSE, intern=TRUE)
+   note.AFNI(mm)
+   
+   if (is.null(pvol)) return(used.AFNI.prefix(grplabelpref))
+   else return(used.AFNI.prefix(grplabelpref)*used.AFNI.prefix(grpppref))
+}
+
+build.labeltable <- function(labeltable=NULL, keys=NULL) {
+   lt <- list(labels=NULL, keys=keys)
+   for (key in keys) {
+      if (is.null(lab <- keylabel.labeltable(key=key, ltfile=labeltable))) {
+         return(NULL);
+      }
+
+      lt$labels <- c(lt$labels, lab) 
+   }
+   return(lt)
+}
+
