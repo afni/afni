66830a98053757d3abd67539c410e728f9747f64
ziad <>
2011-05-26 15:35:26 +0000

    Cput

--- src/plug_crender.c
+++ src/plug_crender.c
@@ -5900,7 +5900,7 @@ ENTRY( "RCREND_func_widgets" );
                                             MCW_BB_noframe ,
                                             RCREND_see_ttatlas_CB , NULL ) ;
 
-     if( TT_retrieve_atlas() == NULL )
+     if( TT_retrieve_atlas_dset("TT_Daemon",0) == NULL )
        XtSetSensitive( wfunc_see_ttatlas_bbox->wrowcol , False ) ;
 
      XtManageChild(wrc) ;                                          /* 24 Jul 2001 */
@@ -7221,10 +7221,11 @@ ENTRY( "RCREND_overlay_ttatlas" );
 
    /* 01 Aug 2001: retrieve Atlas dataset depending on size of brick */
 #if 1
-   dseTT = TT_retrieve_atlas_nz(ovim->nz) ;
+   dseTT = TT_retrieve_atlas_dset_nz(ovim->nz) ;
                                  if( dseTT == NULL ) RET("no dataset\n") ;
 #else
-   dseTT = TT_retrieve_atlas() ; if( dseTT == NULL ) RET("no dataset\n") ;
+   dseTT = TT_retrieve_atlas_dset("TT_Daemon",0) ; 
+   if( dseTT == NULL ) RET("no dataset\n") ;
 #endif
 
    if( DSET_NVOX(dseTT) != nvox )                    RET("dataset mismatch\n");
