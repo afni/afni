0d10b8d6992137d80a43fc4761b548f6e632de08
rwcox <>
2011-08-03 20:33:30 +0000

    Cput

--- src/mri_nwarp.c
+++ src/mri_nwarp.c
@@ -1,8 +1,12 @@
 #include "mrilib.h"
 
+#ifdef USE_OMP
+#include <omp.h>
+#endif
+
 typedef struct {
   int    nx ,  ny ,  nz ;
-  float *xd , *yd , *zd ;
+  float *xd , *yd , *zd , *hv ;
   float xxoff , yyoff , zzoff ,
         xxdel , yydel , zzdel  ;
 } IndexWarp3D ;
@@ -14,19 +18,20 @@ typedef struct {
 
 IndexWarp3D * IW3D_create( int nx , int ny , int nz )
 {
-   IndexWarp3D *WW ;
+   IndexWarp3D *AA ;
 
    if( nx < 9 || ny < 9 || nz < 9 ) return NULL ;
 
-   WW = (IndexWarp3D *)malloc(sizeof(IndexWarp3D)) ;
-   WW->nx = nx ; WW->ny = ny ; WW->nz = nz ;
-   WW->xd = (float *)calloc(nx*ny*nz,sizeof(float)) ;
-   WW->yd = (float *)calloc(nx*ny*nz,sizeof(float)) ;
-   WW->zd = (float *)calloc(nx*ny*nz,sizeof(float)) ;
-   WW->xxoff = WW->yyoff = WW->zzoff = 0.0f ;
-   WW->xxdel = WW->yydel = WW->zzdel = 1.0f ;
+   AA = (IndexWarp3D *)malloc(sizeof(IndexWarp3D)) ;
+   AA->nx = nx ; AA->ny = ny ; AA->nz = nz ;
+   AA->xd = (float *)calloc(nx*ny*nz,sizeof(float)) ;
+   AA->yd = (float *)calloc(nx*ny*nz,sizeof(float)) ;
+   AA->zd = (float *)calloc(nx*ny*nz,sizeof(float)) ;
+   AA->hv = NULL ;
+   AA->xxoff = AA->yyoff = AA->zzoff = 0.0f ;
+   AA->xxdel = AA->yydel = AA->zzdel = 1.0f ;
 
-   return WW ;
+   return AA ;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -34,27 +39,75 @@ IndexWarp3D * IW3D_create( int nx , int ny , int nz )
 void IW3D_destroy( IndexWarp3D *AA )
 {
    if( AA != NULL ){
-     FREEIFNN(AA->xd) ; FREEIFNN(AA->yd) ; FREEIFNN(AA->zd) ;
-     free(AA) ;
+     FREEIFNN(AA->xd); FREEIFNN(AA->yd); FREEIFNN(AA->zd); FREEIFNN(AA->hv);
+     free(AA);
    }
    return ;
 }
 
 /*---------------------------------------------------------------------------*/
 
-IndexWarp3D * IW3D_copy( IndexWarp3D *AA )
+float IW3D_normL1( IndexWarp3D *AA , IndexWarp3D *BB )
+{
+   int qq , nxyz ; float sum , *xda,*yda,*zda ;
+
+   if( AA == NULL ){
+     if( BB == NULL ) return 0.0f ;
+     AA = BB ; BB = NULL ;
+   }
+
+   nxyz = AA->nx * AA->ny * AA->nz ;
+   xda = AA->xd ; yda = AA->yd ; zda = AA->zd ;
+
+   sum = 0.0f ;
+   if( BB == NULL ){
+     for( qq=0 ; qq < nxyz ; qq++ )
+       sum += fabsf(xda[qq])+fabsf(yda[qq])+fabsf(zda[qq]) ;
+   } else if( BB->nx == AA->nx && BB->ny == AA->ny && BB->nz == AA->nz ){
+     float *xdb=BB->xd , *ydb = BB->yd , *zdb = BB->zd ;
+     for( qq=0 ; qq < nxyz ; qq++ )
+       sum += fabsf(xda[qq]-xdb[qq])+fabsf(yda[qq]-ydb[qq])+fabsf(zda[qq]-zdb[qq]) ;
+   }
+
+   return sum ;
+}
+
+/*---------------------------------------------------------------------------*/
+
+float IW3D_normL2( IndexWarp3D *AA , IndexWarp3D *BB )
+{
+   int qq , nxyz ; float sum , *xda,*yda,*zda ;
+
+   if( AA == NULL ){
+     if( BB == NULL ) return 0.0f ;
+     AA = BB ; BB = NULL ;
+   }
+
+   nxyz = AA->nx * AA->ny * AA->nz ;
+   xda = AA->xd ; yda = AA->yd ; zda = AA->zd ;
+
+   sum = 0.0f ;
+   if( BB == NULL ){
+     for( qq=0 ; qq < nxyz ; qq++ )
+       sum += SQR(xda[qq])+SQR(yda[qq])+SQR(zda[qq]) ;
+   } else if( BB->nx == AA->nx && BB->ny == AA->ny && BB->nz == AA->nz ){
+     float *xdb=BB->xd , *ydb = BB->yd , *zdb = BB->zd ;
+     for( qq=0 ; qq < nxyz ; qq++ )
+       sum += SQR(xda[qq]-xdb[qq])+SQR(yda[qq]-ydb[qq])+SQR(zda[qq]-zdb[qq]) ;
+   }
+
+   return sqrtf(sum) ;
+}
+
+/*---------------------------------------------------------------------------*/
+
+IndexWarp3D * IW3D_empty_copy( IndexWarp3D *AA )
 {
    IndexWarp3D *BB ; int nxyz ;
 
    if( AA == NULL ) return NULL ;
 
    BB = IW3D_create( AA->nx , AA->ny , AA->nz ) ;
-
-   nxyz = AA->nx * AA->ny * AA->nz ;
-
-   memcpy( BB->xd , AA->xd , sizeof(float)*nxyz ) ;
-   memcpy( BB->yd , AA->yd , sizeof(float)*nxyz ) ;
-   memcpy( BB->zd , AA->zd , sizeof(float)*nxyz ) ;
 
    BB->xxoff = AA->xxoff ; BB->xxdel = AA->xxdel ;
    BB->yyoff = AA->yyoff ; BB->yydel = AA->yydel ;
@@ -65,11 +118,134 @@ IndexWarp3D * IW3D_copy( IndexWarp3D *AA )
 
 /*---------------------------------------------------------------------------*/
 
-#undef  CLIP
-#define CLIP(mm,nn) if(mm < 0)mm=0; else if(mm > nn)mm=nn
+IndexWarp3D * IW3D_copy( IndexWarp3D *AA , float fac )
+{
+   IndexWarp3D *BB ; int nxyz ;
+
+   if( AA == NULL ) return NULL ;
+
+   BB = IW3D_empty_copy(AA) ;  /* all zero displacements */
+
+   nxyz = AA->nx * AA->ny * AA->nz ;
+
+   if( fac == 1.0f ){
+     memcpy( BB->xd , AA->xd , sizeof(float)*nxyz ) ;
+     memcpy( BB->yd , AA->yd , sizeof(float)*nxyz ) ;
+     memcpy( BB->zd , AA->zd , sizeof(float)*nxyz ) ;
+   } else if( fac != 0.0f ){
+     int qq ;
+     for( qq=0 ; qq < nxyz ; qq++ ){
+       BB->xd[qq] = fac * AA->xd[qq] ;
+       BB->yd[qq] = fac * AA->yd[qq] ;
+       BB->zd[qq] = fac * AA->zd[qq] ;
+     }
+   }
+
+   return BB ;
+}
+
+/*----------------------------------------------------------------------------*/
+/* Volume of a hexahedron (distorted cube) given by 8 corners.
+   Looking down from the top, the bottom plane points are numbered so:
+       2 -- 3
+       |    |  and the top plane is similar (add 4 to each index),
+       0 -- 1  with point #(i+4) 'above' point #i.
+*//*--------------------------------------------------------------------------*/
+
+#undef  TRIPROD
+#define TRIPROD(ax,ay,az,bx,by,bz,cx,cy,cz) ( (ax)*((by)*(cz)-(bz)*(cy)) \
+                                             +(bx)*((cy)*(az)-(cz)*(ay)) \
+                                             +(cx)*((ay)*(bz)-(az)*(by))  )
+#undef  DA
+#undef  DB
+#undef  DC
+#define DA(p,q) (p.a-q.a)
+#define DB(p,q) (p.b-q.b)
+#define DC(p,q) (p.c-q.c)
+
+static INLINE float hexahedron_volume( float_triple x0 , float_triple x1 ,
+                                       float_triple x2 , float_triple x3 ,
+                                       float_triple x4 , float_triple x5 ,
+                                       float_triple x6 , float_triple x7  )
+{
+   float xa,ya,za , xb,yb,zb , xc,yc,zc , vol ;
+
+   xa = DA(x7,x1)+DA(x6,x0); ya = DB(x7,x1)+DB(x6,x0); za = DC(x7,x1)+DC(x6,x0);
+   xb = DA(x7,x2)          ; yb = DB(x7,x2)          ; zb = DC(x7,x2) ;
+   xc = DA(x3,x0)          ; yc = DB(x3,x0)          ; zc = DC(x3,x0) ;
+   vol = TRIPROD(xa,ya,za,xb,yb,zb,xc,yc,zc) ;
+   xa = DA(x6,x0)          ; ya = DB(x6,x0)          ; za = DC(x6,x0) ;
+   xb = DA(x7,x2)+DA(x5,x0); yb = DB(x7,x2)+DB(x5,x0); zb = DC(x7,x2)+DC(x5,x0);
+   xc = DA(x7,x4)          ; yc = DB(x7,x4)          ; zc = DC(x7,x4) ;
+   vol += TRIPROD(xa,ya,za,xb,yb,zb,xc,yc,zc) ;
+   xa = DA(x7,x1)          ; ya = DB(x7,x1)          ; za = DC(x7,x1) ;
+   xb = DA(x5,x0)          ; yb = DB(x5,x0)          ; zb = DC(x5,x0) ;
+   xc = DA(x7,x4)+DA(x3,x0); yc = DB(x7,x4)+DB(x3,x0); zc = DC(x7,x4)+DC(x3,x0);
+   vol += TRIPROD(xa,ya,za,xb,yb,zb,xc,yc,zc) ;
+   return (0.08333333f*vol) ;
+}
+#undef TRIPROD
+#undef DA
+#undef DB
+#undef DC
+
+/*---------------------------------------------------------------------------*/
 
 #undef  IJK
 #define IJK(i,j,k) ((i)+(j)*nx+(k)*nxy)
+
+#undef  C2F
+#define C2F(p,q,r,xx) ( (xx).a = (p) , (xx).b = (q) , (xx).c = (r) )
+
+#undef  D2F
+#define D2F(pqr,xx)   ( (xx).a+=xda[pqr], (xx).b+=yda[pqr], (xx).c+=zda[pqr] )
+
+void IW3D_load_hexvol( IndexWarp3D *AA )
+{
+   float *xda, *yda , *zda , *hva ;
+   int nx,ny,nz , nxy,nxyz ;
+
+   if( AA == NULL ) return ;
+
+   nx = AA->nx ; ny = AA->ny ; nz = AA->nz ; nxy = nx*ny ; nxyz = nxy*nz ;
+
+   xda = AA->xd ; yda = AA->yd ; zda = AA->zd ;
+
+   hva = AA->hv ;
+   if( hva == NULL ) hva = AA->hv = (float *)calloc(nxyz,sizeof(float)) ;
+
+ AFNI_OMP_START ;
+#pragma omp parallel if( nxyz > 33333 )
+ { float_triple x0,x1,x2,x3,x4,x5,x6,x7 ;
+   int ii,jj,kk , ip,jp,kp , ijk , qq ;
+#pragma omp for
+   ii = -1 ; jj = kk = 0 ;
+   for( qq=0 ; qq < nxyz ; qq++ ){
+     ii++; if( ii == nx ){ ii = 0; jj++; if( jj == ny ){ jj = 0; kk++; } }
+     ip = ii+1 ; jp = jj+1 ; kp = kk+1 ;
+     C2F(ii,jj,kk,x0); C2F(ip,jj,kk,x1); C2F(ii,jp,kk,x2); C2F(ip,jp,kk,x3);
+     C2F(ii,jj,kp,x4); C2F(ip,jj,kp,x5); C2F(ii,jp,kp,x6); C2F(ip,jp,kp,x7);
+     if( ip == nx ) ip-- ; if( jp == ny ) jp-- ; if( kp == nz ) kp-- ;
+     ijk = IJK(ip,jj,kk) ; D2F(ijk,x1) ;
+     ijk = IJK(ii,jp,kk) ; D2F(ijk,x2) ;
+     ijk = IJK(ip,jp,kk) ; D2F(ijk,x3) ;
+     ijk = IJK(ii,jj,kp) ; D2F(ijk,x4) ;
+     ijk = IJK(ip,jj,kp) ; D2F(ijk,x5) ;
+     ijk = IJK(ii,jp,kp) ; D2F(ijk,x6) ;
+     ijk = IJK(ip,jp,kp) ; D2F(ijk,x7) ;
+     ijk = qq            ; D2F(ijk,x0) ;
+     hva[qq] = hexahedron_volume(x0,x1,x2,x3,x4,x5,x6,x7) ;
+   }
+ }
+ AFNI_OMP_END ;
+
+ return ;
+}
+
+/*---------------------------------------------------------------------------*/
+
+#undef  CLIP
+#define CLIP(mm,nn) if(mm < 0)mm=0; else if(mm > nn)mm=nn
 
 #undef  AJK
 #define AJK(aaa,j,k) ((aaa)+(j)*nx+(k)*nxy)
@@ -341,8 +517,8 @@ IndexWarp3D * IW3D_compose( IndexWarp3D *AA , IndexWarp3D *BB , int icode )
 
 ENTRY("IW3D_compose") ;
 
-        if( AA == NULL ){ CC = IW3D_copy(BB) ; RETURN(CC) ; }
-   else if( BB == NULL ){ CC = IW3D_copy(AA) ; RETURN(CC) ; }
+        if( AA == NULL ){ CC = IW3D_copy(BB,1.0f) ; RETURN(CC) ; }
+   else if( BB == NULL ){ CC = IW3D_copy(AA,1.0f) ; RETURN(CC) ; }
 
    nx = AA->nx ; ny = AA->ny ; nz = AA->nz ; nxy = nx*ny ; nxyz = nxy*nz ;
 
@@ -354,9 +530,7 @@ ENTRY("IW3D_compose") ;
    yq = (float *)malloc(sizeof(float)*nall) ;
    zq = (float *)malloc(sizeof(float)*nall) ;
 
-   CC = IW3D_create(nx,ny,nz) ;
-   CC->xxoff = AA->xxoff ; CC->yyoff = AA->yyoff ; CC->zzoff = AA->zzoff ;
-   CC->xxdel = AA->xxdel ; CC->yydel = AA->yydel ; CC->zzdel = AA->zzdel ;
+   CC = IW3D_empty_copy(AA) ;
 
    xda = AA->xd ; yda = AA->yd ; zda = AA->zd ;
    xdc = CC->xd ; ydc = CC->yd ; zdc = CC->zd ;
@@ -367,7 +541,6 @@ ENTRY("IW3D_compose") ;
      qtop = MIN( nxyz , pp+nall ) ;  /* process points from pp to qtop-1 */
 
      for( qq=pp ; qq < qtop ; qq++ ){
-
        ii++ ;          /* get the 3D index from the 1D index */
        if( ii == nx ){ /* allowing for wraparounds at nx, ny */
          ii = 0 ; jj++ ; if( jj == ny ){ jj = 0 ; kk++ ; }
@@ -393,4 +566,143 @@ ENTRY("IW3D_compose") ;
    } /* end of loop over segments of length NPER (or less) */
 
    free(zq) ; free(yq) ; free(xq) ; RETURN(CC) ;
+}
+
+/*---------------------------------------------------------------------------*/
+/* Compute B( 2x - A(B(x)) ) = Newton step for computing Ainv(x) */
+
+static IndexWarp3D * IW3D_invert_newt( IndexWarp3D *AA, IndexWarp3D *BB, int icode )
+{
+   int nx,ny,nz,nxy,nxyz , nall , ii,jj,kk,iold,jold,kold , pp,qq,qtop ;
+   float *xda,*yda,*zda , *xq,*yq,*zq,*xr,*yr,*zr , *xdc,*ydc,*zdc , *xdb,*ydb,*zdb ;
+   IndexWarp3D *CC ;
+
+ENTRY("IW3D_invert_newt") ;
+
+   if( AA == NULL || BB == NULL ) RETURN(NULL) ;  /* stoopidd luser */
+
+   nx = AA->nx ; ny = AA->ny ; nz = AA->nz ; nxy = nx*ny ; nxyz = nxy*nz ;
+
+   if( nx != BB->nx || ny != BB->ny || nz != BB->nz ) RETURN(NULL) ;
+
+   nall = MIN(nxyz,NPER) ;
+
+   xq = (float *)malloc(sizeof(float)*nall) ;  /* workspace */
+   yq = (float *)malloc(sizeof(float)*nall) ;
+   zq = (float *)malloc(sizeof(float)*nall) ;
+   xr = (float *)malloc(sizeof(float)*nall) ;
+   yr = (float *)malloc(sizeof(float)*nall) ;
+   zr = (float *)malloc(sizeof(float)*nall) ;
+
+   CC = IW3D_empty_copy(AA) ;
+
+   xda = AA->xd ; yda = AA->yd ; zda = AA->zd ;
+   xdb = BB->xd ; ydb = BB->yd ; zdb = BB->zd ;
+   xdc = CC->xd ; ydc = CC->yd ; zdc = CC->zd ;
+
+   /* Warps are stored as displacements, so we have
+      A(x)              = x + a(x)
+      B(x)              = x + b(x),
+      A(B(x))           = x + b(x) + a(x+b(x))
+      2x - A(B(x))      = x - b(x) - a(x+b(x))
+      B( 2x - A(B(x)) ) = x - b(x) - a(x+b(x)) + b(x-b(x)-a(x+b(x))) */
+
+   ii = -1 ; jj = kk = 0 ;
+   for( pp=0 ; pp < nxyz ; pp+=nall ){  /* loop over segments */
+
+     qtop = MIN( nxyz , pp+nall ) ;  /* process points from pp to qtop-1 */
+     iold = ii ; jold = jj ; kold = kk ;
+
+     /* Compute [xq,yq,zq] = x+b(x) */
+
+     for( qq=pp ; qq < qtop ; qq++ ){
+       ii++ ;          /* get the 3D index from the 1D index */
+       if( ii == nx ){ /* allowing for wraparounds at nx, ny */
+         ii = 0 ; jj++ ; if( jj == ny ){ jj = 0 ; kk++ ; }
+       }
+       xq[qq-pp] = ii + xdb[qq] ;
+       yq[qq-pp] = jj + ydb[qq] ;
+       zq[qq-pp] = kk + zdb[qq] ;
+     }
+
+     /* Compute [xr,yr,zr] = a(x+b(x)) */
+
+     IW3D_interp( icode, nx,ny,nz , xda, yda, zda,
+                         qtop-pp  , xq , yq , zq ,
+                                    xr , yr , zr  ) ;
+
+     /* Compute [xr,yr,zr] = x - b(x) - a(x+b(x)) */
+
+     ii = iold ; jj = jold ; kk = kold ;
+     for( qq=pp ; qq < qtop ; qq++ ){
+       ii++; if( ii == nx ){ ii = 0; jj++; if( jj == ny ){ jj = 0; kk++; } }
+       xr[qq-pp] = ii - xdb[qq] - xr[qq-pp] ;
+       yr[qq-pp] = jj - ydb[qq] - yr[qq-pp] ;
+       zr[qq-pp] = kk - zdb[qq] - zr[qq-pp] ;
+     }
+
+     /* Compute [xq,yq,zq] = b(x-b(x)-a(x+b(x))) */
+
+     IW3D_interp( icode, nx,ny,nz , xdb, ydb, zdb,
+                         qtop-pp  , xr , yr , zr ,
+                                    xq , yq , zq  ) ;
+
+     /* Compute result = -b(x) - a(x+b(x)) + b(x-b(x)-a(x+b(x))) */
+
+     ii = iold ; jj = jold ; kk = kold ;
+     for( qq=pp ; qq < qtop ; qq++ ){
+       ii++; if( ii == nx ){ ii = 0; jj++; if( jj == ny ){ jj = 0; kk++; } }
+       xdc[qq] = xr[qq-pp] - ii + xq[qq-pp] ;
+       ydc[qq] = yr[qq-pp] - jj + yq[qq-pp] ;
+       zdc[qq] = zr[qq-pp] - kk + zq[qq-pp] ;
+     }
+
+   } /* end of loop over segments of length NPER (or less) */
+
+   free(zr); free(yr); free(xr); free(zq); free(yq); free(xq); RETURN(CC);
+}
+
+/*---------------------------------------------------------------------------*/
+
+IndexWarp3D * IW3D_invert( IndexWarp3D *AA , int icode )
+{
+   IndexWarp3D *BB , *CC ;
+   float normAA , normBC ;
+   int ii , jcode=MRI_LINEAR ;
+
+ENTRY("IW3D_invert") ;
+
+   if( AA == NULL ) RETURN(NULL) ;
+
+   normAA = IW3D_normL1( AA , NULL ) ;
+   if( normAA == 0.0f ){
+     BB = IW3D_empty_copy(AA) ; RETURN(BB) ;
+   }
+
+   /* initial guess at inverse */
+
+   BB = IW3D_copy( AA , -0.666f ) ;  /* how beastly */
+
+   /* take 2 Newton steps: BB -> CC -> BB */
+
+   CC = IW3D_invert_newt(AA,BB,jcode) ; IW3D_destroy(BB) ;
+   BB = IW3D_invert_newt(AA,CC,jcode) ;
+
+   for( ii=0 ; ii < 19 ; ii++ ){
+
+     normBC = IW3D_normL1( BB , CC ) ; IW3D_destroy(CC) ;
+
+     if( normBC/normAA < 0.001f ){
+       if( jcode == icode ) RETURN(BB) ;  /* converged */
+       jcode = icode ;
+     }
+
+     /* take another Newton step */
+
+     CC = BB ; BB = IW3D_invert_newt(AA,CC,jcode) ;
+   }
+
+   /* failed to converge */
+
+   IW3D_destroy(CC) ; RETURN(BB) ;
 }
