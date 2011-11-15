595be0bfd8ad22017b4fb8834dc52f37fa59b09b
rwcox <>
2011-11-15 17:52:37 +0000

    Cadd

---
+++ src/dicom_hinfo.c
@@ -0,0 +1,134 @@
+#include "mrilib.h"
+
+int main(int argc, char **argv)
+{
+   char *str ;
+   int ii, iarg=1 , dolast=0 ;
+   int ntag=0 ; char **tag=NULL ;
+
+   if( argc < 2 || strcmp(argv[1],"-help") == 0 ){
+     printf("Usage: dicom_hinfo [options] fname [...]\n"
+            "Prints selected information from the DICOM file 'fname' to stdout.\n"
+            "\n"
+            "OPTIONS:\n"
+            "--------\n"
+            " -tag aaaa,bbbb = print the specified tag.\n"
+            "                  -- multiple tags may follow the '-tag' option.\n"
+            "                  -- a tag consists of 4 hexadecimal digits,\n"
+            "                     followed by a comman, followed by 4 more\n"
+            "                     hexadecimal digits\n"
+            "\n"
+            " -namelast      = Put the filename last on each output line,\n"
+            " *OR* -last       instead of first.\n"
+            "\n"
+            "* The purpose of this program is to be used in scripts to figure out\n"
+            "  which DICOM files to process for various purposes -- see Example #2.\n"
+            "\n"
+            "* One line is output (to stdout) for each DICOM file that the program reads.\n"
+            "* Files that can't be read will be ignored.\n"
+            "* Tags that aren't found in a file will get their value printed as 'null'.\n"
+            "\n"
+            "* How do you know what hexadecimal tags you need?  You can start with using\n"
+            "  dicom_hdr on a single file to get the full list of tags (with names) and\n"
+            "  then experiment to see which tags can be used to meet your whims.\n"
+            "\n"
+            "EXAMPLES:\n"
+            "---------\n"
+            "#1: The command below prints out the acquisition start time and the number\n"
+            "    of rows for each valid DICOM file in the directories below the current one:\n"
+            "\n"
+            "      dicom_hinfo -tag 0008,0031 0028,0010 */*.dcm\n"
+            "\n"
+            "    One sample output line would be\n"
+            "\n"
+            "TASK-A/image-00102-004243.dcm 141255.882508 256\n"
+            "\n"
+            "#2: A more complicated example searches all the directories below the current one,\n"
+            "    then prints out a list of summaries of what look like unique acquisitions.\n"
+            "    This could be used to figure out what kind of data you have when someone gives\n"
+            "    you a bunch of DICOM files with no obvious structure to their filenames.\n"
+            "\n"
+            "      find . -name 'Z??' | xargs dicom_hinfo -tag 0008,0031 0028,0010 | uniq -f 1 -c\n"
+            "\n"
+            "    The output from the above example was\n"
+            "\n"
+            "   9 ./A/A/A/Z01 154116 256\n"
+            "   9 ./A/A/A/Z10 154210 256\n"
+            "  38 ./A/A/A/Z19 154245 64\n"
+            " 126 ./A/A/C/Z05 154326 256\n"
+            "6000 ./A/A/H/Z01 154854 64\n"
+            "2400 ./A/J/D/Z21 155617 64\n"
+            " 126 ./A/M/S/Z03 160228 256\n"
+            "  40 ./A/M/W/Z25 160304 64\n"
+            "  40 ./A/M/Y/Z13 160332 64\n"
+            " 126 ./A/N/A/Z01 160404 256\n"
+            " 126 ./A/N/E/Z23 160411 256\n"
+            " 126 ./A/N/J/Z19 160417 256\n"
+            "   1 ./A/N/O/Z15 161252 960\n"
+            "   1 ./A/N/O/Z16 161403 640\n"
+            "   9 ./A/N/O/Z17 150935 256\n"
+            "   9 ./A/N/P/Z00 151039 256\n"
+            "  37 ./A/N/P/Z10 151122 64\n"
+            " 120 ./A/N/Q/Z21 151203 256\n"
+            "6000 ./A/N/V/Z11 151624 64\n"
+            "2400 ./A/W/S/Z05 153010 64\n"
+            "\n"
+            "    My goal was the find the structural and FMRI collections of images mixed\n"
+            "    in with various localizers and other 'junk'.  Based on the above, it seems:\n"
+            "      * the 126 files starting with ./A/A/C/Z05 are a structural set\n"
+            "      * the 6000 files starting with ./A/A/H/Z01 are an FMRI set\n"
+            "      * the 2400 files starting with ./A/J/D/Z21 are an FMRI set\n"
+            "      * the 126 files starting with ./A/M/S/Z03 are a structural set\n"
+            "    and so on.  This information makes it possible to extract the desired files\n"
+            "    from the giant collection of un-informative filenames, create AFNI datasets\n"
+            "    (using program Dimon and its '-infile_list' option appropriately), and then\n"
+            "    look at them to make final decisions about what to keep.\n"
+            "\n"
+            "--- RWCox - Hope this helps - Nov 2011\n"
+           );
+     exit(0);
+   }
+
+   mainENTRY("dicom_hinfo main") ; machdep() ;
+
+   while( argv[iarg][0] == '-' ){
+
+     if( strcasecmp(argv[iarg],"-last") == 0 || strcasecmp(argv[iarg],"-namelast") == 0 ){
+       dolast = 1 ; iarg++ ; continue ;
+     }
+
+     if( strcasecmp(argv[iarg],"-tag") == 0 ){
+       char *ttt ;
+       if( ++iarg >= argc ) ERROR_exit("need argument after %s",argv[iarg-1]) ;
+       for( ; iarg < argc ; iarg++ ){
+         ttt = argv[iarg] ;
+         if( strlen(ttt) != 9 ) break ;
+         if( ! ( isxdigit(ttt[0]) && isxdigit(ttt[1]) &&
+                 isxdigit(ttt[2]) && isxdigit(ttt[3]) &&
+                 isxdigit(ttt[5]) && isxdigit(ttt[6]) &&
+                 isxdigit(ttt[7]) && isxdigit(ttt[8]) &&
+                 (ttt[4] == ' ' || ttt[4] == ',')     ) ) break ;
+         tag = (char **)realloc( tag , sizeof(char *)*(ntag+1) ) ;
+         tag[ntag++] = strdup(argv[iarg]) ;
+       }
+       continue ;
+     }
+
+     ERROR_exit("Unknown option: %s",argv[iarg]) ;
+   }
+
+   if( iarg >= argc )
+     ERROR_exit("No files on command line?!") ;
+
+   if( ntag == 0 )
+     WARNING_message("No tags given -- just echoing names of DICOM files") ;
+
+   for( ii=iarg ; ii < argc ; ii++ ){
+     str = mri_dicom_hdrinfo( argv[ii] , ntag , tag , dolast ) ;
+     if( str == NULL ) continue ;
+     printf("%s\n",str) ;
+     free(str) ;
+   }
+
+   exit(0) ;
+}
