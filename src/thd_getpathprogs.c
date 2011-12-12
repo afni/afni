b1749fd30c238bbb646b72eb29ead9253c9de945
ziad <>
2011-12-12 17:52:11 +0000

    Cput

--- src/thd_getpathprogs.c
+++ src/thd_getpathprogs.c
@@ -241,3 +241,95 @@ STATUS("call THD_extract_regular_files") ;
 
    RETURN( outar );
 }
+
+/*! Get all executables in directory where afni resides */
+THD_string_array * THD_get_all_afni_executables(void )
+{
+   THD_string_array *outar=NULL, *elist=NULL;
+   char *af=NULL, *etr=NULL;
+   int N_af, N_afni=strlen("afni"), iaf=0, ii=0, smode, *isrt=NULL;
+   char scomm[256]={""};
+   
+   ENTRY("THD_get_all_afni_executables");
+   
+   if (!(elist = get_elist()) ||
+       !(af = THD_find_executable("afni"))) {
+      ERROR_message("Could not find afni, we're doomed daddy!");
+      RETURN(outar);
+   }
+   
+   /* remove afni from the end to get the path */
+   N_af = strlen(af);
+   if (strcmp(af+N_af-N_afni,"afni")) {
+      ERROR_message("This should not be (%s)!", af+N_af-N_afni);
+      RETURN(outar);
+   }
+   af[strlen(af)-N_afni]='\0'; N_af = strlen(af);
+   if (af[N_af-1] != '/') {
+      af[N_af] = '/'; af[N_af+1] = '\0';
+      ++N_af;
+   }
+   /* Now get all executables under af */
+   INIT_SARR( outar );
+   for (ii=0, iaf=0; ii<elist->num ; ii++ ){
+      smode = storage_mode_from_filename(elist->ar[ii]);
+      etr = THD_trailname( elist->ar[ii] , 0 ) ; 
+      if (
+          !THD_is_directory(elist->ar[ii]) &&
+          !strncmp(af, elist->ar[ii], N_af) &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".jpg") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".a") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".c") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".h") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".xbm") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".tex") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".lib") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".so") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".la") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".txt") &&
+    !(STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".py") && !strncmp(etr,"lib_",4)) &&
+          (smode <= STORAGE_UNDEFINED || smode >= LAST_STORAGE_MODE)  &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".sumarc") &&
+          !STRING_HAS_SUFFIX_CASE(elist->ar[ii], ".afnirc")
+              )  {
+         ADDTO_SARR( outar , elist->ar[ii] ) ; ++iaf;
+         /* fprintf(stderr," %d- %s\n", iaf, etr); */
+      } else {
+         /* fprintf(stderr," skip %s (%s)\n", elist->ar[ii], af); */
+      }
+   } 
+   
+   qsort(outar->ar, outar->num, sizeof(char*), 
+      (int(*) (const void *, const void *))compare_string);
+   
+   if( SARR_NUM(outar) == 0 ) DESTROY_SARR(outar) ;
+
+   RETURN( outar );
+}
+
+int list_afni_programs(int withnum)
+{
+   int nprogs=0, ii=0;
+   char *etr=NULL, s[12];
+   THD_string_array *progs=NULL;
+   
+   if (!(progs = THD_get_all_afni_executables())) {
+      ERROR_message("Cannot get list of programs");
+      RETURN(0);
+   }
+
+   for (ii=0; ii<progs->num ; ii++ ){
+      etr = THD_trailname( progs->ar[ii] , 0 ) ;
+      if (withnum) {
+         sprintf(s,"%d", ii);
+         fprintf(stdout,"  %3s.   %s\n", s, etr);
+      } else {
+         fprintf(stdout,"%s\n", etr);
+      }
+   }
+   nprogs = progs->num;
+   
+   DESTROY_SARR(progs);
+   
+   return(nprogs);
+}
