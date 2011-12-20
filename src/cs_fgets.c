1c56f80bdcb3d01c8d2fec03525eb6086d382216
rwcox <>
2011-12-20 20:39:54 +0000

    Cadd

---
+++ src/cs_fgets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#undef  LF
+#undef  CR
+
+#define LF '\n'
+#define CR '\r'
+
+char * afni_fgets( char *buf , int nbuf , FILE *fp )
+{
+   int nin=0 , cin , qin ;
+
+   if( buf == NULL || nbuf <= 1 || fp == NULL ) return NULL ;
+
+   do{
+     cin = fgetc(fp) ;
+     if( cin == EOF ) break ;
+
+     if( cin != CR ) buf[nin++] = (char)cin ;  /* copy input to output */
+     else            buf[nin++] = (char)LF  ;  /* but convert CR to LF */
+
+     if( cin == CR || cin == LF ){  /* end of line */
+       qin = fgetc(fp) ;  /* check next character */
+                          /* if have a CR-LF or LF-CR combo, */
+                          /* skip next char, otherwise push it back */
+       if( (cin == CR && qin != LF) || (cin == LF && qin != CR) ) ungetc(qin,fp) ;
+       break ;  /* in either case, am done with loop */
+     }
+   } while( nin < nbuf-1 ) ;
+
+   buf[nin] = '\0' ;  /* terminator */
+   return buf ;
+}
