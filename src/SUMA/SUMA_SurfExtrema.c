53b047b82dc2c16269b09b764a7e4aefff50af4b
ziad <>
2012-01-26 00:38:09 +0000

    Cput

--- src/SUMA/SUMA_SurfExtrema.c
+++ src/SUMA/SUMA_SurfExtrema.c
@@ -8,38 +8,47 @@ void usage_SurfExtrema (SUMA_GENERIC_ARGV_PARSE *ps)
       s = SUMA_help_basics();
       sio  = SUMA_help_IO_Args(ps);
       printf ( "\n"
-               "Usage: A testing program for computing local statistics.\n"
+      "Usage: A program finding the local extrema in a dataset.\n"
+      "   The program finds the nodes with the highest value within an Rmm\n"
+      "   radius, and at which the gradient of the signal meets a preset\n"
+      "   threshold.\n"
+      "   By default, the program searches for maxima.\n"
+      "\n"
       " -hood R     = Neighborhood of node n consists of nodes within R \n"
       " -nbhd_rad R = distance from n as measured by the shortest \n"
       "               distance along the mesh.\n"
-      " -prefix PREFIX = Prefix of output data set.\n"
-      " -stat sss   = Compute the statistic named 'sss' on the values\n"
-      "               extracted from the region around each voxel:\n"
-      "               * mean   = average of the values\n"
-      /* "               * stdev  = standard deviation\n" 
-      "               * var    = variance (stdev*stdev)\n"
-      "               * cvar   = coefficient of variation = stdev/fabs(mean)\n"
-      "               * median = median of the values\n"
-      "               * MAD    = median absolute deviation\n"
-      "               * min    = minimum\n"
-      "               * max    = maximum\n"
-      "               * absmax = maximum of the absolute values\n"
-      "               * num    = number of the values in the region:\n"
-      "                          with the use of -mask or -automask,\n"
-      "                          the size of the region around any given\n"
-      "                          voxel will vary; this option lets you\n"
-      "                          map that size.  It may be useful if you\n"
-      "                          plan to compute a t-statistic (say) from\n"
-      "                          the mean and stdev outputs.\n"*/
-      "               * FWHM   = compute (like 3dFWHM) image smoothness\n"
-      "                          inside each voxel's neighborhood.  Results\n"
-      "                          are in 3 sub-bricks: FWHMx, FHWMy, and FWHM.\n"
-      "                          Places where an output is -1 are locations\n"
-      "                          where the FWHM value could not be computed\n"
-      "                          (e.g., outside the mask).\n"
-      "               * ALL    = all of the above, in that order\n"
-      "               More than one '-stat' option can be used.\n"
+      "               Default is 8 mm\n"
+      " -thresh TH  = Do not consider nodes with value less than TH\n"
+      "               Default is 0\n"
+      " -gthresh GTH = Do not consider nodes with gradient less than GTH.\n"
+      "                Default is 0.01\n"
+      " -gscale SCL = What scaling to apply to gradient computation.\n"
+      "               Choose from:\n"
+      "        NONE: g[n] = sum(v[n]-v[k])/Nk with k the neighbs. of n\n"
+      "        LMEAN : Divide g[n] by mean of n and its neighbors * 100\n"
+      "        GMEAN : Divide g[n] by mean of all nodes in mask * 100\n"
+      "             Default is LMEAN\n"
+      " -extype TYP = Find maxima, minima, or extrema.\n"
+      "               TYP is one of: MAX (default)\n"
+      "                              MIN \n"
+      "                              ABS \n"
+      " -prefix PREFIX = Prefix of two output data sets.\n"
+      "                  First dset is called PREFIX.grd and contains the \n"
+      "                  scaled average gradient values.\n"
+      "                  Second dset is called PREFIX.ext and contains the \n"
+      "                  nodes with maximum values. The value of a non-zero\n"
+      "                  node is its rank.\n"
+      " -table TABLE = Name of file in which to store a record of the extrema\n"
+      "                found. The header part of TABLE contains examples\n"
+      "                for easily extracting certain values from it.\n"  
       "\n"
+      " Examples:\n"
+      " ---------\n"
+      " 1-  SurfExtrema -i SUMA/std141.rh.smoothwm.asc \\\n"
+      "                 -input pb05.rh.niml.dset'[1]' \\\n"
+      "                 -gscale LMEAN \\\n"
+      "                 -prefix ex1.rh \\\n"
+      "                 -table ex1.log \n"
                " \n"
                "%s"
                "%s"
@@ -50,10 +59,10 @@ void usage_SurfExtrema (SUMA_GENERIC_ARGV_PARSE *ps)
       exit(0);
 }
 
-static int ncode=-1 , code[MAX_NCODE];
 
 
-SUMA_GENERIC_PROG_OPTIONS_STRUCT *SUMA_SurfExtrema_ParseInput(char *argv[], int argc, SUMA_GENERIC_ARGV_PARSE *ps)
+SUMA_GENERIC_PROG_OPTIONS_STRUCT *SUMA_SurfExtrema_ParseInput(
+                     char *argv[], int argc, SUMA_GENERIC_ARGV_PARSE *ps)
 {
    static char FuncName[]={"SUMA_SurfExtrema_ParseInput"}; 
    SUMA_GENERIC_PROG_OPTIONS_STRUCT *Opt=NULL;
@@ -68,7 +77,11 @@ SUMA_GENERIC_PROG_OPTIONS_STRUCT *SUMA_SurfExtrema_ParseInput(char *argv[], int 
    Opt->NodeDbg = -1;
    Opt->out_prefix = NULL;
    Opt->r = 8.0;
-   ncode = 0;
+   Opt->t = 0.0;
+   Opt->t2 = 0.01;
+   Opt->b1 = SUMA_MEAN_GRAD_SCALE;
+   Opt->b2 = SUMA_MAXIMUS;
+   Opt->s = NULL;
    kar = 1;
    brk = NOPE;
 	while (kar < argc) { /* loop accross command ine options */
@@ -104,6 +117,18 @@ SUMA_GENERIC_PROG_OPTIONS_STRUCT *SUMA_SurfExtrema_ParseInput(char *argv[], int 
          brk = YUP;
       }
       
+      if (!brk && (strcmp(argv[kar], "-table") == 0))
+      {
+         if (kar+1 >= argc)
+         {
+            fprintf (SUMA_STDERR, "need a string after -table \n");
+            exit (1);
+         }
+         
+         Opt->s = SUMA_copy_string(argv[++kar]);
+         brk = YUP;
+      }
+      
       if (!brk && (strcmp(argv[kar], "-prefix") == 0))
       {
          if (kar+1 >= argc)
@@ -112,45 +137,73 @@ SUMA_GENERIC_PROG_OPTIONS_STRUCT *SUMA_SurfExtrema_ParseInput(char *argv[], int 
             exit (1);
          }
          
-         Opt->out_prefix = SUMA_copy_string(argv[++kar]);
+         Opt->out_prefix = SUMA_RemoveDsetExtension_s(argv[++kar],
+                                                SUMA_NO_DSET_FORMAT);
          brk = YUP;
       }
       
-      if (!brk && (strcmp(argv[kar], "-stat") == 0))
+      if (!brk && (strcmp(argv[kar], "-thresh") == 0))
       {
-         char *cpt ;
          if (kar+1 >= argc)
          {
-            fprintf (SUMA_STDERR, "need an argument after -stat \n");
+            fprintf (SUMA_STDERR, "need a value after -thresh \n");
+            exit (1);
+         }
+         
+         Opt->t = (float)strtod(argv[++kar],NULL);
+         brk = YUP;
+      }
+      
+      if (!brk && (strcmp(argv[kar], "-gthresh") == 0))
+      {
+         if (kar+1 >= argc)
+         {
+            fprintf (SUMA_STDERR, "need a value after -gthresh \n");
+            exit (1);
+         }
+         
+         Opt->t2 = (float)strtod(argv[++kar],NULL);
+         brk = YUP;
+      }
+      
+      if (!brk && (strcmp(argv[kar], "-gscale") == 0))
+      {
+         if (kar+1 >= argc)
+         {
+            fprintf (SUMA_STDERR, "need a value after -gscale \n");
             exit (1);
          }
          ++kar;
-         cpt = argv[kar] ; if( *cpt == '-' ) cpt++ ;
-              if( strcasecmp(cpt,"mean")  == 0 ) code[ncode++] = NSTAT_MEAN  ;
-         else if( strcasecmp(cpt,"stdev") == 0 ) code[ncode++] = NSTAT_SIGMA ;
-         else if( strcasecmp(cpt,"var")   == 0 ) code[ncode++] = NSTAT_VAR   ;
-         else if( strcasecmp(cpt,"cvar")  == 0 ) code[ncode++] = NSTAT_CVAR  ;
-         else if( strcasecmp(cpt,"median")== 0 ) code[ncode++] = NSTAT_MEDIAN;
-         else if( strcasecmp(cpt,"MAD")   == 0 ) code[ncode++] = NSTAT_MAD   ;
-         else if( strcasecmp(cpt,"min")   == 0 ) code[ncode++] = NSTAT_MIN   ;
-         else if( strcasecmp(cpt,"max")   == 0 ) code[ncode++] = NSTAT_MAX   ;
-         else if( strcasecmp(cpt,"absmax")== 0 ) code[ncode++] = NSTAT_ABSMAX;
-         else if( strcasecmp(cpt,"num")   == 0 ) code[ncode++] = NSTAT_NUM   ;
-         else if( strcasecmp(cpt,"fwhm")  == 0 ) code[ncode++] = NSTAT_FWHMx ;
-         else if( strcasecmp(cpt,"ALL")   == 0 ){
-            code[ncode++] = NSTAT_MEAN  ; code[ncode++] = NSTAT_SIGMA ;
-            code[ncode++] = NSTAT_VAR   ; code[ncode++] = NSTAT_CVAR  ;
-            code[ncode++] = NSTAT_MEDIAN; code[ncode++] = NSTAT_MAD   ;
-            code[ncode++] = NSTAT_MIN   ; code[ncode++] = NSTAT_MAX   ;
-            code[ncode++] = NSTAT_ABSMAX; code[ncode++] = NSTAT_NUM   ;
-            code[ncode++] = NSTAT_FWHMx ; 
-         }
+         if (!strcmp(argv[kar],"NONE"))        Opt->b1=SUMA_NO_GRAD_SCALE;
+         else if (!strcmp(argv[kar],"LMEAN")) Opt->b1=SUMA_MEAN_GRAD_SCALE;
+         else if (!strcmp(argv[kar],"GMEAN"))
+                                                 Opt->b1=SUMA_GMEAN_GRAD_SCALE;
          else {
-            fprintf (SUMA_STDERR, 
-               "-stat '%s' is an unknown statistic type",argv[kar]) ;
+            SUMA_S_Errv("Bad value (%s) for option -gscale\n", argv[kar]);
+            exit (1);
          }
          brk = YUP;
       }
+      
+      if (!brk && (strcmp(argv[kar], "-extype") == 0))
+      {
+         if (kar+1 >= argc)
+         {
+            fprintf (SUMA_STDERR, "need a string after -extype \n");
+            exit (1);
+         }
+         ++kar;
+         if (!strcmp(argv[kar],"MIN"))        Opt->b2=SUMA_MINIMUS;
+         else if (!strcmp(argv[kar],"MAX")) Opt->b2=SUMA_MAXIMUS;
+         else if (!strcmp(argv[kar],"ABS")) Opt->b2=SUMA_EXTREMUS;
+         else {
+            SUMA_S_Errv("Bad value (%s) for option -gscale\n", argv[kar]);
+            exit (1);
+         }
+         brk = YUP;
+      }
+      
+      
       
       if (!brk && (strcmp(argv[kar], "-hood") == 0 || 
                    strcmp(argv[kar], "-nbhd_rad") == 0))
@@ -188,25 +241,22 @@ SUMA_GENERIC_PROG_OPTIONS_STRUCT *SUMA_SurfExtrema_ParseInput(char *argv[], int 
       SUMA_S_Errv("neighborhood radius is not set (have %f).\n", Opt->r);
 		exit (1);
    }
-   if (!ncode) {
-      SUMA_S_Note("No stat specified, doing -stat mean\n");
-      code[0] = NSTAT_MEAN;
-      ncode = 1;
-   }
+
    SUMA_RETURN(Opt);
 }
+
 
 int main (int argc,char *argv[])
 {/* Main */    
    static char FuncName[]={"SurfExtrema"}; 
    SUMA_GENERIC_PROG_OPTIONS_STRUCT *Opt;  
    SUMA_GENERIC_ARGV_PARSE *ps=NULL;
-   SUMA_DSET_FORMAT iform = SUMA_NO_DSET_FORMAT;
-   SUMA_DSET *din=NULL, *dout=NULL;
+   SUMA_DSET_FORMAT iform = SUMA_NO_DSET_FORMAT, oform = SUMA_NO_DSET_FORMAT;
+   SUMA_DSET *din=NULL, *dout=NULL, *doute=NULL;
    SUMA_SurfSpecFile *Spec = NULL;
-   int i, N_Spec, N_inmask = -1;
+   int i, N_Spec, N_inmask = -1, exists=0;
    SUMA_SurfaceObject *SO=NULL, *SOf=NULL;
-   char *ooo=NULL;
+   char *ooo=NULL, *s=NULL;
    SUMA_Boolean LocalHead = NOPE;
    
    SUMA_STANDALONE_INIT;
@@ -214,7 +264,7 @@ int main (int argc,char *argv[])
 
    /* Allocate space for DO structure */
 	SUMAg_DOv = SUMA_Alloc_DisplayObject_Struct (SUMA_MAX_DISPLAYABLE_OBJECTS);
-   ps = SUMA_Parse_IO_Args(argc, argv, "-i;-t;-spec;-m;-dset;-talk;");
+   ps = SUMA_Parse_IO_Args(argc, argv, "-i;-t;-spec;-sv;-m;-dset;-talk;");
    
    if (argc < 2) {
       usage_SurfExtrema(ps);
@@ -222,7 +272,14 @@ int main (int argc,char *argv[])
    }
    
    Opt = SUMA_SurfExtrema_ParseInput (argv, argc, ps);
-
+   
+   SUMA_CHECK_OUTPUT_SDSET_STATUS(Opt->out_prefix, 
+                                  Opt->ps->dsetname[0], oform, 
+                                  NULL, ".grd", exists);
+   SUMA_CHECK_OUTPUT_SDSET_STATUS(Opt->out_prefix, 
+                                  Opt->ps->dsetname[0], oform, 
+                                  NULL, ".ext", exists);
+                                  
    if (Opt->debug > 2) LocalHead = YUP;
    if (Opt->ps->N_dsetname != 1) {
       SUMA_S_Errv("Need one and only one dset please."
@@ -272,15 +329,31 @@ int main (int argc,char *argv[])
          exit(1);
    }
 
-   if (!(dout = SUMA_DsetAvgGradient(SO, NULL, din, Opt->nmask, 0)))  {
+   if (!(dout = SUMA_DsetAvgGradient(SO, NULL, din, Opt->nmask, 
+                                     0, Opt->b1))) {
       SUMA_S_Err("Failed in SUMA_DsetAvgGradient");
       exit(1);
    }
-   
    /* write it out */
-   ooo = SUMA_WriteDset_s(Opt->out_prefix, dout, iform, 
+   s = SUMA_OutputDsetFileStatus(Opt->out_prefix, NULL, &oform, 
+                                    NULL, ".grd", &exists); 
+   ooo = SUMA_WriteDset_s(s, dout, oform, 
                            THD_ok_overwrite(), 0);
-   SUMA_free(ooo); ooo=NULL;
+   SUMA_free(ooo); ooo=NULL; SUMA_free(s); s = NULL;
+   
+   if (!(doute = SUMA_DsetExtrema(SO, NULL, din, dout, 
+                                  Opt->r, Opt->t, Opt->t2, 
+                                  Opt->nmask, 0, Opt->b2, Opt->s))) {
+      SUMA_S_Err("Failed in SUMA_DsetAvgGradient");
+      exit(1);
+   }
+   /* write it out */
+   s = SUMA_OutputDsetFileStatus(Opt->out_prefix, NULL, &oform, 
+                                    NULL, ".ext", &exists); 
+   ooo = SUMA_WriteDset_s(s, doute, oform, 
+                           THD_ok_overwrite(), 0);
+   SUMA_free(ooo); ooo=NULL; SUMA_free(s); s = NULL;
+   
    
    if (ps) SUMA_FreeGenericArgParse(ps); ps = NULL;
    if (Opt) Opt = SUMA_Free_Generic_Prog_Options_Struct(Opt);
