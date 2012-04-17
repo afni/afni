7ddcdd6753f034877ddc8e614264db1946fde646
rwcox <>
2012-04-17 20:56:21 +0000

    Cput

--- src/3dDimRed.c
+++ src/3dDimRed.c
@@ -7,7 +7,7 @@
 /*----------------------------- macros and prototypes ------------------------*/
 
 #undef  DR_workspace
-#define DR_workspace(na,nb) (double *)malloc(sizeof(double)*(na)*(nb))
+#define DR_workspace(na,nb) (double *)malloc(sizeof(double)*(2*(na)+3)*(nb))
 
 void vec_dimred( int nt , int nv , int nfixed , float *inar , int rdim ,
                  int polort , float dt , float fbot , float ftop ,
@@ -21,17 +21,43 @@ void preproc_dimred( int nt , int nvv , float *inar ,
 
 /*----------------------------------------------------------------------------*/
 
-static char *chnkfnam = NULL ;  /* for -chunk temp storage */
-static FILE *chnkfile = NULL ;
+static char *chunk_fnam = NULL ;  /* for -chunk temp storage */
+static FILE *chunk_file = NULL ;
 
-static void DR_atexit(void) /*-- called by exit(): delete chnkfile --*/
+static void DR_atexit(void) /*-- called by exit(): delete chunk_file --*/
 {
-   if( chnkfile != NULL ){ fclose(chnkfile) ; chnkfile = NULL ; }
-   if( chnkfnam != NULL ){
-     INFO_message("Deleting -chunk file %s",chnkfnam) ;
-     remove(chnkfnam) ; chnkfnam = NULL ;
+   if( chunk_file != NULL ){ fclose(chunk_file) ; chunk_file = NULL ; }
+   if( chunk_fnam != NULL ){
+     INFO_message("Deleting -chunk file %s",chunk_fnam) ;
+     remove(chunk_fnam) ; chunk_fnam = NULL ;
    }
    return ;
+}
+
+/*----------------------------------------------------------------------------*/
+
+static char * append_to_prefix( char *pre , char *app )
+{
+   char *prA , *prB , *oot ; int lll ;
+
+   prA = strdup(pre) ; lll = strlen(prA) ;
+
+   if( STRING_HAS_SUFFIX(prA,".nii") && lll > 4 ){
+     prA[lll-4] = '\0' ;
+     prB        = strdup(".nii") ;
+   } else if( STRING_HAS_SUFFIX(prA,".nii.gz") && lll > 7 ){
+     prA[lll-7] = '\0' ;
+     prB        = strdup(".nii.gz") ;
+   } else {
+     prB        = strdup("\0") ;
+   }
+
+   oot = (char *)malloc(sizeof(char)*(lll+strlen(app)+9)) ;
+   strcpy(oot,prA) ;
+   strcat(oot,"_") ;
+   strcat(oot,app) ;
+   strcat(oot,prB) ;
+   free(prB) ; free(prA) ; return oot ;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -63,7 +89,7 @@ void DR_syntax(void)
      "                   * If 'rr' is set to 0, or is not given, then no output\n"
      "                     3D+time datasets will be produced!\n"
      "\n"
-     " -prefix pp     =  The string 'pp' is the root prefix for the output datasets.\n"
+     " -prefix pp      = The string 'pp' is the root prefix for the output datasets.\n"
      "                   The first one will get the prefix 'pp_001', etc.\n"
      "                   * If '-prefix' is not given, then the default root prefix\n"
      "                     is 'dimred'.\n"
@@ -111,8 +137,8 @@ void DR_syntax(void)
      " -chunk          = 3dDimRed can use up a LOT of memory.  This option lets\n"
      "                   you cut down on that by having it operate on the voxels\n"
      "                   in chunks.  The downside to doing this is more disk I/O.\n"
-     "                   * '-chunk' along means do 10,000 voxels at one time.\n"
-     "                   * '-chunk 1000' means to do 1,000 voxels at one time (etc).\n"
+     "                   * '-chunk' alone means do 10,000 voxels at one time.\n"
+     "                   * '-chunk 999' means to do 999 voxels at one time (etc).\n"
    ) ;
    PRINT_AFNI_OMP_USAGE("3dDimRed",NULL) ;
    PRINT_COMPILE_DATE ; exit(0) ;
@@ -125,14 +151,16 @@ int main( int argc , char *argv[] )
    int nopt=1 , verb=1 ;
    int do_despike=0 , do_vnorm=1 , do_automask=0 , do_sing=0 , polort=-1 ;
    float fbot=-666.0f, ftop=-999.9f , dt=0.0f ; int have_freq=0 ;
-   byte *mask=NULL ; int mask_nx=0,mask_ny=0,mask_nz=0,nmask=0 ;
+   byte *mask=NULL ; int mask_nx=0,mask_ny=0,mask_nz=0,nmask=0 , *imask=NULL ;
    int nx,ny,nz,nt,nvox , nfft=0 , rdim=0 , kk,ii,qq,pp , nbad=0 ;
-   int ndset=0,nim=0,nvim=0 , ndim=0 , chunk=0,ivbot,ivtop ;
+   int ndset=0,nim=0,nvim=0 , ndim=0 , chunk=0,im,imbot,imtop ;
    char *prefix="dimred" ;
    MRI_IMARR *imarc ;
    XtPointer_array *dsar ;
    THD_3dim_dataset *iset=NULL,*kset ; MRI_IMAGE *tim ;
    float *cfixv=NULL , *cvect=NULL , *csave=NULL ;
+   size_t msize , ntsiz , ndsiz ;
+   char *prefout , prefapp[32] ;
 
    if( argc < 2 || strcasecmp(argv[1],"-help") == 0 ) DR_syntax() ;
 
@@ -360,11 +388,22 @@ int main( int argc , char *argv[] )
      if( verb && nvox > 1 ) INFO_message("No mask ==> processing all %d voxels",nvox);
    }
 
+   /* make list of voxels to process, from mask */
+
+   imask = (int *)malloc(sizeof(int)*nmask) ;
+   for( qq=im=0 ; im < nvox ; im++ ){
+     if( mask[im] ) imask[qq++] = im ;
+   }
+   free(mask) ; mask = NULL ;
+
    ndim = ndset + nvim ;  /* total number of input dimensions */
    if( ndim <= rdim ){
      ERROR_message("Input dimension = %d is not bigger than output = %d",ndim,rdim) ;
      nbad++ ;
    }
+
+   ndsiz = sizeof(float)*ndim ;  /* size of singular values vector */
+   ntsiz = sizeof(float)*nt ;    /* size of time series vectors */
 
    if( nbad > 0 )
      ERROR_exit("Can't continue after above ERRORs") ;  /*** BAD USER ***/
@@ -373,17 +412,20 @@ int main( int argc , char *argv[] )
 
    if( chunk > 0 && ndset == 0 ) chunk = 0 ;
 
-   if( chunk > 0 && chunk < nvox ){
-     chnkfnam    = UNIQ_idcode() ;
-     chnkfnam[0] = 'D' ; chnkfnam[1] = 'R' ; chnkfnam[2] = 'X' ;
-     chnkfile    = fopen( chnkfnam , "w+b" ) ;
-     if( chnkfile == NULL )
-       ERROR_exit("Unable to create -chunk temporary file %s",chnkfnam) ;
-     INFO_message("Creating -chunk temporary file %s",chnkfnam) ;
+   if( chunk > 0 && chunk < nmask ){
+     chunk_fnam    = UNIQ_idcode() ;
+     chunk_fnam[0] = 'D' ; chunk_fnam[1] = 'R' ; chunk_fnam[2] = 'X' ;
+     chunk_file    = fopen( chunk_fnam , "w+b" ) ;
+     if( chunk_file == NULL )
+       ERROR_exit("Unable to create -chunk temporary file %s",chunk_fnam) ;
+     INFO_message("Creating -chunk temporary file %s",chunk_fnam) ;
      atexit(DR_atexit) ;
    }
 
-   if( chunk <= 0 || chunk > nvox ) chunk = nvox ;
+   if( chunk <= 0 || chunk > nmask ) chunk = nmask ;
+
+   if( verb )
+     INFO_message("Chunk size = %s voxels",commaized_integer_string(nmask)) ;
 
    set_svd_sort(-1) ;
 
@@ -391,37 +433,70 @@ int main( int argc , char *argv[] )
    /*--- for each voxel, rdim vectors of length nt,    ---*/
    /*--- plus the singular values, of length ndim.     ---*/
 
-   csave = (float *)malloc(sizeof(float)*chunk*(rdim*nt+ndim)) ;
+/* macro == vi-th output nt-vector for the ic-th voxel */
+
+#undef  CSUU
+#define CSUU(ic,vi) (csave+((ic)*(rdim*nt+ndim)+(vi)*nt))
+
+/* macro == output ndim-vector of singular values for the ic-th voxel */
+
+#undef  CSSV
+#define CSSV(ic)    (csave+((ic)*(rdim*nt+ndim)+rdim*nt))
+
+/* macro == time series nt-vector from the kd-th dataset, ic-th voxel */
+
+#undef  CVD
+#define CVD(kd,ic)  (cvect+(kd+(ic)*ndset)*nt)
+
+/* macro == ic-th fixed time series nt-vector */
+
+#undef  CFV
+#define CFV(ic)     (cfixv+(ic)*nt)
+
+   msize = (rdim*ntsiz+ndsiz)*chunk ;
+   csave = (float *)malloc(msize) ;
+   if( verb || csave == NULL )
+     ININFO_message("Output memory chunk = %s bytes (%s)",
+                    commaized_integer_string(msize) ,
+                    approximate_number_string((double)msize) ) ;
    if( csave == NULL )
-     ERROR_exit("Can't allocate memory for output chunk of %d voxels",chunk) ;
+     ERROR_exit("Can't allocate memory for output chunk") ;
 
    /* memory to hold dataset inputs from one chunk: ndset vectors of length nt */
 
    if( ndset > 0 ){
-     cvect = (float *)malloc(sizeof(float)*chunk*nt*ndset) ;
+     msize = ntsiz*chunk*ndset ;
+     cvect = (float *)malloc(msize) ;
+     if( verb || cvect == NULL )
+       ININFO_message("Dataset input memory chunk = %s bytes (%s)",
+                      commaized_integer_string(msize) ,
+                      approximate_number_string((double)msize) ) ;
      if( cvect == NULL )
-       ERROR_exit("Can't allocate memory for input chunk of %d voxels",chunk) ;
+       ERROR_exit("Can't allocate memory for input chunk") ;
    }
 
    /* memory for fixed vectors, and pre-process them now */
 
    if( nvim > 0 ){
      MRI_IMAGE *tim ; float *tar ;
-     cfixv = (float *)malloc(sizeof(float)*nt*nvim) ;
+     cfixv = (float *)malloc(ntsiz*nvim) ;
+     if( cfixv == NULL )
+       ERROR_exit("Can't allocate memory for fixed vectors") ;
      for( qq=kk=0 ; kk < nim ; kk++ ){
        tim = IMARR_SUBIM(imarc,kk) ; tar = MRI_FLOAT_PTR(tim) ;
-       for( pp=0 ; pp < tim->ny ; pp++,qq++ ){
-         memcpy( cfixv+qq*nt , tar+pp*nt , sizeof(float)*nt ) ;
-       }
+       for( pp=0 ; pp < tim->ny ; pp++,qq++ )
+         AAmemcpy( CFV(qq) , tar+pp*nt , ntsiz ) ;
      }
+     DESTROY_IMARR(imarc) ;
      preproc_dimred( nt , nvim , cfixv ,
                      polort , dt , fbot , ftop , do_despike , do_vnorm ) ;
    }
 
    /********** loop over chunks **********/
 
-   for( ivbot=0 ; ivbot < nvox ; ivbot += nchunk ){
-     ivtop = ivbot + chunk ; if( ivtop > nvox ) ivtop = nvox ;
+   for( imbot=0 ; imbot < nmask ; imbot += chunk ){
+
+     imtop = imbot + chunk ; if( imtop > nmask ) imtop = nmask ;
 
      /* extract dataset data into cvect */
 
@@ -430,50 +505,158 @@ int main( int argc , char *argv[] )
        DSET_load(kset) ;
        if( !DSET_LOADED(kset) )
          ERROR_exit("Can't load data from dataset %s",DSET_BRIKNAME(kset)) ;
-       for( ii=ivbot ; ii < ivtop ; ii++ )
-         THD_extract_array( ii , kset , 0 , cvect+(kk+(ii-ivbot)*ndset)*nt ) ;
+       for( ii=imbot ; ii < imtop ; ii++ )
+         THD_extract_array( imask[ii] , kset , 0 , CVD(kk,ii-imbot) ) ;
        DSET_unload(kset) ;
      }
 
  AFNI_OMP_START ;
-#pragma omp parallel if( nvox > 66 )
-     { float *tsar , *umat , *sval ; double *ws ; int iv ;
-       tsar = (float *)malloc(sizeof(float)*nt*ndim) ;  /* input vectors */
-       umat = (float *)malloc(sizeof(float)*nt*rdim) ;  /* output vectors */
-       sval = (float *)malloc(sizeof(float)*ndim) ;     /* output singular values */
+#pragma omp parallel if( imtop-imbot > 66 )
+     { float *tsar , *umat , *sval ; double *ws ; int iv,kd ;
+       tsar = (float *)malloc(ntsiz*ndim) ;               /* input vectors */
+       umat = (rdim == 0) ? NULL
+                          : (float *)malloc(ntsiz*rdim) ; /* output vectors */
+       sval = (float *)malloc(ndsiz) ;                    /* output sing vals */
        ws   = DR_workspace(nt,ndim) ;
 #pragma omp for
-       for( iv=ivbot ; iv < ivtop ; iv++ ){
+       for( iv=imbot ; iv < imtop ; iv++ ){
          /* copy fixed data into tsar */
          if( cfixv != NULL ){
+           AAmemcpy( tsar , cfixv , ntsiz*nvim ) ;
          }
          /* copy dataset data into tsar */
          if( cvect != NULL ){
+           for( kd=0 ; kd < ndset ; kd++ )
+             AAmemcpy( tsar+(nvim+kd)*ntsiz , CVD(kd,iv-imbot) , ntsiz ) ;
          }
          /* process it into umat and sval */
          vec_dimred( nt , ndim , nvim , tsar , rdim ,
                      polort , dt , fbot , ftop , do_despike , do_vnorm ,
                      ws , umat , sval ) ;
          /* copy umat and sval into csave */
-       }
-       free(ws) ; free(sval) ; free(umat) ; free(tsar) ;
-     } /* end of parallel-ized loop over voxels */
+         if( umat != NULL )
+           AAmemcpy( CSUU(iv-imbot,0) , umat , ntsiz*rdim ) ;
+         AAmemcpy( CSSV(iv-imbot) , sval , ndsiz ) ;
+       } /* end of parallel-ized loop over voxels */
+       free(ws) ; free(sval) ; free(tsar) ; if( umat != NULL ) free(umat) ;
+     } /* end of parallel-ized code */
      AFNI_OMP_END ;
 
      /* if using temporary file, write csave to disk */
 
-     if( chnkfile != NULL ){
+     if( chunk_file != NULL ){
+       msize = fwrite( csave, (rdim*ntsiz+ndsiz), imtop-imbot, chunk_file ) ;
+       if( msize < imtop-imbot )
+         ERROR_exit("Failure to write to -chunk file %s -- is disk full?",
+                    chunk_fnam ) ;
      }
 
    } /* end of loop over chunks of voxels */
 
-   /* create the output datasets */
+   if( cvect != NULL ) free(cvect) ;
+   if( cfixv != NULL ) free(cfixv) ;
 
-   /* populate the output datasets from csave
-      (if necessary, re-loaded from disk in chunks) */
+   if( ndset > 0 && nvox > 1 ){ /* create output datasets for vectors */
 
-   if( chnkfile != NULL ) rewind(chnkfile) ;
+     for( kk=0 ; kk < rdim ; kk++ ){
 
+       /* create the empty dataset */
+
+       sprintf(prefapp,"%03d",kk+1) ;
+       prefout = append_to_prefix( prefix , prefapp ) ;
+       iset = (THD_3dim_dataset *)XTARR_XT(dsar,0) ;
+       kset = EDIT_empty_copy(iset) ;
+       EDIT_dset_items( kset , ADN_prefix , prefout , NULL ) ;
+       tross_Copy_History( iset , kset ) ;
+       tross_Make_History( "3dDimRed" , argc,argv , kset ) ;
+       free(prefout) ;
+
+       /* put in the sub-bricks */
+       for( pp=0 ; pp < nt ; pp++ )
+         EDIT_substitute_brick( kset , pp , MRI_float , NULL ) ;
+
+       if( chunk_file != NULL ) rewind(chunk_file) ;
+
+       /* loop over chunks, fill dataset time series */
+
+       for( imbot=0 ; imbot < nmask ; imbot += chunk ){
+
+         imtop = imbot + chunk ; if( imtop > nmask ) imtop = nmask ;
+
+         if( chunk_file != NULL ){  /* get data back from disk */
+           msize = fread( csave, (rdim*ntsiz+ndsiz), imtop-imbot, chunk_file ) ;
+           if( msize < imtop-imbot )  /* should never happen */
+             ERROR_exit("Failure to read from -chunk file %s :-(",chunk_fnam) ;
+         }
+
+         for( im=imbot ; im < imtop ; im++ )  /* shove it into the time series */
+           THD_insert_series( imask[im], kset, nt, MRI_float, CSUU(im-imbot,kk),0 ) ;
+       }
+
+       DSET_write(kset) ; WROTE_DSET(kset) ; DSET_delete(kset) ;
+     }
+
+   } else if( rdim > 0 ){  /* create a 1D output file */
+
+     MRI_IMAGE *qim ; float *qar ; char *prefout ;
+
+     qim = mri_new(nt,rdim,MRI_float) ; qar = MRI_FLOAT_PTR(qim) ;
+     AAmemcpy( qar , CSUU(0,0) , ntsiz*rdim ) ;
+     prefout = append_to_prefix( prefix , "_vect.1D" ) ;
+     mri_write_1D(prefout,qim) ; mri_free(qim) ; free(prefout) ;
+
+   } /* end of writing out vectors */
+
+   /* write the singular values dataset? */
+
+   if( do_sing ){
+     if( ndset > 0 && nvox > 1 ){
+       prefout = append_to_prefix( prefix , "sing" ) ;
+       iset = (THD_3dim_dataset *)XTARR_XT(dsar,0) ;
+       kset = EDIT_empty_copy(iset) ;
+       EDIT_dset_items( kset ,
+                          ADN_prefix , prefout ,
+                          ADN_nvals  , ndim    ,
+                        NULL ) ;
+       tross_Copy_History( iset , kset ) ;
+       tross_Make_History( "3dDimRed" , argc,argv , kset ) ;
+       free(prefout) ;
+
+       for( pp=0 ; pp < ndim ; pp++ )
+         EDIT_substitute_brick( kset , pp , MRI_float , NULL ) ;
+
+       if( chunk_file != NULL ) rewind(chunk_file) ;
+
+       for( imbot=0 ; imbot < nmask ; imbot += chunk ){
+
+         imtop = imbot + chunk ; if( imtop > nmask ) imtop = nmask ;
+
+         if( chunk_file != NULL ){
+           msize = fread( csave, (rdim*ntsiz+ndsiz), imtop-imbot, chunk_file ) ;
+           if( msize < imtop-imbot )  /* should never happen */
+             ERROR_exit("Failure to read from -chunk file %s :-(",chunk_fnam) ;
+         }
+
+         for( im=imbot ; im < imtop ; im++ )
+           THD_insert_series( imask[im], kset, ndim, MRI_float, CSSV(im-imbot),0 ) ;
+
+       }
+
+       DSET_write(kset) ; WROTE_DSET(kset) ; DSET_delete(kset) ;
+
+     } else {  /* write a 1D file with the singular values */
+
+       MRI_IMAGE *qim ; float *qar ; char *prefout ;
+
+       qim = mri_new(ndim,1,MRI_float) ; qar = MRI_FLOAT_PTR(qim) ;
+       AAmemcpy( qar , CSSV(0) , ndsiz ) ;
+       prefout = append_to_prefix( prefix , "_sing.1D" ) ;
+       mri_write_1D(prefout,qim) ; mri_free(qim) ; free(prefout) ;
+     }
+
+   } /* end of dosing */
+
+   free(csave) ;
    exit(0) ;
 }
 
@@ -576,9 +759,9 @@ void vec_dimred( int nt , int nv , int nfixed , float *inar , int rdim ,
 
    wss = ws ; if( wss == NULL ) wss = DR_workspace(nt,nv) ;
 
-   amat = wss ;
-   umat = amat + nt*nv ;
-   sval = umat + nt*nv ;
+   amat = wss ;          /* ntv long */
+   umat = amat + ntv ;   /* ntv long */
+   sval = umat + ntv ;   /* nv  long */
 
    for( ii=0 ; ii < ntv ; ii++ ) amat[ii] = (double)inar[ii] ;
 
