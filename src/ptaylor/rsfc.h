0c2a6a824ff5e693af40cf2fb9d74955c1b7d700
ziad <>
2012-07-18 20:58:59 +0000

    Cadd

---
+++ src/ptaylor/rsfc.h
@@ -0,0 +1,16 @@
+#ifndef _RSFC_HEADER_
+#define _RSFC_HEADER_
+
+#define EPS_V  (0.001)
+
+
+
+
+
+float ReHoIt(float ****RANKS, int ***TIED, int *iam, int *DIM, 
+				 int ***MASK,int HOOD, int *realHOOD);
+
+
+
+
+#endif /* _RSFC_HEADER_ */
