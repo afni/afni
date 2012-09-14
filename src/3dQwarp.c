f137926d5e8f323ff299582ff8e1c608c57f5292
rwcox <>
2012-09-14 20:42:03 +0000

    Cadd

---
+++ src/3dQwarp.c
@@ -0,0 +1,249 @@
+#include "mrilib.h"
+#include "r_new_resam_dset.h"
+
+#ifdef USE_OMP
+#include <omp.h>
+#include "mri_genalign.c"
+#include "mri_genalign_util.c"
+#include "mri_nwarp.c"
+#endif
+
+static int auto_weight    = 2 ;
+static float auto_wclip   = 0.0f ;
+static float auto_wpow    = 1.0f ;
+static int auto_dilation  = 5 ;
+static float wt_medsmooth = 2.25f ;
+static float wt_gausmooth = 4.50f ;
+
+/*---------------------------------------------------------------------------*/
+/*! Turn an input image into a weighting factor (for -autoweight).
+    If acod == 2, then make a binary mask at the end.
+    If acod == 3, then make a boxed binary mask at the end.
+-----------------------------------------------------------------------------*/
+
+MRI_IMAGE * mri_weightize( MRI_IMAGE *im, int acod, int ndil, float aclip, float apow )
+{
+   float *wf,clip,clip2 ;
+   int xfade,yfade,zfade , nx,ny,nz,nxy,nxyz , ii,jj,kk,ff ;
+   byte *mmm ;
+   MRI_IMAGE *qim , *wim ;
+
+   /*-- copy input image --*/
+
+   qim = mri_to_float(im) ; wf = MRI_FLOAT_PTR(qim) ;
+   nx = qim->nx; ny = qim->ny; nz = qim->nz; nxy = nx*ny; nxyz = nxy*nz;
+   for( ii=0 ; ii < nxyz ; ii++ ) wf[ii] = fabsf(wf[ii]) ;
+
+   /*-- zero out along the edges --*/
+#undef  WW
+#define WW(i,j,k) wf[(i)+(j)*nx+(k)*nxy]
+
+   xfade = (int)(0.05*qim->nx+3.0) ;                 /* number of points */
+   yfade = (int)(0.05*qim->ny+3.0) ;                 /* along each face */
+   zfade = (int)(0.05*qim->nz+3.0) ;                 /* to set to zero */
+   if( 5*xfade >= qim->nx ) xfade = (qim->nx-1)/5 ;
+   if( 5*yfade >= qim->ny ) yfade = (qim->ny-1)/5 ;
+   if( 5*zfade >= qim->nz ) zfade = (qim->nz-1)/5 ;
+   if( Hverb )
+     ININFO_message("Weightize: xfade=%d yfade=%d zfade=%d",xfade,yfade,zfade);
+   for( jj=0 ; jj < ny ; jj++ )
+    for( ii=0 ; ii < nx ; ii++ )
+     for( ff=0 ; ff < zfade ; ff++ ) WW(ii,jj,ff) = WW(ii,jj,nz-1-ff) = 0.0f;
+   for( kk=0 ; kk < nz ; kk++ )
+    for( jj=0 ; jj < ny ; jj++ )
+     for( ff=0 ; ff < xfade ; ff++ ) WW(ff,jj,kk) = WW(nx-1-ff,jj,kk) = 0.0f;
+   for( kk=0 ; kk < nz ; kk++ )
+    for( ii=0 ; ii < nx ; ii++ )
+     for( ff=0 ; ff < yfade ; ff++ ) WW(ii,ff,kk) = WW(ii,ny-1-ff,kk) = 0.0f;
+
+   if( aclip > 0.0f ){  /* 31 Jul 2007 */
+     int nleft , nclip ;
+     for( nclip=nleft=ii=0 ; ii < nxyz ; ii++ ){
+       if( wf[ii] > 0.0f ){
+         if( wf[ii] < aclip ){ nclip++; wf[ii] = 0.0f; } else nleft++ ;
+       }
+     }
+     if( Hverb ) ININFO_message("Weightize: user clip=%g #clipped=%d #left=%d",
+                                   aclip,nclip,nleft) ;
+   }
+
+   /*-- squash super-large values down to reasonability --*/
+
+   clip = 3.0f * THD_cliplevel(qim,0.5f) ;
+   if( Hverb ) ININFO_message("Weightize: (unblurred) top clip=%g",clip) ;
+   for( ii=0 ; ii < nxyz ; ii++ ) if( wf[ii] > clip ) wf[ii] = clip ;
+
+   /*-- blur a little: median then Gaussian;
+          the idea is that the median filter smashes localized spikes,
+          then the Gaussian filter does a litte extra general smoothing. --*/
+
+   mmm = (byte *)malloc( sizeof(byte)*nxyz ) ;
+   for( ii=0 ; ii < nxyz ; ii++ ) mmm[ii] = (wf[ii] > 0.0f) ; /* mask */
+   if( wt_medsmooth > 0.0f ){
+     wim = mri_medianfilter( qim , wt_medsmooth , mmm , 0 ) ; mri_free(qim) ;
+   } else {
+     wim = qim ;
+   }
+   wf = MRI_FLOAT_PTR(wim) ;
+   if( wt_gausmooth > 0.0f )
+     FIR_blur_volume_3d( wim->nx , wim->ny , wim->nz ,
+                         1.0f , 1.0f , 1.0f ,  wf ,
+                         wt_gausmooth , wt_gausmooth , wt_gausmooth ) ;
+
+   /*-- clip off small values, and
+        keep only the largest cluster of supra threshold voxels --*/
+
+   clip  = 0.05f * mri_max(wim) ;
+   clip2 = 0.33f * THD_cliplevel(wim,0.33f) ;
+   clip  = MAX(clip,clip2) ;
+   if( Hverb ) ININFO_message("Weightize: (blurred) bot clip=%g",clip) ;
+   for( ii=0 ; ii < nxyz ; ii++ ) mmm[ii] = (wf[ii] >= clip) ;
+   THD_mask_clust( nx,ny,nz, mmm ) ;
+   THD_mask_erode( nx,ny,nz, mmm, 1 ) ;  /* cf. thd_automask.c */
+   THD_mask_clust( nx,ny,nz, mmm ) ;
+   for( ii=0 ; ii < nxyz ; ii++ ) if( !mmm[ii] ) wf[ii] = 0.0f ;
+   free((void *)mmm) ;
+
+   /*-- convert to 0..1 range [10 Sep 2007] --*/
+
+   clip = 0.0f ;
+   for( ii=0 ; ii < nxyz ; ii++ ) if( wf[ii] > clip ) clip = wf[ii] ;
+   if( clip == 0.0f )
+     ERROR_exit("Can't compute autoweight: max value seen as 0") ;
+   clip = 1.0f / clip ;
+   for( ii=0 ; ii < nxyz ; ii++ ) wf[ii] *= clip ;
+
+   /*-- power? --*/
+
+   if( apow > 0.0f && apow != 1.0f ){
+     if( Hverb ) ININFO_message("Weightize: raising to %g power",apow) ;
+     for( ii=0 ; ii < nxyz ; ii++ )
+       if( wf[ii] > 0.0f ) wf[ii] = powf( wf[ii] , apow ) ;
+   }
+
+   /*-- binarize (acod==2)?  boxize (acod==3)? --*/
+
+#undef  BPAD
+#define BPAD 4
+   if( acod == 2 || acod == 3 ){  /* binary weight: mask=2 or maskbox=3 */
+     if( Hverb ) ININFO_message("Weightize: binarizing") ;
+     for( ii=0 ; ii < nxyz ; ii++ ) if( wf[ii] != 0.0f ) wf[ii] = 1.0f ;
+     if( ndil > 0 ){  /* 01 Mar 2007: dilation */
+       byte *mmm = (byte *)malloc(sizeof(byte)*nxyz) ;
+       if( Hverb ) ININFO_message("Weightize: dilating") ;
+       for( ii=0 ; ii < nxyz ; ii++ ) mmm[ii] = (wf[ii] != 0.0f) ;
+       for( ii=0 ; ii < ndil ; ii++ ){
+         THD_mask_dilate     ( nx,ny,nz , mmm , 3 ) ;
+         THD_mask_fillin_once( nx,ny,nz , mmm , 2 ) ;
+       }
+       for( ii=0 ; ii < nxyz ; ii++ ) wf[ii] = (float)mmm[ii] ;
+       free(mmm) ;
+     }
+     if( acod == 3 ){  /* boxize */
+       int xm,xp , ym,yp , zm,zp ;
+       MRI_autobbox_clust(0) ;
+       MRI_autobbox( wim , &xm,&xp , &ym,&yp , &zm,&zp ) ;
+       xm -= BPAD ; if( xm < 1    ) xm = 1 ;
+       ym -= BPAD ; if( ym < 1    ) ym = 1 ;
+       zm -= BPAD ; if( zm < 1    ) zm = 1 ;
+       xp += BPAD ; if( xp > nx-2 ) xp = nx-2 ;
+       yp += BPAD ; if( yp > ny-2 ) yp = ny-2 ;
+       zp += BPAD ; if( zp > nz-2 ) zp = nz-2 ;
+       if( Hverb )
+         ININFO_message("Weightize: box=%d..%d X %d..%d X %d..%d = %d voxels",
+                        xm,xp , ym,yp , zm,zp , (xp-xm+1)*(yp-ym+1)*(zp-zm+1) ) ;
+       for( kk=zm ; kk <= zp ; kk++ )
+        for( jj=ym ; jj <= yp ; jj++ )
+         for( ii=xm ; ii <= xp ; ii++ ) WW(ii,jj,kk) = 1.0f ;
+     }
+   }
+
+   return wim ;
+}
+
+/*---------------------------------------------------------------------------*/
+
+static THD_3dim_dataset *qset = NULL ;
+
+void Qsaver(char *lab, MRI_IMAGE *im)
+{
+   static int first=1 ;
+
+   if( im == NULL || qset == NULL ) return ;
+
+   if( first ){
+     EDIT_substitute_brick( qset, 0, MRI_float,  MRI_FLOAT_PTR(im) ) ; first = 0 ;
+   } else {
+     EDIT_add_brick( qset, MRI_float, 0.0f, MRI_FLOAT_PTR(im) ) ;
+   }
+   if( lab != NULL && *lab != '\0' )
+     EDIT_BRICK_LABEL(qset,DSET_NVALS(qset)-1,lab) ;
+
+   mri_clear_data_pointer(im) ; return ;
+}
+
+/*---------------------------------------------------------------------------*/
+
+int main( int argc , char *argv[] )
+{
+   THD_3dim_dataset *bset , *sset , *oset ;
+   MRI_IMAGE *bim , *wbim , *sim , *oim ;
+
+   if( argc < 2 || strcasecmp(argv[1],"-help") == 0 ){
+     printf("Usage: 3dQwarp base source\n") ; exit(0) ;
+   }
+
+   mainENTRY("3dQwarp") ;
+
+   bset = THD_open_dataset(argv[1]) ; if( bset == NULL ) ERROR_exit("Can't open bset") ;
+   sset = THD_open_dataset(argv[2]) ; if( sset == NULL ) ERROR_exit("Can't open sset") ;
+   if( !EQUIV_GRIDS(bset,sset) ) ERROR_exit("Grid mismatch") ;
+
+   DSET_load(bset) ; CHECK_LOAD_ERROR(bset) ;
+   bim = THD_extract_float_brick(0,bset) ; DSET_unload(bset) ;
+
+   DSET_load(sset) ; CHECK_LOAD_ERROR(sset) ;
+   sim = THD_extract_float_brick(0,sset) ; DSET_unload(sset) ;
+
+#if 1
+   qset = EDIT_empty_copy(bset) ;
+   EDIT_dset_items( qset ,
+                      ADN_prefix    , "QwarpSAVE" ,
+                      ADN_nvals     , 1 ,
+                      ADN_ntt       , 0 ,
+                      ADN_datum_all , MRI_float ,
+                    ADN_none ) ;
+   EDIT_BRICK_FACTOR(qset,0,0.0) ;
+   iterfun = Qsaver ;
+#endif
+
+   wbim = mri_weightize(bim,auto_weight,auto_dilation,auto_wclip,auto_wpow) ;
+
+   Hblur = 3.45678f ;
+
+#if 0
+   oim = IW3D_warp_s2bim( bim,wbim , sim , MRI_LINEAR , GA_MATCH_PEARSON_SCALAR , 0 ) ;
+#else
+   oim = IW3D_warp_s2bim( bim,wbim , sim , MRI_LINEAR , GA_MATCH_HELLINGER_SCALAR , 0 ) ;
+#endif
+
+   if( oim == NULL ) ERROR_exit("s2bim fails") ;
+
+   oset = EDIT_empty_copy(bset) ;
+   EDIT_dset_items( oset ,
+                      ADN_prefix    , "Qwarp" ,
+                      ADN_nvals     , 1 ,
+                      ADN_ntt       , 0 ,
+                      ADN_datum_all , MRI_float ,
+                    ADN_none ) ;
+   EDIT_BRICK_FACTOR(oset,0,0.0) ;
+   EDIT_substitute_brick( oset, 0, MRI_float, MRI_FLOAT_PTR(oim) ) ;
+   DSET_write(oset) ; WROTE_DSET(oset) ; DSET_delete(oset) ;
+
+   if( qset != NULL ){
+     INFO_message("saving QwarpSAVE dataset with %d sub-bricks",DSET_NVALS(qset)) ;
+     EDIT_dset_items( qset , ADN_ntt , DSET_NVALS(qset) , ADN_none ) ;
+     DSET_write(qset) ; WROTE_DSET(qset) ;
+   }
+   exit(0) ;
+}
