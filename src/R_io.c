2b36991bfb28555ddcf032c64d780eda2b5cf3d7
ziad <>
2012-11-15 18:48:33 +0000

    Cput

--- src/R_io.c
+++ src/R_io.c
@@ -236,22 +236,33 @@ SEXP R_THD_write_dset(SEXP Sfname, SEXP Sdset, SEXP Opts)
       return(R_NilValue);
    }
    if (debug > 2) {
-         INFO_message("Have header of %d, %d, %d, %d\n", 
+         INFO_message("Have header of %d, %d, %d, %d, scale=%d\n", 
                        DSET_NX(dset), DSET_NY(dset), 
-                       DSET_NZ(dset), DSET_NVALS(dset));
+                       DSET_NZ(dset), DSET_NVALS(dset), scale);
    }
    
    for (i=0; i<DSET_NVALS(dset); ++i) {
-      if (  ( DSET_BRICK_TYPE(dset,i) != MRI_byte && 
-      	     DSET_BRICK_TYPE(dset,i) != MRI_short ) ||
-	     scale == 0 ) {
-      	EDIT_substitute_brick(dset, i, 
-                            MRI_double, dv+i*DSET_NVOX(dset));	
-      } else {
-      	EDIT_substscale_brick(dset, i, 
+      if (debug > 2) {
+         INFO_message("Putting values in sub-brick %d, type %d\n", 
+                       i, DSET_BRICK_TYPE(dset,i));
+      }
+                            
+      if (  ( DSET_BRICK_TYPE(dset,i) == MRI_byte || 
+      	     DSET_BRICK_TYPE(dset,i) == MRI_short ) ) {
+         EDIT_substscale_brick(dset, i, 
                             MRI_double, dv+i*DSET_NVOX(dset),
-                            DSET_BRICK_TYPE(dset,i), -1.0);
-      }                      
+                            DSET_BRICK_TYPE(dset,i), scale ? -1.0:1.0);
+      } else if ( DSET_BRICK_TYPE(dset,i) == MRI_double ) {
+        EDIT_substitute_brick(dset, i, 
+                            MRI_double, dv+i*DSET_NVOX(dset));
+      } else if ( DSET_BRICK_TYPE(dset,i) == MRI_float ) {
+        float *ff=(float*)calloc(DSET_NVOX(dset), sizeof(float));
+        double *dvi=dv+i*DSET_NVOX(dset);
+        for (ip=0; ip<DSET_NVOX(dset); ++ip) {
+         ff[ip] = dvi[ip];
+        }
+        EDIT_substitute_brick(dset, i, MRI_float, ff);
+      }
    }
    
    /* THD_update_statistics( dset ) ; */
