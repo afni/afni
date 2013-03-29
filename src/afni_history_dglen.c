6edd6b3b5c0e2afd8395ae8350f582501655d1b3
dglen <>
2013-03-29 19:06:16 +0000

    Cput

--- src/afni_history_dglen.c
+++ src/afni_history_dglen.c
@@ -53,7 +53,7 @@ afni_history_struct dglen_history[] = {
     MINOR , TYPE_NEW_ENV, 
     "pigz for faster gzip-like compression"
     "pigz is a gzip utility that makes use of multiple CPU's.\n"
-    "It produces and uses .gz files. To use, set AFNI_COMPRESSOR to pigz."
+    "It produces and uses .gz files. To use, set AFNI_COMPRESSOR to PIGZ."
 } ,
 { 27 , MAR, 2013 , DRG , "3dBrickStat" , 
     MINOR , TYPE_NEW_OPT, 
