497e24ec3dc055bc9aa089803dcf43f5a0f16c27
rickr <>
2013-07-12 14:22:55 +0000

    Cadd

---
+++ src/python_scripts/afni_system_check.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+# system libraries
+import sys, os
+
+# AFNI libraries
+import option_list as OL
+import afni_util as UTIL        # not actually used, but probably will be
+import lib_system_check as SC
+
+
+g_help_string = """
+=============================================================================
+afni_system_check.py    - perform various system checks
+
+This program is intended to be helpful for figuring out AFNI installation
+issues.  This is just a start.
+
+current usage (run without options):
+
+        afni_system_check.py
+
+-----------------------------------------------------------------------------
+details displayed (just run to see):
+
+   - ...
+
+-----------------------------------------------------------------------------
+options:
+
+   -help                : show this help
+   -hist                : show program history
+   -show_valid_opts     : show valid options for program
+   -todo                : show current todo list
+   -ver                 : show current version
+
+
+-----------------------------------------------------------------------------
+R Reynolds    July, 2013
+=============================================================================
+"""
+
+g_todo = """
+todo: afni_system_check.py
+
+   - add some help
+   - for Unix or OSX, run ldd or 'otool -L' to check for shared libs in:
+      - afni, suma, 3dSkullStrip, 
+      - NOTE: 'otool -L' does not show whether libraries resolve
+   - run module tests on some libraries, like PyQt4
+   - check for gcc for mac?  can we tell whether openMP is supported?
+
+   - check for .afnirc/.sumarc .afni/help
+   - check for data under any passed -data_root
+   - check disk space
+"""
+
+g_history = """
+   afni_system_check.py history:
+
+   0.0  Jul 11, 2013    - initial version
+"""
+
+g_version = "afni_system_check.py version 0.0, July 11, 2013"
+
+
+class CmdInterface:
+   """interface class"""
+
+   def __init__(self, verb=1):
+      # main variables
+      self.status          = 0                  # exit value
+      self.valid_opts      = None
+      self.user_opts       = None
+
+      self.sinfo           = None               # system info class
+
+      # general variables
+      self.verb            = 1
+
+      # initialize valid_opts
+      self.init_options()
+
+   def init_options(self):
+      self.valid_opts = OL.OptionList('valid opts')
+
+      # terminal options
+      self.valid_opts.add_opt('-help', 0, [],           \
+                      helpstr='display program help')
+      self.valid_opts.add_opt('-hist', 0, [],           \
+                      helpstr='display the modification history')
+      self.valid_opts.add_opt('-show_valid_opts', 0, [],\
+                      helpstr='display all valid options')
+      self.valid_opts.add_opt('-todo', 0, [],            \
+                      helpstr='display the current "todo list"')
+      self.valid_opts.add_opt('-ver', 0, [],            \
+                      helpstr='display the current version number')
+
+      return 0
+
+   def process_options(self, argv=sys.argv):
+
+      # process any optlist_ options
+      self.valid_opts.check_special_opts(argv)
+
+      # process terminal options without the option_list interface
+      # (so that errors are not reported)
+
+      # if no arguments are given, apply -help
+      if '-help' in argv:
+         print g_help_string
+         return 0
+
+      if '-hist' in argv:
+         print g_history
+         return 0
+
+      if '-show_valid_opts' in argv:
+         self.valid_opts.show('', 1)
+         return 0
+
+      if '-todo' in argv:
+         print g_todo
+         return 0
+
+      if '-ver' in argv:
+         print g_version
+         return 0
+
+      # ============================================================
+      # read options specified by the user
+      self.user_opts = OL.read_options(argv, self.valid_opts)
+      uopts = self.user_opts            # convenience variable
+      if not uopts: return 1            # error condition
+
+      # ------------------------------------------------------------
+      # process options, go after -verb first
+
+      val, err = uopts.get_type_opt(int, '-verb')
+      if val != None and not err: self.verb = val
+
+      for opt in uopts.olist:
+
+         # an unhandled option
+         print '** option %s not yet supported' % opt.name
+         return 1
+
+      return None
+
+   def show_system_info(self):
+
+      print UTIL.section_divider('system check', hchar='=')
+      print 
+
+      self.sinfo = SC.SysInfo()
+
+      self.sinfo.show_general_sys_info()
+      self.sinfo.show_general_afni_info()
+      self.sinfo.show_python_lib_info(['PyQt4'], verb=3)
+      self.sinfo.show_path_vars()
+
+   def execute(self):
+
+      self.show_system_info()
+
+def main():
+   me = CmdInterface()
+   if not me: return 1
+
+   rv = me.process_options()
+   if rv != None: return rv
+
+   rv = me.execute()
+   if rv != None: return rv
+
+   return me.status
+
+if __name__ == '__main__':
+   sys.exit(main())
+
+
---
+++ src/python_scripts/lib_system_check.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+# library for performing various system checks
+
+import os, sys, platform
+import module_test_lib as MT
+import afni_base as BASE
+import afni_util as UTIL
+
+class SysInfo:
+   """system info class"""
+
+   def __init__(self):
+
+      self.system          = platform.system()
+      self.home_dir        = os.environ['HOME']
+
+   def get_afni_dir(self):
+      s, so, se = BASE.simple_shell_exec('which afni', capture=1)
+      if s: return ''
+      return so.strip()
+
+   def show_general_sys_info(self, header=1):
+      if header: print UTIL.section_divider('general', hchar='-')
+
+      def tostr(some_tuple):
+         if type(some_tuple) == str: return some_tuple
+         tlist = [t for t in list(some_tuple) if type(t) == str]
+         return ' '.join(tlist)
+
+      print 'architecture:         %s' % tostr(platform.architecture())
+      print 'system:               %s' % platform.system()
+      print 'release:              %s' % platform.release()
+      print 'version:              %s' % platform.version()
+
+      # check distributions by type
+      if   self.system == 'Linux':  dstr = tostr(platform.linux_distribution())
+      elif self.system == 'Darwin': dstr = tostr(platform.mac_ver())
+      else: dstr = tostr(platform.dist())
+      print 'distribution:         %s' % dstr
+         
+      print 'number of CPUs:       %s' % self.get_cpu_count()
+
+      # note shell, and if we are not in login shell
+      logshell = UTIL.get_login_shell()
+      curshell = UTIL.get_current_shell()
+      if logshell == curshell: note = ''
+      else:                    note = '  (current shell is %s)' % curshell
+
+      print 'apparent login shell: %s%s' % (logshell, note)
+      print
+
+   def show_python_lib_info(self, plibs, header=1, verb=2):
+      if header: print UTIL.section_divider('python libs', hchar='-')
+      for lib in plibs: MT.test_import(lib, verb=verb)
+      print
+
+   def show_path_vars(self, header=1):
+      print UTIL.section_divider('path vars', hchar='-')
+      for evar in ['PATH', 'PYTHON_PATH',
+                   'LD_LIBRARY_PATH',
+                   'DYLD_LIBRARY_PATH', 'DYLD_FALLBACK_LIBRARY_PATH']:
+         if os.environ.has_key(evar): print "%s = %s\n" % (evar, os.environ[evar])
+         else: print "%s = " % evar
+      print
+
+   def show_general_afni_info(self, header=1):
+      print UTIL.section_divider('AFNI and related program tests', hchar='-')
+      for prog in ['afni', 'python', 'R', 'tcsh']:
+         cmd = 'which %s' % prog
+         s, so, se = BASE.simple_shell_exec(cmd, capture=1)
+         if s: print '%-20s : %s' % (cmd, se)
+         else:
+            print '%-20s : %s' % (cmd, so.strip())
+            s, v = self.get_prog_version(prog)
+            if s: print '%-20s : %s' % ('%s version'%prog, v)
+      print
+
+      rv, files = UTIL.search_path_dirs('afni', exact=1)
+      if not rv:
+         print "instances of 'afni' found in PATH: %d" % len(files)
+         if len(files) > 0: print '   '+'\n   '.join(files)
+      print
+
+      ind = '%8s' % ' '
+      indn = '\n%8s' % ' '
+      print 'testing ability to start various programs...'
+      for prog in ['afni', 'suma', '3dSkullStrip', 'uber_subject.py',
+                   '3dAllineate']:
+         st, so, se = BASE.shell_exec2('%s -help'%prog, capture=1)
+         if st:
+            print '    %-20s : FAILURE' % prog
+            print ind + indn.join(se)
+         else: print '    %-20s : success' % prog
+
+      print
+
+   def get_prog_version(self, prog):
+      """return a simple string with program version
+
+         each program might be handled a little differently
+
+         return status and string
+            - status > 0 means to use, == 0 means not, < 0 means error
+      """
+      if prog == 'afni':
+         cmd = 'afni -ver'
+         s, so, se = UTIL.limited_shell_exec(cmd, nlines=2)
+         if s: return 1, se[0]
+         off1 = so[1].find('[[')
+         off2 = so[1].find(']]')
+         if off1 >= 0 and off2 >= 0: return 1, so[1][off1+2:off2]
+         else: return 1, so[1]
+
+      elif prog == 'python':
+         return 1, platform.python_version()
+
+      elif prog == 'R':
+         cmd = 'R --version'
+         s, so, se = UTIL.limited_shell_exec(cmd, nlines=1)
+         if s: return 1, se[0]
+         else: return 1, so[0]
+
+      elif prog == 'tcsh':      # no version
+         return 0, ''
+
+      else:
+         print '** no version method for prog : %s' % prog
+         return -1, ''
+
+   def get_cpu_count(self):
+       """
+       Number of virtual or physical CPUs on this system, i.e. user/real as
+       output by time(1) when called with an optimally scaling userspace-only
+       program.
+
+       from phihag @ stackoverflow.com, partially via other users
+       """
+       import re,subprocess
+
+       # Python 2.6+
+       try:
+           import multiprocessing
+           return multiprocessing.cpu_count()
+       except (ImportError,NotImplementedError):
+           pass
+
+       # http://code.google.com/p/psutil/
+       try:
+           import psutil
+           return psutil.NUM_CPUS
+       except (ImportError, AttributeError):
+           pass
+
+       # POSIX
+       try:
+           res = int(os.sysconf('SC_NPROCESSORS_ONLN'))
+           if res > 0: return res
+       except (AttributeError,ValueError):
+           pass
+
+       # Linux
+       try:
+           res = open('/proc/cpuinfo').read().count('processor\t:')
+           if res > 0: return res
+       except IOError:
+           pass
+
+       # Solaris
+       try:
+           pseudoDevices = os.listdir('/devices/pseudo/')
+           expr = re.compile('^cpuid@[0-9]+$')
+           res = 0
+           for pd in pseudoDevices:
+               if expr.match(pd) != None: res += 1
+           if res > 0: return res
+       except OSError:
+           pass
+
+       # BSD
+       try:
+           sysctl = subprocess.Popen(['sysctl', '-n', 'hw.ncpu'],
+                                         stdout=subprocess.PIPE)
+           scStdout = sysctl.communicate()[0]
+           res = int(scStdout)
+           if res > 0: return res
+       except (OSError, ValueError):
+           pass
+
+       # Other UNIXes (heuristic)
+       try:
+           try:
+               dmesg = open('/var/run/dmesg.boot').read()
+           except IOError:
+               dmesgProcess = subprocess.Popen(['dmesg'], stdout=subprocess.PIPE)
+               dmesg = dmesgProcess.communicate()[0]
+           res = 0
+           while '\ncpu' + str(res) + ':' in dmesg:
+               res += 1
+           if res > 0: return res
+       except OSError:
+           pass
+
+       # Windows
+       try:
+           res = int(os.environ['NUMBER_OF_PROCESSORS'])
+           if res > 0: return res
+       except (KeyError, ValueError):
+           pass
+
+       # jython
+       try:
+           from java.lang import Runtime
+           runtime = Runtime.getRuntime()
+           res = runtime.availableProcessors()
+           if res > 0: return res
+       except ImportError:
+           pass
+
+       print '** Can not determine number of CPUs on this system'
+       return 1
+
+
+if __name__ == '__main__':
+   print 'lib_system_check.py: not intended as a main program'
+
