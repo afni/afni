439db2338978c05307af4e4c66823928adbbebd2
rwcox <>
2013-07-23 14:11:09 +0000

    Cput

--- src/afni_history_rwcox.c
+++ src/afni_history_rwcox.c
@@ -43,7 +43,11 @@
 
 afni_history_struct rwcox_history[] = {
 /*=====BELOW THIS LINE=====*/
-  { 19 , JUL , 2013 , RWC , "3dDeconvolve" , MICRO , TYPE_MODIFY ,
+  { 23 , JUL , 2013 , RWC , "afni" , MICRO , TYPE_NEW_ENV ,
+   "Make AFNI_RECENTER_VIEWING an editable (in the GUI) variable" ,
+   NULL } ,
+
+ { 19 , JUL , 2013 , RWC , "3dDeconvolve" , MICRO , TYPE_MODIFY ,
    "Added warning if censor array is too long" ,
    NULL } ,
 
