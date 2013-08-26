3934d5091443b9034c5715cce2a97c3e706725d0
rickr <>
2013-08-26 14:40:24 +0000

    Cput

--- src/python_scripts/afni_system_check.py
+++ src/python_scripts/afni_system_check.py
@@ -16,12 +16,38 @@ afni_system_check.py    - perform various system checks
 This program is intended to be helpful for figuring out AFNI installation
 issues.  This is just a start.
 
-current usage (run without options):
+examples
 
-        afni_system_check.py
+   1.  afni_system_check.py -check_all
+   2a. afni_system_check.py -find_prog python
+   2b. afni_system_check.py -find_prog python -exact yes
 
 -----------------------------------------------------------------------------
-details displayed (just run to see):
+terminal options:
+
+   -help                : show this help
+   -hist                : show program history
+   -show_valid_opts     : show valid options for program
+   -todo                : show current todo list
+   -ver                 : show current version
+
+   NOTE: either a terminal or an action option is required
+
+action options:
+
+   -check_all           : perform all system checks
+                          - see section, "details displayed via -check_all"
+   -find_prog PROG      : search PATH for PROG
+                          - default is *PROG*, case-insensitive
+                          - see also -casematch, -exact
+
+other options:
+
+   -casematch yes/no    : match case in -find_prog
+   -exact yes/no        : search for PROG without wildcards in -find_prog
+
+-----------------------------------------------------------------------------
+details displayed via -check_all (just run to see):
 
    general information:
       - CPU, operating system and version, # CPUs, login shell
@@ -36,16 +62,6 @@ details displayed (just run to see):
 
    path vars:
       - show some environment variables related to the PATH
-
------------------------------------------------------------------------------
-options:
-
-   -help                : show this help
-   -hist                : show program history
-   -show_valid_opts     : show valid options for program
-   -todo                : show current todo list
-   -ver                 : show current version
-
 
 -----------------------------------------------------------------------------
 R Reynolds    July, 2013
@@ -73,12 +89,16 @@ g_history = """
    0.1  Jul 16, 2013 - added a couple of checks for early python versions
    0.2  Aug 02, 2013 - check for multiple R and python programs in PATH
    0.3  Aug 14, 2013
-                - PYTHON_PATH should be PYTHONPATH
-                - added a look at any /sw/bin/python* programs
+        - PYTHON_PATH should be PYTHONPATH
+        - added a look at any /sw/bin/python* programs
    0.4  Aug 19, 2013 - update to match that of afni_util.search_path_dirs()
+   0.5  Aug 26, 2013
+        - system check is now run via -check_all
+        - added -find_prog to search for PROG in PATH
+        - added -casematch and -exact as options for -find_prog
 """
 
-g_version = "afni_system_check.py version 0.4, August 19, 2013"
+g_version = "afni_system_check.py version 0.5, August 26, 2013"
 
 
 class CmdInterface:
@@ -86,13 +106,20 @@ class CmdInterface:
 
    def __init__(self, verb=1):
       # main variables
-      self.status          = 0                  # exit value
+      self.status          = 0          # exit value
       self.valid_opts      = None
       self.user_opts       = None
 
-      self.sinfo           = None               # system info class
+      self.sinfo           = None       # system info class
+
+      # action variables
+      self.find_prog       = ''         # program name to find
+      self.sys_check       = 0
 
       # general variables
+      self.act             = 0          # perform SOME action
+      self.casematch       = -1         # >= 0 means user-specified
+      self.exact           = 0          # use exact matching or not
       self.verb            = 1
 
       # initialize valid_opts
@@ -113,6 +140,18 @@ class CmdInterface:
       self.valid_opts.add_opt('-ver', 0, [],            \
                       helpstr='display the current version number')
 
+      # action options
+      self.valid_opts.add_opt('-casematch', 1, [],      \
+                      acplist=['yes','no'],
+                      helpstr='yes/no: specify case sensitivity in -find_prog')
+      self.valid_opts.add_opt('-check_all', 0, [],      \
+                      helpstr='perform all system checks')
+      self.valid_opts.add_opt('-exact', 1, [],          \
+                      acplist=['yes','no'],
+                      helpstr='yes/no: use exact matching in -find_prog')
+      self.valid_opts.add_opt('-find_prog', 1, [],      \
+                      helpstr='search path for *PROG*')
+
       return 0
 
    def process_options(self, argv=sys.argv):
@@ -124,7 +163,7 @@ class CmdInterface:
       # (so that errors are not reported)
 
       # if no arguments are given, apply -help
-      if '-help' in argv:
+      if '-help' in argv or len(argv) < 2:
          print g_help_string
          return 0
 
@@ -158,8 +197,35 @@ class CmdInterface:
 
       for opt in uopts.olist:
 
+         if opt.name == '-casematch':
+            if OL.opt_is_yes(opt): self.casematch = 1
+            else:                  self.casematch = 0
+            continue
+
+         if opt.name == '-check_all':
+            self.act = 1
+            self.sys_check = 1
+            continue
+
+         if opt.name == '-exact':
+            if OL.opt_is_yes(opt):
+               self.exact = 1
+               # default of casematching is 1 for this
+               if self.casematch < 0: self.casematch = 1
+            else: self.exact = 0
+            continue
+
+         if opt.name == '-find_prog':
+            self.act = 1
+            self.find_prog = opt.parlist[0]
+            continue
+
          # an unhandled option
          print '** option %s not yet supported' % opt.name
+         return 1
+
+      if not self.act:
+         print '** no action option found, please see -help output\n'
          return 1
 
       return None
@@ -178,7 +244,12 @@ class CmdInterface:
 
    def execute(self):
 
-      self.show_system_info()
+      if self.sys_check: self.show_system_info()
+      if self.find_prog:
+         # note casematching
+         cm = self.casematch
+         if cm < 0: cm = 0
+         UTIL.show_found_in_path(self.find_prog, mtype=self.exact, casematch=cm)
 
 def main():
    me = CmdInterface()
