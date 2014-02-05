93c366e2e935bf72b97f4b5f84b753d15f09f4f8
rwcox <>
2014-02-05 18:09:05 +0000

    Cput

--- src/netcdf-3.5.0/src/libsrc/ncconfig.h
+++ src/netcdf-3.5.0/src/libsrc/ncconfig.h
@@ -21,7 +21,7 @@
 /* #undef size_t */
 
 /* Define if you have the ANSI C header files.  */
-/* #undef STDC_HEADERS */
+#define STDC_HEADERS 1
 
 /* Define if your processor stores words with the most significant
    byte first (like Motorola and SPARC, unlike Intel and VAX).  */
@@ -34,48 +34,48 @@
 /* #undef NO_SYS_TYPES_H */
 
 /* Define if you have the ftruncate function  */
-/* #undef HAVE_FTRUNCATE */
+#define HAVE_FTRUNCATE 1
 
 /* Define if you have alloca, as a function or macro.  */
-/* #undef HAVE_ALLOCA */
+#define HAVE_ALLOCA 1
 
 /* Define if you have <alloca.h> and it should be used (not on Ultrix).  */
-/* #undef HAVE_ALLOCA_H */
+#define HAVE_ALLOCA_H 1
 
 /* Define if you don't have the strerror function  */
-#define NO_STRERROR 1
+/* #undef NO_STRERROR */
 
 /* The number of bytes in a size_t */
-#define SIZEOF_SIZE_T 0
+#define SIZEOF_SIZE_T 8
 
 /* The number of bytes in a off_t */
-#define SIZEOF_OFF_T 0
+#define SIZEOF_OFF_T 8
 
 /* Define to `int' if system doesn't define.  */
 /* #undef ssize_t */
 
 /* Define to `int' if system doesn't define.  */
-#define ptrdiff_t int
+/* #undef ptrdiff_t */
 
 /* Define to `unsigned char' if system doesn't define.  */
 #define uchar unsigned char
 
 /* Define if the system does not use IEEE floating point representation */
-#define NO_IEEE_FLOAT 1
+/* #undef NO_IEEE_FLOAT */
 
 /* The number of bytes in a double.  */
-#define SIZEOF_DOUBLE 0
+#define SIZEOF_DOUBLE 8
 
 /* The number of bytes in a float.  */
-#define SIZEOF_FLOAT 0
+#define SIZEOF_FLOAT 4
 
 /* The number of bytes in a int.  */
-#define SIZEOF_INT 0
+#define SIZEOF_INT 4
 
 /* The number of bytes in a long.  */
-#define SIZEOF_LONG 0
+#define SIZEOF_LONG 8
 
 /* The number of bytes in a short.  */
-#define SIZEOF_SHORT 0
+#define SIZEOF_SHORT 2
 
 #endif /* !_NCCONFIG_H_ */
