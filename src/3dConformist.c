90de374035a769235823213310dbe0b5137b48bb
rwcox <>
2014-03-28 13:25:19 +0000

    Cput

--- src/3dConformist.c
+++ src/3dConformist.c
@@ -15,26 +15,37 @@ int main( int argc , char *argv[] )
    int ndset=0 , ii ;
    THD_3dim_dataset **dset ;
 
-mainENTRY("3dConformist") ;
-
    if( argc < 3 || strcasecmp(argv[1],"-help") == 0 ){
      printf(
-       "Program 3dConformist reads in a collection of datasets and\n"
-       "zero pads them to the same size.  The input dataset ARE ALTERED!\n"
+       "** Program 3dConformist reads in a collection of datasets and\n"
+       "   zero pads them to the same size.\n"
+       "** The output volume size is the smallest region that includes\n"
+       "   all datasets (i.e., the minimal covering box).\n"
+       "** If the datasets cannot be processed (e.g., different grid\n"
+       "   spacings), then nothing will happen except for error messages.\n"
+       "** The purpose of this program is to be used in scripts that\n"
+       "   process lots of datasets and needs to make them all conform\n"
+       "   to the same size for collective voxel-wise analyses.\n"
+       "** The input datasets ARE ALTERED (embiggened)! <<<<<<------******\n"
+       "   Therefore, don't use this program casually.\n"
      ) ;
      exit(0) ;
    }
 
+   mainENTRY("3dConformist") ; machdep() ; PRINT_VERSION("3dConformist") ;
+
    ndset = argc-1 ;
    dset  = (THD_3dim_dataset **)malloc(sizeof(THD_3dim_dataset *)*ndset) ;
    for( ii=0 ; ii < ndset ; ii++ ){
-     dset[ii] = THD_open_dataset(argv[ii+1]) ; CHECK_OPEN_ERROR(dset[ii],argv[ii+1]) ;
+     dset[ii] = THD_open_dataset(argv[ii+1]) ;
+     CHECK_OPEN_ERROR(dset[ii],argv[ii+1]) ;
    }
 
    ii = THD_conformist(ndset,dset) ;
 
    switch(ii){
      default: INFO_message ("3dConformist: Re-wrote %d datasets",ii) ; break ;
+     case  0: INFO_message ("3dConformist: all datasets matched OK") ; break ;
      case -1: ERROR_message("3dConformist: bad input")               ; break ;
      case -2: ERROR_message("3dConformist: bad inputs")              ; break ;
      case -3: ERROR_message("3dConformist: can't match grids")       ; break ;
@@ -70,20 +81,29 @@ ENTRY("THD_conformist") ;
    /* construct the dataxes that encloses all the input datasets */
 
    cx = THD_superset_dataxes( dset[0]->daxes , dset[1]->daxes ) ;
-   if( cx == NULL ) RETURN(-3) ;
+   if( cx == NULL ){
+     ERROR_message("3dConformist: '%s' and '%s' aren't compatible",
+                   DSET_HEADNAME(dset[0]) , DSET_HEADNAME(dset[1]) ) ;
+     RETURN(-3) ;
+   }
 
    for( iset=2 ; iset < ndset ; iset++ ){
      dx = THD_superset_dataxes( cx , dset[iset]->daxes ) ;
-     if( dx == NULL ){ myXtFree(cx) ; RETURN(-3) ; }
+     if( dx == NULL ){
+       ERROR_message("3dConformist: '%s' is not compatible with others",
+                     DSET_HEADNAME(dset[iset]) ) ;
+       myXtFree(cx) ; RETURN(-3) ;
+     }
      myXtFree(cx) ; cx = dx ; dx = NULL ;
    }
 
    /* now, re-create and re-write all datasets */
 
-   fprintf(stderr,"3dConformist loop: ") ;
+   fprintf(stderr,"++ 3dConformist re-write loop: ") ;
    for( nwrit=iset=0 ; iset < ndset ; iset++ ){
-     fprintf(stderr,".") ;
-     if( EQUIV_DATAXES(cx,dset[iset]->daxes) ) continue ; /* already OK */
+     if( EQUIV_DATAXES(cx,dset[iset]->daxes) ){ /* already OK */
+       fprintf(stderr,"-") ; continue ;
+     }
      pm = zpadax_pm( cx->nxx , cx->xxorg ,
                      dset[iset]->daxes->nxx , dset[iset]->daxes->xxorg ,
                      cx->xxdel ) ;
@@ -100,13 +120,16 @@ ENTRY("THD_conformist") ;
                          xpad_m,xpad_p , ypad_m,ypad_p , zpad_m,zpad_p ,
                          "BertieWooster" , ZPAD_PURGE | ZPAD_IJK ) ;
      if( qset == NULL ){
-       ERROR_message("3dConformist: skipping dataset %s",DSET_HEADNAME(dset[iset])) ;
+       fprintf(stderr,"\n") ;
+       ERROR_message("3dConformist: skipping dataset %s",
+                     DSET_HEADNAME(dset[iset])) ;
        continue ;
      }
      qset->idcode = dset[iset]->idcode ;
      EDIT_dset_items( qset , ADN_prefix , DSET_PREFIX(dset[iset]) , ADN_none ) ;
      THD_delete_3dim_dataset( dset[iset] , True ) ;
      DSET_overwrite(qset) ; DSET_unload(qset) ; dset[iset] = qset ; nwrit++ ;
+     fprintf(stderr,"+") ;
    }
    fprintf(stderr,"\n") ;
 
