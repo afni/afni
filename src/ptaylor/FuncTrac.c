6c1eaa5ab256d2aa085e5fa1eac8b72c7ee1cd56
rickr <>
2014-06-09 18:28:34 +0000

    Cadd

---
+++ src/ptaylor/FuncTrac.c
@@ -0,0 +1,81 @@
+
+
+#include <afni.h> 
+#include <FuncTrac.h>
+
+
+
+
+
+
+/*
+  In new struct-entry running mode, these are the defaults.
+ */
+TRACK_RUN_PARAMS SetInitialTrackingDefaults(void)
+{
+   TRACK_RUN_PARAMS defopt;
+   
+   // SET THEM:
+   defopt.NAMEIN_mode = NULL;             //  "-mode"
+   defopt.NAMEIN_netrois = NULL;          //  "-netrois"
+   defopt.prefix = NULL;                  //  "-prefix"
+
+   // DTI input names/opts
+   defopt.dti_inpref = NULL;              //  "-dti_in"
+   defopt.dti_listname = NULL;            //  "-dti_list"
+   defopt.in_EXTRA = NULL;                //  "-dti_extra"
+   defopt.NO_NONDTI_SEARCH = 0; // "-dti_search_NO"; won't keep scal/nonDTI pars
+
+   // HARDI input 
+   defopt.hardi_dir = NULL;               //  "-hardi_dirs"
+   defopt.hardi_gfa = NULL;               //  "-hardi_gfa"
+   defopt.hardi_pars = NULL;              //  "-hardi_pars"
+
+   // mask
+   defopt.NAMEIN_mask = NULL;             //  "-mask"
+   defopt.NAMEIN_thru = NULL;               // "-thru_mask"
+
+   // algorithm options 
+   defopt.algopt_file_name = NULL;  //  "-algopt"
+   defopt.MinFA=0.2;                //  "-alg_Thresh_FA"     - DET,MINIP,PROB
+   defopt.MaxAngDeg=60.;            //  "-alg_Thresh_ANG"    - DET,MINIP,PROB
+   defopt.MinL=20.;                 //  "-alg_Thresh_Len"    - DET,MINIP,PROB
+   defopt.NmNsFr=0.001;             //  "-alg_Thresh_Frac"   - PROB
+   defopt.Nseed=5;                  //  "-alg_Nseed_Vox"     - PROB
+   defopt.Nmonte=1000;              //  "-alg_Nmonte"        - PROB
+   defopt.SeedPerV[0]=2;      //  "-alg_Nseed_{X,Y,Z}" - DET,MINIP
+   defopt.SeedPerV[1]=2;      //  "-alg_Nseed_{X,Y,Z}" - DET,MINIP
+   defopt.SeedPerV[2]=2;      //  "-alg_Nseed_{X,Y,Z}" - DET,MINIP
+
+   // uncert filename
+   defopt.NAMEIN_uncert = NULL;           //  "-uncert"
+   // these mins get strdup'ed
+   defopt.NAMEIN_uncFA = NULL;            //  "-unc_min_FA"
+   defopt.NAMEIN_uncEI = NULL;            //  "-unc_min_V"
+
+   // other control, minip/logic/.trk/rand-seed
+   defopt.LOG_TYPE=-1;                 //  "-logic"
+   // default now TO cut off tracks at ROI endpts
+   defopt.ONLY_BT=1;                   //  "-uncut_at_rois"
+   // default: no mini monte carloing for det_net
+   defopt.MINI_PROB_NM=0;              //  "-mini_num"
+   defopt.CHOOSE_SEED = 0;             //  "-choose_seed"
+   defopt.OUTPUT_TRK = 1;              //  "-no_trk_out"
+
+   // minorly used control stuff, mostly default
+   defopt.NAMEIN_outmode = NULL;       //  "-tract_out_mode"
+   defopt.dump_opts=0;                 //  "-write_opts"
+   defopt.ROIS_OUT = 0;                //  "-write_rois"
+   defopt.DUMP_ORIG_LABS=0;            //  "-lab_orig_rois"
+   // switch about having no min threshold,and dumping ROIs
+   defopt.POST=0;                      //  "-posteriori"
+// switch about whether to dump ascii/afni/both
+   defopt.DUMP_TYPE=-1;                //  "-dump_rois"
+   defopt.RECORD_ORIG=0;               //  "-rec_orig"
+             
+   // EXPIRED OPTIONS, just gives message/error:
+   // "-cut_at_rois"
+   // "-det_net"
+
+   return defopt;
+}
---
+++ src/ptaylor/FuncTrac.h
@@ -0,0 +1,88 @@
+#ifndef FUNCTRAC_INCLUDED
+#define FUNCTRAC_INCLUDED
+
+
+
+/* new struct of all input parameters, allows program to be called
+   internally
+*/
+typedef struct {
+
+   // -help, -h
+   // -verb 
+
+   // Necessary for all running
+   char *NAMEIN_mode;             //  "-mode"
+   char *NAMEIN_netrois;          //  "-netrois"
+   char *prefix;                  //  "-prefix"
+
+   // DTI input names/opts
+   char *dti_inpref;              //  "-dti_in"
+   char *dti_listname;            //  "-dti_list"
+   char *in_EXTRA;                //  "-dti_extra"
+   int NO_NONDTI_SEARCH;          //  "-dti_search_NO"
+
+   // HARDI input 
+   char *hardi_dir;               //  "-hardi_dirs"
+   char *hardi_gfa;               //  "-hardi_gfa"
+   char *hardi_pars;              //  "-hardi_pars"
+
+   // mask
+   char *NAMEIN_mask;             //  "-mask"
+   char *NAMEIN_thru;             // "-thru_mask"
+
+   // algorithm options 
+   char *algopt_file_name;        //  "-algopt"
+   float MinFA;                   //  "-alg_Thresh_FA"     - DET,MINIP,PROB
+   float MaxAngDeg;               //  "-alg_Thresh_ANG"    - DET,MINIP,PROB
+   float MinL;                    //  "-alg_Thresh_Len"    - DET,MINIP,PROB
+   float NmNsFr;                  //  "-alg_Thresh_Frac"   - PROB
+   int Nseed;                     //  "-alg_Nseed_Vox"     - PROB
+   int Nmonte;                    //  "-alg_Nmonte"        - PROB
+   int SeedPerV[3];               //  "-alg_Nseed_{X,Y,Z}" - DET,MINIP
+
+   // uncert filename
+   char *NAMEIN_uncert;           //  "-uncert"
+   // these mins get strdup'ed
+   char *NAMEIN_uncFA;            //  "-unc_min_FA"
+   char *NAMEIN_uncEI;            //  "-unc_min_V"
+
+   // other control, minip/logic/.trk/rand-seed
+   int LOG_TYPE;                  //  "-logic"
+   int ONLY_BT;                   //  "-uncut_at_rois"
+   int MINI_PROB_NM;              //  "-mini_num"
+   int CHOOSE_SEED;               //  "-choose_seed"
+   int OUTPUT_TRK;                //  "-no_trk_out"
+
+   // minorly used control stuff, mostly default
+   char *NAMEIN_outmode;          //  "-tract_out_mode"
+   int dump_opts;                 //  "-write_opts"
+   int ROIS_OUT;                  //  "-write_rois"
+   int DUMP_ORIG_LABS;            //  "-lab_orig_rois"
+   int POST;                      //  "-posteriori"
+   int DUMP_TYPE;                 //  "-dump_rois"
+   int RECORD_ORIG;               //  "-rec_orig"
+             
+   // EXPIRED OPTIONS, just gives message/error:
+   // "-cut_at_rois"
+   // "-det_net"
+
+} TRACK_RUN_PARAMS;
+
+
+
+
+
+TRACK_RUN_PARAMS SetInitialTrackingDefaults(void);
+
+
+
+
+
+
+
+
+
+
+
+#endif
