3a9a7529c3d17f6709c84180179a7129729fa0f5
rwcox <>
2014-07-18 13:34:49 +0000

    Cput

--- src/afni.c
+++ src/afni.c
@@ -1492,6 +1492,7 @@ void AFNI_sigfunc_alrm(int sig)
      "Remember -- Statistics are no substitute for judgment"         ,
      "Remember -- A thing can be true, and still be desperate folly" ,
      "Remember -- Everything popular is wrong"                       ,
+     "Remember -- Aquaman cares"                                     ,
      "Forgive your enemy -- but remember the bastard's name"         ,
      "If the facts don't fit the theory, change the facts"           ,
      "All generalizations are false, including this one"             ,
