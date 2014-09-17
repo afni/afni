e625a70c832f1d411626632877e29f518635a023
dglen <>
2014-09-17 22:18:22 +0000

    Cput

--- src/afni_history_dglen.c
+++ src/afni_history_dglen.c
@@ -49,9 +49,18 @@
 
 
 afni_history_struct dglen_history[] = {
+{ 17 , SEP, 2014 , DRG , "MNI nonlinear templates" , 
+    MINOR , TYPE_MODIFY, 
+    "Nonlinear version of MNI-152 template in distribution",
+    "The nonlinear 2009c version is more useful, particularly as a base for\n"
+    "nonlinear alignment. AFNI_atlas_spaces.niml includes descriptions for\n"
+    "native MNI152 2009c T1 template and a transformed version aligned to\n"
+    "the TT_N27 dataset in Talairach space"
+} ,
+
 { 17 , SEP, 2014 , DRG , "auto_warp.py" , 
     MICRO , TYPE_BUG_FIX, 
-    "Did not pass qw_opts properly to 3dQwarp",
+    "Did not pass qw_opts properly to 3dQwarp"
 } ,
 
 { 4 , SEP, 2014 , DRG , "AFNI_ATLAS_PATH" , 
@@ -61,7 +70,7 @@ afni_history_struct dglen_history[] = {
     "This variable may hold multiple directories that specify the possible\n"
     "locations of atlases when the atlas definition does not include the\n"
     "path in the filename. The atlases are typically defined in \n"
-    "AFNI_atlas_spaces.niml file or in a CustomAtlases.niml file."
+    "AFNI_atlas_spaces.niml file or in a CustomAtlases.niml file"
 } ,
 
 { 9 , JUL, 2014 , DRG , "3dDWItoDT" , 
