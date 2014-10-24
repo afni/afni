1e5eaf0946c64c70bd9068c1f41c2a805ff32605
rwcox <>
2014-10-24 21:48:38 +0000

    Cput

--- src/im_to_mov.c
+++ src/im_to_mov.c
@@ -1,5 +1,47 @@
 #include "mrilib.h"
 
+static char *ppmto_mpeg_filter = NULL ;
+#define MPEG_ENCODE_SUFFIX "-realquiet %s"
+
+/*----------------------------------------------------------------------------*/
+
+int suck_file( char *fname , char **fbuf )
+{
+   int len , fd , ii ;
+   char *buf ;
+
+   if( fname == NULL || fname[0] == '\0' || fbuf == NULL ) return 0;
+
+   len = THD_filesize( fname ); if( len <= 0 ) return 0;
+
+   buf = (char *) malloc( sizeof(char) * (len+4) ); if( buf == NULL ) return 0;
+
+   fd = open( fname , O_RDONLY ); if( fd < 0 ) return 0;
+
+   ii = read( fd , buf , len ); close( fd ); if( ii <= 0 ){ free(buf); return 0; }
+   *fbuf = buf; return ii;
+}
+
+/*----------------------------------------------------------------------------*/
+
+void file_copy( char *fname , int ncopy , char **fcopy )
+{
+   char *fbuf=NULL ; int nbuf=0 ; int ii ; FILE *fp ;
+
+   if( fname == NULL || ncopy < 1 || fcopy == NULL ) return ;
+
+   nbuf = suck_file( fname , &fbuf ) ;
+   if( nbuf <= 0 || fbuf == NULL ) return ;
+
+   for( ii=0 ; ii < ncopy ; ii++ ){
+     if( fcopy[ii] == NULL || fcopy[ii][0] == '\0' ) continue ;
+     fp = fopen( fcopy[ii] , "w" ) ; if( fp == NULL ) continue ;
+     fwrite( fbuf , nbuf , 1 , fp ) ; fclose(fp) ;
+   }
+
+   free(fbuf) ; return ;
+}
+
 /*----------------------------------------------------------------------------*/
 
 MRI_IMAGE *mri_rgb_scladd( float fa, MRI_IMAGE *ima, float fb, MRI_IMAGE *imb )
@@ -135,11 +177,11 @@ MRI_IMAGE * mri_rgb_scaledown_by4( MRI_IMAGE *inim )
 int main( int argc , char *argv[] )
 {
    int npure=10 , nfade=5 , iarg=1 , ii,jj,kk , nbad , nx,ny,nin ;
-   char *prefix="i2m" , *outname ;
+   char *prefix="i2m" , *outname ; char **fcopy ;
    MRI_IMARR *in_imar=NULL ;
    MRI_IMAGE *inim , *qim , *jnim ;
    float fac ;
-   int down=1 ;
+   int down=1 ; int do_jpg=1 ;
 
    if( argc < 3 ){
      printf("Usage: im_to_mov [options] imagefile1 imagefile2 ...\n") ;
@@ -150,6 +192,8 @@ int main( int argc , char *argv[] )
             " -nfade F  = number of transition fades between pairs [5]\n"
             " -down  X  = downsample by factor X = 2 or 3 or 4 [none]\n"
             " -prefix Q = prefix for output files [i2m]\n"
+            " -jpg      = write JPEG output files\n"
+            " -mpg      = write PPM files, convert to MPEG-1, delete PPMs\n"
            ) ;
      exit(0) ;
    }
@@ -157,6 +201,13 @@ int main( int argc , char *argv[] )
    /*---- scan options ----*/
 
    while( iarg < argc && argv[iarg][0] == '-' ){
+
+     if( strcasecmp(argv[iarg],"-jpg") == 0 ){
+       do_jpg = 1 ; iarg++ ; continue ;
+     }
+     if( strcasecmp(argv[iarg],"-mpg") == 0 ){
+       do_jpg = 0 ; iarg++ ; continue ;
+     }
 
      if( strcasecmp(argv[iarg],"-npure") == 0 ){
        if( ++iarg >= argc ) ERROR_exit("Need arg after -npure") ;
@@ -189,11 +240,25 @@ int main( int argc , char *argv[] )
      ERROR_exit("Illegal option '%s'",argv[iarg]) ;
    }
 
+   /*--- if doing MPEG-1, find the program for that now ---*/
+
+   if( !do_jpg ){
+     char *pg = THD_find_executable( "mpeg_encode" ) ;
+     if( pg != NULL ){
+        ppmto_mpeg_filter = malloc( sizeof(char) * (strlen(pg)+64) ) ;
+        sprintf(ppmto_mpeg_filter,"%s %s",pg,MPEG_ENCODE_SUFFIX) ;
+      } else {
+        ERROR_exit("Can't find mpeg_encode program!") ;
+      }
+   }
+
    /*--- read input images ---*/
 
    if( argc-iarg < 2 ) ERROR_exit("Need at least 2 input images!") ;
 
    INIT_IMARR(in_imar) ;
+
+   fprintf(stderr,"Reading") ;
 
    for( nbad=0,ii=iarg ; ii < argc ; ii++ ){
      inim = mri_read(argv[ii]) ;
@@ -215,8 +280,9 @@ int main( int argc , char *argv[] )
        ERROR_message("Image '%s' has (nx,ny)=(%d,%d) mismatch from (%d,%d)",
                      argv[ii] , inim->nx,inim->ny , nx,ny ) ; nbad++ ; continue ;
      }
-     ADDTO_IMARR(in_imar,inim) ;
+     ADDTO_IMARR(in_imar,inim) ; fprintf(stderr,".") ;
    }
+   fprintf(stderr,"!\n") ;
 
    if( nbad > 0 ) ERROR_exit("Can't continue after such blatant stupidities") ;
 
@@ -224,38 +290,190 @@ int main( int argc , char *argv[] )
 
    outname = (char *)malloc(sizeof(char)*(strlen(prefix)+32)) ;
    nin     = IMARR_COUNT(in_imar) ;
-   INFO_message("Read in %d images %dx%d",nin,nx,ny) ;
+   fac     = 1.0f / ( nfade + 1.0f ) ;
 
-   fac = 1.0f / ( nfade + 1.0f ) ;
+   if( down < 2 )
+     INFO_message("Read in %d images %dx%d",nin,nx,ny) ;
+   else
+     INFO_message("Read in %d images, downsampled to %dx%d",nin,nx,ny) ;
 
-   fprintf(stderr,"++ Writing") ;
-   for( kk=ii=0 ; ii < nin-1 ; ii++ ){
-     inim = IMARR_SUBIM(in_imar,ii) ;
-     jnim = IMARR_SUBIM(in_imar,ii+1) ;
+   if( do_jpg ){  /*------------------ Just write JPEGs ------------------*/
 
-     for( jj=0 ; jj < npure ; jj++ ){  /* copies of ii-th input */
+     fprintf(stderr,"++ Writing JPEGs ") ;
+
+     fcopy = (char **)malloc(sizeof(char *)*npure) ;
+     for( jj=0 ; jj < npure ; jj++ )
+       fcopy[jj] = (char *)malloc(sizeof(char)*strlen(outname)) ;
+
+     for( kk=ii=0 ; ii < nin-1 ; ii++ ){
+       inim = IMARR_SUBIM(in_imar,ii) ;
+       jnim = IMARR_SUBIM(in_imar,ii+1) ;
+
+#ifdef USE_COPY /* write image once, then simply copy file several times */
        sprintf(outname,"%s_%06d.jpg",prefix,kk) ; kk++ ;
        mri_write_jpg(outname,inim) ;
+       if( npure > 1 ){
+         for( jj=0 ; jj < npure-1 ; jj++ ){
+           sprintf(fcopy[jj],"%s_%06d.jpg",prefix,kk) ; kk++ ;
+         }
+         file_copy( outname , npure-1 , fcopy ) ;
+       }
+#else  /* just write image repeatedly */
+       for( jj=0 ; jj < npure ; jj++ ){  /* copies of ii-th input */
+         sprintf(outname,"%s_%06d.jpg",prefix,kk) ; kk++ ;
+         mri_write_jpg(outname,inim) ;
+       }
+#endif
+
+       for( jj=0 ; jj < nfade ; jj++ ){  /* interpolate for fade */
+         qim = mri_rgb_scladd( 1.0f-(jj+1)*fac , inim , (jj+1)*fac , jnim ) ;
+         sprintf(outname,"%s_%06d.jpg",prefix,kk) ; kk++ ;
+         mri_write_jpg(outname,qim) ;
+       }
+       mri_free(inim) ;
+       fprintf(stderr,".") ;
      }
 
-     for( jj=0 ; jj < nfade ; jj++ ){  /* interpolate for fade */
-       qim = mri_rgb_scladd( 1.0f-(jj+1)*fac , inim , (jj+1)*fac , jnim ) ;
+     /* final copies of last image */
+
+#ifdef USE_COPY
        sprintf(outname,"%s_%06d.jpg",prefix,kk) ; kk++ ;
-       mri_write_jpg(outname,qim) ;
+       mri_write_jpg(outname,jnim) ;
+       if( npure > 1 ){
+         for( jj=0 ; jj < npure-1 ; jj++ ){
+           sprintf(fcopy[jj],"%s_%06d.jpg",prefix,kk) ; kk++ ;
+         }
+         file_copy( outname , npure-1 , fcopy ) ;
+       }
+#else
+     for( jj=0 ; jj < npure ; jj++ ){
+       sprintf(outname,"%s_%06d.jpg",prefix,kk) ; kk++ ;
+       mri_write_jpg(outname,jnim) ;
      }
-     mri_free(inim) ;
-     fprintf(stderr,".") ;
-   }
+#endif
+     mri_free(jnim) ;
 
-   /* final copies of last image */
+     fprintf(stderr,"!\n") ;
 
-   for( jj=0 ; jj < npure ; jj++ ){
-     sprintf(outname,"%s_%06d.jpg",prefix,kk) ; kk++ ;
-     mri_write_jpg(outname,jnim) ;
-   }
-   mri_free(jnim) ;
-   fprintf(stderr,".\n") ;
+     INFO_message("Output %d images with filename format %s_xxxxxx.jpg",kk,prefix) ;
 
-   INFO_message("Output %d images with filename format %s_xxxxxx.jpg",kk,prefix) ;
+   } else { /*------------- PPM then MPEG ------------------------------------------*/
+
+     fprintf(stderr,"++ Writing PPMs ") ;
+
+     fcopy = (char **)malloc(sizeof(char *)*npure) ;
+     for( jj=0 ; jj < npure ; jj++ )
+       fcopy[jj] = (char *)malloc(sizeof(char)*strlen(outname)) ;
+
+     for( kk=ii=0 ; ii < nin-1 ; ii++ ){
+       inim = IMARR_SUBIM(in_imar,ii) ;
+       jnim = IMARR_SUBIM(in_imar,ii+1) ;
+
+#ifdef USE_COPY /* write image once, then simply copy file several times */
+       sprintf(outname,"%s_%06d.ppm",prefix,kk) ; kk++ ;
+       mri_write_pnm(outname,inim) ;
+       if( npure > 1 ){
+         for( jj=0 ; jj < npure-1 ; jj++ ){
+           sprintf(fcopy[jj],"%s_%06d.ppm",prefix,kk) ; kk++ ;
+         }
+         file_copy( outname , npure-1 , fcopy ) ;
+       }
+#else  /* just write image repeatedly */
+       for( jj=0 ; jj < npure ; jj++ ){  /* copies of ii-th input */
+         sprintf(outname,"%s_%06d.ppm",prefix,kk) ; kk++ ;
+         mri_write_pnm(outname,inim) ;
+       }
+#endif
+
+       for( jj=0 ; jj < nfade ; jj++ ){  /* interpolate for fade */
+         qim = mri_rgb_scladd( 1.0f-(jj+1)*fac , inim , (jj+1)*fac , jnim ) ;
+         sprintf(outname,"%s_%06d.ppm",prefix,kk) ; kk++ ;
+         mri_write_pnm(outname,qim) ;
+       }
+       mri_free(inim) ;
+       fprintf(stderr,".") ;
+     }
+
+     /* final copies of last image */
+
+#ifdef USE_COPY
+       sprintf(outname,"%s_%06d.ppm",prefix,kk) ; kk++ ;
+       mri_write_pnm(outname,jnim) ;
+       if( npure > 1 ){
+         for( jj=0 ; jj < npure-1 ; jj++ ){
+           sprintf(fcopy[jj],"%s_%06d.ppm",prefix,kk) ; kk++ ;
+         }
+         file_copy( outname , npure-1 , fcopy ) ;
+       }
+#else
+     for( jj=0 ; jj < npure ; jj++ ){
+       sprintf(outname,"%s_%06d.ppm",prefix,kk) ; kk++ ;
+       mri_write_pnm(outname,jnim) ;
+     }
+#endif
+     mri_free(jnim) ;
+
+     fprintf(stderr,"!\n") ;
+
+     INFO_message("Output %d images with filename format %s_xxxxxx.ppm",kk,prefix) ;
+
+     /* now create MPEG from PPMs [stolen from imseq.c] */
+
+     { int alen ; char *alf , *oof , *par , *frate ;
+       char *qscale , *pattrn ; int mpar=0 ;
+       FILE *fpar ;
+
+       /* write mpeg_encode parameter file */
+
+       par = malloc( sizeof(char)*(strlen(prefix)+32) ) ;
+       sprintf(par,"%s.PARAM",prefix) ;
+       fpar = fopen( par , "w" ) ;
+       if( fpar == NULL ) ERROR_exit("Can't create MPEG parameter file %s",par) ;
+       oof = malloc( sizeof(char)*(strlen(prefix)+32) ) ; /* output fname */
+       sprintf(oof,"%s.mpg",prefix) ;
+       qscale="5" ;
+       frate ="24" ;
+       pattrn = calloc(sizeof(char),(npure+nfade+16)) ;
+       pattrn[0] = 'I' ;
+       memset( pattrn+1 , 'P' , npure-1 ) ;
+       memset( pattrn+npure , 'I' , nfade ) ;
+       fprintf(fpar,
+                  "OUTPUT %s\n"             /* oof */
+                  "GOP_SIZE          %d\n"  /* npure+nfade */
+                  "SLICES_PER_FRAME  1\n"
+                  "FRAME_RATE        %s\n"  /* frate */
+                  "BASE_FILE_FORMAT  PPM\n"
+                  "INPUT_CONVERT     *\n"
+                  "INPUT_DIR         .\n"
+                  "PATTERN           %s\n"  /* pattrn */
+                  "IQSCALE           %s\n"  /* qscale */
+                  "PQSCALE           31\n"
+                  "BQSCALE           31\n"
+                  "PIXEL             HALF\n"
+                  "RANGE             10 4\n"
+                  "PSEARCH_ALG       LOGARITHMIC\n"
+                  "BSEARCH_ALG       SIMPLE\n"
+                  "REFERENCE_FRAME   ORIGINAL\n"
+                  "INPUT\n"
+                  "%s_*.ppm [%06d-%06d]\n"  /* prefix, from, to */
+                  "END_INPUT\n"
+               , oof , npure+nfade , frate , pattrn , qscale ,
+                 prefix,0,kk-1 ) ;
+       fclose(fpar) ; free(pattrn) ;
+
+       /* make command to run */
+
+       alen = strlen(par)+strlen(ppmto_mpeg_filter)+128 ;
+       alf  = malloc(sizeof(char)*alen) ;
+       sprintf(alf , ppmto_mpeg_filter, par ) ; /* command to run */
+       INFO_message("Running '%s' to produce %s\n",alf,oof) ;
+       system(alf) ;                            /* so run it!    */
+       ININFO_message("** DONE -- deleting ppm files") ;
+       sprintf(alf,"\\rm -f %s_??????.ppm",prefix) ; system(alf) ;
+       remove(par); free(alf); free(oof); free(par); /* free trash */
+     }
+
+   } /*---------------------------------------------------------------------------*/
+
    exit(0) ;
 }
