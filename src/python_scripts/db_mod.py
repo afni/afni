b25472ce694f1717760977288e546d9a57a8c0cb
rick reynolds <reynoldr@mail.nih.gov>
2015-01-15 12:25:29 -0500

    a few small udpates

--- src/python_scripts/db_mod.py
+++ src/python_scripts/db_mod.py
@@ -2402,8 +2402,9 @@ def anat_mask_command(proc, block):
                    % (proc.mask_epi.pv(), proc.mask_anat.pv())
             cmd = cmd + rcmd
 
-            rcmd = "# note correlation as well\n"                     \
-                   "3ddot %s %s |& tee out.mask_ae_corr.txt\n\n"      \
+            rcmd = "# note Pearson correlation of masks, as well\n"   \
+                   "3ddot -demean %s %s \\\n"                         \
+                   "      |& tee out.mask_ae_corr.txt\n\n"            \
                    % (proc.mask_epi.pv(), proc.mask_anat.pv())
             cmd = cmd + rcmd
 
