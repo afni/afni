81271658ff6cee1195e8cefb1df4071dd181a335
Ziad S. Saad <saadz@mail.nih.gov>
2015-01-29 10:20:30 -0500

    Bug fix in view modification in SUMA_ModifyParsedName(). Made it also work when name has no storage mode defined

--- src/suma_utils.c
+++ src/suma_utils.c
@@ -1636,8 +1636,9 @@ SUMA_PARSED_NAME * SUMA_ModifyParsedName (SUMA_PARSED_NAME *pn,
       char vval[6]={""};
       if (!val) SUMA_RETURN(NULL);
       if (val[0] != '+') sprintf(vval,"+%c%c%c%c",val[0],val[1],val[2],val[3]);
-      else sprintf(vval,"%c%c%c%c",val[0],val[1],val[2],val[3]);
-      if (pn->StorageMode ==  STORAGE_BY_BRICK) {
+      else sprintf(vval,"%c%c%c%c%c",val[0],val[1],val[2],val[3], val[4]);
+      if (pn->StorageMode ==  STORAGE_BY_BRICK ||
+          pn->StorageMode ==  STORAGE_UNDEFINED) {
          fullname=NULL;
          if(strstr(pn->NameAsParsed,"/")) 
             fullname = SUMA_append_replace_string(fullname, 
