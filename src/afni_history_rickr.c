eae839d1dbbd5aca9a6e301856e8e4cd80e594d4
rick reynolds <reynoldr@mail.nih.gov>
2015-03-12 12:09:54 -0400

    shell_exec2 fix

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,16 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 12,  Mar, 2015, RCR, "afni_base.py", MICRO, TYPE_BUG_FIX,
+   "fixed capture in shell_exec2 for old python, where readlines() would hang",
+   NULL
+ } ,
+
+ { 12,  Mar, 2015, RCR, "afni_util.py", MICRO, TYPE_ENHANCE,
+   "implemented fast=0 in get/show_process_stack",
+   NULL
+ } ,
+
  { 11,  Mar, 2015, RCR, "afni_util.py", MINOR, TYPE_ENHANCE,
    "added covary and linear_fit; -listfunc takes -/stdin to read from stdin",
    NULL
