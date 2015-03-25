12bb54dead445071a3c3612accdc65e6c93c1fbf
RWCox <robertcox@mail.nih.gov>
2015-03-25 13:00:34 -0400

    afni.c tweak

--- src/afni.c
+++ src/afni.c
@@ -1629,7 +1629,10 @@ void AFNI_sigfunc_alrm(int sig)
      "  They mean:         I am implacably opposed to your proposal"                          ,
 
      "\n  If reasonable priors lead to different conclusions, then it's time to\n"
-     "  look for more data, think harder, mumble inaudibly, or take a wild guess"
+     "  look for more data, think harder, mumble inaudibly, or take a wild guess"             ,
+
+     "\n  To be stupid, selfish, and have good health are three requirements for\n"
+     "   happiness; though if stupidity is lacking, all is lost.\n"
    } ;
 #undef NTOP
 #ifdef USE_SONNETS
