df630c4b27c931a775ef697f62a34f23244996bb
Rick Reynolds <reynoldr@mail.nih.gov>
2015-04-29 16:32:58 -0400

    allow unknown ecodes

--- src/nifti/niftilib/nifti1_io.c
+++ src/nifti/niftilib/nifti1_io.c
@@ -4570,8 +4570,8 @@ static int nifti_fill_extension( nifti1_extension *ext, const char * data,
               (void *)ext, data, len);
       return -1;
    } else if( ! nifti_is_valid_ecode(ecode) ){
-      fprintf(stderr,"** fill_ext: invalid ecode %d\n", ecode);
-      return -1;
+      fprintf(stderr,"** warning: writing unknown ecode %d\n", ecode);
+      /* should not be fatal    29 Apr 2015 [rickr] */
    }
 
    /* compute esize, first : len+8, and take ceiling up to a mult of 16 */
@@ -4701,8 +4701,8 @@ int valid_nifti_extensions(const nifti_image * nim)
    for ( c = 0; c < nim->num_ext; c++ ){
       if( ! nifti_is_valid_ecode(ext->ecode) ) {
          if( g_opts.debug > 1 )
-            fprintf(stderr,"-d ext %d, invalid code %d\n", c, ext->ecode);
-         errs++;
+            fprintf(stderr,"-d ext %d, unknown code %d\n", c, ext->ecode);
+         /* should not be fatal    29 Apr 2015 [rickr] */
       }
 
       if( ext->esize <= 0 ){
@@ -4761,7 +4761,7 @@ static int nifti_check_extension(nifti_image *nim, int size, int code, int rem)
    if( ! nifti_is_valid_ecode(code) ) {
       if( g_opts.debug > 2 )
          fprintf(stderr,"-d invalid extension code %d\n",code);
-      return 0;
+      /* should not be fatal    29 Apr 2015 [rickr] */
    }
 
    if( size < 16 ){
