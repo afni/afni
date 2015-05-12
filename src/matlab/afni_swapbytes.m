11e04a7072ea43b1a7d60a23ed54ab561895ecf0
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-05-12 17:37:42 +0200

    NF: new feature swapbytes that works for single arrays in Octave

---
+++ src/matlab/afni_swapbytes.m
@@ -0,0 +1,31 @@
+function y=afni_swapbytes(x)
+% Perform byteswap for input data of type int, single or double
+
+    c=class(x);
+
+    switch c
+        case {'int8','uint8'}
+            % single byte, no swap needed
+            y=x;
+            return;
+
+        case {'int16','uint16'}
+            nbytes=2;
+
+        case {'int32','uint32','single'}
+            nbytes=4;
+
+        case {'int64','uint64','double'}
+            nbytes=8;
+
+        otherwise
+            error('Cannot swap bytes for input of type ''%s''', c);
+    end
+
+    % convert x to bytes, with the k-th column the byte representation of
+    % the k-th element of x
+    x_bytes=reshape(typecast(x,'uint8'),nbytes,[]);
+
+    % swap each columna and convert back to original format
+    y=reshape(typecast(x_bytes(end:-1:1,:),c),size(x));
+
