3c2e32a28712e8130dd9e52cedb880c6e56a84e6
rick reynolds <reynoldr@mail.nih.gov>
2015-05-27 09:33:39 -0400

    demo var

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 27,  May, 2015, RCR, "@Install_TSrestMovieDemo", MICRO, TYPE_BUG_FIX,
+   "set and applied $demo as Suma_TSrestMovieDemo",
+   NULL
+ } ,
+
  { 26,  May, 2015, RCR, "3dBlurToFWHM", MICRO, TYPE_MODIFY,
    "make -help output consistent in using FWHM (along with 3dLocalstat)",
    NULL
