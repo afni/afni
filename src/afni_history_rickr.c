1937add78a527456d2c75005acf688d937d90eff
rick reynolds <reynoldr@mail.nih.gov>
2015-10-26 13:58:43 -0400

    TSNR censor

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 26,  Oct, 2015, RCR, "afni_proc.py", MINOR, TYPE_MODIFY,
+   "compute TSNR restricted to uncensored TRs",
+   NULL
+ } ,
+
  { 26,  Oct, 2015, RCR, "afni", MICRO, TYPE_MODIFY,
    "move version to show_AFNI_version and call on -ver",
    NULL
