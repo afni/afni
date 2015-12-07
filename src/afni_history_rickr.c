69180a901d7ad1982692a6c0b90e5a0024cfc410
rick reynolds <reynoldr@mail.nih.gov>
2015-12-07 10:26:44 -0500

    AP help updates

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ {  7,  Dec, 2015, RCR, "afni_proc.py", MICRO, TYPE_ENHANCE,
+   "help update: modify example 11 to use SurfVol and add FREESURFER NOTE",
+   NULL
+ } ,
+
  { 17,  Nov, 2015, RCR, "afni-general", MICRO, TYPE_GENERAL,
    "rename g_info to g_dicom_ctrl to avoid FreeBSD build conflict",
    "Thanks to J Bacon for noting the conflict."
