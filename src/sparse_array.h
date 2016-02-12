5583cad1c3b45d7af0734939ebf445da05e23122
Ubuntu <ubuntu@ip-172-31-52-10.ec2.internal>
2016-02-12 05:33:57 +0000

    integrated sparse array in to power methods

--- src/sparse_array.h
+++ src/sparse_array.h
@@ -15,8 +15,15 @@ struct _sparse_array_node
    sparse_array_node* next; 
 };
 
+typedef struct _sparse_array_head_node
+{
+   long num_nodes;
+   sparse_array_node* nodes; 
+} sparse_array_head_node;
+
+
 /* function for freeing a sparse array */
-sparse_array_node* free_sparse_array( sparse_array_node* sparray );
+sparse_array_head_node* free_sparse_array( sparse_array_head_node* sparray );
 
 /* function for creating a sparse array from the thresholded
    correlation matrix of a vectim.
@@ -27,17 +34,20 @@ sparse_array_node* free_sparse_array( sparse_array_node* sparray );
    inputs:
        xvectim: the input time courses that will be correlated
        sparsity: the fraction of the top correlations that should be retained
-       threshold: a threshold that should be applied to determine if a correlation 
-           should be retained. For sparsity thresholding this value will be used
-           as an inititial guess to speed calculation and a higher threshold may
-           ultimately be calculated through the adaptive process.
+       threshold: a threshold that should be applied to determine if a
+           correlation should be retained. For sparsity thresholding this value
+           will be used as an inititial guess to speed calculation and a higher
+           threshold may ultimately be calculated through the adaptive process.
 
     output:
-        sparse_array_node: the list of remaining correlation values, or NULL if there
-             was an error
+        sparse_array_node: the list of remaining correlation values, or NULL
+        if there was an error
 
     note:
-        this function can use a _lot_ of memory if you the sparsity is too high, we implement
-        a crude check to try and avoid killing the system, but it may not always work!*/
-sparse_array_node* create_sparse_corr_array( MRI_vectim* xvectim, double sparsity, double threshold, double (*corfun)(long,float*,float*), long memory_allowance  );
+        this function can use a _lot_ of memory if you the sparsity is too
+        high, we implement a crude check to try and avoid killing the system,
+        but it may not always work!*/
+sparse_array_head_node* create_sparse_corr_array( MRI_vectim* xvectim,
+    double sparsity, double threshold, double (*corfun)(long,float*,float*),
+    long memory_allowance  );
 #endif
