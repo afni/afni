f532e2f841ae0c0e5bb7d223c2ca5cd639ad5bbb
Ubuntu <ubuntu@ip-172-31-52-10.ec2.internal>
2016-02-12 01:41:54 +0000

    began adding sparse arrays to 3dECM

---
+++ src/sparse_array.h
@@ -0,0 +1,43 @@
+#ifndef _sparse_array_h
+
+#define _sparse_array_h
+
+#include "mrilib.h"
+
+/* define sparse array node data structure */
+typedef struct _sparse_array_node sparse_array_node;
+
+struct _sparse_array_node
+{
+   long row;
+   long column;
+   double weight;
+   sparse_array_node* next; 
+};
+
+/* function for freeing a sparse array */
+sparse_array_node* free_sparse_array( sparse_array_node* sparray );
+
+/* function for creating a sparse array from the thresholded
+   correlation matrix of a vectim.
+
+   This approach uses a histogram approach to implement a sparsity threshold if
+   it is so desired.
+
+   inputs:
+       xvectim: the input time courses that will be correlated
+       sparsity: the fraction of the top correlations that should be retained
+       threshold: a threshold that should be applied to determine if a correlation 
+           should be retained. For sparsity thresholding this value will be used
+           as an inititial guess to speed calculation and a higher threshold may
+           ultimately be calculated through the adaptive process.
+
+    output:
+        sparse_array_node: the list of remaining correlation values, or NULL if there
+             was an error
+
+    note:
+        this function can use a _lot_ of memory if you the sparsity is too high, we implement
+        a crude check to try and avoid killing the system, but it may not always work!*/
+sparse_array_node* create_sparse_corr_array( MRI_vectim* xvectim, double sparsity, double threshold, double (*corfun)(long,float*,float*), long memory_allowance  );
+#endif
