d43d7dd534e8fb1479bfb633e04f7992a6062b18
rick reynolds <reynoldr@mail.nih.gov>
2016-03-31 16:30:39 -0400

    no db.AFNI.args

--- src/R_scripts/3dMEMA.R
+++ src/R_scripts/3dMEMA.R
@@ -2115,7 +2115,10 @@ tTop <- 100   # upper bound for t-statistic
    if (!exists('.DBG_args')) { 
       args = (commandArgs(TRUE))  
       rfile <- first.in.path(sprintf('%s.R',ExecName))  
-      try(save(args, rfile, file=".3dMEMA.dbg.AFNI.args", ascii = TRUE), silent=TRUE)
+      # do not save these log files on -help    31 Mar 2016 [rickr]
+      if ( ! '-help' %in% args ) {
+         try(save(args, rfile, file=".3dMEMA.dbg.AFNI.args", ascii = TRUE), silent=TRUE)
+      }
    } else {
       note.AFNI("Using .DBG_args resident in workspace");
       args <- .DBG_args
