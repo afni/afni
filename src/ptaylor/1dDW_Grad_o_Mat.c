87a5d06ed98d3ffa710860d35a0bbc9113bc4890
mrneont <neon.taylor@gmail.com>
2016-09-14 16:16:59 -0400

    grad_o_mat

--- src/ptaylor/1dDW_Grad_o_Mat.c
+++ src/ptaylor/1dDW_Grad_o_Mat.c
@@ -1060,7 +1060,7 @@ int main(int argc, char *argv[])
    fclose(fout);
 
    INFO_message("\t-> DONE with grad/matr. "
-                "Check output file '%s' for results\n\n",
+                "Check output file '%s'\n\n",
                 Fname_output);
 
    if( BVAL_OUT_SEP ) {
@@ -1202,7 +1202,7 @@ int main(int argc, char *argv[])
 
 
    if(dwset) {
-      INFO_message("\t->DONE with data_set. Check '%s'\n\n",
+      INFO_message("\t-> DONE with data_set. Check '%s'\n\n",
                    DSET_FILECODE(dwout));
    }
    exit(0);   
