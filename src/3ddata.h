c8d06e6799f53f6c067651783b71b2e51ca0647b
RWCox <robertcox@mail.nih.gov>
2017-01-30 21:08:50 -0500

    update 3dTcorr1D to enable -short

--- src/3ddata.h
+++ src/3ddata.h
@@ -5796,7 +5796,7 @@ extern float THD_dice_coef_f_masked(int,float *,float *,byte *);/* 28 Jul'15 */
 extern THD_3dim_dataset * THD_Tcorr1D(THD_3dim_dataset *xset,
                               byte *mask, int nmask,
                               MRI_IMAGE *ysim,
-                              char *smethod, char *prefix); /* Apr. 2014 */
+                              char *smethod, char *prefix,int do_short); /* Apr. 2014 */
 extern float THD_quantile_corr( int,float *,float *) ;  /* 10 May 2012 */
 extern float quantile_corr( int n , float *x , float rv , float *r ) ;
 extern void THD_quantile_corr_setup( int ) ;
