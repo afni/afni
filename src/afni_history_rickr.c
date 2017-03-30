d1edb690eac305383bc0c92a7e77f456e4f069c6
rick reynolds <reynoldr@mail.nih.gov>
2017-03-30 15:18:39 -0400

    subj_dir

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 30,  Mar, 2017, RCR, "uber_subject.py", MICRO, TYPE_ENHANCE,
+   "allow subj_dir to affect the GUI (so no subject_results)",
+   NULL
+ } ,
+
  { 30,  Mar, 2017, RCR, "gen_ss_review_scripts.py", MICRO, TYPE_MODIFY,
    "run 3dclust -DAFNI_ORIENT=RAI to match SET_DICOM_XYZ coordinate order",
    "Suggested by J Rajendra."
