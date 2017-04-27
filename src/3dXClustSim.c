9dd4061d9e88d10e25583f89a1c173d476fee3aa
RWCox <robertcox@mail.nih.gov>
2017-04-27 10:09:55 -0400

    tweak 3dXClustSim

--- src/3dXClustSim.c
+++ src/3dXClustSim.c
@@ -1157,7 +1157,7 @@ int main( int argc , char *argv[] )
          if( nfff > nfom ) nfff = nfom ; /* very very unlikely */
 
          fmax = AFNI_numenv("AFNI_XCLUSTSIM_FMAX") ;
-         if( fmax <= 0.01f || fmax > 1.0f ) fmax = 0.567f ;
+         if( fmax <= 0.01f || fmax > 1.0f ) fmax = 0.666f ;
 
          /* global threshold computed from tail of FOM distribution */
 
