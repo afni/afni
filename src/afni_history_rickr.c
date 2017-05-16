830b83282a0977fe57b7431cdd2e57b6ad812aa4
rick reynolds <reynoldr@mail.nih.gov>
2017-05-16 16:32:22 -0400

    hist

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 16,  May, 2017, RCR, "meica.py", MINOR, TYPE_ENHANCE,
+   "sync with https://bitbucket.org/prantikk/me-ica/src",
+   "Update from version 2.5 beta9 to 2.5 beta11."
+ } ,
+
  { 10,  May, 2017, RCR, "model_conv_PRF_DOG", MINOR, TYPE_NEW_PROG,
    "new model: same as PRF, but Difference of Gaussians",
    "For E Silson and C Baker."
