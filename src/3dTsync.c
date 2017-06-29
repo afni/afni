e59ae6c57b7019a3078b3780e7cd6506f4d5f441
RWCox <robertcox@mail.nih.gov>
2017-06-28 18:30:46 -0700

    add stuff to 3dTsync

--- src/3dTsync.c
+++ src/3dTsync.c
@@ -6,7 +6,7 @@
 
 int   verb   = 0 ;
 char *prefix = "Tsync" ;
-THD_3dim_dataset *dsetB=NULL , *dsetC=NULL , *maskset=NULL ;
+THD_3dim_dataset *dsetB=NULL , *dsetC=NULL , *maskset=NULL , *outset=NULL ;
 
 /*----------------------------------------------------------------------------*/
 
@@ -121,8 +121,8 @@ static void compute_brainsync( int m , int n ,
    for( jj=0 ; jj < m ; jj++ ){
      for( ii=0 ; ii < m ; ii++ ){
        bsum = 0.0 ;
-       for( kk=0 ; kk < mm ; kk++ ) bsum += V(ii,kk)*U(jj,kk) ;
-       OO(ii,jj) = bsum ;
+       for( kk=0 ; kk < m ; kk++ ) bsum += V(ii,kk)*U(jj,kk) ;
+       OO(ii,jj) = (float)bsum ;
    }}
 
    free(umat) ; free(vmat) ;
@@ -140,10 +140,86 @@ static int is_vector_constant( int n , float *v )
 
 /*----------------------------------------------------------------------------*/
 
+void TSY_process_data(void)
+{
+   byte *vmask ;
+   int nvmask , ii,jj,kk , mm , nvox ;
+   MRI_IMAGE *bim, *cim ;
+   float *bar, *car, *bmat, *cmat, *omat, *cvec, csum ;
+
+   /* build mask */
+
+   nvox = DSET_NVOX(dsetB) ;
+
+   if( maskset != NULL ){  /*** explicit mask ***/
+     vmask = THD_makemask( maskset , 0 , 1.0f,0.0f ) ;
+     DSET_unload(maskset) ;
+     if( vmask == NULL )
+       ERROR_exit("Can't make mask from -mask dataset '%s'",DSET_BRIKNAME(maskset)) ;
+     nvmask = THD_countmask( nvox , vmask ) ;
+     INFO_message("%d voxels in the spatial mask",nvmask) ;
+     if( nvmask == 0 )
+       ERROR_exit("Mask from -mask dataset %s has 0 voxels",DSET_BRIKNAME(maskset)) ;
+   } else {               /*** all voxels */
+     vmask = (byte *)malloc(sizeof(byte)*(nvox+2)) ; nvmask = nvox ;
+     for( jj=0 ; jj < nvox ; jj++ ) vmask[jj] = 1 ;
+     INFO_message("no -mask option ==> processing all %d voxels in dataset",nvox) ;
+   }
+
+   /* load datasets into matrices */
+
+   mm   = DSET_NVALS(dsetB) ;
+   bmat = (float *)calloc( sizeof(float) , mm*nvmask ) ;
+   cmat = (float *)calloc( sizeof(float) , mm*nvmask ) ;
+   omat = (float *)calloc( sizeof(float) , mm*mm     ) ;
+
+   for( ii=0 ; ii < mm ; ii++ ){
+     bim = THD_extract_float_brick( ii , dsetB ) ; bar = MRI_FLOAT_PTR(bim) ;
+     cim = THD_extract_float_brick( ii , dsetC ) ; car = MRI_FLOAT_PTR(cim) ;
+     for( kk=jj=0 ; jj < nvox ; jj++ ){
+       if( vmask[jj] ){
+         bmat[ii+kk*mm] = bar[jj] ;
+         cmat[ii+kk*mm] = car[jj] ; kk++ ;
+       }
+     }
+     mri_free(bim) ; mri_free(cim) ;
+   }
+   DSET_unload(dsetB) ; DSET_unload(dsetC) ;
+
+   /* compute orthogonal matrix omat */
+
+   compute_brainsync( mm , nvmask , bmat , cmat , omat ) ;
+
+   /* transform input matric C */
+
+   free(bmat) ;
+   cvec = (float *)calloc( sizeof(float) , mm ) ;
+
+#undef  OO
+#undef  C
+#define OO(i,j) omat[(i)+(j)*mm]
+#define C(i,j)  cmat[(i)+(j)*mm]
+
+   for( jj=0 ; jj < nvmask ; jj++ ){
+     for( ii=0 ; ii < mm ; ii++ ){
+       csum = 0.0f ;
+       for( kk=0 ; kk < mm ; kk++ ) csum += OO(ii,kk) * C(kk,jj) ;
+       cvec[ii] = csum ;
+     }
+     for( ii=0 ; ii < mm ; ii++ ) C(ii,jj) = cvec[ii] ;
+   }
+
+   free(cvec) ; free(omat) ;
+
+   /* load transformed C matrix into output dataset */
+
+}
+
+/*----------------------------------------------------------------------------*/
+
 int main( int argc , char *argv[] )
 {
-   int iarg=1 , ct , nact=0 ;
-   int nbl=0 , *blist=NULL , tt , noblock=0 ;
+   int iarg=1 , mm,nn , ct ;
 
    /*----------*/
 
@@ -237,7 +313,7 @@ int main( int argc , char *argv[] )
      ERROR_exit("-inset1 only has %d time points -- need at least 10 :(",mm) ;
    if( DSET_NVALS(dsetC) != mm )
      ERROR_exit("-inset1 has %d time points but -inset2 has %d -- they should match :(",
-                mm , DSET_NVALS(dsetC) ;
+                mm , DSET_NVALS(dsetC) ) ;
 
    if( maskset != NULL && !EQUIV_GRIDXYZ(dsetB,maskset) )
      ERROR_exit("-mask and -inset1 datsets are NOT on the same 3D grid :(") ;
