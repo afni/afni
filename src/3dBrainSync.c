fbc91d7a1512dd68e25c6143754f11ba8a047d14
RWCox <robertcox@mail.nih.gov>
2017-07-28 15:23:58 -0400

    3dBrainSync tweaks

--- src/3dBrainSync.c
+++ src/3dBrainSync.c
@@ -5,6 +5,7 @@
 #endif
 
 #undef MEMORY_CHECK
+#if 0
 #ifdef USING_MCW_MALLOC
 # define MEMORY_CHECK(mm)                                               \
    do{ if( verb > 5 ) mcw_malloc_dump() ;                               \
@@ -16,7 +17,10 @@
          ININFO_message(" status = %s",mcw_malloc_status(NULL,0)) ;     \
        }                                                                \
    } while(0)
-#else
+#endif
+#endif
+
+#ifndef MEMORY_CHECK
 # define MEMORY_CHECK(mm) /*nada*/
 #endif
 
@@ -25,6 +29,9 @@ static char *prefix = "brainsync" ;
 static char *matpre = NULL ;
 static int do_joshi = 1 ;
 static int do_norm  = 0 ;
+
+static int ct ;
+#define TIMER nice_time_string(NI_clock_time()-ct)
 
 static THD_3dim_dataset *dsetB=NULL, *dsetC=NULL, *maskset=NULL, *outset=NULL ;
 
@@ -57,19 +64,23 @@ void TSY_help_the_pitiful_user(void)
    " -normalize          = Normalize the output dataset so that each\n"
    "                       time series has sum-of-squares = 1.\n"
    "\n"
-   " -mask mset          = Only operate on voxels nonzero in the mset dataset.\n"
-   "                       ++ Voxels outside the mask will be filled with zeros.\n"
+   " -mask mset          = Only operate on nonzero voxels in the mset dataset.\n"
+   "                       ++ Voxels outside the mask will not be used in computing\n"
+   "                          the transformation matrix Q, but WILL be transformed\n"
+   "                          for your edification later.\n"
+   "                       ++ For FMRI purposes, a gray matter mask would make\n"
+   "                          sense here, or at least a brain mask.\n"
    "                       ++ If no masking option is given, then all voxels\n"
-   "                          will be processed.\n"
+   "                          will be processed in computing Q.\n"
    "                       ++ Any voxel which is all constant in time\n"
    "                          (in either input) will be added to the mask.\n"
    "                       ++ This dataset must be on the same spatial grid\n"
-   "                          as the other input datasets.\n"
+   "                          as the other input datasets!\n"
    "\n"
    " -joshi              = Use the Joshi method, with an orthogonal matrix\n"
    " -orthogonal           for the transformation. [This is the default]\n"
    "                       ++ At present, there is no other method implemented,\n"
-   "                          so this option is VERY optional.\n"
+   "                          so this option is VERY optional :)\n"
    "\n"
 #if 0
    " -non-orthgonal      = Allow the use of a non-orthgonal matrix for the\n"
@@ -84,6 +95,7 @@ void TSY_help_the_pitiful_user(void)
    "NOTES:\n"
    "------\n"
    "* Is this program useful? Not even The Shadow knows!\n"
+   "  (But don't call it BS.)\n"
    "\n"
    "* The output dataset is in floating point format.\n"
    "\n"
@@ -146,7 +158,7 @@ void TSY_help_the_pitiful_user(void)
 static void compute_joshi_matrix( int m , int n ,
                                   float *bmat , float *cmat , float *qmat )
 {
-   int ii,jj,kk ;
+   int ii,jj,kk , kbot ;
    double *bmatn , *cmatn ;
    double *amat , *umat , *vmat , *sval ;
    register double bsum , csum ;
@@ -180,6 +192,8 @@ ENTRY("compute_joshi_matrix") ;
 
    /* copy input matrices into bmatn and cmatn, normalizing as we go */
 
+   ININFO_message("Normalizing time series [%s]",TIMER) ;
+
    for( jj=0 ; jj < n ; jj++ ){
      bsum = csum = 0.0 ;
      for( ii=0 ; ii < m ; ii++ ){
@@ -197,10 +211,14 @@ MEMORY_CHECK("a") ;
 
    /* form A matrix = BN * CN' */
 
+   ININFO_message("forming BC' matrix [%s]",TIMER) ;
+
+   kbot = n%2 ; /* 1 if odd, 0 if even */
    for( jj=0 ; jj < m ; jj++ ){
      for( ii=0 ; ii < m ; ii++ ){
-       bsum = 0.0 ;
-       for( kk=0 ; kk < n ; kk++ ) bsum += BN(ii,kk) * CN(jj,kk) ;
+       bsum = (kbot) ? BN(ii,0)*CN(jj,0) : 0.0 ;
+       for( kk=kbot ; kk < n ; kk+=2 ) /* unrolled by 2 */
+         bsum += BN(ii,kk)*CN(jj,kk) + BN(ii,kk+1)*CN(ii,kk+1) ;
        A(ii,jj) = bsum ;
    }}
 MEMORY_CHECK("b") ;
@@ -212,13 +230,15 @@ MEMORY_CHECK("b") ;
 
    /* compute SVD of scaled matrix */
 
-MEMORY_CHECK("c1") ;
+   ININFO_message("SVD-ing BC' matrix %dx%d [%s]",m,m,TIMER) ;
+
    svd_double( m , m , amat , sval , umat , vmat ) ;
-MEMORY_CHECK("c2") ;
 
    free(amat) ; free(sval) ;
 
    /* compute QQ = output matrix = U V' */
+
+   ININFO_message("computing Q matrix [%s]",TIMER) ;
 
    for( jj=0 ; jj < m ; jj++ ){
      for( ii=0 ; ii < m ; ii++ ){
@@ -240,6 +260,7 @@ MEMORY_CHECK("d") ;
      qim = mri_new_vol_empty( m,m,1, MRI_float ) ;
      mri_fix_data_pointer( qmat , qim ) ;
      mri_write( fname , qim ) ;
+     ININFO_message("wrote Q matrix to %s [%s]",fname,TIMER) ;
      mri_clear_data_pointer( qim ) ; mri_free(qim) ; free(fname) ;
    }
 
@@ -260,7 +281,7 @@ static void compute_nonorth_matrix( int m , int n ,
 {
 ENTRY("compute_nonorth_matrix") ;
 
-   /* a little more code needed here */
+   /* a little more code needed here :) */
 
    EXRETURN ;
 }
@@ -274,6 +295,73 @@ static int is_vector_constant( int n , float *v )
    return (ii==n) ;
 }
 
+/*------------------- 08 Oct 2010: functions for despiking ----------------*/
+
+#undef  SWAP
+#define SWAP(x,y) (temp=x,x=y,y=temp)
+
+#undef  SORT2
+#define SORT2(a,b) if(a>b) SWAP(a,b)
+
+/*--- fast median of 9 values ---*/
+
+static INLINE float median9f(float *p)
+{
+    register float temp ;
+    SORT2(p[1],p[2]) ; SORT2(p[4],p[5]) ; SORT2(p[7],p[8]) ;
+    SORT2(p[0],p[1]) ; SORT2(p[3],p[4]) ; SORT2(p[6],p[7]) ;
+    SORT2(p[1],p[2]) ; SORT2(p[4],p[5]) ; SORT2(p[7],p[8]) ;
+    SORT2(p[0],p[3]) ; SORT2(p[5],p[8]) ; SORT2(p[4],p[7]) ;
+    SORT2(p[3],p[6]) ; SORT2(p[1],p[4]) ; SORT2(p[2],p[5]) ;
+    SORT2(p[4],p[7]) ; SORT2(p[4],p[2]) ; SORT2(p[6],p[4]) ;
+    SORT2(p[4],p[2]) ; return(p[4]) ;
+}
+#undef SORT2
+#undef SWAP
+
+/*--- get the local median and MAD of values vec[j-4 .. j+4] ---*/
+
+#undef  mead9
+#define mead9(j)                                               \
+ { float qqq[9] ; int jj = (j)-4 ;                             \
+   if( jj < 0 ) jj = 0; else if( jj+8 >= num ) jj = num-9;     \
+   qqq[0] = vec[jj+0]; qqq[1] = vec[jj+1]; qqq[2] = vec[jj+2]; \
+   qqq[3] = vec[jj+3]; qqq[4] = vec[jj+4]; qqq[5] = vec[jj+5]; \
+   qqq[6] = vec[jj+6]; qqq[7] = vec[jj+7]; qqq[8] = vec[jj+8]; \
+   med    = median9f(qqq);     qqq[0] = fabsf(qqq[0]-med);     \
+   qqq[1] = fabsf(qqq[1]-med); qqq[2] = fabsf(qqq[2]-med);     \
+   qqq[3] = fabsf(qqq[3]-med); qqq[4] = fabsf(qqq[4]-med);     \
+   qqq[5] = fabsf(qqq[5]-med); qqq[6] = fabsf(qqq[6]-med);     \
+   qqq[7] = fabsf(qqq[7]-med); qqq[8] = fabsf(qqq[8]-med);     \
+   mad    = median9f(qqq); }
+
+/*-------------------------------------------------------------------------*/
+/*! Remove spikes from a time series, in a very simplistic way.
+    Return value is the number of spikes that were squashed [RWCox].
+*//*-----------------------------------------------------------------------*/
+
+static int despike9( int num , float *vec )
+{
+   int ii , nsp ; float *zma,*zme , med,mad,val ;
+
+   if( num < 9 || vec == NULL ) return 0 ;
+   zme = (float *)malloc(sizeof(float)*num) ;
+   zma = (float *)malloc(sizeof(float)*num) ;
+
+   for( ii=0 ; ii < num ; ii++ ){
+     mead9(ii) ; zme[ii] = med ; zma[ii] = mad ;
+   }
+   mad = qmed_float(num,zma) ; free(zma) ;
+   if( mad <= 0.0f ){ free(zme); return 0; }  /* should not happen */
+   mad *= 6.789f ;  /* threshold value */
+
+   for( nsp=ii=0 ; ii < num ; ii++ )
+     if( fabsf(vec[ii]-zme[ii]) > mad ){ vec[ii] = zme[ii]; nsp++; }
+
+   free(zme) ; return nsp ;
+}
+#undef mead9
+
 /*----------------------------------------------------------------------------*/
 /* compute the output dataset */
 
@@ -286,9 +374,12 @@ void TSY_process_data(void)
 
 ENTRY("TSY_process_data") ;
 
+   INFO_message("begin BrainSync [%s]",TIMER) ;
+
    /* build mask */
 
    nvox = DSET_NVOX(dsetB) ;
+   mm   = DSET_NVALS(dsetB) ;
 
    if( maskset != NULL ){  /*** explicit mask ***/
      vmask = THD_makemask( maskset , 0 , 1.0f,0.0f ) ;
@@ -296,19 +387,19 @@ ENTRY("TSY_process_data") ;
      if( vmask == NULL )
        ERROR_exit("Can't make mask from -mask dataset '%s'",DSET_BRIKNAME(maskset)) ;
      nvmask = THD_countmask( nvox , vmask ) ;
-     INFO_message("%d voxels in the spatial mask",nvmask) ;
+     ININFO_message("%d voxels in the spatial mask [%s]",nvmask,TIMER) ;
      if( nvmask == 0 )
        ERROR_exit("Mask from -mask dataset %s has 0 voxels",DSET_BRIKNAME(maskset)) ;
    } else {               /*** all voxels */
      vmask = (byte *)malloc(sizeof(byte)*(nvox+2)) ; nvmask = nvox ;
      for( jj=0 ; jj < nvox ; jj++ ) vmask[jj] = 1 ;
-     INFO_message("no -mask option ==> processing all %d voxels in dataset",nvox) ;
+     ININFO_message("no -mask option ==> processing all %d voxels in dataset [%s]",nvox,TIMER) ;
    }
 MEMORY_CHECK("P") ;
 
    /* find and eliminate any voxels with all-constant vectors */
 
-   mm  = DSET_NVALS(dsetB) ;
+   ININFO_message("looking for all-constant time series [%s]",TIMER) ;
    bar = (float *)malloc(sizeof(float)*mm) ;
    for( kk=0 ; kk < nvox ; kk++ ){
      if( vmask[kk] ){
@@ -318,16 +409,21 @@ MEMORY_CHECK("P") ;
        if( is_vector_constant(mm,bar) ){ vmask[kk] = 0 ; ncut++ ; continue ; }
      }
    }
+   free(bar) ;
    if( ncut > 0 ){
      nvmask = THD_countmask(nvox,vmask) ;
-     INFO_message("removed %d voxel%s for being constant in time" ,
-                  ncut , (ncut > 1) ? "s" : "\0" ) ;
+     ININFO_message("removed %d voxel%s for being constant in time [%s]" ,
+                    ncut , (ncut > 1) ? "s" : "\0" , TIMER ) ;
    }
+   if( nvmask <= 2*mm )
+     ERROR_exit("not enough voxels in mask to process :(") ;
 
    /* load datasets into matrices */
 
+   ININFO_message("loading datasets into matrices [%s]",TIMER) ;
+
    bmat = (float *)calloc( sizeof(float) , mm*nvmask ) ;
-   cmat = (float *)calloc( sizeof(float) , mm*nvmask ) ;
+   cmat = (float *)calloc( sizeof(float) , mm*nvox   ) ; /* extra big */
    qmat = (float *)calloc( sizeof(float) , mm*mm     ) ;
 
    for( ii=0 ; ii < mm ; ii++ ){
@@ -342,20 +438,47 @@ MEMORY_CHECK("P") ;
      mri_free(bim) ; mri_free(cim) ;
    }
 MEMORY_CHECK("Q") ;
-   DSET_unload(dsetB) ; DSET_unload(dsetC) ;
+   DSET_unload(dsetB) ;
 MEMORY_CHECK("R") ;
+
+   /* despike matrix columns */
+
+   ININFO_message("despiking time series [%s]",TIMER) ;
+
+   for( ncut=kk=0 ; kk < nvmask ; kk++ ){
+     ncut += despike9( mm , bmat+kk*mm ) ;
+     ncut += despike9( mm , cmat+kk*mm ) ;
+   }
+   if( ncut > 0 )
+     ININFO_message("removed %d spike%s from data vectors [%s]",
+                    ncut , (ncut==1)?"\0":"s" , TIMER ) ;
 
    /* compute transform matrix qmat */
 
    if( do_joshi )
      compute_joshi_matrix  ( mm , nvmask , bmat , cmat , qmat ) ;
-   else
+   else  /* not implemented */
      compute_nonorth_matrix( mm , nvmask , bmat , cmat , qmat ) ;
-
-   /* transform input matric C */
 MEMORY_CHECK("S") ;
 
    free(bmat) ; /* not needed no more */
+
+   /* reload input matrix C with ALL voxels, not just mask */
+
+   if( nvmask < nvox ){
+     ININFO_message("reloading C matrix with all voxels [%s]",TIMER) ;
+     for( ii=0 ; ii < mm ; ii++ ){
+       cim = THD_extract_float_brick( ii , dsetC ) ; car = MRI_FLOAT_PTR(cim) ;
+       for( jj=0 ; jj < nvox ; jj++ ) cmat[ii+jj*mm] = car[jj] ;
+       mri_free(cim) ;
+     }
+   }
+   DSET_unload(dsetC) ;
+
+   /* now transform input matrix C */
+
+   ININFO_message("transforming dataset [%s]",TIMER) ;
+
    cvec = (float *)calloc( sizeof(float) , mm ) ;
 
 #undef  QQ
@@ -363,7 +486,7 @@ MEMORY_CHECK("S") ;
 #define QQ(i,j) qmat[(i)+(j)*mm]
 #define C(i,j)  cmat[(i)+(j)*mm]
 
-   for( jj=0 ; jj < nvmask ; jj++ ){
+   for( jj=0 ; jj < nvox ; jj++ ){
      for( ii=0 ; ii < mm ; ii++ ){
        csum = 0.0f ;
        for( kk=0 ; kk < mm ; kk++ ) csum += QQ(ii,kk) * C(kk,jj) ;
@@ -385,6 +508,8 @@ MEMORY_CHECK("T") ;
 
    /* load transformed C matrix into output dataset */
 
+   ININFO_message("filling output dataset [%s]",TIMER) ;
+
    outset = EDIT_empty_copy( dsetB ) ;
    EDIT_dset_items( outset ,
                       ADN_prefix    , prefix ,
@@ -393,9 +518,7 @@ MEMORY_CHECK("T") ;
                     ADN_none ) ;
    for( ii=0 ; ii < mm ; ii++ ){
      car = (float *)calloc(sizeof(float),nvox) ;
-     for( kk=jj=0 ; jj < nvox ; jj++ ){
-       if( vmask[jj] ){ car[jj] = C(ii,kk) ; kk++ ; }
-     }
+     for( jj=0 ; jj < nvox ; jj++ ) car[jj] = C(ii,jj) ;
      EDIT_substitute_brick( outset , ii , MRI_float , car ) ;
    }
 MEMORY_CHECK("U") ;
@@ -408,7 +531,7 @@ MEMORY_CHECK("U") ;
 
 int main( int argc , char *argv[] )
 {
-   int iarg=1 , mm,nn , ct ;
+   int iarg=1 , mm,nn ;
 
    /*----------*/
 
@@ -449,6 +572,7 @@ int main( int argc , char *argv[] )
 
        dsetB = THD_open_dataset(argv[iarg]) ;
        CHECK_OPEN_ERROR(dsetB,argv[iarg]);
+       DSET_mallocize(dsetB) ;
        iarg++ ; continue ;
      }
 
@@ -462,6 +586,7 @@ int main( int argc , char *argv[] )
 
        dsetC = THD_open_dataset(argv[iarg]) ;
        CHECK_OPEN_ERROR(dsetC,argv[iarg]);
+       DSET_mallocize(dsetC) ;
        iarg++ ; continue ;
      }
 
@@ -543,6 +668,7 @@ int main( int argc , char *argv[] )
    if( maskset != NULL && !EQUIV_GRIDXYZ(dsetB,maskset) )
      ERROR_exit("-mask and -inset1 datsets are NOT on the same 3D grid :(") ;
 
+   INFO_message("reading datasets [%s]",TIMER) ;
    DSET_load(dsetB) ; CHECK_LOAD_ERROR(dsetB) ;
    DSET_load(dsetC) ; CHECK_LOAD_ERROR(dsetC) ;
    if( maskset != NULL ){ DSET_load(maskset) ; CHECK_LOAD_ERROR(maskset) ; }
