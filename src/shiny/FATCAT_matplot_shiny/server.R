f12a9079e3bbd9bc3d6003be8109330f9a1a44cc
discoraj <discoraj@gmail.com>
2017-10-31 15:47:12 -0400

    FATCAT_matplot added circos plots

--- src/shiny/FATCAT_matplot_shiny/server.R
+++ src/shiny/FATCAT_matplot_shiny/server.R
@@ -6,60 +6,69 @@
 ######################################################################################
 ## code for server for input and output
 shinyServer(function(input,output,session) {
+  
+  ## hide some warnings and quit app when browser/tab closes
   options(warn =-1)
   session$onSessionEnded(stopApp)
+  
   ############################################
   ## get the rois and info
-
   observe({
     ## get the selected file and info
     num.rois <- as.numeric(strsplit(readLines(input$net_file,n=1),
                                     split=' ')[[1]][2])
     num.mat <- as.numeric(strsplit(readLines(input$net_file,n=2)[2],
                                    split=' ')[[1]][2])
-
+    
     ## rois for choices
     roi.lab <- as.matrix(read.table(input$net_file,nrows=1,header=FALSE))
     if(is.numeric(roi.lab)){ roi.lab <- paste0('roi_',roi.lab) }
-
+    
     stat.list <- c()
     stat.names <- c()
     start.line <- 5
     withProgress(message = 'Finding stat types.', value = 0, {
       for(i in 1:num.mat){
-
+        
         ## cut out the stat and add to list
         stat.type <- as.character(scan(input$net_file,what='character',n=2,
                                        skip=start.line,nlines=1,
                                        quiet=TRUE)[2][1])
         stat.list <- rbind(stat.list,stat.type[1])
-
+        
         ## get the pretty name and add to name list
         name.temp <- ifelse(stat.type[1] %in% stat.df$label,
                             stat.df$description[stat.df$label == stat.type[1]],
                             stat.type[1])
         stat.names <- rbind(stat.names,name.temp)
-
+        
         start.line <- 5 + (num.rois*i+1*i)
         incProgress(1/num.mat,detail=stat.type[[1]])
       }
       names(stat.list) <- stat.names
       updateSelectInput(session,'stat_sel',choices=stat.list)
     })
-    ## if there are too many regions...
     updateSelectInput(session,'rois',choices=roi.lab,selected=roi.lab)
   })   ## end observe for rois
-
-  ## threshold
-  observe({
-    t.min <- round(input$range_min,2)
-    t.max <- round(input$range_max,2)
-    updateSliderInput(session,'cor_thresh',value=c((t.max+t.min)/2-0.1,
-                                                   (t.max+t.min)/2+0.1),
-                      min=input$range_min,
-                      max=input$range_max)
+  
+  ############################################
+  ## color min/max reset
+  observeEvent(input$col_thresh,{
+    if(length(input$rois) > 1){
+      ## get the data and subset and find min max
+      net.mat <- mat_read()
+      net.mat <- net.mat[rownames(net.mat) %in% input$rois, ]
+      net.mat <- net.mat[, colnames(net.mat) %in% input$rois]
+      data.range <- range(net.mat)
+      
+      ## reset if they select no
+      if(input$col_thresh == 'No'){
+        updateNumericInput(session,'range_min',value=data.range[1])
+        updateNumericInput(session,'range_max',value=data.range[2])
+      }
+    }
   })
-
+  
   ############################################
   ## read matrices
   mat_read <- reactive({
@@ -71,7 +80,7 @@ shinyServer(function(input,output,session) {
                             [[1]][2])
       roi.lab <- as.matrix(read.table(input$net_file,nrows=1,header=FALSE))
       if(is.numeric(roi.lab)){ roi.lab <- paste0('roi_',roi.lab) }
-
+      
       ## read in data
       net.mat <- as.matrix(fread(input$net_file,skip=input$stat_sel,
                                  nrows=num.rois,header=FALSE))
@@ -79,7 +88,7 @@ shinyServer(function(input,output,session) {
         net.mat <- net.mat[,1:ncol(net.mat)-1]
       }
       rownames(net.mat) <- colnames(net.mat) <- roi.lab
-
+      
       data.range <- range(net.mat)
       updateNumericInput(session,'range_min',value=data.range[1])
       updateNumericInput(session,'range_max',value=data.range[2])
@@ -90,22 +99,23 @@ shinyServer(function(input,output,session) {
       return(net.mat)
     }
   })   ## end mat_read
-
+  
+  ############################################
   ## get the data reorganized with the clustering
   heat_data <- reactive({
-
+    
     if(length(input$rois) < 2){ stop('ERROR: select more than one ROI!') }
-
+    
     ## get data
     net.mat <- mat_read()
     net.mat <- net.mat[rownames(net.mat) %in% input$rois, ]
     net.mat <- net.mat[, colnames(net.mat) %in% input$rois]
     if(length(net.mat) < 2){ stop('select more than one ROI!') }
-
+    
     ## suppress the stupid extra pdf
     if(length(dev.list()) > 0 ){ graphics.off() }
     pdf(NULL)
-
+    
     ## cluster if you want
     if(input$h_clust != 'none'){
       if(input$dist_meth == "canberra" & min(net.mat,na.rm=TRUE) <= 0){
@@ -122,7 +132,7 @@ shinyServer(function(input,output,session) {
                        breaks=seq(input$range_min,input$range_max,
                                   length.out=101))
     }  ## end cluster check
-
+    
     ## threshold and remove zeros
     if(input$thresh_yn){
       hm2$carpet[hm2$carpet > input$thresh_min &
@@ -133,15 +143,13 @@ shinyServer(function(input,output,session) {
   })   ## end heat_data
   
   ############################################
-  ## outputs
-
   ## plot heat map
   output$cor_heatmap_plot <- renderPlotly({
-
+    
     ## get data and make sure it is enough
     heat.mat <- heat_data()
     if(length(heat.mat) < 2){ stop('select more than one ROI!') }
-
+    
     ## select upper/lower/full
     if(input$tri_sel == 'Upper'){
       heat.mat <- heat.mat[,rev(seq.int(ncol(heat.mat)))]
@@ -156,7 +164,7 @@ shinyServer(function(input,output,session) {
     ## suppress the stupid extra pdf
     if(length(dev.list()) > 0 ){ graphics.off() }
     pdf(NULL)
-
+    
     ## get title checking if clustered
     if(input$h_clust != 'none'){
       if(input$dist_meth == "canberra" & min(heat.mat,na.rm=TRUE) <= 0){
@@ -171,19 +179,21 @@ shinyServer(function(input,output,session) {
                           stat.df$description[stat.df$label == input$stat_sel],
                           sep='\n')
     }
-
+    
     ## make sure something is left
-    if(all(is.na(heat.mat))){
-      stop('Nothing survives thresholding!')
-    }
-
+    if(all(is.na(heat.mat))){ stop('Nothing survives thresholding!') }
+    
     ## plot margins and color
     lab.width <- max(strwidth(rownames(heat.mat),'inches')) * 96
     if(lab.width < 125){ lab.width <- 125 }
     m <- list(l=lab.width,r=lab.width,b=lab.width,t=lab.width,pad=4)
-    col.ramp <- colorRampPalette(c(input$col1,input$col2,input$col3),
-                                 alpha=TRUE)
-
+    if(input$col_2_3 == 2){
+      col.ramp <- colorRampPalette(c(input$col1,input$col3),alpha=FALSE)
+    } else {
+      col.ramp <- colorRampPalette(c(input$col1,input$col2,input$col3),
+                                   alpha=FALSE)
+    }
+    
     ## plot
     plot.ly <- plot_ly(x=~colnames(heat.mat),y=~rownames(heat.mat),
                        z=~heat.mat,
@@ -193,13 +203,13 @@ shinyServer(function(input,output,session) {
     layout(plot.ly,xaxis=list(title=''),yaxis=list(title=''),margin=m,
            title=plot.title)
   })   ## end heat map
-
+  
   ############################################
   ## download static version
   output$downloadPlot <- downloadHandler(
-    filename = function() {
-      paste0(tools::file_path_sans_ext(basename(input$net_file)),'_',
-                                input$stat_sel,'_heatmap.png') },
+    filename = function(){
+      paste0(basename(input$net_file),'_',input$stat_sel,'_heatmap.png')
+    },
     content = function(file) {
       ## get data and make sure it is enough and select rois
       net.mat <- mat_read()
@@ -208,22 +218,26 @@ shinyServer(function(input,output,session) {
       if(length(net.mat) < 2){ 
         showNotification("select more than one ROI!",type="error",duration=3)
         stop('select more than one ROI!') 
-        }
-
+      }
+      
       ## get the margin size from the names
       lab.width <- max(nchar(rownames(net.mat))) / 3
       if(lab.width < 10){ lab.width <- 10 }
-
+      
       ## suppress the stupid extra pdf
       pdf(NULL)
-
+      
+      ## colors and labels
+      if(input$col_2_3 == 2){
+        col.ramp <- colorRampPalette(c(input$col1,input$col3),alpha=FALSE)
+      } else {
+        col.ramp <- colorRampPalette(c(input$col1,input$col2,input$col3),
+                                     alpha=FALSE)
+      }
+      stat.lab <- stat.df$description[stat.df$label == input$stat_sel]
+      
       ############################################
       ## calculate first
-      col.ramp <- colorRampPalette(c(input$col1,input$col2,input$col3),
-                                   alpha=TRUE)
-      stat.lab <- stat.df$description[stat.df$label == input$stat_sel]
-
-      ## cluster if you want
       if(input$h_clust != 'none'){
         if(input$dist_meth == "canberra" & min(net.mat,na.rm=TRUE) <= 0){
           showNotification("Canberra does not work with none positive values!",
@@ -238,7 +252,7 @@ shinyServer(function(input,output,session) {
                          hclustfun=function(c) hclust(c,method=input$h_clust))
         plot.title <- paste(basename(input$net_file),stat.lab,
                             paste0('clust: ',input$h_clust,
-                                  ', dist: ',input$dist_meth),sep='\n')
+                                   ', dist: ',input$dist_meth),sep='\n')
       } else {
         hm2 <- heatmap.2(net.mat,Rowv=FALSE,
                          breaks=seq(input$range_min,input$range_max,
@@ -246,20 +260,20 @@ shinyServer(function(input,output,session) {
                          trace='none',symm=TRUE,revC=FALSE)
         plot.title <- paste(basename(input$net_file),stat.lab,sep='\n')
       }
-
+      
       ## threshold and remove zeros
       if(input$thresh_yn){
         hm2$carpet[hm2$carpet > input$thresh_min &
                      hm2$carpet < input$thresh_max] <- NA
       }
       if(input$zero_yn){  hm2$carpet[hm2$carpet == 0] <- NA }
-
+      
       ############################################
       ## plot
       ## open graphics device (close any old ones just in case)
       graphics.off()
       png(file,width=20,height=20,units='in',res=input$static_dpi)
-
+      
       ## different orders depending on clustering
       if(input$h_clust != 'none'){
         ## select upper/lower/full
@@ -272,7 +286,7 @@ shinyServer(function(input,output,session) {
           hm2$carpet[(lower.tri(hm2$carpet,diag=TRUE))] <- NA
           hm2$carpet <- hm2$carpet[,rev(seq_len(ncol(hm2$carpet)))]
         }
-
+        
         ## hide/show dendro
         if(input$dendro){
           show_dendro <- 'column'
@@ -305,10 +319,10 @@ shinyServer(function(input,output,session) {
           hm2$carpet[(lower.tri(hm2$carpet,diag=TRUE))] <- NA
           hm2$carpet <- hm2$carpet[,rev(seq_len(ncol(hm2$carpet)))]
         }
-
+        
         revRowInd <- match(c(1:length(hm2$rowInd)), hm2$rowInd)
         revColInd <- match(c(1:length(hm2$colInd)), hm2$colInd)
-
+        
         heatmap.2(t(hm2$carpet[revRowInd, revColInd]),
                   col=col.ramp(100),breaks=seq(input$range_min,input$range_max,
                                                length.out=101),
@@ -322,17 +336,17 @@ shinyServer(function(input,output,session) {
                   colsep=seq(1,ncol(hm2$carpet),2),
                   rowsep=seq(1,nrow(hm2$carpet),2))
       }  ## end cluster check
-
+      
       dev.off()
     })   ## end static heatmap
-
+  
   ############################################
   ## plot histogram
   output$cor_hist_plot <- renderPlotly({
     ## get data and make sure it is enough
     cor.in <- heat_data()
     if(length(cor.in) < 2){ stop('select more than one ROI!') }
-
+    
     ## select upper/lower/full
     if(input$tri_sel == 'Upper'){
       cor.in <- cor.in[,rev(seq.int(ncol(cor.in)))]
@@ -346,11 +360,11 @@ shinyServer(function(input,output,session) {
     
     ## as vector
     cor.in <- c(cor.in)
-
+    
     ## plot margins and color
     stat.lab <- stat.df$description[stat.df$label == input$stat_sel]
     m <- list(l=100,r=100,b=100,t=125,pad=4)
-
+    
     ## plot
     plot.ly <- plot_ly(x=~cor.in,type="histogram",
                        marker=list(color='lightblue',
@@ -359,14 +373,14 @@ shinyServer(function(input,output,session) {
            title=paste(basename(input$net_file),stat.lab,sep='\n'),
            margin=m)
   })   ## end histogram
-
+  
   ############################################
   ## circos
   output$downloadCircos <- downloadHandler(
-    filename = function() {
-      paste0(tools::file_path_sans_ext(basename(input$net_file)),'_',
-             input$stat_sel,'_circos.png') },
-    content = function(file) {
+    filename = function(){
+      paste0(basename(input$net_file),'_',input$stat_sel,'_circos.png') 
+    },
+    content = function(file){
       
       ## read in data, check it and mirror it
       net.mat <- heat_data()
@@ -376,14 +390,14 @@ shinyServer(function(input,output,session) {
       ## show the wait
       showNotification("Creating Circos PNG.",type="error",
                        id="circos",duration=0)
-
+      
       net.upper <- upper.tri(net.mat,diag=FALSE)
       net.df <- data.frame(seg1=rownames(net.mat)[row(net.mat)[net.upper]],
                            start1=0,end1=3,
                            seg2=rownames(net.mat)[col(net.mat)[net.upper]],
                            start2=0,end2=3,
                            value=(net.mat)[net.upper])
-
+      
       ## make segments for rois 
       roi.lab <- t(rownames(net.mat))
       roi.seg <- data.frame(seg.name=NULL,seg.Start=NULL,
@@ -398,10 +412,14 @@ shinyServer(function(input,output,session) {
       ## get the angles from the segments
       roi.seg.cust <- segAnglePo(roi.seg,seg=roi.lab)
       
-      ## colors and label
-      col.ramp <- colorRampPalette(c(input$col1,input$col3),
-                                   alpha=TRUE)
-
+      ## colors and labels
+      if(input$col_2_3 == 2){
+        col.ramp <- colorRampPalette(c(input$col1,input$col3),alpha=TRUE)
+      } else {
+        col.ramp <- colorRampPalette(c(input$col1,input$col2,input$col3),
+                                     alpha=TRUE)
+      }
+      
       color_fun <- function(x,n=10000){ col.ramp(n)[cut(x,n)] }
       stat.lab <- stat.df$description[stat.df$label == input$stat_sel]
       plot.title <- paste(basename(input$net_file),stat.lab,sep='\n')
@@ -415,18 +433,98 @@ shinyServer(function(input,output,session) {
       par(mar=c(2,2,2,2))
       plot(c(1,input$circos_dim*100),c(1,input$circos_dim*100),type="n",
            axes=FALSE,xlab="",ylab="",main=plot.title)
-
+      
       circos(R=circ.center*0.7,xc=circ.center,yc=circ.center,cir=roi.seg.cust,
              type="chr",print.chr.lab=TRUE,W=10,cex=1,col='black')
       circos(R=circ.center*0.68,xc=circ.center,yc=circ.center,cir=roi.seg.cust,
              W=40,mapping=net.df,type="link.pg",lwd=1,
              col=color_fun(net.df$value))
       dev.off()
-
+      
       removeNotification(id="circos")
       
     })   ## end circos
   
+  ############################################
+  ## log file download
+  output$downloadLog <- downloadHandler(
+    filename = function(){
+      paste0(basename(input$net_file),'_',input$stat_sel,'_log.csv')
+    },
+    content = function(file){
+      
+      ## read in data, check it and mirror it
+      heat.mat <- heat_data()
+      if(length(heat.mat) < 2){ stop('select more than one ROI!') }
+      
+      ## mirror then reverse rows
+      heat.mat <- heat.mat[,c(nrow(heat.mat):1),drop=FALSE]
+      heat.mat <- heat.mat[nrow(heat.mat):1,]
+      
+      ## select upper/lower/full (reversed to match above)
+      if(input$tri_sel == 'Lower'){
+        heat.mat <- heat.mat[,rev(seq.int(ncol(heat.mat)))]
+        heat.mat[(upper.tri(heat.mat,diag=TRUE))] <- NA
+        heat.mat <- heat.mat[,rev(seq_len(ncol(heat.mat)))]
+      } else if(input$tri_sel == 'Upper'){
+        heat.mat <- heat.mat[,rev(seq.int(ncol(heat.mat)))]
+        heat.mat[(lower.tri(heat.mat,diag=TRUE))] <- NA
+        heat.mat <- heat.mat[,rev(seq_len(ncol(heat.mat)))]
+      }
+      
+      ## un-mirror and add labels as the first column
+      heat.mat <- heat.mat[,c(nrow(heat.mat):1),drop=FALSE]
+      out.mat <- data.frame(Label=row.names(heat.mat))
+      out.mat <- cbind(out.mat,heat.mat)
+      
+      ## paste as comma separated strings per row
+      out.data <- apply(out.mat,1,paste,collapse=",")
+
+      ## main header description
+      header.lab <- paste0("FileName,Stat,LowPass,HighPass,Min,Max,Matrix,",
+                           "RemoveZero,ClusterLinkage,DistanceMethod,",
+                           "ColorLow,ColorMid,ColorHigh")
+      
+      ## some checks
+      if(input$thresh_yn){
+        t.min <- input$thresh_min
+        t.max <- input$thresh_max
+      } else {
+        t.min <- NA
+        t.max <- NA
+      }
+      if(input$h_clust == 'none'){ 
+        dist.meth <- "none" 
+      } else {
+        dist.meth <- input$dist_meth
+      }
+      if(input$col_2_3 == 2){ 
+        col.2 <- "none" 
+      } else {
+        col.2 <- input$col2 
+      }
+      
+      header.info <- paste(c(input$net_file,input$stat_sel,t.min,t.max,
+                           input$range_min,input$range_max,input$tri_sel,
+                           input$zero_yn,input$h_clust,dist.meth,
+                           input$col1,col.2,input$col3),
+                           collapse=",")
+            
+      ## open file
+      datafile <- file(file, open = 'wt')
+      
+      ## write out
+      writeLines(header.lab,con=datafile)
+      writeLines(header.info,con=datafile)
+      writeLines(" ",con=datafile)
+      
+      writeLines(paste(t(colnames(out.mat)),collapse=","),con=datafile)
+        
+      writeLines(out.data,con=datafile)
+        
+
+      close(datafile)
+    })
   ############################################
   ## help links
   output$hclust_link <- renderUI({
@@ -439,5 +537,5 @@ shinyServer(function(input,output,session) {
            href='https://stat.ethz.ch/R-manual/R-devel/library/stats/html/dist.html',
            'Distance descriptions link')
   })
-
+  
 })   ## end server
