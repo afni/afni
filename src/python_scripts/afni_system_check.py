6cc034afde47e6fae4a39e444d817dca41d160d4
rick reynolds <reynoldr@mail.nih.gov>
2017-11-07 11:33:54 -0500

    more

--- src/python_scripts/afni_system_check.py
+++ src/python_scripts/afni_system_check.py
@@ -5,7 +5,15 @@
 # system libraries
 import sys, os
 
-# AFNI libraries
+# AFNI libraries (test first)
+import module_test_lib
+g_testlibs = ['option_list', 'afni_util', 'lib_system_check']
+if module_test_lib.num_import_failures(g_testlibs,details=0,verb=0):
+   print("\n** failed to load standard AFNI python libraries")
+   print("   python version = %s" % sys.version.split()[0])
+   sys.exit(1)
+
+# now load AFNI libraries by name
 import option_list as OL
 import afni_util as UTIL        # not actually used, but probably will be
 import lib_system_check as SC
