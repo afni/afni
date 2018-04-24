039358e31c4b9667c4e6492f568b2ad273a2af51
rick reynolds <reynoldr@mail.nih.gov>
2018-04-24 15:05:45 -0400

    gen_epi_reivew.py for python3

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 24, Apr, 2018, RCR, "gen_epi_review.py", MINOR, TYPE_ENHANCE,
+   "python3 compatible as of version 0.4",
+   NULL
+ } ,
+
  { 24, Apr, 2018, RCR, "auto_warp.py", MINOR, TYPE_ENHANCE,
    "python3 compatible as of version 0.4",
    NULL
