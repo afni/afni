9dd4965c7801b7ae7576fd90c09b5b86064a082b
rick reynolds <reynoldr@mail.nih.gov>
2018-04-25 14:45:44 -0400

    meica numpy array, compare via _is_ None

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,12 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 25, Apr, 2018, RCR, "meica.py", MINOR, TYPE_BUG_FIX,
+   "deal with numpy update that fails for 'array == None', use 'is None'",
+   "Thanks to dowdlele on MB for noting this and pointing to the emdupre\n"
+   "update on https://github.com/ME-ICA/me-ica."
+ } ,
+
  { 25, Apr, 2018, RCR, "gen_ss_review_scripts.py", MINOR, TYPE_ENHANCE,
    "python3 compatible as of version 1.0",
    NULL
