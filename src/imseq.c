c82fe23ffc658efa6199acc466323b81859ef141
RWCox <robertcox@mail.nih.gov>
2018-05-10 09:54:26 -0400

    no more AFNI_ENFORCE_ASPECT

--- src/imseq.c
+++ src/imseq.c
@@ -1133,7 +1133,7 @@ if( PRINT_TRACING ){
                           | KeyPressMask        /* get keystrokes */
                           | ButtonPressMask     /* button presses */
                           | ExposureMask        /* exposures */
-                          | StructureNotifyMask /* resizes */
+                          | StructureNotifyMask /* resizes (Configure events) */
                           | Button1MotionMask   /* motion while #1 is down */
                           | ButtonReleaseMask   /* button releases */
                          ,
@@ -1650,7 +1650,7 @@ if( PRINT_TRACING ){
                             0
                           | ButtonPressMask      /* button presses */
                           | ExposureMask         /* exposures */
-                          | StructureNotifyMask  /* resizes */
+                          | StructureNotifyMask  /* resizes (Configure events) */
                          ,
                          FALSE ,                 /* nonmaskable events? */
                          ISQ_drawing_EV ,        /* super-handler! */
@@ -6185,6 +6185,20 @@ STATUS("scroll wheel ==> change slice") ;
          if( am_active ) break ;      /* prevent recursion */
          am_active = 1 ;
 
+#if 0
+         /* Scan forward and see if another such event is
+            pending.  If so, don't process this one.
+            [doesn't work well - can result in not redrawing at proper size?] */
+
+         { XEvent evjunk ; int egood ;
+           egood = XCheckWindowEvent(XtDisplay(w),XtWindow(w),StructureNotifyMask,&evjunk) ;
+           if( egood && evjunk.type == ConfigureNotify ){
+             am_active = 0 ; break ;
+           }
+         }
+#endif
+
+
  if(PRINT_TRACING){
   char str[256] ;
   sprintf(str," .. ConfigureNotify: width=%d height=%d",
@@ -6197,15 +6211,15 @@ STATUS("scroll wheel ==> change slice") ;
 
          if( w == seq->wimage ){
 
-            int ntime=NI_clock_time(); /* 09 May 2018 */
             int nx,ny ;
+#if 0
+            int ntime=NI_clock_time(); /* 09 May 2018 [useless?] */
             static int ltime=-666;
-
-            if( ntime-ltime < 2 ){ am_active = 0 ; break ; }
+            if( ntime-ltime < 2 ){ am_active = 0 ; break ; } /* too fast? */
             ltime = ntime ;
+#endif
 
             MCW_widget_geom( seq->wimage , &nx , &ny , NULL,NULL ) ;
-          
             if( (seq->sized_xim == NULL)       ||
                 (nx != seq->sized_xim->width ) ||    /* modified 09 May 2018 */
                 (ny != seq->sized_xim->height)   ){  /* to check nx and ny */
@@ -6221,15 +6235,17 @@ STATUS("scroll wheel ==> change slice") ;
 fprintf(stderr,"ConfigureNotify: width=%d height=%d\n",event->width,event->height);
 #endif
 
+#if 0 /* removed 10 May 2018 */
                if( AFNI_yesenv("AFNI_ENFORCE_ASPECT") && !seq->opt.free_aspect ){
-                 static int last_time=0 ; int now_time=NI_clock_time() ;
-                 if( now_time == 0 || now_time-last_time > 33 )
+                 static int last_time=-666 ; int now_time=NI_clock_time() ;
+                 if( now_time-last_time > 555 )
                    ISQ_reset_dimen( seq, seq->last_width_mm, seq->last_height_mm ) ;
 #if 0
 else fprintf(stderr,"  -- too soon to enforce aspect!\n") ;
 #endif
                  last_time = now_time ;
                }
+#endif
 
                /*-- now show the image in the new window size --*/
 
@@ -6240,10 +6256,11 @@ INFO_message("ConfigureNotify") ;
             }
 
          } else if( w == seq->wbar ){
-
-             if( (seq->sized_xbar == NULL)                  ||
-                 (event->width  != seq->sized_xbar->width ) ||
-                 (event->height != seq->sized_xbar->height)   ){
+             int nx,ny ;
+             MCW_widget_geom( seq->wbar , &nx , &ny , NULL,NULL ) ;
+             if( (seq->sized_xbar == NULL)       ||
+                 (nx != seq->sized_xbar->width ) ||
+                 (ny != seq->sized_xbar->height)   ){
 
                KILL_2ndXIM( seq->given_xbar , seq->sized_xbar ) ;
                ISQ_show_bar( seq ) ;
