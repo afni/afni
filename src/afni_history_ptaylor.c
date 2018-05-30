33df428aeea0bac7b0d7d1fef5195108a67e8d0f
mrneont <neon.taylor@gmail.com>
2018-05-29 20:07:58 -0400

    chauffeur: cropability

--- src/afni_history_ptaylor.c
+++ src/afni_history_ptaylor.c
@@ -66,6 +66,11 @@
 afni_history_struct ptaylor_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+{ 29, May , 2018 , PT , "@chauffeur_afni" , MINOR , TYPE_NEW_OPT,
+   "Can crop the saved images.",
+   "See the '-crop_*' options.\n"
+},
+
 { 27, May , 2018 , PT , "3dClusterize" , MINOR , TYPE_GENERAL,
    "Make report cleaner, and add in INT_MAP property to output clust map.",
    "Thanks, D. Glen for more useful suggestions.\n"
