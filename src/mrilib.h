cd21d3d6c5e6f307561f3a445def85df683541f7
RWCox <robertcox@mail.nih.gov>
2018-06-16 12:15:50 +0800

    tweak mrilib.h

--- src/mrilib.h
+++ src/mrilib.h
@@ -1186,7 +1186,6 @@ extern void mri_flatfilter_usedxyz  ( int i ) ;
 
 void mri_Set_KO_catwrap(void);
 void mri_Set_OK_catwrap(void);
-void mri_Set_OK_catwrap(void);
 void mri_Set_OK_catrandwrap(void);
 extern MRI_IMAGE * mri_cat2D( int,int,int,void *,MRI_IMARR *) ;
 extern MRI_IMARR * mri_uncat2D( int , int , MRI_IMAGE * im ) ; /* 09 May 2000 */
