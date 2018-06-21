9b50b679c9e42ab9417d396f6aadbd3a9e66db4a
rick reynolds <reynoldr@mail.nih.gov>
2018-06-21 16:09:11 -0400

    more help

--- src/get_afni_model_PRF_6.c
+++ src/get_afni_model_PRF_6.c
@@ -17,7 +17,21 @@ int main(int argc, char * argv[])
 
    if( argc != 7 ) {
       fprintf(stderr, "** usage: %s A x y sigma sigrat theta\n", argv[0]);
-      return 1;
+      fprintf(stderr,
+"\n"
+"   consider something like:\n"
+"\n"
+"       setenv AFNI_CONVMODEL_REF conv.ref.GAM.1D\n"
+"       setenv AFNI_MODEL_PRF_STIM_DSET stim.144.LIA.bmask.resam+orig\n"
+"       setenv AFNI_MODEL_PRF_ON_GRID NO\n"
+"\n"
+"       setenv AFNI_MODEL_PRF_GAUSS_FILE gauss_dset\n"
+"\n"
+"       # A=2, x=0.2, y=0.5,  sigma=0.05  sigrat=5  theta=PI/8=0.3927\n"
+"       get_afni_model_PRF_6 2 .2 .5 .05 5 0.3927\n"
+"\n");
+
+      return 0;
    }
 
    /* fill parameter array */
