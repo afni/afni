bb06ad240b7354a85245658ea8a5b38068cedd12
RWCox <robertcox@mail.nih.gov>
2018-08-23 12:07:02 -0400

    tweak 1dsound

--- src/1dsound.c
+++ src/1dsound.c
@@ -5,17 +5,26 @@
 void usage_1dsound(int detail)
 {
    printf(
+     "\n"
      "Usage: 1dsound [options] tsfile\n"
+     "\n"
+     "Program to create a sound file from a 1D file (column of numbers).\n"
      "\n"
      "-------\n"
      "OPTIONS\n"
      "-------\n"
      " -prefix ppp  = Output filename will be ppp.au\n"
-     "                [Sun audio format]\n"
+     "                [old Sun audio format]\n"
      " -8PCM        = Output in 8-bit linear PCM encoding\n"
      "                [default is 8-bit mu-law encoding]\n"
      " -tper X      = X seconds of sound per time point in 'tsfile'\n"
      "                [default is 0.5 s]\n"
+     "\n"
+     "--------\n"
+     "EXAMPLES\n"
+     "--------\n"
+     "  1dsound -prefix fred '1D: 0 1 2 1 0 1 2 0 1 2'\n"
+     "  1deval -num 200 -expr 'sin(x)' | 1dsound -tper 0.1 -prefix ethel 1D:stdin\n"
      "\n"
      "-----\n"
      "NOTES\n"
@@ -25,9 +34,9 @@ void usage_1dsound(int detail)
      "    [min to max in tsfile]\n"
      "* File can be played with the 'sox' audio library command\n"
      "    play ppp.au gain -5\n"
-     "  Here 'gain -5' turns the volume down.\n"
+     "  + Here 'gain -5' turns the volume down.\n"
      "  + sox is not provided with AFNI :(\n"
-     "  + to see if sox is on your system, type the command 'which sox'\n"
+     "  + To see if sox is on your system, type the command 'which sox'\n"
      "* Creation of the file does not depend on sox, so if you have\n"
      "  another way to play .au files, you can use that.\n"
      "  * Mac OS X: Quicktime (GUI) or afplay (command line)\n"
@@ -57,10 +66,9 @@ int main( int argc , char *argv[] )
 
    /*---------- startup bureaucracy ----------*/
 
-#if 0
+   if( argc < 2 ){ usage_1dsound(1) ; exit(0) ; }
+
    mainENTRY("1dsound main"); machdep();
-   PRINT_VERSION("1dsound"); AUTHOR("RWC et al.");
-#endif
 
    /*------------ scan arguments that X11 didn't eat ------------*/
 
@@ -76,6 +84,8 @@ int main( int argc , char *argv[] )
      }
 
      if( strcmp(argv[iarg],"-prefix") == 0 ){
+       if( iarg >= argc-1 )
+         ERROR_exit("need arg after %s",argv[iarg]) ;
        prefix = strdup(argv[++iarg]) ; iarg++ ; continue ;
      }
 
@@ -84,11 +94,12 @@ int main( int argc , char *argv[] )
      }
 
      if( strcmp(argv[iarg],"-tper") == 0 || strcmp(argv[iarg],"-dt") == 0 ){
+       if( iarg >= argc-1 )
+         ERROR_exit("need arg after %s",argv[iarg]) ;
        tper = (float)strtod(argv[++iarg],NULL) ;
        if( tper < 0.01f || tper > 1.00f )
          ERROR_exit("1dsound: %s %s is out of range 0.01..1.0",
                     argv[iarg-1],argv[iarg]) ;
-       nsper = (int)rintf( SRATE * tper ) ;
        iarg++ ; continue ;
      }
 
@@ -109,6 +120,8 @@ int main( int argc , char *argv[] )
    if( inim == NULL )
      ERROR_exit("Can't read input file '%s' iarg=%d\n",argv[iarg],iarg) ;
 
+   nsper = (int)rintf( SRATE * tper ) ;
+
    phim = mri_sound_1D_to_FM( inim ,
                               0.0f , 0.0f , SRATE , nsper ) ;
 
@@ -117,12 +130,14 @@ int main( int argc , char *argv[] )
 
    if( phim != NULL ){
      char fname[1024] ;
-     sprintf(fname,"%s.au",prefix) ;
+     if( STRING_HAS_SUFFIX(prefix,".au") ) strcpy(fname,prefix) ;
+     else                                  sprintf(fname,"%s.au",prefix) ;
      if( do_8PCM ){
-       sound_write_au_8PCM( fname, phim->nx, MRI_FLOAT_PTR(phim), SRATE, 0.2f ) ;
+       sound_write_au_8PCM( fname, phim->nx, MRI_FLOAT_PTR(phim), SRATE, 0.2f );
      } else {
-       sound_write_au_ulaw( fname, phim->nx, MRI_FLOAT_PTR(phim), SRATE, 0.2f ) ;
+       sound_write_au_ulaw( fname, phim->nx, MRI_FLOAT_PTR(phim), SRATE, 0.2f );
      }
+     INFO_message("output sound file %s",fname) ;
    }
 
    exit(0) ;
