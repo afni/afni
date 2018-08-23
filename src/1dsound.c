c4af3c762c62c39ad5084a0a05ec67a9da43ed99
RWCox <robertcox@mail.nih.gov>
2018-08-23 11:37:54 -0400

    new program 1dsound

--- src/1dsound.c
+++ src/1dsound.c
@@ -10,7 +10,31 @@ void usage_1dsound(int detail)
      "-------\n"
      "OPTIONS\n"
      "-------\n"
-     " -prefix p  = Output filename\n"
+     " -prefix ppp  = Output filename will be ppp.au\n"
+     "                [Sun audio format]\n"
+     " -8PCM        = Output in 8-bit linear PCM encoding\n"
+     "                [default is 8-bit mu-law encoding]\n"
+     " -tper X      = X seconds of sound per time point in 'tsfile'\n"
+     "                [default is 0.5 s]\n"
+     "\n"
+     "-----\n"
+     "NOTES\n"
+     "-----\n"
+     "* At this time, program is very limited in what it does:\n"
+     "  + frequency modulated between 110 and 1760 Hz\n"
+     "    [min to max in tsfile]\n"
+     "* File can be played with the 'sox' audio library command\n"
+     "    play ppp.au gain -5\n"
+     "  Here 'gain -5' turns the volume down.\n"
+     "  + sox is not provided with AFNI :(\n"
+     "  + to see if sox is on your system, type the command 'which sox'\n"
+     "* Creation of the file does not depend on sox, so if you have\n"
+     "  another way to play .au files, you can use that.\n"
+     "  * Mac OS X: Quicktime (GUI) or afplay (command line)\n"
+     "  * Linux:    sox is probably the simplest\n"
+     "\n"
+     "--- Quick hack for experimentation - RWCox - Aug 2018 ---\n"
+     "\n"
    ) ;
 
    return;
@@ -20,12 +44,17 @@ void usage_1dsound(int detail)
 /* This program is a very elaborate wrapper for the plot_ts.c functions. */
 /*---------------------------------------------------------------------------*/
 
+#define SRATE 48000
+
 int main( int argc , char *argv[] )
 {
    int iarg ;
    char *prefix = "sound.au" ;
    MRI_IMAGE *inim , *phim ;
    float *far ;
+   int do_8PCM=0 ;
+   float tper=0.5f ; int nsper ;
+
    /*---------- startup bureaucracy ----------*/
 
 #if 0
@@ -50,6 +79,19 @@ int main( int argc , char *argv[] )
        prefix = strdup(argv[++iarg]) ; iarg++ ; continue ;
      }
 
+     if( strcmp(argv[iarg],"-8PCM") == 0 ){
+       do_8PCM = 1 ; iarg++ ; continue ;
+     }
+
+     if( strcmp(argv[iarg],"-tper") == 0 || strcmp(argv[iarg],"-dt") == 0 ){
+       tper = (float)strtod(argv[++iarg],NULL) ;
+       if( tper < 0.01f || tper > 1.00f )
+         ERROR_exit("1dsound: %s %s is out of range 0.01..1.0",
+                    argv[iarg-1],argv[iarg]) ;
+       nsper = (int)rintf( SRATE * tper ) ;
+       iarg++ ; continue ;
+     }
+
      /*--- symplectically stoopid user ---*/
 
      ERROR_message("Unknown option: %s\n",argv[iarg]) ;
@@ -68,7 +110,7 @@ int main( int argc , char *argv[] )
      ERROR_exit("Can't read input file '%s' iarg=%d\n",argv[iarg],iarg) ;
 
    phim = mri_sound_1D_to_FM( inim ,
-                              0.0f , 0.0f , 8000 , 4000 ) ;
+                              0.0f , 0.0f , SRATE , nsper ) ;
 
    if( phim == NULL )
      ERROR_exit("mri_sound_1D_to_FM fails") ;
@@ -76,11 +118,11 @@ int main( int argc , char *argv[] )
    if( phim != NULL ){
      char fname[1024] ;
      sprintf(fname,"%s.au",prefix) ;
-#if 0
-     sound_write_au_ulaw( fname, phim->nx, MRI_FLOAT_PTR(phim), 8000, 0.05f ) ;
-#else
-     sound_write_au_8bitPCM( fname, phim->nx, MRI_FLOAT_PTR(phim), 8000, 0.05f ) ;
-#endif
+     if( do_8PCM ){
+       sound_write_au_8PCM( fname, phim->nx, MRI_FLOAT_PTR(phim), SRATE, 0.2f ) ;
+     } else {
+       sound_write_au_ulaw( fname, phim->nx, MRI_FLOAT_PTR(phim), SRATE, 0.2f ) ;
+     }
    }
 
    exit(0) ;
