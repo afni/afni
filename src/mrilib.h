3eea6d0605c99a96ea1017a2cd70490458f7b52c
RWCox <robertcox@mail.nih.gov>
2018-09-12 16:59:24 -0400

    begin support for .tsv files

--- src/mrilib.h
+++ src/mrilib.h
@@ -2405,6 +2405,7 @@ extern void set_sound_note_type( char *typ ) ;
 extern void set_sound_gain_value( int ggg ) ;
 extern void set_sound_twotone( int ggg ) ;      /* do not use this */
 extern char * get_sound_player(void) ;
+extern void sound_set_note_ADSR(int) ;
 
 extern void sound_write_au_header( FILE *fp, int nn, int srate, int code ) ;
 extern void sound_write_au_ulaw( char *fname, int nn, float *aa, int srate, float scl ) ;
