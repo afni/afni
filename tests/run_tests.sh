3f4ddcf09102fc712bb9853bce5b8ad414c54a95
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-09-12 16:45:54 -0400

    update test scripts

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,17 @@
-#! /bin/bash
+#!/bin/bash
+
+set -ex
+
+# Download data.
 git clone https://github.com/nih-fmrif/afni_test_data /usr/afni_build_dir/tests/pytest_tests/afni_test_data
 curl -fL https://afni.nimh.nih.gov/pub/dist/edu/data/AFNI_data6.tgz | tar xzC /usr/afni_build_dir/tests/pytest_tests/afni_test_data
+
+# Run tests.
 cd /usr/afni_build_dir/tests
 pytest
 afni_system_check.py -check_all
 cd /usr/afni_build_dir/src/afni_src
 gcov *.c
+
+# QUESTION(kaczmrj): should this be codecov?
 coverage
