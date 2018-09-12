aadcd75077fdd98df9362287f06213ee7ee2f5aa
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-09-12 15:44:11 -0400

    download afni data6

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 git clone https://github.com/nih-fmrif/afni_test_data /usr/afni_build_dir/tests/pytest_tests/afni_test_data
+curl -fL https://afni.nimh.nih.gov/pub/dist/edu/data/AFNI_data6.tgz | tar xzC /usr/afni_build_dir/tests/pytest_tests/afni_test_data
 cd /usr/afni_build_dir/tests
 pytest
 afni_system_check.py -check_all
