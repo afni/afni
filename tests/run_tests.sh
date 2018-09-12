658db7b2d9a772f6da98a063afda58e59d900046
shotgunosine <adenosine@gmail.com>
2018-09-12 15:31:12 -0400

    Add proof of concept executable tests with coverage

---
+++ tests/run_tests.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+git clone https://github.com/nih-fmrif/afni_test_data /usr/afni_build_dir/tests/pytest_tests/afni_test_data
+cd /usr/afni_build_dir/tests
+pytest
+afni_system_check.py -check_all
+cd /usr/afni_build_dir/src/afni_src
+gcov *.c
+coverage
