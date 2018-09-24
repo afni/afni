c5956f3f748e16dad7deaa1be037cc9bf9b6699f
rick reynolds <reynoldr@mail.nih.gov>
2018-09-24 10:26:21 -0400

    mpeg_encode -help status

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 24, Sep, 2018, RCR, "afni-general", MICRO, TYPE_MODIFY,
+   "have programs return 0 on terminal options, like -help",
+   "Adding: mpeg_encode."
+ } ,
+
  { 21, Sep, 2018, RCR, "afni-general", MICRO, TYPE_MODIFY,
    "separate testGL build target from SUMA_PROGS",
    "Again, for accuracy of 'make prog_list'."
