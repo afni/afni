bf165d0fbec660ffaad40d639065e1d06632741b
shotgunosine <adenosine@gmail.com>
2018-09-24 13:28:42 -0400

    fix tabs

--- tests/pytest_tests/test_3dSkullStrip.py
+++ tests/pytest_tests/test_3dSkullStrip.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 def test_3dSkullStrip():
-	wd = Path(__file__).parent.as_posix()
+    wd = Path(__file__).parent.as_posix()
     print(wd)
     cp = subprocess.run(f'cd {wd}/test_dirs/3dSkullStrip && tcsh runit',
                         check=True,
