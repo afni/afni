2e1dfcc59f0e43e172220ecd8f0ab7645f4ebfbd
Daniel Glen <glend@mail.nih.gov>
2018-10-12 18:13:26 -0400

    long name support and auto_warp.py update

--- src/thd_ttatlas_query.c
+++ src/thd_ttatlas_query.c
@@ -42,6 +42,9 @@ static int neurosynth_link = -1;
 static int sumsdb_link = -1;
 static int linkrbrain_link = -1;
 
+/* atlas name type selected yet for region labels - name, long, both*/
+static int atlas_name_code = -1;
+
 /* global web browser is used here, not sure where else to put it...      */
 char *GLOBAL_browser = NULL ;   /* 30 Dec 2005, moved 22 Feb 2012 [rickr] */
 
@@ -3462,6 +3465,7 @@ void Set_Show_Atlas_Mode(int md)
    AtlasShowMode = md;
    return;
 }
+
 void Show_Atlas_Region (AFNI_ATLAS_REGION *aar)
 {
    int k = 0;
@@ -3487,7 +3491,11 @@ void Show_Atlas_Region (AFNI_ATLAS_REGION *aar)
       }
       fprintf(stdout,"\n");
    } else {
-      fprintf(stdout,"%c:%s:%-3d\n",
+      if((aar->longname) && (strlen(aar->longname)!=0))
+         fprintf(stdout,"%c:%s:%-3d [%s]\n",
+                     aar->side, STR_PRINT(aar->orig_label), aar->id, aar->longname);
+      else
+         fprintf(stdout,"%c:%s:%-3d\n",
                      aar->side, STR_PRINT(aar->orig_label), aar->id);
    }
 
@@ -3515,6 +3523,7 @@ AFNI_ATLAS_REGION * Free_Atlas_Region (AFNI_ATLAS_REGION *aar)
 
    if (aar->orig_label) free(aar->orig_label);
    if (aar->atlas_name) free(aar->atlas_name);
+   if (aar->longname) free(aar->longname);
    free(aar);
 
    RETURN(NULL);
@@ -3536,7 +3545,7 @@ byte Same_Chunks(AFNI_ATLAS_REGION *aar1, AFNI_ATLAS_REGION *aar2)
 /*!
    given a string, chunk it
 */
-AFNI_ATLAS_REGION * Atlas_Chunk_Label(char *lbli, int id, char *atlas_name)
+AFNI_ATLAS_REGION * Atlas_Chunk_Label(char *lbli, int id, char *atlas_name, char *longname)
 {
    AFNI_ATLAS_REGION *aar = NULL;
    char lachunk[500], sd, *lbl = NULL;
@@ -3561,6 +3570,8 @@ AFNI_ATLAS_REGION * Atlas_Chunk_Label(char *lbli, int id, char *atlas_name)
    aar = (AFNI_ATLAS_REGION*) calloc(1,sizeof(AFNI_ATLAS_REGION));
    aar->side = 'u';
    aar->orig_label = strdup(lbl);
+   if(longname) aar->longname = strdup(longname);
+   else  aar->longname = NULL;
    aar->atlas_name = NULL;
    if (atlas_name) aar->atlas_name = nifti_strdup(atlas_name); /* for clarity*/
    aar->id = id;
@@ -3706,7 +3717,8 @@ AFNI_ATLAS *Build_Atlas (char *aname, ATLAS_LIST *atlas_list)
    for (k=0; k<aa->N_regions; ++k) {
       aa->reg[k] = Atlas_Chunk_Label(atlas->adh->apl2->at_point[k].name,
                                      atlas->adh->apl2->at_point[k].tdval,
-                                     Atlas_Name(atlas));
+                                     Atlas_Name(atlas),
+                                     atlas->adh->apl2->at_point[k].longname);
       /* Show_Atlas_Region (aa->reg[k]); */
    }
 
@@ -3855,7 +3867,7 @@ AFNI_ATLAS_REGION *ROI_String_Decode(char *str, ATLAS_LIST *atlas_list)
       fprintf(stderr,"lbl from %s(%d to %d) is : '%s'\n", str, shft, nc, lbl);
 
    /* Now get aar */
-   if (!(aar = Atlas_Chunk_Label(lbl, 0, atlas_name))) {
+   if (!(aar = Atlas_Chunk_Label(lbl, 0, atlas_name,NULL))) {
       if (LocalHead || SpeakEasy) ERROR_message("Failed in processing label");
       RETURN(aar) ;
    }
@@ -3896,9 +3908,17 @@ char *Report_Found_Regions(AFNI_ATLAS *aa, AFNI_ATLAS_REGION *ur ,
    /* do we have a search by number ? */
    if (ur->id > 0 && ur->N_chnks == 0) {
       if (as->nmatch) {
-         snprintf (lbuf, 480*sizeof(char),
+         if((ur->longname) && (strlen(ur->longname)!=0))
+            snprintf (lbuf, 480*sizeof(char),
+                     "Best match for %s [%s] (code %-3d):", ur->orig_label, ur->longname, ur->id);
+         else
+            snprintf (lbuf, 480*sizeof(char),
                      "Best match for %s (code %-3d):", ur->orig_label, ur->id);
          for (ii=0; ii<as->nmatch; ++ii) {
+            if((aa->reg[as->iloc[ii]]->longname) && (strlen(aa->reg[as->iloc[ii]]->longname)!=0))
+                snprintf (lbuf, 480*sizeof(char), "%s\n   %s [%s]", lbuf,
+                     aa->reg[as->iloc[ii]]->orig_label, aa->reg[as->iloc[ii]]->longname);
+            else
             snprintf (lbuf, 480*sizeof(char), "%s\n   %s", lbuf,
                      aa->reg[as->iloc[ii]]->orig_label);
          }
@@ -5781,6 +5801,10 @@ char * Atlas_Code_String(int c)
 
 }
 
+/* this function strips off trailing periods ......
+ * and puts the result in a static string.
+ * this was relevant for the original hard coded atlases like the Talairach Daemon
+* not so interesting today */
 char * Clean_Atlas_Label( char *lb)
 {
    static char lab_buf[256];
@@ -6732,11 +6756,79 @@ char *NoLeftRight (char *name)
    else RETURN(name);
 }
 
+
+/* get atlas name type - show either short name, long name or both together
+ * This applies to individual region labels like
+*   amyl, amygdala or "amyl amygdala" */ 
+int Atlas_name_type()
+{
+   char *nametype;
+
+   /* see if type is already set. Use environment only once */
+   if(atlas_name_code!=-1)
+      return(atlas_name_code);
+
+   /* maybe name something else? */
+   nametype = my_getenv("AFNI_ATLAS_NAME_TYPE");
+ 
+//   atlas_name_code = 0;
+   atlas_name_code = 2;  /* make default - show both name - short name and long name */
+
+   if(nametype!=NULL) {
+      if(!strcasecmp(nametype, "longname")) {
+         atlas_name_code = 1;
+      }
+      else if(!strcasecmp(nametype, "bothnames"))
+         atlas_name_code = 2;
+      else if(!strcasecmp(nametype, "name"))
+         atlas_name_code = 0;
+   }
+
+   return(atlas_name_code);
+}
+
+void set_atlas_name_code(int code)
+{
+   atlas_name_code = code;
+}
+
+/* return region name, long region name, or both combined */
+char *Atlas_name_choice(ATLAS_POINT *atp)
+{
+   static char tmps[600];
+
+   /* get environment setting for name, long name or both names */
+   switch(Atlas_name_type()){
+     /* just the long name */ 
+      case 1:
+          if (atp->longname && strlen(atp->longname))          
+             sprintf(tmps, "%s", atp->longname);
+          else
+             sprintf(tmps, "%s", atp->name);               
+          break;
+      /* combination - both name and long name with brackets around long name*/
+      case 2:
+          if (atp->longname && strlen(atp->longname))
+             sprintf(tmps, "%s [%s]", atp->name, atp->longname);
+          else
+             sprintf(tmps, "%s", atp->name);               
+          break;
+      /* just the short, original name */
+      case 0:
+      default:
+          sprintf(tmps, "%s", atp->name);
+   }
+
+   return(tmps);
+}
+
 /* See also atlas_key_label */
+/* this function used only by whereami overlap mask computation for now */
 const char *Atlas_Val_Key_to_Val_Name(ATLAS *atlas, int tdval)
 {
    int ii, cmax = 600;
    static char tmps[600];
+   ATLAS_POINT *aptr;
 
    ENTRY("Atlas_Val_Key_to_Val_Name");
 
@@ -6754,13 +6846,19 @@ const char *Atlas_Val_Key_to_Val_Name(ATLAS *atlas, int tdval)
    if (!atlas->adh->duplicateLRentries) {
       /* quicky */
       if (tdval < MAX_ELM(atlas->adh->apl2)) {
-         if (atlas->adh->apl2->at_point[tdval].tdval == tdval)
-               RETURN(Clean_Atlas_Label(atlas->adh->apl2->at_point[tdval].name));
+         if (atlas->adh->apl2->at_point[tdval].tdval == tdval){
+               aptr = &atlas->adh->apl2->at_point[tdval];
+               RETURN(Atlas_name_choice(aptr));
+/*               RETURN(Clean_Atlas_Label(atlas->adh->apl2->at_point[tdval].name));*/
+         }
       }
       /* longy */
       for( ii=0 ; ii < MAX_ELM(atlas->adh->apl2) ; ii++ ) {
-         if (atlas->adh->apl2->at_point[ii].tdval == tdval)
-               RETURN(Clean_Atlas_Label(atlas->adh->apl2->at_point[ii].name));
+         if (atlas->adh->apl2->at_point[ii].tdval == tdval) {
+               aptr = &atlas->adh->apl2->at_point[ii];
+               RETURN(Atlas_name_choice(aptr));
+/*               RETURN(Clean_Atlas_Label(atlas->adh->apl2->at_point[ii].name));*/
+         }
       }
    } else {
       int fnd[30], nfnd =0;
@@ -6774,8 +6872,9 @@ const char *Atlas_Val_Key_to_Val_Name(ATLAS *atlas, int tdval)
       if ( nfnd == 0) RETURN(NULL);
 
       if ( nfnd == 1) {
-         RETURN(NoLeftRight(
-                Clean_Atlas_Label(atlas->adh->apl2->at_point[fnd[0]].name)));
+         aptr = &atlas->adh->apl2->at_point[fnd[0]];
+         RETURN(NoLeftRight(Atlas_name_choice(aptr)));
+/*                Clean_Atlas_Label(atlas->adh->apl2->at_point[fnd[0]].name)));*/
       }
 
       if (nfnd > 2) {
@@ -6787,7 +6886,7 @@ const char *Atlas_Val_Key_to_Val_Name(ATLAS *atlas, int tdval)
                   Clean_Atlas_Label(atlas->adh->apl2->at_point[fnd[ii]].name));
          }
       } else {
-         /* get ridd of the LR business */
+         /* get rid of the LR business */
          if (!strcmp(NoLeftRight(
                    Clean_Atlas_Label(atlas->adh->apl2->at_point[fnd[0]].name)),
                      NoLeftRight(
@@ -7263,7 +7362,7 @@ ATLAS_POINT_LIST *atlas_point_to_atlas_point_list(ATLAS_POINT * apl, int n_pts)
    for (i=0; i<n_pts; ++i) {
       NI_strncpy(apl2->at_point[i].name,apl[i].name,ATLAS_CMAX);
       NI_strncpy(apl2->at_point[i].sblabel,apl[i].sblabel,ATLAS_CMAX);
-
+      NI_strncpy(apl2->at_point[i].longname,apl[i].longname,ATLAS_CMAX);
       apl2->at_point[i].tdval = apl[i].tdval;
       apl2->at_point[i].okey = apl[i].okey;
       apl2->at_point[i].tdlev = apl[i].tdlev;
@@ -7376,7 +7475,8 @@ byte is_probabilistic_atlas(ATLAS *atlas) {
 }
 
 /* return the label associated with a key,
-   see also Atlas_Val_Key_to_Val_Name */
+   see also Atlas_Val_Key_to_Val_Name 
+*  main function to get label from index key*/
 char *atlas_key_label(ATLAS *atlas, int key, ATLAS_COORD *ac) {
    char *klab = NULL;
    int ii;
@@ -7385,7 +7485,8 @@ char *atlas_key_label(ATLAS *atlas, int key, ATLAS_COORD *ac) {
          if( key == atlas->adh->apl2->at_point[ii].tdval ) break ;
       }
       if( ii < MAX_ELM(atlas->adh->apl2) )  {          /* always true? */
-         klab = atlas->adh->apl2->at_point[ii].name;
+         // klab = atlas->adh->apl2->at_point[ii].name;
+         klab = Atlas_name_choice(&atlas->adh->apl2->at_point[ii]);
          if( atlas->adh->duplicateLRentries && ac ) {
                klab =
                   AddLeftRight( NoLeftRight(atlas->adh->apl2->at_point[ii].name),
@@ -8751,7 +8852,7 @@ char **atlas_chooser_formatted_labels(char *atname, int flipxy ) {
    at_labels = (char **) calloc(apl->n_points, sizeof(char*));
    for( ii=0 ; ii < apl->n_points ; ii++ ){
       at_labels[ii] = (char *) malloc( sizeof(char) * TTO_LMAX ) ;
-      sprintf( at_labels[ii] , TTO_FORMAT , apl->at_point[ii].name ,
+      sprintf( at_labels[ii] , TTO_FORMAT , Atlas_name_choice(&apl->at_point[ii]),
          sgnxy*(apl->at_point[ii].xx) , sgnxy*(apl->at_point[ii].yy) , apl->at_point[ii].zz ) ;
    }
 
@@ -9880,6 +9981,24 @@ char * atlas_suppinfo_connpage(ATLAS *atlas, char *blab)
     else
        sprintf(webpage, "%s%s.html", atlas->supp_conn_info, blab);
     return (webpage);
+}
+
+/* return longname, if available and environment requested, for atlas region blab
+ *  in newly allocated string */
+char * atlas_suppinfo_longname(ATLAS *atlas, char *blab)
+{
+/*    static char webpage[256];*/
+
+   char *longname, *slongptr;
+
+    if(Atlas_name_type() == 0) return(NULL);
+
+    longname = calloc(256,sizeof(char));
+    if(longname==NULL) return(NULL);
+    slongptr = atlas_point_long_name_named(atlas->adh->apl2, blab);
+    if(slongptr)
+       sprintf(longname, "%s", slongptr);
+    return (longname);
 }
 
 /* set minimum probabilty to use in probabilistic atlases */
