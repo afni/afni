8de965ea004c8ac4d2fbf34afe35490510616de2
mrneont <neon.taylor@gmail.com>
2018-10-23 08:38:42 -0400

    minor html additions (temporary, for bootcamp)

--- src/python_scripts/afni_python/lib_apqc_html.py
+++ src/python_scripts/afni_python/lib_apqc_html.py
@@ -6,6 +6,7 @@
 #########################################################################
 
 # mostly ways to read & wrap text & images.
+import sys
 
 def wrap_block_lab(x, vpad=0):
     y = """<h3><center>block: """+x+"""</center></h3>"""
@@ -17,7 +18,6 @@ def wrap_block_lab(x, vpad=0):
 # -------------------------------------------------------------------
 
 def wrap_image_title(x, vpad=0, addclass=""):
-    #y = """<h3><center>"""+x+"""</center></h3>"""
     y = """<pre """
     y+= ''' {} '''.format(addclass)
     y+= """ width="80"><center><b>"""+x+"""</b></center></pre>"""
@@ -36,6 +36,26 @@ def wrap_image_txt(x, vpad=0, addclass=""):
     if vpad:
         y= """\n"""+y
         y+="""\n"""
+    return y
+
+# -------------------------------------------------------------------
+
+def make_pbar_line(pbar_min, pbar_max, pbar_thr, imgpbar, vpad=0, addclass=""):
+
+    y = """<div """
+    y+= '''{} >'''.format(addclass)
+    y+= """<pre>{} """.format(pbar_min)
+    y+= '''<img style="display: inline; margin: -5 -5px;" '''
+    y+= '''src="{}" '''.format(imgpbar)
+    y+= '''vertical-align="middle" height="20px"> '''
+#    y+= '''{} </pre></div>\n'''.format(pbar_max)
+    y+= '''{}\n'''.format(pbar_max)
+    y+= '''vthr: {} (90 %ile) </pre></div>\n'''.format(pbar_thr)
+    if vpad:
+        y= """\n"""+y
+        y+="""\n"""
+
+    #y = '''\n<div class=container><pre> {} <img style="display: inline; margin: 0 0px;" src="{}" align="middle" width="160px" height="25px"> {} </pre></div>\n'''.format(pbar_min, imgpbar, pbar_max)
     return y
 
 # -------------------------------------------------------------------
@@ -101,6 +121,43 @@ def read_dat(x):
     out = ''.join(txt)
     return out
 
+def read_pbar_range(x, dtype="NA"):
+
+    fff = open(x, 'r')
+    txt = fff.readlines()
+    fff.close()
+
+    Nlines = len(txt)
+    if not(Nlines):
+        sys.exit("** ERROR: no lines of text in {}?".format(x))
+    elif Nlines > 1:
+        sys.exit("** ERROR: too many lines (={}) in {}?".format(Nlines, x))
+
+    l0 = txt[0]
+    y  = l0.split()
+
+    Nnums = len(y) 
+    if Nnums != 3:
+        sys.exit("** ERROR: wrong number of nums (={}) in {}?".format(Nnums, x))
+
+    out = []
+    if dtype == int :
+        for nn in y:
+            z = int(nn)
+            out.append(z)
+    elif dtype == float :
+        for nn in y:
+            z = float(nn)
+            out.append(z)
+    else: # the NA or other cases...
+        for nn in y:
+            if nn.__contains__('.'):
+                z = float(nn)
+            else:
+                z = int(nn)
+            out.append(z)
+
+    return out[0], out[1], out[2]
 
 
 
