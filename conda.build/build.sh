0610cb08d0317719e069cd38221b4feac47c5bba
leej3 <johnleenimh+kraken@gmail.com>
2019-01-25 14:35:04 -0500

    add preliminary conda files

---
+++ conda.build/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir ../build && cd ../build
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
+-DBUILD_SHARED_LIBS=ON -DUSE_CIFTI_CODE=ON -DUSE_NIFTI2_CODE=ON $SRC_DIR
