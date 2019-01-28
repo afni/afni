88394fb0517bd92228d7788e05e48cb547a3f4ac
rick reynolds <reynoldr@mail.nih.gov>
2019-01-28 11:39:34 -0500

    updates for make prog_list

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,13 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 28, Jan, 2019, RCR, "afni-general", MINOR, TYPE_ENHANCE,
+   "updates to make target, prog_list",
+   "   - grep out CMakeLists.txt\n"
+   "   - add some ptaylor python scripts\n"
+   "   - define and add DISCO_SCRIPTS"
+ } ,
+
  { 22, Jan, 2019, RCR, "@update.afni.binaries", MINOR, TYPE_NEW_OPT,
    "add -show_obsoletes[_grep] and -show_system_progs",
    "Inspired by Z Saad."
