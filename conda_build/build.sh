0f37e1a0257f97d795295c824f31cfda8836c4b2
leej3 <johnleenimh@gmail.com>
2019-03-08 09:41:43 -0500

    conda build mac fixes

--- conda_build/build.sh
+++ conda_build/build.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
-CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+#!/bin/bash
+declare -a CMAKE_PLATFORM_FLAGS
+# this is how sys dependencies are managed in libnetcdf
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+  export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
+else
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
 mkdir ../build
 cd ../build
 
