872a9e51c0e0cbdbd06ac63e63bf72a571835bb3
rick reynolds <reynoldr@mail.nih.gov>
2019-05-23 16:13:29 -0400

    afni_open: pref mozilla

--- src/machdep.c
+++ src/machdep.c
@@ -120,15 +120,15 @@ char * GetAfniWebBrowser(void)
 #ifdef DARWIN
    if( awb == NULL ) awb = "open" ;  /* for Mac OS X */
 #endif
-   if( awb == NULL ) awb = THD_find_executable( "chrome" )   ;
-   if( awb == NULL ) awb = THD_find_executable( "google-chrome" ) ;
    if( awb == NULL ) awb = THD_find_executable( "firefox" )  ;
    if( awb == NULL ) awb = THD_find_executable( "mozilla" )  ;
+   if( awb == NULL ) awb = THD_find_executable( "chrome" )   ;
+   if( awb == NULL ) awb = THD_find_executable( "google-chrome" ) ;
+   if( awb == NULL ) awb = THD_find_executable( "google-chrome-unstable" ) ;
    if( awb == NULL ) awb = THD_find_executable( "netscape" ) ;
    if( awb == NULL ) awb = THD_find_executable( "opera" )    ;
    if( awb == NULL ) awb = THD_find_executable( "epiphany" ) ;
    if( awb == NULL ) awb = THD_find_executable( "midori" )   ;
-   if( awb == NULL ) awb = THD_find_executable( "google-chrome-unstable" ) ;
    return(awb);
 }
 
