9f8982c16abf3879c0cd3f9b3b488f35e080164b
Ross Markello <rossmarkello@gmail.com>
2019-06-07 16:22:23 +0200

    Whelp, speeding up tests didn't work as expected
    Some combination of CircleCI and pulling from docker was...unhappy.
    Rolling back some changes, trying some new things out, we'll see what
    happens.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,9 +3,10 @@
 set -e
 
 # Run tests
+cd /opt/afni/tests
 pytest --runveryslow --cov=/opt/afni/abin
 # pytest --testpython2 # add this for testing python 2
-cd ../src
 
 # Check code coverage
+cd /opt/afni
 codecov
