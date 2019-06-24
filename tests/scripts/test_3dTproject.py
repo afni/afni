344cb7c4845bd572067586b3adb7c336e8320596
leej3 <johnleenimh@gmail.com>
2019-06-24 12:19:01 -0400

    fix 3dtproject test failing for very small absolute differences

--- tests/scripts/test_3dTproject.py
+++ tests/scripts/test_3dTproject.py
@@ -9,6 +9,7 @@ data_paths = {"epi": "AFNI_data6/afni/epi_r1+orig.HEAD"}
 @pytest.mark.parametrize("add_env_vars", [({}), ({"OMP_NUM_THREADS": "2"})])
 def test_3dTproject_basic(data, add_env_vars):
     kwargs_log = {"append_to_ignored": ["clock time"]}
+    kwargs_scans = {"data_kwargs": {"atol": 0.0001}}
 
     outfile = data.outdir / "out.nii.gz"
     cmd = """
@@ -26,6 +27,10 @@ def test_3dTproject_basic(data, add_env_vars):
 
     # Run command and test all outputs match
     differ = tools.OutputDiffer(
-        data, cmd, kwargs_log=kwargs_log, add_env_vars=add_env_vars
+        data,
+        cmd,
+        kwargs_log=kwargs_log,
+        kwargs_scans=kwargs_scans,
+        add_env_vars=add_env_vars,
     )
     differ.run()
