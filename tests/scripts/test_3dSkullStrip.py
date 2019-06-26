0c1bea2fd41261598e7f5f28867e6c8689302d6d
mrneont <neon.taylor@gmail.com>
2019-06-26 16:51:22 -0400

    stuff for new 3dSkullStrip test (orient)

--- tests/scripts/test_3dSkullStrip.py
+++ tests/scripts/test_3dSkullStrip.py
@@ -1,12 +1,22 @@
 import pytest
 from .utils import tools
 
-data_paths = {"anat": "mini_data/anat_3mm.nii.gz"}
+data_paths = {"anat": "mini_data/anat_3mm.nii.gz",
+              "anatrpi": "mini_data/anat_3mm_RPI.nii.gz"}
 
 
 @pytest.mark.veryslow
-def test_3dSkullStrip_basic(data):
-    cmd = """3dSkullStrip -input {data.anat}"""
+@pytest.mark.parametrize(
+    "dset_name",
+    [
+        "anat",
+        "anatrpi",
+    ],
+)
+def test_3dSkullStrip_basic(data, dset_name):
+    ifile = getattr(data, dset_name)
+    ofile = data.outdir / "out_ss.nii.gz"
+    cmd = """3dSkullStrip -prefix {ofile} -input {ifile}"""
     cmd = " ".join(cmd.format(**locals()).split())
 
     # Run command and test all outputs match
