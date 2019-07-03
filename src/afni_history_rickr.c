704ffc2af026304c53177335dadaddc9089d1165
rick reynolds <reynoldr@mail.nih.gov>
2019-07-03 15:56:55 -0400

    volreg: quiet zpad warning

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -49,6 +49,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ {  3, Jul, 2019, RCR, "3dvolreg", MICRO, TYPE_BUG_FIX,
+   "make 'second -zpad option' warning appropriate again",
+   NULL
+ } ,
+
  {  3, Jul, 2019, RCR, "gen_ss_review_scripts.py", MICRO, TYPE_MODIFY,
    "let X.stim.xmat.1D be empty for non-task case",
    NULL
