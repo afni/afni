8b6855b86083f2f09e8d78560552982c430fe811
leej3 <johnleenimh@gmail.com>
2019-10-08 13:39:07 -0400

    tidy afni target function

--- cmake/get_build_macros_and_functions.cmake
+++ cmake/get_build_macros_and_functions.cmake
@@ -42,34 +42,32 @@ macro(get_afni_rpath)
   # file(RELATIVE_PATH _rel_nifti ${INSTALL_PATH} ${_nifti_libraries})
   # file(RELATIVE_PATH _rel_gifti ${INSTALL_PATH} ${_gifti_libraries})
   if(APPLE)
-    set(message_RPATH 
+    set(afni_target_RPATH 
       "@loader_path/${_rel_afni}"
       # "@loader_path/${_rel_nifti}"
       # "@loader_path/${_rel_gifti}"
       )
   else()
-    set(message_RPATH 
+    set(afni_target_RPATH 
       "\$ORIGIN/${_rel_afni}"
       # "\$ORIGIN/${_rel_nifti}"
       # "\$ORIGIN/${_rel_gifti}"
       )
   endif()
-  # file(TO_NATIVE_PATH "${_rpath}" message_RPATH)
+  # file(TO_NATIVE_PATH "${_rpath}" afni_target_RPATH)
 endmacro()
 
 macro(add_afni_target_properties target)
   # this macro sets some default properties for targets in this project
   get_afni_rpath()
-  # define installation prefix
   get_target_property(TARGET_TYPE ${target} TYPE)
-  # Set the target properties
-  # Currently this includes setting up post-install linking
 
+  # Set the target properties
   if(NOT DEFINED ENV{CONDA_BUILD})
     set_target_properties(${target} PROPERTIES
       SKIP_BUILD_RPATH OFF
       BUILD_WITH_INSTALL_RPATH OFF
-      INSTALL_RPATH "${message_RPATH}"
+      INSTALL_RPATH "${afni_target_RPATH}"
       INSTALL_RPATH_USE_LINK_PATH ON
       )
   endif()
@@ -79,7 +77,7 @@ macro(add_afni_target_properties target)
 
   if (CONDA_BUILD)
     set_target_properties(${target} PROPERTIES
-      INSTALL_RPATH "${message_RPATH}"
+      INSTALL_RPATH "${afni_target_RPATH}"
       )
   endif()
     # INSTALL_RPATH_USE_LINK_PATH ON
