ae1bb513f30c3ba6fe89af32ca35f7af2be92e0e
leej3 <johnleenimh@gmail.com>
2019-10-10 17:13:35 -0400

    set libmri free of Xt

--- src/replaceXt.h
+++ src/replaceXt.h
@@ -8,7 +8,6 @@
 #  define offsetof(st, m) ((size_t)&(((st *)0)->m))
 # endif
 
-#undef  REPLACE_XT
 #ifdef  REPLACE_XT           /* this is the finesse */
 
 typedef   void*           RwcPointer ;
@@ -18,7 +17,7 @@ typedef   void*           RwcPointer ;
 # define  RwcRealloc      realloc
 # define  RwcMalloc       malloc
 # define  RwcNew(t)       ((t *)calloc(1,sizeof(t)))
-# define  RwcNewString(s) (strcpy(malloc(1+strlen(s)),str))
+# define  RwcNewString(s) (strcpy(malloc(1+strlen(s)),s))
 
 #else                        /* this is the crudesse */
 
