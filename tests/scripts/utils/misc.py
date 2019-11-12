45599608b56a6ebe0b8486be016232e0d2f7aa0c
Yaroslav Halchenko <debian@onerussian.com>
2019-11-12 16:56:56 -0500

    BF: remove tests/ prefix to xvfb-driver

--- tests/scripts/utils/misc.py
+++ tests/scripts/utils/misc.py
@@ -13,7 +13,7 @@ import pytest
 def run_x_prog(cmd):
     import subprocess as sp
     res = sp.run(
-        "tests/scripts/utils/xvfb-driver -- " + cmd,
+        "scripts/utils/xvfb-driver -- " + cmd,
         shell=True,
         stdout=sp.PIPE,
         stderr=sp.STDOUT
