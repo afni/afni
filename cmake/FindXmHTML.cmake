44a1fb6d29bd0768b90dcf47834bc47a4731cc8e
Yaroslav Halchenko <debian@onerussian.com>
2019-11-13 14:20:56 -0500

    BF: Finish up FindXmHTML
    - removed left-over references to expat
    - provided version parsing
    - for now hardcodes the added library to be a shared one
    - RFed dependent code to use XMHTML target (not just XmHTML library name)

--- cmake/FindXmHTML.cmake
+++ cmake/FindXmHTML.cmake
@@ -31,35 +31,40 @@ find_package(PkgConfig QUIET)
 
 pkg_check_modules(PC_XMHTML QUIET XmHTML)
 
+# message("!!! FOUND DIRS ${PC_XMHTML_INCLUDE_DIRS}")
 # Look for the header file.
-find_path(XMHTML_INCLUDE_DIR NAMES expat.h HINTS ${PC_XMHTML_INCLUDE_DIRS})
+find_path(XMHTML_INCLUDE_DIR NAMES XmHTML/XmHTML.h HINTS ${PC_XMHTML_INCLUDE_DIRS})
+# message("!!! FOUND DIRS2 ${XMHTML_INCLUDE_DIR}  CMAKE Setting BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} ")
 
 # Look for the library.
-find_library(XMHTML_LIBRARY NAMES expat libexpat HINTS ${PC_XMHTML_LIBRARY_DIRS})
+find_library(XMHTML_LIBRARY NAMES XmHTML HINTS ${PC_XMHTML_LIBRARY_DIRS})
+# message("!!! FOUND LIB ${XMHTML_LIBRARY}")
 
-if (XMHTML_INCLUDE_DIR AND EXISTS "${XMHTML_INCLUDE_DIR}/expat.h")
-    file(STRINGS "${XMHTML_INCLUDE_DIR}/expat.h" expat_version_str
-         REGEX "^#[\t ]*define[\t ]+XML_(MAJOR|MINOR|MICRO)_VERSION[\t ]+[0-9]+$")
+if (XMHTML_INCLUDE_DIR AND EXISTS "${XMHTML_INCLUDE_DIR}/XmHTML/XmHTML.h")
+  file(STRINGS "${XMHTML_INCLUDE_DIR}/XmHTML/XmHTML.h" _version_str
+       REGEX "^#[\t ]*define[\t ]+XmHTML(VERSION|REVISION|UPDATE_LEVEL)[\t ]+[0-9]+$")
 
-    unset(XMHTML_VERSION_STRING)
-    foreach(VPART MAJOR MINOR MICRO)
-        foreach(VLINE ${expat_version_str})
-            if(VLINE MATCHES "^#[\t ]*define[\t ]+XML_${VPART}_VERSION[\t ]+([0-9]+)$")
-                set(XMHTML_VERSION_PART "${CMAKE_MATCH_1}")
-                if(XMHTML_VERSION_STRING)
-                    string(APPEND XMHTML_VERSION_STRING ".${XMHTML_VERSION_PART}")
-                else()
-                    set(XMHTML_VERSION_STRING "${XMHTML_VERSION_PART}")
-                endif()
-            endif()
-        endforeach()
+  unset(XMHTML_VERSION_STRING)
+  foreach(VPART VERSION REVISION UPDATE_LEVEL MINOR MICRO)
+    foreach(VLINE ${_version_str})
+      if(VLINE MATCHES "^#[\t ]*define[\t ]+XmHTML${VPART}[\t ]+([0-9]+)$")
+        set(XMHTML_VERSION_PART "${CMAKE_MATCH_1}")
+        if(XMHTML_VERSION_STRING)
+          string(APPEND XMHTML_VERSION_STRING ".${XMHTML_VERSION_PART}")
+        else()
+          set(XMHTML_VERSION_STRING "${XMHTML_VERSION_PART}")
+        endif()
+      endif()
     endforeach()
+  endforeach()
+  # message("!!!!! Found XmHTML version ${XMHTML_VERSION_STRING}")
 endif ()
 
-include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(XMHTML
-                                  REQUIRED_VARS XMHTML_LIBRARY XMHTML_INCLUDE_DIR
-                                  VERSION_VAR XMHTML_VERSION_STRING)
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+  XMHTML
+  REQUIRED_VARS XMHTML_LIBRARY XMHTML_INCLUDE_DIR
+  VERSION_VAR XMHTML_VERSION_STRING)
 
 # Copy the results to the output variables and target.
 if(XMHTML_FOUND)
@@ -67,11 +72,17 @@ if(XMHTML_FOUND)
   set(XMHTML_INCLUDE_DIRS ${XMHTML_INCLUDE_DIR})
 
   if(NOT TARGET XMHTML)
-    add_library(XMHTML UNKNOWN IMPORTED)
+    # GLOBAL is needed for alias below
+    # Changing SHARED to
+    #  - UNKNOWN would fail to treat it as a library later on
+    #  - STATIC seems to have no effect even if -DBUILD_SHARED_LIBS=OFF
+    #    probably because above it should find .a not .so (TODO)
+    add_library(XMHTML SHARED IMPORTED GLOBAL)
     set_target_properties(XMHTML PROPERTIES
       IMPORTED_LINK_INTERFACE_LANGUAGES "C"
       IMPORTED_LOCATION "${XMHTML_LIBRARY}"
       INTERFACE_INCLUDE_DIRECTORIES "${XMHTML_INCLUDE_DIRS}")
+    get_target_property(TARGET_TYPE XMHTML TYPE)
   endif()
   add_library(XMHTML::XMHTML ALIAS XMHTML)
 endif()
