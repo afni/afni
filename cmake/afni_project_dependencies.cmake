563b12d96f22820eb3d60dd9a1f99c77dc825f97
leej3 <johnleenimh+kraken@gmail.com>
2019-12-10 15:15:28 -0500

    improve dependency specification
    system qhull should be used by default.
    NetCDF is required

--- cmake/afni_project_dependencies.cmake
+++ cmake/afni_project_dependencies.cmake
@@ -1,7 +1,7 @@
 include(CPack)
 include(CMakePackageConfigHelpers)
 find_package(Motif REQUIRED)
-include(FindNetCDF)
+find_package(NetCDF REQUIRED)
 include(FindX11)
 include(FindStandardMathLibrary)
 include(FindLibR)
@@ -16,6 +16,7 @@ if(NOT USE_OMP)
   message(FATAL_ERROR REQUIRED)
 endif()
 
+set_if_not_defined(USE_SYSTEM_QHULL ON)
 # Find or fetch nifti_clib a prefix can be used to avoid collisions with pre-existing
 # installations of the nifti libraries
 include(get_nifti_and_gifti_targets)
