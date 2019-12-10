2a438e4f65e9eb20e44d2b8981f6d977f25edf8c
leej3 <our email address>
2019-12-10 15:13:09 -0500

    enable use of system installed f2c

--- cmake/afni_project_dependencies.cmake
+++ cmake/afni_project_dependencies.cmake
@@ -1,6 +1,6 @@
 include(CPack)
 include(CMakePackageConfigHelpers)
-find_package(Motif REQUIRED)
+include(FindMotif)
 include(FindNetCDF)
 include(FindX11)
 include(FindStandardMathLibrary)
@@ -16,13 +16,17 @@ if(NOT USE_OMP)
   message(FATAL_ERROR REQUIRED)
 endif()
 
+set_if_not_defined(USE_SYSTEM_F2C ON)
 # Find or fetch nifti_clib a prefix can be used to avoid collisions with pre-existing
 # installations of the nifti libraries
 include(get_nifti_and_gifti_targets)
 
 # set(Python_FIND_VIRTUALENV FIRST)
 set(CMAKE_FIND_FRAMEWORK LAST)
-find_package(Python 3 REQUIRED COMPONENTS Interpreter)
+find_package(Python 3 
+  REQUIRED COMPONENTS Interpreter
+  )
 if(NOT ${Python_FOUND})
   message(FATAL_ERROR "Cannot find python interpreter (FOUND: ${Python_EXECUTABLE})")
 endif()
+
