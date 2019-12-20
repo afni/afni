d8e57b4c253463b5ceff0695f2845318c79198b3
RWCox <robertcox@mail.nih.gov>
2019-12-20 15:02:03 -0500

    tweak uncomment.c again

--- src/uncomment.c
+++ src/uncomment.c
@@ -67,7 +67,7 @@ INFO_message("THD_filesize = %d",len) ;
 int main( int argc , char * argv[] )
 {
    char * infile ;
-   int nin , comm , ii , quote , lpos=0 , iarg=1 , nskip=0 ;
+   int nin , comm , ii , quote , iarg=1 , gitskip=0 , inskip=0 ;
 
    /*- help? -*/
 
@@ -85,27 +85,13 @@ int main( int argc , char * argv[] )
              "  wc -l afni.c                                          => 13836\n"
              "  uncomment afni.c | grep -v -e '^ *$' -e '^$' | wc -l  =>  9621\n" ) ;
       printf("\n") ;
-#if 0
-      printf("EXAMPLE:\n"
-             "  git blame afni.c | uncomment -skip 58 - | \\\n"
-             "    grep -v '[0-9]) *$' | grep -i cox | wc -l\n"
-             "Counts the number of actual lines of code in\n"
-             "afni.c that are credited to author 'cox'.\n"
-             "The same thing without the un-commenting:\n"
-             "  git blame afni.c | grep -v '[0-9]) *$' | grep -i cox | wc -l\n"
-             "At this moment, the two answers are 8957 and 10058.\n"
-            ) ;
-#endif
       exit(0) ;
    }
 
    /*- the only option -*/
 
-   if( strcasecmp(argv[iarg],"-skip") == 0 ){
-     iarg++ ; if( iarg >= argc ) ERROR_exit("need arg after %s",argv[iarg-1]) ;
-     nskip = (int)strtod(argv[iarg],NULL) ;
-     if( nskip < 0 ) nskip = 0 ;
-     iarg++ ;
+   if( strcasecmp(argv[iarg],"-gitskip") == 0 ){
+     iarg++ ; gitskip = 1 ;
    }
    if( iarg >= argc ) ERROR_exit("need a filename to read") ;
 
@@ -119,53 +105,55 @@ int main( int argc , char * argv[] )
    comm  = 0 ;
    quote = 0 ;
    ii    = 0 ;
-   lpos  = 0 ;
+
+   inskip = gitskip ;
 
    while( ii < nin ){  /* scan each character */
 
       /* if end of line */
 
       if( infile[ii] == '\n' ){
-        putchar(infile[ii++]) ; lpos = 0 ; continue ; /* reset line pos */
+        putchar(infile[ii++]) ; inskip = gitskip ; continue ; /* reset line pos */
       }
 
-      /* if in the skip zone for this line (early lpos) */
+      /* if in the skip zone for this line = before first "<digit>)" sequence */
 
-      if( lpos < nskip ){
-        putchar(infile[ii++]) ; lpos++ ; continue ;
+      if( inskip ){
+        putchar(infile[ii]) ;
+        if( infile[ii] == ')' && isdigit(infile[ii-1]) ) inskip = 0 ;
+        ii++ ; continue ;
       }
 
       /* if inside a quote, and see an end quote */
 
       if( quote && infile[ii] == '"' && infile[ii-1] != '\\' ){
-         putchar(infile[ii++]) ; quote = 0 ; lpos++ ; continue ;
+         putchar(infile[ii++]) ; quote = 0 ; continue ;
       }
 
       /* if not inside a quote or comment, and see a start quote */
 
       if( !quote && !comm && infile[ii] == '"' && infile[ii-1] != '\\' ){
-         putchar(infile[ii++]) ; quote = 1 ; lpos++ ; continue ;
+         putchar(infile[ii++]) ; quote = 1 ; continue ;
       }
 
       /* if inside a comment and see an end comment */
 
       if( comm && infile[ii] == '*' && infile[ii+1] == '/' ){
-         ii += 2 ; comm = 0 ; lpos += 2 ; continue ;
+         ii += 2 ; comm = 0 ; continue ;
       }
 
       /* if not inside a quote or comment, and see a start comment */
 
       if( !quote && !comm && infile[ii] == '/' && infile[ii+1] == '*' ){
-         putchar(' ') ; ii += 2 ; comm = 1 ; lpos += 2 ; continue ;
+         putchar(' ') ; ii += 2 ; comm = 1 ; continue ;
       }
 
       /* normal case: output character if not inside a comment: */
 
       if( !comm ) putchar(infile[ii]) ;
-      if( infile[ii] == '\n' ) lpos = 0 ; else lpos++ ;
       ii++ ;
    }
-   if( lpos > 0 ) putchar('\n') ;
+   if( infile[ii-1] != '\n' ) putchar('\n') ;
 
         if( comm  ) WARNING_message("ended inside a comment!") ;
    else if( quote ) WARNING_message("ended inside a quote!") ;
