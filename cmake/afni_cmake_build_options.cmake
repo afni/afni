d7560bf7bcd3cdf6305ea5a1bf9a00dade6f090a
leej3 <our email address>
2020-01-08 16:43:18 -0500

    improve options specification and dependencies for cmake build

--- cmake/afni_cmake_build_options.cmake
+++ cmake/afni_cmake_build_options.cmake
@@ -7,28 +7,38 @@ cmake_dependent_option(
   BUILD_BINARIES "Build a large portion of the C executables" ON
   "NOT AFNI_BUILD_CORELIBS_ONLY" OFF
 )
+
 cmake_dependent_option(
-  DO_NOT_INSTALL_SCRIPTS "Omits script installation" OFF "NOT AFNI_BUILD_CORELIBS_ONLY"
-  OFF
+  DO_NOT_INSTALL_SCRIPTS "Omits script installation" OFF
+  "NOT AFNI_BUILD_CORELIBS_ONLY;BUILD_BINARIES" OFF
 )
 mark_as_advanced(DO_NOT_INSTALL_SCRIPTS)
+
+cmake_dependent_option(
+  ADD_AFNI_RSTATS "Includes scripts and libraries only used for statistics in R." ON 
+  "NOT AFNI_BUILD_CORELIBS_ONLY;" OFF
+)
+mark_as_advanced(ADD_AFNI_RSTATS)
+
 cmake_dependent_option(
   BUILD_X_DEPENDENT_GUI_PROGS "Build GUI applications with plugins." ON
-  "NOT AFNI_BUILD_CORELIBS_ONLY" OFF
+  "NOT AFNI_BUILD_CORELIBS_ONLY;BUILD_BINARIES" OFF
 )
+
 cmake_dependent_option(
-  BUILD_PLUGINS "Build plugins for AFNI GUI." ON "BUILD_X_DEPENDENT_GUI_PROGS;" OFF
+  BUILD_PLUGINS "Build plugins for AFNI GUI." ON 
+  "BUILD_X_DEPENDENT_GUI_PROGS;" OFF
 )
 mark_as_advanced(BUILD_PLUGINS)
-set_if_not_defined(BUILD_COREPLUGINS "By default a core set of plugins are built." ON)
+
 cmake_dependent_option(
   BUILD_OPENGL_DEPENDENT_GUI_PROGS
   "Build OPEN_GL dependent GUI applications with plugins." ON
-  "NOT AFNI_BUILD_CORELIBS_ONLY;BUILD_X_DEPENDENT_GUI_PROGS" OFF
+  "BUILD_X_DEPENDENT_GUI_PROGS" OFF
 )
-# Define options to customize the build-process
+
+# Define other customizations to the build-process
+set_if_not_defined(BUILD_COREPLUGINS "By default a core set of plugins are built." ON)
 option(USE_SYSTEM_GLW "Build and use AFNI's local copy of libGLw" ON)
-
-
 option(USE_OMP "Use OpenMP to enamble <omp.h>" ON)
 option(ENABLE_TESTS "Enable tests" OFF)
