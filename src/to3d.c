d9e97ea2cf719119fc435a293aa7f20cf7c2e9f8
daniel glen <glend@mail.nih.gov>
2020-01-27 17:48:06 -0500

    allow multiframe DICOM, debug printing off

--- src/to3d.c
+++ src/to3d.c
@@ -3,7 +3,7 @@
    of Wisconsin, 1994-2000, and are released under the Gnu General Public
    License, Version 2.  See the file README.Copyright for details.
 ******************************************************************************/
-#define AFNI_DEBUG 1
+/*#define AFNI_DEBUG 1 */
 #include "to3d.h"
 /*#define DEBUG_PRINT 1*/
 
