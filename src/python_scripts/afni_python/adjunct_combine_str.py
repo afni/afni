d06834cbdbbdb4c7ca37f0089c50b6a05ba2db9a
mrneont <neon.taylor@gmail.com>
2020-02-19 15:55:49 -0500

    @djunct*py:  rename all such ptaylor-written progs to be adjunct*.py

--- src/python_scripts/afni_python/adjunct_combine_str.py
+++ src/python_scripts/afni_python/adjunct_combine_str.py
@@ -24,7 +24,7 @@ VER_DATE  = "Dec 5, 2018"
 import sys       as sys
 import afni_util as au     # they are gold, indeed!
 
-THIS_PROG = '@djunct_combine_str.py'
+THIS_PROG = 'adjunct_combine_str.py'
 MIN_NUM_ARGS  = 3                          # three input args needed
 OUT_FNAME = ''
 
