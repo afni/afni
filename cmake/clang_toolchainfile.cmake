4f06257fe49501121a7b673a49b254b7faee845c
leej3 <our email address>
2020-03-03 14:22:56 -0500

    switch to using gcc instead of clang

--- cmake/clang_toolchainfile.cmake
+++ cmake/clang_toolchainfile.cmake
@@ -2,9 +2,9 @@
 # It is used by calling cmake with the additional option
 # "-DCMAKE_TOOLCHAIN_FILE=path/to/this/file". The clang packaged with Xcode does not
 # have openmp support. Instead install clang via llvm: "brew install llvm"
-set(CMAKE_C_COMPILER /usr/local/opt/llvm/bin/clang)
-set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/llvm/lib")
-# set(CMAKE_LDFLAGS "-L/usr/local/opt/llvm/lib") set(CMAKE_C_COMPILER
-# "/usr/local/Cellar/llvm/9.0.0/bin/clang") set(OPENMP_LIBRARIES
-# "/usr/local/Cellar/llvm/9.0.0/lib") set(OPENMP_INCLUDES
-# "/usr/local/Cellar/llvm/9.0.0/bin/include") export LDFLAGS="-L/usr/local/opt/llvm/lib"
+# set(CMAKE_C_COMPILER /usr/local/opt/llvm/bin/clang)
+# set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/llvm/lib")
+
+# Alternatively one can use gcc (brew install gcc)
+set(CMAKE_C_COMPILER /usr/local/bin/gcc-8)
+set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/gcc/lib")
