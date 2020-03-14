db62eebd1b720fe31a68913cc4bb16d9f38da4bd
leej3 <our email address>
2020-03-14 10:39:03 -0400

    switch back to clang on osx for now
    gcc is not working. It appears to be a similar error to the one
    described in https://github.com/ghdl/ghdl/issues/949
    
    Not sure what changed for this error to suddenly occur though.
    Perhaps circleci upgraded to catalina or something. will debug
    at a later time.

--- cmake/clang_toolchainfile.cmake
+++ cmake/clang_toolchainfile.cmake
@@ -2,9 +2,9 @@
 # It is used by calling cmake with the additional option
 # "-DCMAKE_TOOLCHAIN_FILE=path/to/this/file". The clang packaged with Xcode does not
 # have openmp support. Instead install clang via llvm: "brew install llvm"
-# set(CMAKE_C_COMPILER /usr/local/opt/llvm/bin/clang)
-# set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/llvm/lib")
+set(CMAKE_C_COMPILER /usr/local/opt/llvm/bin/clang)
+set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/llvm/lib")
 
 # Alternatively one can use gcc (brew install gcc)
-set(CMAKE_C_COMPILER /usr/local/bin/gcc-9)
-set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/gcc/lib")
+# set(CMAKE_C_COMPILER /usr/local/bin/gcc-9)
+# set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/gcc/lib")
