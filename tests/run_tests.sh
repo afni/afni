bbc2a832b15196a9ec811837ecccb56c910b24ba
leej3 <our email address>
2020-03-18 10:46:44 -0400

    fix location of repo in containers

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Run tests
-cd /opt/afni/tests
-pytest --runveryslow --cov=/opt/afni/abin --cov-report xml:/opt/afni/coverage.xml
+cd /opt/src/afni/tests
+pytest --runveryslow --cov=/opt/src/afni/abin --cov-report xml:/opt/src/afni/coverage.xml
 # pytest --testpython2 # add this for testing python 2
 
 # Check code coverage
-cd /opt/afni
+cd /opt/src/afni
 codecov
