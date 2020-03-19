a601156f4fdbdac725b77e0868f77bf66715af78
leej3 <our email address>
2020-03-19 10:03:48 -0400

    add install-url change from #156

--- src/python_scripts/setup.py
+++ src/python_scripts/setup.py
@@ -4,8 +4,8 @@ SCRIPTS = [str(f) for f in Path('scripts').glob('*.py')]
 
 setup(name='afnipy',
       version='0.0.1',
-      description='AFNI python packages',
-      url='git+https://github.com/leej3/template_making.git',
+      description='AFNI python code installed as a package. Much of the functionality requires a working installation of AFNI.',
+      url='git+https://github.com/afni/afni.git@master#egg=afnipy&subdirectory=src/python_scripts',
       author='AFNI team',
       author_email='afni.bootcamp@gmail.com',
       license='Public Domain',
