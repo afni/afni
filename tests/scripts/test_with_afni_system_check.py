2b0edc75fbf2750624f786113b12078dcd813f45
leej3 <our email address>
2020-03-30 10:46:19 -0400

    test lib_system_check when platform.dist is removed

--- tests/scripts/test_with_afni_system_check.py
+++ tests/scripts/test_with_afni_system_check.py
@@ -1,8 +1,22 @@
 import pytest
 from .utils.tools import run_cmd
-
+from unittest.mock import MagicMock
+import platform
+from afnipy import lib_system_check as SC
 
 @pytest.mark.slow
 def test_with_afni_system_check(data):
     cmd = "afni_system_check.py -check_all"
     run_cmd(data, cmd)
+
+
+def test_with_afni_system_check_with_dist_deprecated(data):
+    platform.dist = MagicMock(
+        side_effect=AttributeError("module 'platform' has no attribute 'dist'"),
+    )
+    platform.linux_distribution = MagicMock(
+        side_effect=ValueError("Non-specific error to force execution of except clause"),
+    )
+    sinfo = SC.SysInfo()
+    sinfo.show_general_sys_info()
+
