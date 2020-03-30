6fa3337590269b21868315ada2361db819945266
leej3 <our email address>
2020-03-30 12:31:10 -0400

    blacken test scripts

--- tests/scripts/test_with_afni_system_check.py
+++ tests/scripts/test_with_afni_system_check.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock
 import platform
 from afnipy import lib_system_check as SC
 
+
 @pytest.mark.slow
 def test_with_afni_system_check(data):
     cmd = "afni_system_check.py -check_all"
@@ -12,11 +13,10 @@ def test_with_afni_system_check(data):
 
 def test_with_afni_system_check_with_dist_deprecated(data):
     platform.dist = MagicMock(
-        side_effect=AttributeError("module 'platform' has no attribute 'dist'"),
+        side_effect=AttributeError("module 'platform' has no attribute 'dist'")
     )
     platform.linux_distribution = MagicMock(
-        side_effect=ValueError("Non-specific error to force execution of except clause"),
+        side_effect=ValueError("Non-specific error to force execution of except clause")
     )
     sinfo = SC.SysInfo()
     sinfo.show_general_sys_info()
-
