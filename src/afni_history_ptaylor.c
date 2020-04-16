171f5793b1d7c4681ead23164b3ae691ace8cefc
Paul Taylor <pault>
2020-04-16 14:01:33 -0400

    history

--- src/afni_history_ptaylor.c
+++ src/afni_history_ptaylor.c
@@ -66,6 +66,16 @@
 afni_history_struct ptaylor_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+{ 16, Apr , 2020 , PT , "adjunct_simplify_cost.py" , MICRO , TYPE_NEW_PROG,
+   "Adjunct program for (soon to be updated) @SSwarper.",
+   "Convert cost name to simpler version, for some application(s).\n"
+},
+
+{ 16, Apr , 2020 , PT , "@djunct_ssw_intermed_edge_imgs" , MICRO , TYPE_NEW_PROG,
+   "Adjunct program for (soon to be updated) @SSwarper.",
+   "Generates images for intermediate QC/tracking.\n"
+},
+
 { 27, Mar , 2020 , PT , "apqc_make_html.py" , MICRO , TYPE_GENERAL,
    "Rearrange variable/function definitions in afnipy libs (no more interdep).",
    "All changes just 'under the hood'---should be no output differences.\n"
