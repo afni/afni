7c221c5d2e1f0b2f71dda81a2d6d897111f71298
leej3 <johnleenimh@gmail.com>
2020-05-18 16:57:06 -0400

    adapt jupyter-docker-stack permissions model
    For example of the gruesome details:
    https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html
    
    NB_UID and NB_GID have been replaced with CONTAINER_UID etc.
    The user jovyan has been changed to afni_user.

---
+++ .docker/test_container_options.py
@@ -0,0 +1,119 @@
+# Original code:
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import time
+
+import pytest
+
+
+def test_uid_change(container):
+    """Container should change the UID of the default user."""
+    c = container.run(
+        tty=True,
+        user='root',
+        environment=['CONTAINER_UID=1010'],
+        command=['start.sh', 'bash', '-c', 'id && touch /home/afni_user/test-file']
+    )
+    # usermod is slow so give it some time
+    c.wait(timeout=120)
+    assert 'uid=1010(afni_user)' in c.logs(stdout=True).decode('utf-8')
+
+
+def test_gid_change(container):
+    """Container should change the GID of the default user."""
+    c = container.run(
+        tty=True,
+        user='root',
+        environment=['CONTAINER_GID=110'],
+        command=['start.sh', 'id']
+    )
+    c.wait(timeout=10)
+    logs = c.logs(stdout=True).decode('utf-8')
+    assert 'gid=110(afni_user)' in logs
+    assert 'groups=110(afni_user),100(users)' in logs
+
+
+def test_chown_extra(container):
+    """Container should change the UID/GID of CHOWN_EXTRA."""
+    c = container.run(
+        tty=True,
+        user='root',
+        environment=['CONTAINER_UID=1010',
+                     'CONTAINER_GID=101',
+                     'CHOWN_EXTRA=/usr/bin',
+                     'CHOWN_EXTRA_OPTS=-R',
+        ],
+        command=['start.sh', 'bash', '-c', 'stat -c \'%n:%u:%g\' /usr/bin/python']
+    )
+    # chown is slow so give it some time
+    c.wait(timeout=5)
+    assert '/usr/bin/python:1010:101' in c.logs(stdout=True).decode('utf-8')
+
+
+def test_chown_home(container):
+    """Container should change the CONTAINER_USER home directory owner and 
+    group to the current value of CONTAINER_UID and CONTAINER_GID."""
+    c = container.run(
+        tty=True,
+        user='root',
+        environment=['CONTAINER_UID=1010',
+                     'CONTAINER_GID=101',
+                     'CHOWN_HOME=yes',
+                     'CHOWN_HOME_OPTS=-R',
+        ],
+        command=['start.sh', 'bash', '-c', 'stat -c \'%n:%u:%g\' /home/afni_user']
+    )
+    c.wait(timeout=2)
+    assert "Changing ownership of /home/afni_user to 1010:101 with options '-R'" in c.logs(stdout=True).decode('utf-8')
+
+
+def test_sudo(container):
+    """Container should grant passwordless sudo to the default user."""
+    c = container.run(
+        tty=True,
+        user='root',
+        environment=['GRANT_SUDO=yes'],
+        command=['start.sh', 'sudo', 'id']
+    )
+    rv = c.wait(timeout=10)
+    assert rv == 0 or rv["StatusCode"] == 0
+    assert 'uid=0(root)' in c.logs(stdout=True).decode('utf-8')
+
+
+def test_sudo_path(container):
+    """Container should have usable /usr/local/bin in the sudo secure_path."""
+    c = container.run(
+        tty=True,
+        user='root',
+        environment=['GRANT_SUDO=yes'],
+        command=['start.sh', 'sudo', 'which', 'ipython3']
+    )
+    rv = c.wait(timeout=10)
+    assert rv == 0 or rv["StatusCode"] == 0
+    assert c.logs(stdout=True).decode('utf-8').rstrip().endswith('/usr/bin/ipython3')
+
+
+def test_sudo_path_without_grant(container):
+    """Container should have usable /usr/local/bin in the unprivileged user"""
+    c = container.run(
+        tty=True,
+        user='root',
+        command=['start.sh', 'which', 'ipython3']
+    )
+    rv = c.wait(timeout=10)
+    assert rv == 0 or rv["StatusCode"] == 0
+    assert c.logs(stdout=True).decode('utf-8').rstrip().endswith('/usr/bin/ipython3')
+
+
+def test_group_add(container, tmpdir):
+    """Container should run with the specified uid, gid, and secondary
+    group.
+    """
+    c = container.run(
+        user='1010:1010',
+        group_add=['users'],
+        command=['start.sh', 'id']
+    )
+    rv = c.wait(timeout=5)
+    assert rv == 0 or rv["StatusCode"] == 0
+    assert 'uid=1010 gid=1010 groups=1010,100(users)' in c.logs(stdout=True).decode('utf-8')
