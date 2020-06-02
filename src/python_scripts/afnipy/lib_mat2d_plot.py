306b1bf0d80c5ffec73afb063561a5e027308554
Paul Taylor <pault>
2020-06-02 13:13:05 -0400

    lib_mat2d_plot.py: basic plot working

--- src/python_scripts/afnipy/lib_mat2d_plot.py
+++ src/python_scripts/afnipy/lib_mat2d_plot.py
@@ -17,6 +17,14 @@
 ver = '0.1' ; date = 'June 2, 2020'
 # [PT] debugged, testing on netcc and grid files
 #
+#ver = '0.11' ; date = 'June 2, 2020'
+# [PT] fix using roi_intvals as labels
+#
+ver = '0.12' ; date = 'June 2, 2020'
+# [PT] tested in py2 and py3
+#    + fixed plotting size stuff with tight layout
+#    + put in fix for matplotlib bug for yaxes range
+#
 # --------------------------------------------------------------------------
 
 import sys, os, copy
@@ -62,6 +70,7 @@ class plot_mat2d:
         self.plt_xticks_ON     = True
         self.plt_xticks_ticks  = None
         self.plt_xticks_labels = None
+        self.plt_xticks_N      = 0
         self.plt_xticks_rot    = 45
         self.plt_xticks_ha     = 'right'
         self.plt_xticks_FS     = 10
@@ -69,11 +78,15 @@ class plot_mat2d:
         self.plt_yticks_ON     = True
         self.plt_yticks_ticks  = None
         self.plt_yticks_labels = None
+        self.plt_yticks_N      = 0
         self.plt_yticks_rot    = 0
         self.plt_yticks_FS     = 10
 
         self.plt_title_txt     = ''
         self.plt_title_FS      = 10
+
+        self.tight_layout_ON   = True
+        self.hold_image_ON     = False
 
         self.do_show_cbar      = True
 
@@ -131,36 +144,38 @@ class plot_mat2d:
         # yticks/labels
         if self.m.col_strlabs :
             self.plt_yticks_labels = self.m.col_strlabs
-        elif self.col_intvals :
-            self.plt_yticks_labels = self.m.col_strlabs
+        elif self.m.col_intvals :
+            self.plt_yticks_labels = [str(w) for w in self.m.col_intvals]
         else: 
             self.plt_yticks_labels = ['']*self.m.ncol # shouldn't happen
-        nyticks = len(self.plt_yticks_labels)
-        self.plt_yticks_ticks  = [i for i in range(nyticks) ]
+        self.plt_yticks_N      = len(self.plt_yticks_labels)
+        self.plt_yticks_ticks  = [i for i in range(self.plt_yticks_N) ]
 
         # xticks/labels
         if self.m.row_strlabs :
             self.plt_xticks_labels = self.m.row_strlabs
-        elif self.row_intvals :
-            self.plt_xticks_labels = self.m.row_strlabs
+        elif self.m.row_intvals :
+            self.plt_xticks_labels = [str(w) for w in self.m.row_intvals]
         else: 
             self.plt_xticks_labels = ['']*self.m.nrow # shouldn't happen
-        nxticks = len(self.plt_xticks_labels)
-        self.plt_xticks_ticks  = [i for i in range(nxticks) ]
+        self.plt_xticks_N      = len(self.plt_xticks_labels)
+        self.plt_xticks_ticks  = [i for i in range(self.plt_xticks_N) ]
         
         # plot title
         self.plt_title_txt = self.m.label
 
         
+
     def make_plot(self):
         """
         Construct the plot obj
         """
 
-        plt.figure( figsize=self.figsize, 
-                    dpi=self.dpi, 
-                    facecolor=self.facecolor)
+        fig = plt.figure( figsize=self.figsize, 
+                          dpi=self.dpi, 
+                          facecolor=self.facecolor)
         subb = plt.subplot(111)
+
         ax   = plt.gca()
         box  = ax.get_position()
 
@@ -183,24 +198,38 @@ class plot_mat2d:
                     rotation = self.plt_yticks_rot,
                     fontsize = self.plt_yticks_FS )
 
+        # this is do deal with an apparent bug in some versions of
+        # matplotlib:
+        # https://github.com/matplotlib/matplotlib/issues/14751
+        ax.set_ylim(self.plt_yticks_N-0.5, -0.5)
+
         plt.title( self.plt_title_txt,
                    fontsize=self.plt_title_FS )
-        
-
-        plt.savefig( self.file_out,
-                     dpi = self.dpi )
 
         ab.IP("Made plot: {}".format(self.file_out))
 
-        #if TIGHT_LAY :
-        #    plt.tight_layout()
-        #else:
-        #    subb.set_position([box.x0+box.width*0.0, box.y0+box.height*0.05,  
-        #                       box.width*0.95, box.height*0.95])
+        if self.tight_layout_ON :
+            #print("style1")
+            plt.tight_layout()
+        else:
+            #print("style2")
+            subb.set_position([ box.x0+box.width*0.0, 
+                                box.y0+box.height*0.05,  
+                                box.width*0.95, 
+                                box.height*0.95 ])
 
-        plt.ion()
-        plt.show()
-        
-        #if HOLD_IMG :
-        #    #        plt.show()
-        #    raw_input()
+        # ------------ save -----------
+        plt.savefig( self.file_out,
+                     dpi = self.dpi )
+
+        if self.hold_image_ON :
+            plt.ion()
+            plt.show()
+
+            try:
+                input()
+            except:
+                raw_input()
+
+
+
