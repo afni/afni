2707492ff85026f3b3d32d217b19754f1eff6f11
RWCox <robertcox@mail.nih.gov>
2020-07-10 17:09:57 -0400

    patch XtMalloc out of model_*.c funcs

--- src/model_convgamma2a.c
+++ src/model_convgamma2a.c
@@ -198,7 +198,7 @@ MODEL_interface * initialize_model ()
 
   /*----- allocate memory space for model interface -----*/
 
-  mi = (MODEL_interface *) XtMalloc (sizeof(MODEL_interface));
+  mi = (MODEL_interface *) RwcMalloc (sizeof(MODEL_interface));
 
   /*----- name of this model -----*/
 
