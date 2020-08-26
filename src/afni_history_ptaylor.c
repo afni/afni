bbe178c98f4d5d2a73e4e8a0dea5ef2f8a37eb94
mrneont <neon.taylor@gmail.com>
2020-08-26 11:21:46 -0400

    @animal_warper: fix help display when no args input

--- src/afni_history_ptaylor.c
+++ src/afni_history_ptaylor.c
@@ -66,6 +66,11 @@
 afni_history_struct ptaylor_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+{ 26, Aug , 2020 , PT , "@animal_warper" , MICRO , TYPE_BUG_FIX,
+   "Fix case of running prog with no args.",
+   "Should show help; now it DOES show help, with no error.\n"
+},
+
 { 21, Aug , 2020 , PT , "3dTrackID" , MINOR , TYPE_BUG_FIX,
    "Fix header deps of underlying progs (namely, readglob.c).",
    "Was crashing on some NIML reading cases.\n"
