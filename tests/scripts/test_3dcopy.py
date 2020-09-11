22431007c60c7ba3f6326474b67ae6f6f97212d9
leej3 <johnleenimh@gmail.com>
2020-09-11 06:27:51 -0700

    remove stray comment

--- tests/scripts/test_3dcopy.py
+++ tests/scripts/test_3dcopy.py
@@ -10,7 +10,6 @@ def test_3dcopy_basic(data):
     3dcopy {data.anatomical} {outfile}
     """
     cmd = " ".join(cmd.format(**locals()).split())
-    # assert cmd == "bamboozle"
     # Run command and test all outputs match
     differ = tools.OutputDiffer(data, cmd)
     differ.run()
