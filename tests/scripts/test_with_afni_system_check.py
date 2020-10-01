cf8b9a30fafce03f79f9b71243229b66336653e2
leej3 <johnleenimh@gmail.com>
2020-10-01 10:40:20 -0400

    improving mocking for system check test

--- tests/scripts/test_with_afni_system_check.py
+++ tests/scripts/test_with_afni_system_check.py
@@ -12,12 +12,26 @@ def test_with_afni_system_check(data):
     run_cmd(data, cmd)
 
 
-def test_with_afni_system_check_with_dist_deprecated(data):
-    platform.dist = MagicMock(
-        side_effect=AttributeError("module 'platform' has no attribute 'dist'")
-    )
-    platform.linux_distribution = MagicMock(
-        side_effect=ValueError("Non-specific error to force execution of except clause")
-    )
-    sinfo = SC.SysInfo()
-    sinfo.show_general_sys_info()
+def test_with_afni_system_check_with_dist_deprecated(data, monkeypatch):
+    with monkeypatch.context() as m:
+        if hasattr(platform, "dist"):
+            m.setattr(
+                platform,
+                "dist",
+                MagicMock(
+                    side_effect=AttributeError("'platform' has no attribute 'dist'")
+                ),
+            )
+        if hasattr(platform, "linux_distribution"):
+            m.setattr(
+                platform,
+                "linux_distribution",
+                MagicMock(
+                    side_effect=ValueError(
+                        "Non-specific error to force execution of except clause"
+                    )
+                ),
+            )
+
+        sinfo = SC.SysInfo()
+        sinfo.show_general_sys_info()
