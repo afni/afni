7c24cc4f662a3892fd662776734a9eddabffba62
mrneont <neon.taylor@gmail.com>
2020-10-16 13:42:05 -0400

    @animal_warper: put in status checks through, exit by first failure

--- src/afni_history_ptaylor.c
+++ src/afni_history_ptaylor.c
@@ -69,6 +69,11 @@
 afni_history_struct ptaylor_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+{ 16, Oct , 2020 , PT , "@animal_warper" , MINOR , TYPE_GENERAL,
+   "Add in status checks after many afni progs, to exit at/near first failure.",
+   "Should be no change in output for users (in successful runs).\n"
+},
+
 { 16, Oct , 2020 , PT , "@animal_warper" , MINOR , TYPE_BUG_FIX,
    "Now, first cp+resample src to RAI; else, shft is bad for non-xyz orients.",
    "Output warps can still apply to original orient dset fine.\n"
