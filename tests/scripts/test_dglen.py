8d35dc135e25af30d077fb27fb1ab7911a47d056
daniel glen <glend@mail.nih.gov>
2020-10-29 16:51:16 -0400

    add veryslow but basic aea test

--- tests/scripts/test_dglen.py
+++ tests/scripts/test_dglen.py
@@ -42,3 +42,31 @@ def test_align_epi_anat(data):
         },
     )
     differ.run()
+
+
+@pytest.mark.veryslow
+def test_align_epi_anat_with_exec(data):
+    outname = "aligned"
+    if OMP:
+        outname += "_with_omp"
+    outfile = data.outdir / (outname + ".nii.gz")
+    out_1d = outfile.parent / (outfile.stem.split(".")[0] + ".1D")
+
+    cmd = """
+    align_epi_anat.py
+        -anat {data.anat1}
+        -epi {data.epi}
+        -epi_base 0
+        -suffix _aligned
+        -anat_has_skull no
+        -epi_strip 3dAutomask
+    """
+    cmd = " ".join(cmd.format(**locals()).split())
+
+    # Run command and test all outputs match
+    differ = tools.OutputDiffer(
+        data,
+        cmd,
+        ignore_file_patterns=None,
+    )
+    differ.run()
