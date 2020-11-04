9ce3c21a51f28bd306c009cdbf64a9dce207f6d5
leej3 <johnleenimh@gmail.com>
2020-11-04 15:36:49 -0500

    add some fixes

--- tests/scripts/test_dglen.py
+++ tests/scripts/test_dglen.py
@@ -8,9 +8,10 @@ OMP = is_omp("3dAllineate")
 # Define Data
 data_paths = {
     "anat1": "mini_data/anat_3mm_no_skull.nii.gz",
-    "epi": "AFNI_data6/afni/epi_r1+orig.BRIK",
-    "epi_head": "AFNI_data6/afni/epi_r1+orig.HEAD",
+    "epi": "mini_data/small_epi+orig.BRIK.gz",
+    "epi_head": "mini_data/small_epi+orig.HEAD",
 }
+
 
 @pytest.mark.parametrize(
     "test_case,params",
@@ -57,6 +58,8 @@ def test_align_epi_anat(data, test_case, params):
         -anat {data.anat1}
         -epi {data.epi}
         -epi_base 0
+        -epi_strip 3dAutomask
+        -anat_has_skull no
         -suffix _aligned
         {params["extra_args"]}
     """
@@ -71,6 +74,7 @@ def test_align_epi_anat(data, test_case, params):
         kwargs_log={
             "append_to_ignored": [
                 "3dAttribute DELTA",
+                "total CPU to here",
             ]
         },
     )
@@ -103,6 +107,7 @@ def test_align_epi_anat_with_outdir(data):
         kwargs_log={
             "append_to_ignored": [
                 "3dAttribute DELTA",
+                "total CPU to here",
             ]
         },
     )
@@ -133,7 +138,6 @@ def test_align_epi_anat_with_outdir(data):
     ],
 )
 def test_align_epi_anat_dryrun(data, test_case, params):
-    outname = "aligned"
 
     cmd = f"""
     align_epi_anat.py
@@ -159,60 +163,3 @@ def test_align_epi_anat_dryrun(data, test_case, params):
         },
     )
     differ.run()
-
-
-# @pytest.mark.veryslow
-# def test_align_epi_anat_with_exec(data):
-# outname = "aligned"
-# if OMP:
-# outname += "_with_omp"
-# outfile = data.outdir / (outname + ".nii.gz")
-# out_1d = outfile.parent / (outfile.stem.split(".")[0] + ".1D")
-
-# cmd = """
-# align_epi_anat.py
-# -anat {data.anat1}
-# -epi {data.epi}
-# -epi_base 0
-# -suffix _aligned
-# -anat_has_skull no
-# -epi_strip 3dAutomask
-# """
-# cmd = " ".join(cmd.format(**locals()).split())
-
-# # Run command and test all outputs match
-# differ = tools.OutputDiffer(
-# data,
-# cmd,
-# ignore_file_patterns=None,
-# )
-# differ.run()
-
-
-# @pytest.mark.veryslow
-# def test_align_epi_anat_with_outdir(data):
-# outname = "aligned"
-# if OMP:
-# outname += "_with_omp"
-# outfile = data.outdir / (outname + ".nii.gz")
-# out_1d = outfile.parent / (outfile.stem.split(".")[0] + ".1D")
-
-# cmd = """
-# align_epi_anat.py
-# -anat {data.anat1}
-# -epi {data.epi}
-# -epi_base 0
-# -suffix _aligned
-# -anat_has_skull no
-# -epi_strip 3dAutomask
-# -outdir
-# """
-# cmd = " ".join(cmd.format(**locals()).split())
-
-# # Run command and test all outputs match
-# differ = tools.OutputDiffer(
-# data,
-# cmd,
-# ignore_file_patterns=None,
-# )
-# differ.run()
