5d794da6c4828f1bbb40051686ce2f924b6e3ae5
afni-gangc <gangchen@mail.nih.gov>
2021-03-13 22:38:21 -0500

    Added options for tstat and se as input for MBA

--- src/R_scripts/MBA.R
+++ src/R_scripts/MBA.R
@@ -29,7 +29,7 @@ help.MBA.opts <- function (params, alpha = TRUE, itspace='   ', adieu=FALSE) {
                       Welcome to MBA ~1~
     Matrix-Based Analysis Program through Bayesian Multilevel Modeling 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Version 1.0.3, March 13, 2021
+Version 1.0.4, March 13, 2021
 Author: Gang Chen (gangchen@mail.nih.gov)
 Website - https://afni.nimh.nih.gov/gangchen_homepage
 SSCC/NIMH, National Institutes of Health, Bethesda MD 20892
@@ -207,6 +207,26 @@ Example 1 --- Simplest scenario. Values from region pairs are the input from
      S03   lFFA lAmygdala  0.249
      S04   lFFA lAmygdala  0.568
      ...
+
+   If the data is skewed or has outliers, consider using the Student t-distribution
+   through the option -distY:
+
+   MBA -prefix myWonderfulResult -chains 4 -WCP 6 -StanPath '~/my/stan/path' \\
+       -iterations 1000 -model 1 -EOI 'Intercept' -distY 'student' -dataTable myData.txt  \\ 
+
+   If t-statistic (or standard error) values corresponding to the response variable
+   Y are available, add the t-statistic (or standard error) values as a column in the input
+   data table so that they can be incorporated into the BML model using the option -tstat
+   or -se with the following script (assuming the tstat column is named as 'tvalue),
+
+   MBA -prefix myWonderfulResult -chains 4 -WCP 6 -StanPath '~/my/stan/path' \\
+       -iterations 1000 -model 1 -EOI 'Intercept' -tstat tvalue -dataTable myData.txt  \\
+ 
+   or (assuming the se column is named as 'SE'),
+
+   MBA -prefix myWonderfulResult -chains 4 -WCP 6 -StanPath '~/my/stan/path' \\
+       -iterations 1000 -model 1 -EOI 'Intercept' -se SE -dataTable myData.txt  \\
+
  \n"         
      
    ex2 <-
@@ -428,6 +448,21 @@ read.MBA.opts.batch <- function (args=NULL, verb = 0) {
              sep = '\n'
              ) ),
 
+      '-tstat' = apl(n = 1, d = NA,  h = paste(
+   "-tstat var_name: var_name is used to specify the column name that lists",
+   "        the t-statistic values, if available, for the response variable 'Y'.", 
+   "        In the case where standard errors are available for the effect", 
+   "        estiamtes of 'Y', use the option -se.\n", sep = '\n'
+                     ) ),
+
+      '-se'  = apl(n = 1, d = 0, h = paste(
+   "-se: This option indicates that standard error for the response variable is",
+   "         available as input, and a column is designated for the standard error",
+   "         in the data table. If effect estimates and their t-statistics are the",
+   "         output from preceding analysis, standard errors can be obtained by",
+   "         dividing the effect estimatrs ('betas') by their t-statistics. The",
+   "         default assumes that standard error is not part of the input.\n", sep='\n')),
+
       '-Y' = apl(n = 1, d = NA,  h = paste(
    "-Y var_name: var_name is used to specify the column name that is designated as",
    "        as the response/outcome variable. The default (when this option is not",
@@ -531,6 +566,8 @@ read.MBA.opts.batch <- function (args=NULL, verb = 0) {
       lop$qContr  <- NA
       lop$Y      <- 'Y'
       lop$distY  <- 'gaussian'
+      lop$se     <- NULL
+      lop$tstat  <- NULL
       lop$Subj   <- 'Subj'
       lop$ROI1   <- 'ROI1'
       lop$ROI2   <- 'ROI2'
@@ -561,6 +598,8 @@ read.MBA.opts.batch <- function (args=NULL, verb = 0) {
              qContr = lop$qContr <- ops[[i]],       
              Y      = lop$Y      <- ops[[i]],
              distY  = lop$distY  <- ops[[i]],
+             se         = lop$se     <- ops[[i]],
+             tstat      = lop$tstat  <- ops[[i]],
              Subj   = lop$Subj   <- ops[[i]],
              ROI1   = lop$ROI1   <- ops[[i]],
              ROI2   = lop$ROI2   <- ops[[i]],
@@ -750,6 +789,15 @@ if(lop$r2z) lop$dataTable$Y <- fisher(lop$dataTable$Y)
 #levels(lop$dataTable$ROI1) <- union(levels(lop$dataTable$ROI1), levels(lop$dataTable$ROI2))
 #levels(lop$dataTable$ROI2) <- union(levels(lop$dataTable$ROI1), levels(lop$dataTable$ROI2))
 
+# change the se column name when se is provided as input
+if(!is.null(lop$se)) names(lop$dataTable)[which(names(lop$dataTable)==lop$se)] <- 'se'
+
+# convert tstat to se when tstat is provided as input
+if(!is.null(lop$tstat)) {
+   lop$se <- TRUE
+   lop$dataTable$se <- lop$dataTable$Y/lop$dataTable[[lop$tstat]]
+}
+
 # standardization
 if(!is.na(lop$stdz)) {
    sl <- strsplit(lop$stdz, '\\,')[[1]]
@@ -769,12 +817,22 @@ ptm <- proc.time()
 #lop$dataTable$V2 <- rnorm(nrow(lop$dataTable), mean=0.5, sd=1)
 #lop$model <- '1+V1+V2'
 
-##################### MCMC ####################
-if(lop$model==1) modelForm <- as.formula(paste('Y ~ 1 + (1|Subj) + (1|ROI1:ROI2) + 
+##### model formulation #####
+if(is.null(lop$se))  {
+   if(lop$model==1) modelForm <- as.formula(paste('Y ~ 1 + (1|Subj) + (1|ROI1:ROI2) + 
       (1|mm(ROI1, ROI2, weights = cbind(w, w), scale=FALSE)) + 
       (1|mm(ROI1:Subj, ROI2:Subj, weights = cbind(w, w), scale=FALSE))')) else
    modelForm <- as.formula(paste('Y~', lop$model, '+(1|Subj)+(', lop$model, '|ROI1:ROI2)+(', 
       lop$model, '|mm(ROI1, ROI2, weights = cbind(w, w), scale=FALSE))'))
+} else {
+   if(lop$model==1) modelForm <- as.formula(paste('Y|se(se, sigma = TRUE) ~ 1 + (1|Subj) + (1|ROI1:ROI2) +
+      (1|mm(ROI1, ROI2, weights = cbind(w, w), scale=FALSE)) +
+      (1|mm(ROI1:Subj, ROI2:Subj, weights = cbind(w, w), scale=FALSE))')) else
+   modelForm <- as.formula(paste('Y|se(se, sigma = TRUE)~', lop$model, '+(1|Subj)+(', lop$model, '|ROI1:ROI2)+(',  
+      lop$model, '|mm(ROI1, ROI2, weights = cbind(w, w), scale=FALSE))'))
+}
+
+##################### MCMC ####################
 
 if(lop$WCP) {
    if(lop$model==1) fm <- brm(modelForm, data=lop$dataTable, family=lop$distY, chains = lop$chains, 
