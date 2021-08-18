049acd5588afb86da57b38cab52cbc8b28e84024
RWCox <robertcox@mail.nih.gov>
2021-08-18 10:44:37 -0400

    update 3dTfitter help to reflect OpenMP

--- src/Aomp.h
+++ src/Aomp.h
@@ -67,8 +67,8 @@ static INLINE void AAmemset( void *ooo , int c , size_t nnn )
     "   automatic parallelizer software toolkit, which splits the work across\n"   \
     "   multiple CPUs/cores on the same shared memory computer.\n"                 \
     "* OpenMP is NOT like MPI -- it does not work with CPUs connected only\n"      \
-    "   by a network (e.g., OpenMP doesn't work with 'cluster' setups).\n"         \
-    "* For implementation and compilation details, please see\n"                   \
+    "   by a network (e.g., OpenMP doesn't work across cluster nodes).\n"          \
+    "* For some implementation and compilation details, please see\n"              \
     "   https://afni.nimh.nih.gov/pub/dist/doc/misc/OpenMP.html\n"                 \
     "* The number of CPU threads used will default to the maximum number on\n"     \
     "   your system. You can control this value by setting environment variable\n" \
@@ -100,12 +100,15 @@ static INLINE void AAmemset( void *ooo , int c , size_t nnn )
     "* This binary version of %s is NOT compiled using OpenMP, a\n"                \
     "   semi-automatic parallelizer software toolkit, which splits the work\n"     \
     "   across multiple CPUs/cores on the same shared memory computer.\n"          \
-    "* However, the source code is modified for OpenMP, and can be compiled\n"     \
-    "   with an OpenMP-capable compiler, such as gcc 4.2+, Intel's icc, and\n"     \
+    "* However, the source code is compatible with OpenMP, and can be compiled\n"  \
+    "   with an OpenMP-capable compiler, such as gcc 8.x+, Intel's icc, and\n"     \
     "   Oracle Developer Studio.\n"                                                \
     "* If you wish to compile this program with OpenMP, see the man page for\n"    \
     "   your C compiler, and (if needed) consult the AFNI message board, and\n"    \
     "   https://afni.nimh.nih.gov/pub/dist/doc/misc/OpenMP.html\n"                 \
+    "* However, it would probably be simplest to download a pre-compiled AFNI\n"   \
+    "   binary set that uses OpenMP!\n"                                            \
+    "   https://afni.nimh.nih.gov/pub/dist/doc/htmldoc/index.html\n"               \
     , (pnam)                                                                       \
   )
 #endif
