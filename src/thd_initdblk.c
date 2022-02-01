f48b636da29ee1d32b441e0301ddd3bc8a2152f7
afni-discoraj <justin.rajendra@nih.gov>
2022-02-01 15:29:58 -0500

    frac_diff and looping

--- src/thd_initdblk.c
+++ src/thd_initdblk.c
@@ -880,8 +880,8 @@ ENTRY("THD_datablock_apply_atr") ;
 
    if( ATR_IS_STR(ATRNAME_IDSTRING) ){
        if( validate ){               /* disco change */
-           printf("\nold idcode %s\n", dset->idcode.str);
-           printf("new idcode %s\n\n", atr_str->ch);
+           printf("\nold idcode: %s\n", dset->idcode.str);
+           printf("new idcode: %s\n\n", atr_str->ch);
 
        }
        MCW_strncpy( dset->idcode.str , atr_str->ch , MCW_IDSIZE ) ;
@@ -889,8 +889,8 @@ ENTRY("THD_datablock_apply_atr") ;
 
     if( ATR_IS_STR(ATRNAME_IDDATE) ){
       if( validate ){               /* disco change */
-          printf("old id date %s\n", dset->idcode.date);
-          printf("new id date %s\n\n", atr_str->ch);
+          printf("old id date: %s\n", dset->idcode.date);
+          printf("new id date: %s\n\n", atr_str->ch);
       }
       MCW_strncpy( dset->idcode.date , atr_str->ch , MCW_IDDATE ) ;
      }
@@ -1143,40 +1143,56 @@ ENTRY("THD_datablock_apply_atr") ;
           if( validate ){               /* disco change */
 
               /* just testing here */
-              float fracdiff;
+              printf("ijk xx: %13.6s %13.6s %13.6s\n", "old", "new", "diff?");
 
-              fracdiff = frac_diff(atr_flt->fl[0],
-                                   dset->daxes->ijk_to_dicom_real.m[0][0]);
-
-              printf("fracdiff %13.6f\n", fracdiff);
-
-              printf("ijk xx: %13.6s %13.6s\n", "old", "new");
-              printf("ijk 00: %13.6f %13.6f\n",
-                     atr_flt->fl[0], dset->daxes->ijk_to_dicom_real.m[0][0]);
-              printf("ijk 01: %13.6f %13.6f\n",
-                     atr_flt->fl[1], dset->daxes->ijk_to_dicom_real.m[0][1]);
-              printf("ijk 02: %13.6f %13.6f\n",
-                     atr_flt->fl[2], dset->daxes->ijk_to_dicom_real.m[0][2]);
-              printf("ijk 02: %13.6f %13.6f\n",
-                     atr_flt->fl[2], dset->daxes->ijk_to_dicom_real.m[0][2]);
-              printf("ijk 03: %13.6f %13.6f\n",
-                     atr_flt->fl[3], dset->daxes->ijk_to_dicom_real.m[0][3]);
-              printf("ijk 04: %13.6f %13.6f\n",
-                     atr_flt->fl[4], dset->daxes->ijk_to_dicom_real.m[0][4]);
-              printf("ijk 05: %13.6f %13.6f\n",
-                     atr_flt->fl[5], dset->daxes->ijk_to_dicom_real.m[0][5]);
-              printf("ijk 06: %13.6f %13.6f\n",
-                     atr_flt->fl[6], dset->daxes->ijk_to_dicom_real.m[0][6]);
-              printf("ijk 07: %13.6f %13.6f\n",
-                     atr_flt->fl[7], dset->daxes->ijk_to_dicom_real.m[0][7]);
-              printf("ijk 08: %13.6f %13.6f\n",
-                     atr_flt->fl[8], dset->daxes->ijk_to_dicom_real.m[0][8]);
-              printf("ijk 09: %13.6f %13.6f\n",
-                     atr_flt->fl[9], dset->daxes->ijk_to_dicom_real.m[0][9]);
-              printf("ijk 10: %13.6f %13.6f\n",
-                     atr_flt->fl[10], dset->daxes->ijk_to_dicom_real.m[0][10]);
-              printf("ijk 11: %13.6f %13.6f\n",
-                     atr_flt->fl[11], dset->daxes->ijk_to_dicom_real.m[0][11]);
+              int label_c = 0, count_o = 0, count_i = 0;
+              float f_diff, eps = 0.00000001;
+              char diff_yn[2];
+              for (int count_o = 0; count_o < 3; count_o++) {
+                  for (int count_i = 0; count_i < 4; count_i++) {
+                      f_diff = frac_diff(
+                          atr_flt->fl[label_c],
+                          dset->daxes->ijk_to_dicom_real.m[count_o][count_i]);
+                      if (f_diff > eps) {
+                          strcpy(diff_yn, "Y");
+                      } else {
+                          strcpy(diff_yn, "N");
+                      }
+                      printf("ijk %02d: %13.4f %13.4f %13s\n", label_c,
+                             atr_flt->fl[label_c],
+                             dset->daxes->ijk_to_dicom_real.m[count_o][count_i],
+                             diff_yn);
+                      label_c++;
+                  }
+              }
+              printf("\n");
+              // printf("ijk xx: %13.6s %13.6s %13.6s\n", "old", "new", "diff?");
+              // printf("ijk 00: %13.6f %13.6f\n",
+              //        atr_flt->fl[0], dset->daxes->ijk_to_dicom_real.m[0][0]);
+              // printf("ijk 01: %13.6f %13.6f\n",
+              //        atr_flt->fl[1], dset->daxes->ijk_to_dicom_real.m[0][1]);
+              // printf("ijk 02: %13.6f %13.6f\n",
+              //        atr_flt->fl[2], dset->daxes->ijk_to_dicom_real.m[0][2]);
+              // printf("ijk 02: %13.6f %13.6f\n",
+              //        atr_flt->fl[2], dset->daxes->ijk_to_dicom_real.m[0][2]);
+              // printf("ijk 03: %13.6f %13.6f\n",
+              //        atr_flt->fl[3], dset->daxes->ijk_to_dicom_real.m[0][3]);
+              // printf("ijk 04: %13.6f %13.6f\n",
+              //        atr_flt->fl[4], dset->daxes->ijk_to_dicom_real.m[0][4]);
+              // printf("ijk 05: %13.6f %13.6f\n",
+              //        atr_flt->fl[5], dset->daxes->ijk_to_dicom_real.m[0][5]);
+              // printf("ijk 06: %13.6f %13.6f\n",
+              //        atr_flt->fl[6], dset->daxes->ijk_to_dicom_real.m[0][6]);
+              // printf("ijk 07: %13.6f %13.6f\n",
+              //        atr_flt->fl[7], dset->daxes->ijk_to_dicom_real.m[0][7]);
+              // printf("ijk 08: %13.6f %13.6f\n",
+              //        atr_flt->fl[8], dset->daxes->ijk_to_dicom_real.m[0][8]);
+              // printf("ijk 09: %13.6f %13.6f\n",
+              //        atr_flt->fl[9], dset->daxes->ijk_to_dicom_real.m[0][9]);
+              // printf("ijk 10: %13.6f %13.6f\n",
+              //        atr_flt->fl[10], dset->daxes->ijk_to_dicom_real.m[0][10]);
+              // printf("ijk 11: %13.6f %13.6f\n",
+              //        atr_flt->fl[11], dset->daxes->ijk_to_dicom_real.m[0][11]);
           }
         LOAD_MAT44(dset->daxes->ijk_to_dicom_real, \
             atr_flt->fl[0], atr_flt->fl[1], atr_flt->fl[2], atr_flt->fl[3], \
