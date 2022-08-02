f4b0557ad7e9c4cf5668d5e7e8a75dd42c1fe418
mrneont <neon.taylor@gmail.com>
2022-08-02 18:18:15 -0400

    open_apqc.py: updating a bit

--- src/python_scripts/scripts/open_apqc.py
+++ src/python_scripts/scripts/open_apqc.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, request, jsonify
+from flask      import Flask, send_from_directory, request, jsonify
 from flask_cors import CORS # to circumvent 'no access issues from UI'
 import json
 import sys
@@ -10,8 +10,21 @@ import signal
 ### PT:  have to do something to capture ~ and interpret it
 def parse_qc_html_path(pstr):
     """Take in a string pstr that should be the path to an APQC index.html
-file, and return two things:
+file, and return path information to a QC HTML
 
+    Parameters
+    ----------
+    pstr   : str
+             path to subject's index.html in QC_${str}/
+
+    Return
+    ------
+    qc_dir : str
+             relative path to (and including) QC_${subj} dir, which contains
+             the index.html file
+
+    apqc_json : str
+             relative path to (and including) apqc_${subj}.json
     """
 
     plist = pstr.split('/')
@@ -20,8 +33,8 @@ file, and return two things:
         print("** ERROR: input should be path to, and including, index.html")
         sys.exit(1)
 
-    # make sure the dir holding index.html exists as a separate thing,
-    # even though this case should never occur in practice
+    # make sure the dir holding index.html exists as a separate link
+    # the path name (even though this should never occur in practice)
     if ndir == 1 :
         plist.insert(0, '.')
         ndir+= 1
@@ -40,6 +53,7 @@ file, and return two things:
 
     return qc_dir, apqc_json[0]
 
+# ========================== input stuff ==========================
 
 start_dir = os.getcwd()
 
@@ -50,31 +64,40 @@ parser = argparse.ArgumentParser()
 #exemplar = dirs[0]
 #dirname = os.path.dirname(exemplar) #"FT_NEW.results/QC_FT/" # os.path.dirname(exemplar)
 
-
 parser.add_argument('-i', nargs='+') # relative path to index.html
 args  = parser.parse_args()
 ipath = args.i[0]           # later deal with LIST ASPECT
 
 
+# these variables are used below
 qc_dir, apqc_json = parse_qc_html_path(ipath)
 
 print("++ ipath     : {}".format(ipath))
 print("++ apqc_json : {}".format(apqc_json))
 print("++ qc_dir    : {}".format(qc_dir))
 
-app = Flask(__name__) # initialize flask app
-CORS(app) # let CORS package upgrade app
+# ========================== flask/decorator stuff ==========================
+
+app = Flask(__name__)           # initialize flask app
+CORS(app)                       # let CORS package upgrade app
 
 @app.route("/<path:path>", methods=["GET"])
 def index(path):
-    '''
-    comment this  
+    '''comment this...
+
+    'path' is the name of the apqc_*.json file, which sits in the
+    directory 'qc_dir'.
+
     '''
     print("++ path is:", path)
     return send_from_directory(qc_dir, path)
 
 @app.route("/save", methods=["POST"])
 def save_json():
+    """
+    Function to save the QC button/rating info to the apqc*.json file.
+    """
+
     jdata = request.get_json()
     with open(apqc_json, 'w', encoding='utf-8') as f:
         json.dump(jdata, f, ensure_ascii=False, indent=4)
@@ -89,5 +112,8 @@ def save_json():
 def load_json():
     print('loading json')
 
+# ================================ main =====================================
+
 if __name__ == "__main__":
     app.run()
+
