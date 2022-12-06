e5681aecbb624b758fd4f6bb29bd669326af216a
mrneont <neon.taylor@gmail.com>
2022-12-06 14:14:24 -0500

    @SSwarper: protect paths with quotes, because of OneDrive spaces

--- src/afni_history_ptaylor.c
+++ src/afni_history_ptaylor.c
@@ -69,6 +69,11 @@
 afni_history_struct ptaylor_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+{ 6, Dec , 2022 , PT , "@SSwarper" , MICRO , TYPE_GENERAL,
+   "Put quotes around (hopefully) every path, so spaces in paths are OK.",
+   "This is because of OneDrive.  Grrrr.\n"
+},
+
 { 2, Dec , 2022 , PT , "@animal_warper" , MINOR , TYPE_ENHANCE,
    "Add another QC image if  '-init_scale ..' is used.",
    "Useful to check if scaling is approximately appropriate.\n"
