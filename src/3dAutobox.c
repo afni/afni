9372e11d8b8ef1a44b7431f8419d94827079521f
mrneont <neon.taylor@gmail.com>
2023-01-17 17:53:11 -0500

    3dAutobox: update/improve the ijkord functionality, and explain more

--- src/3dAutobox.c
+++ src/3dAutobox.c
@@ -24,6 +24,39 @@
 
  */
 
+
+/*
+  A function that takes a dataset as input, and based on its
+  orientation outputs a string of 'i', 'j' and 'k' values. For
+  example, the orientation 'RPI' -> 'ijk', and 'AIL' -> 'jki'.
+*/
+int THD_fill_char_int_3_ijk(THD_dataxes * daxes, char ostr[4]);
+
+int THD_fill_char_int_3_ijk(THD_dataxes * daxes, char ostr[4])
+{
+   int nn;
+   char *ref_str = "ijk\0";
+
+   if ( ! daxes || ! ostr ) return 1;
+
+   /*
+   INFO_message("TEST1: %d %d %d", 
+                ORIENT_xyzint[daxes->xxorient]-1, 
+                ORIENT_xyzint[daxes->yyorient]-1,
+                ORIENT_xyzint[daxes->zzorient]-1);
+   INFO_message("TEST1: %c %c %c",
+                ref_str[ORIENT_xyzint[daxes->xxorient]-1 ], 
+                ref_str[ORIENT_xyzint[daxes->yyorient]-1 ], 
+                ref_str[ORIENT_xyzint[daxes->zzorient]-1 ]);
+   */
+
+   ostr[0] = ref_str[ORIENT_xyzint[daxes->xxorient]-1 ];
+   ostr[1] = ref_str[ORIENT_xyzint[daxes->yyorient]-1 ];
+   ostr[2] = ref_str[ORIENT_xyzint[daxes->zzorient]-1 ];
+
+   return 0;
+};
+
 void help_autobox()
 {
    printf(
@@ -71,23 +104,18 @@ void help_autobox()
      "                     imid jmid kmid\n"
      "                 These are obtained via: (imin + imax)/2, etc.\n"
      "\n"
-     "-extent_ijkord  :Write out the 6 auto bbox ijk slice numbers to\n"
-     "                 screen, *ordered* so the first two values refer to\n"
-     "                 the x-axis, the next to the y-axis, etc.:\n"
-     "                     iordmin iordmax jordmin jordmax kordmin kordmax\n"
-     "                 In this case, orientation rearrangement (RAI to AIR, \n"
-     "                 RIA, IRA, etc.) would *not* affect the index output,\n"
-     "                 though other changing orientation lettering (RAI to \n"
-     "                 LAI, SLP, RAS, etc.) *would* affect the output.\n"
-     "                   NB: This option is quite useful if you want to use\n"
+     "-extent_ijkord  :Write out the 6 auto bbox ijk slice numbers to screen\n"
+     "                 but in a particular order and format (see 'NOTE on\n"
+     "                 *ijkord* format', below).\n"
+     "                   NB: This ordering is useful if you want to use\n"
      "                 the output indices in 3dcalc expressions.\n"
      "\n"
      "-extent_ijkord_to_file FFORRD\n"
-     "                :Write out the 6 *ordered* auto bbox ijk slice numbers\n"
-     "                 to a simple-formatted text file FFORD (single row \n"
-     "                 file):\n"
-     "                     iordmin iordmax jordmin jordmax kordmin kordmax\n"
-     "                 (same notes as above apply).\n"
+     "                :Write out the 6 auto bbox ijk slice numbers to a file\n"
+     "                  but in a particular order and format (see 'NOTE on\n"
+     "                  *ijkord* format', below).\n"
+     "                   NB: This option is quite useful if you want to use\n"
+     "                 the output indices in 3dcalc expressions.\n"
      "\n"
      "-extent_xyz_to_file GG\n"
      "                :Write out the 6 auto bbox xyz coords to\n"
@@ -113,9 +141,63 @@ void help_autobox()
      "                 each index is bounded to be in range [0, L-1], where L\n"
      "                 is matrix length along that dimension.\n"
      "\n"
+     "\n"
+     "NOTE on *ijkord* format  ~1~\n"
+     "\n"
+     "Using any of the '-*ijkord*' options above will output pairs of ijk\n"
+     "indices just like the regular ijk options, **but** they will be ordered\n"
+     "in a way that you can associate each of the i, j, and k indices with\n"
+     "a standard x, y and z coordinate direction.  Without this ordering,\n"
+     "resampling a dataset could change what index is associated with which\n"
+     "coordinate axis.  That situation can be confusing for scripting (and\n"
+     "by confusing, we mean 'bad').\n"
+     "\n"
+     "The output format for any '-*ijkord*' options is a 3x3 table, where\n"
+     "the first column is the index value (i, j or k), and the next two\n"
+     "columns are the min and max interval boundaries for the autobox.\n"
+     "Importantly, the rows are placed in order so that the top corresponds\n"
+     "to the x-axis, the middle to the y-axis and the bottom to the z-axis.\n"
+     "\n"
+     "So, if you had the following table output for a dset:\n"
+     "     k       10      170\n"
+     "     i       35      254\n"
+     "     j       21      199\n"
+     "\n"
+     "... you would look at the third row for the min/max slice values\n"
+     "along the z-axis, and you would use the index 'j' to refer to it in,\n"
+     "say, a 3dcalc expression.\n"
+     "\n"
+     "Note that the above example table output came from a dataset with ASL\n"
+     "orientation.  We can see how that fits, recalling that the first,\n"
+     "second and third rows tell us about x, y and z info, respectively; and\n"
+     "that i, j and k refer to the first, second and third characters in the\n"
+     "orientation string.  So, the third (z-like) row contains a j, which\n"
+     "points us at the middle character in the orientation, which is S, which\n"
+     "is along the z-axis---all consistent!  Similarly, the top (x-like) row\n"
+     "contains a k, which points us at the last char in the orientation,\n"
+     "which is L and that is along the x-axis---phew!\n"
+     "\n"
+     "The main point of this would be to extra this information and use it\n"
+     "in a script.  If you knew that you wanted the z-slice range to use\n"
+     "in a 3dcalc 'within()' expression, then you could extract the 3rd row\n"
+     "to get the correct index and slice ranges, e.g., in tcsh:\n"
+     "    set vvv = `sed -n 1p FILE_ijkord.txt`\n"
+     "\n"
+     "... where now ${vvv} will have 3 values, the first of which is the\n"
+     "relevant index letter, then the min and max slice range values.\n"
+     "So an example 3dcalc expression to keep values only within\n"
+     "that slice range:\n"
+     "    3dcalc                                                   \\\n"
+     "        -a      DSET                                         \\\n"
+     "        -expr   \"a*within(${vvv[1]},${vvv[2]},${vvv[3]})\"    \\\n"
+     "        -prefix DSET_SUBSET\n"
+     "\n"
    ) ;
    PRINT_COMPILE_DATE ; return ;
 }
+
+
+
 
 /*----------------------------------------------------------------------------*/
 
@@ -146,6 +228,7 @@ int main( int argc , char * argv[] )
       "-*ijkord*" opts
    */
    int ijk_as_rai[6] = {-1, -1, -1, -1, -1};
+   char ijk_order[4] = "---\0";
 
    /* PT: default npad can give indices outside dset.  Activating this
       flag with the '-npad_safety_on' opt will constrain each index to
@@ -321,11 +404,13 @@ int main( int argc , char * argv[] )
       INFO_message("Auto bbox: x=%d..%d  y=%d..%d  z=%d..%d\n",
                    xm,xp,ym,yp,zm,zp ) ;
 
-      // [PT: Jan 17, 2023] do the work for ijk_RAI
+      // [PT: Jan 17, 2023] do the work for ijkord, including getting
+      // the associated string values
       {
          int i;
          int axes_ord[3] = {-10, -10, -10};
-         
+         char test_str[] = "---\0";
+
          // get int orient codes
          i = THD_fill_orient_int_3_rlpais(dset->daxes, axes_ord);
          // ... and convert the codes, ordering for x (=0), y (=1) and z (=2)
@@ -341,6 +426,14 @@ int main( int argc , char * argv[] )
 
          ijk_as_rai[2*axes_ord[2]]   = zm;
          ijk_as_rai[2*axes_ord[2]+1] = zp;
+
+         THD_fill_char_int_3_ijk(dset->daxes, test_str);
+         //INFO_message("TEST: %s", test_str);
+
+         for( i=0 ; i<3 ; i++ )
+            ijk_order[i] = test_str[axes_ord[i]];
+         //INFO_message("TESTb: %s", ijk_order);
+                  
 
          //INFO_message("Auto bbox: iord=%d..%d  jord=%d..%d kord=%d..%d\n",
          //             ijk_as_rai[0], ijk_as_rai[1], ijk_as_rai[2], 
@@ -371,9 +464,10 @@ int main( int argc , char * argv[] )
       }
 
       if( extent_ijkord ) 
-         printf( "%8d %8d %8d %8d %8d %8d\n",
-                 ijk_as_rai[0], ijk_as_rai[1], ijk_as_rai[2], 
-                 ijk_as_rai[3], ijk_as_rai[4], ijk_as_rai[5] );
+         printf( "%c %8d %8d\n%c %8d %8d\n%c %8d %8d\n",
+                 ijk_order[0], ijk_as_rai[0], ijk_as_rai[1], 
+                 ijk_order[1], ijk_as_rai[2], ijk_as_rai[3], 
+                 ijk_order[2], ijk_as_rai[4], ijk_as_rai[5] );
 
       if ( (extent && !prefix) || (extent_xyz_midslice && !prefix) || \
            (oxyzext && !prefix) || (oijkordext && !prefix) )
@@ -381,10 +475,10 @@ int main( int argc , char * argv[] )
 
       if( prefix ){
          outset = THD_zeropad( dset ,
-                            -xm, xp-nx+1,
-                            -ym, yp-ny+1,
-                            -zm, zp-nz+1,
-                            prefix , ZPAD_IJK ) ;
+                               -xm, xp-nx+1,
+                               -ym, yp-ny+1,
+                               -zm, zp-nz+1,
+                               prefix , ZPAD_IJK ) ;
          if( THD_deathcon() && THD_is_file(DSET_HEADNAME(outset)) )
             ERROR_exit("3dAutobox: output file %s already exists :-(",
                        DSET_HEADNAME(outset) ) ;
@@ -431,9 +525,11 @@ int main( int argc , char * argv[] )
                fprintf(stderr, "\n\nError opening file %s.\n", oijkordext);
                exit(1);
             }
-            fprintf( fout_ijkordext, "%d  %d  %d  %d  %d  %d\n",
-                     ijk_as_rai[0], ijk_as_rai[1], ijk_as_rai[2], 
-                     ijk_as_rai[3], ijk_as_rai[4], ijk_as_rai[5] );
+            fprintf( fout_ijkordext, 
+                     "%c %8d %8d\n%c %8d %8d\n%c %8d %8d\n",
+                     ijk_order[0], ijk_as_rai[0], ijk_as_rai[1], 
+                     ijk_order[1], ijk_as_rai[2], ijk_as_rai[3], 
+                     ijk_order[2], ijk_as_rai[4], ijk_as_rai[5] );
             fclose(fout_ijkordext);
             INFO_message("Wrote ijkord extents file: %s", oijkordext);
          }
--- src/afni_history_ptaylor.c
+++ src/afni_history_ptaylor.c
@@ -69,6 +69,11 @@
 afni_history_struct ptaylor_history[] = {
 /*=====BELOW THIS LINE=====*/
 
+{ 17, Jan , 2023 , PT , "3dAutobox" , MINOR , TYPE_ENHANCE,
+   "And just like that, update/improve the ijkord functionality and format.",
+   "Also explain more clearly what is happening in a help section.\n"
+},
+
 { 17, Jan , 2023 , PT , "3dAutobox" , MINOR , TYPE_NEW_OPT,
    "More new options, in particular for ijkord (-> useful for 3dcalc expr).",
    "Also clean up other features/help. Add -npad_safety_on, too.\n"
