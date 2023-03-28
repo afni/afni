38c80a68a578add7ec61def0e541f6dbe9c11da9
afni-laurenpd <peterdlauren@gmail.com>
2023-03-27 21:03:24 -0400

    Replaced lib_retro_out.py with object oriented version, lib_retro_outObj.py, that treats cardiac and respiriatory output as separate objects

--- src/retroicor/retroicorTaylor.py
+++ src/retroicor/retroicorTaylor.py
@@ -7,6 +7,7 @@ import lib_retro_opts    as lro
 import lib_retro_reading as lrr
 import lib_retro_proc as lrp
 import lib_retro_out as RET
+import lib_retro_outObj as RETO
 
 
 # ================================ main =====================================
@@ -27,18 +28,21 @@ if __name__ == "__main__":
               'noise components')
         sys.exit()
 
-    # parameters['OutDir'] = OutDir
-    if RET.outputInNimlFormat(physiologicalNoiseComponents, test_retro_obj):
+    if RETO.outputInNimlFormat(physiologicalNoiseComponents, test_retro_obj):
         print('** ERROR outputting SliBase file')
         sys.exit()
+
+    # if RET.outputInNimlFormat(physiologicalNoiseComponents, test_retro_obj):
+    #     print('** ERROR outputting SliBase file')
+    #     sys.exit()
     
-    if len(physiologicalNoiseComponents['resp_phases']) > 0 and\
-        (test_retro_obj.save_graph_level > 1 or 
-         test_retro_obj.show_graph_level > 1):
-        status = RET.show_rvt_peak(physiologicalNoiseComponents, test_retro_obj)
-        if status == 1:
-            print('*** Error in retro_ts')
-            print('Failure to show RVT peak')
-            sys.exit()
+    # if len(physiologicalNoiseComponents['resp_phases']) > 0 and\
+    #     (test_retro_obj.save_graph_level > 1 or 
+    #      test_retro_obj.show_graph_level > 1):
+    #     status = RET.show_rvt_peak(physiologicalNoiseComponents, test_retro_obj)
+    #     if status == 1:
+    #         print('*** Error in retro_ts')
+    #         print('Failure to show RVT peak')
+    #         sys.exit()
 
     print("++ DONE.  Goodbye.")
