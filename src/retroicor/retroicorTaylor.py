937a45b700cfade7f564e6d29fb4159c603e7e09
afni-laurenpd <peterdlauren@gmail.com>
2023-04-11 10:09:28 -0400

    Added TOTO, and DONE, list to retroicorTaylor.py

--- src/retroicor/retroicorTaylor.py
+++ src/retroicor/retroicorTaylor.py
@@ -1,6 +1,53 @@
 #!/usr/bin/env python
 
 # this program runs the newer/test command line interface for retroicor.
+"""
+    2022-2023 Peter Lauren, Paul Taylor
+    peterdlauren@gmail.com
+
+    "retroicorTaylor" is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    "retroicorLauren2" is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with "retroicorLauren".  If not, see <http://www.gnu.org/licenses/>.
+    
+    TODO:
+        - Allow dataset, as input, to determine TR, # slices, # time points and
+            slice pattern.  Command line options could overwrite that. JSON file
+        - EPI data set (Being used in afniproc command)
+        - alt-Z (alternating positive and megative z-direction)
+        - Multiband (multiple slices at same point)
+        - RVT without shifts
+        - Variance in linear model used to assess quality (Convolve RVT with some 
+            function using physiological regressors)
+        - Get percentage of variance accounted for by cardio
+        - Large smoothing to find peaks and troughs
+        - Small smoothing to remove outliers
+        - Histogram of model
+        - Remove large outliers in cardio
+        - Duplicate current code over all slices
+        - Per slice with cardio to deal with temporal offsets across slices
+        - Try weird examples from physio dB
+        - Options that might change do not have default
+        - Add options for Peter Lauren-written alternatives for
+            - Findging peaks
+            - Determining phase
+            - Determining final output
+        - Write alternative functions for
+            - Finding peaks
+            - Determining phase
+            - Determining final output
+            
+    DONE:
+        - Offset for every slice relative to TR
+            - afnipy/afni_util.py:slice_pattern_to_timing() gets the actual timing
+        - Implement slice pattern.  Required option 
+"""
 
 import sys
 import lib_retro_opts    as lro
