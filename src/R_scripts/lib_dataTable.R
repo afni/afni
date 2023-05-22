259db3f6e1875dfbc86de0a2e0a973bb9970f9cc
afni-discoraj <justin.rajendra@nih.gov>
2023-05-22 10:37:52 -0400

    table checker added Note message functions

--- src/R_scripts/lib_dataTable.R
+++ src/R_scripts/lib_dataTable.R
@@ -34,6 +34,10 @@ dtCheck_good <- function(msg,nl=2){   ## need your own ending punctuation
     if( nl == 2 ){ end.line <- "\n\n" } else { end.line <- "\n" }
     dtCheck_log_print(paste0("++Good: ",msg,end.line)) 
 }
+dtCheck_note <- function(msg,nl=2){   ## need your own ending punctuation
+    if( nl == 2 ){ end.line <- "\n\n" } else { end.line <- "\n" }
+    dtCheck_log_print(paste0("--Note: ",msg,end.line)) 
+}
 
 ## log file prefix name #################
 ## takes some string from pprefix.AFNI.name in AFNIio.R
@@ -89,8 +93,10 @@ dtCheck_log_df <- function(df.in){
 ## takes data frame, output of some 3dinfo
 ## a number to compare against and if it should equal or not equal (T/F)
 ## msg gets passed to the err message function
+## note get passed to note function AFTER error message
 ## returns nothing, prints to screen and adds to log.out
-dtCheck_bad_files <- function(data.in,cmd.num,bad.num=0,equals=TRUE,msg=""){
+dtCheck_bad_files <- function(data.in,cmd.num,bad.num=0,equals=TRUE,
+                              msg="",note=""){
     
     ## get the bad indices
     if( equals ){
@@ -115,10 +121,11 @@ dtCheck_bad_files <- function(data.in,cmd.num,bad.num=0,equals=TRUE,msg=""){
     }
     
     ## output
-    dtCheck_err(msg,1)
+    if( msg != "" ){ dtCheck_err(msg,1) }
+    if( note != "" ){ dtCheck_note(note,1) }
     dtCheck_log_df(tab.out)  ## save to log
     cat(tab.out,sep="\n")    ## print to screen
-    cat('\n')
+    cat("\n")
 }   ## end dtCheck_bad_files
 
 ## write log.out to file
@@ -337,10 +344,6 @@ dtCheck_1_vol <- function(data.in){
     } else {
         dtCheck_bad_files(data.in,cmd.num,1,FALSE,
                           "Datasets have more than 1 volume")
-        # bad.dsets <- data.in$InputFile[which(cmd.num != 1)]
-        # dtCheck_err("Datasets have more than 1 volume",1)
-        # dtCheck_log_print(paste0(paste0(as.character(bad.dsets),
-        #                                 collapse="\n"),"\n\n"))
         return(1)
     }
 }   ## end dtCheck_1_vol
@@ -361,10 +364,15 @@ dtCheck_same_grid <- function(data.in){
         dtCheck_good('All InputFiles are on the same grid.')
         return(0)
     } else {
-        bad.dsets <- data.in$InputFile[which(cmd.num == 0)]
-        dtCheck_err("Datasets not on the same grid",1)
-        dtCheck_log_print(paste0(paste0(as.character(bad.dsets),
-                                        collapse="\n"),"\n\n"))
+        
+        dtCheck_bad_files(data.in,cmd.num,0,TRUE,
+                          "Datasets have more than 1 volume",
+                          "Grids are compared to the 1st InputFile.")
+        # bad.dsets <- data.in$InputFile[which(cmd.num == 0)]
+        # dtCheck_err("Datasets not on the same grid",1)
+        # dtCheck_note("Grids are compared to the 1st InputFile.",1)
+        # dtCheck_log_print(paste0(paste0(as.character(bad.dsets),
+        #                                 collapse="\n"),"\n\n"))
         return(1)
     }
 }   ## end dtCheck_same_grid
