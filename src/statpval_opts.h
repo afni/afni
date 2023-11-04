b049fc594d2988d437904e585015c543d0d5eff3
mrneont <neon.taylor@gmail.com>
2023-11-04 06:35:05 -0400

    p2dsetstat: now a C language version (with more helpful output text)

---
+++ src/statpval_opts.h
@@ -0,0 +1,35 @@
+#ifndef THD_STATPVAL_INCLUDED
+#define THD_STATPVAL_INCLUDED
+
+
+/*
+  Object for things used with either p2dsetstat or dsetstat2p. Even
+  though each prog has different inputs, in the end all of hte same
+  things have to be known.
+*/
+typedef struct {
+
+   char *input_name;    // 3D vol (prob from a 4D dset with subbrick selector)
+   char *brick_lab;     // brick label
+
+   float pval;          // will be either provided or calc'ed
+   int   as_1_sided;    // 1 = 1sided; 0 = 2sided, bisided
+   char *sidedness;     // str of sidedness
+   char *stat_and_pars; // str of stat name and params
+
+   float statval;       // will be either calc'ed or provided by user
+   int params[10];      // far more space than needed, just to be safe!
+
+   int verb;            // verbosity level
+
+} PARAMS_statpval;
+
+// -------------------------------------------------------------------------
+
+/* function to initialize params */
+PARAMS_statpval set_p2dsetstat_defaults(void);
+
+
+
+
+#endif
