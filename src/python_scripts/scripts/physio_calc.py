f58e825b3a6b71a20f5e8015fe7d145b4b6f03cd
mrneont <neon.taylor@gmail.com>
2024-08-28 15:52:06 -0400

    physio_calc.py is public

--- src/python_scripts/scripts/physio_calc.py
+++ src/python_scripts/scripts/physio_calc.py
@@ -4,17 +4,6 @@
 """
     2022-2023 Peter Lauren, Paul Taylor
     peterdlauren@gmail.com
-
-    "retroicorTaylor" is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    "retroicorLauren2" is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with "retroicorLauren".  If not, see <http://www.gnu.org/licenses/>.
     
     TODO:
         - Variance in linear model used to assess quality.  (How it affects the
