584a07c9b35055f392a7f66a00648b0f7c437878
rick reynolds <reynoldr@mail.nih.gov>
2025-05-19 11:00:39 -0400

    apply fprintf clean from Peter

--- src/SUMA/SUMA_xColBar.c
+++ src/SUMA/SUMA_xColBar.c
@@ -1099,7 +1099,6 @@ int SUMA_set_threshold_one(SUMA_ALL_DO *ado, SUMA_OVERLAYS *colp,
    colp->OptScl->ThreshRange[0] = val;
 
    colp->OptScl->UseThr = XmToggleButtonGetState (SurfCont->Thr_tb);
-   fprintf(stderr, "colp->OptScl->UseThr = %d\n", colp->OptScl->UseThr);
 
    if (LocalHead) {
       fprintf( SUMA_STDERR,
@@ -1204,14 +1203,10 @@ int SUMA_SwitchColPlaneIntensity(
         // colpC->OptScl->UseThr = 1;
       }
    }
-/*
-    */
+
     SUMA_SurfaceObject *SO = (SUMA_SurfaceObject *)ado;
     colp->OptScl->UseThr = !(colp->AlphaOpacityFalloff);
-    /*
-    SUMA_ScaleToMap_Interactive ( colp );
-    colp->OptScl->UseThr = 1;
-*/
+
    SUMA_RETURN(1);
 }
 
@@ -1451,23 +1446,17 @@ void SUMA_cb_SwitchIntensity(Widget w, XtPointer client_data, XtPointer call)
 
    SUMA_ENTRY;
    
-   fprintf(stderr, "%s\n", FuncName);
-
    /* get the surface object that the setting belongs to */
    datap = (SUMA_MenuCallBackData *)client_data;
    ado = (SUMA_ALL_DO *)datap->ContID;
    curColPlane = SUMA_ADO_CurColPlane(ado);
    imenu = (INT_CAST)datap->callback_data;
 
-   fprintf(stderr, "1: curColPlane->OptScl->UseThr = %d\n", curColPlane->OptScl->UseThr);
-   
    if (imenu-1 == curColPlane->OptScl->find) {
       SUMA_RETURNe; /* nothing to be done */
    }
 
    SUMA_SwitchColPlaneIntensity(ado, curColPlane, imenu -1, 0);
-
-   fprintf(stderr, "2: curColPlane->OptScl->UseThr = %d\n", curColPlane->OptScl->UseThr);
    
    curColPlane->intensitySwitched = 1;
 
@@ -1490,8 +1479,6 @@ int SUMA_SwitchColPlaneThreshold(
 
    SUMA_ENTRY;
    
-   fprintf(stderr, "%s\n", FuncName);
-
    if (!SUMA_SwitchColPlaneThreshold_one(ado, colp, ind, setmen)) {
       SUMA_S_Err("Failed in _one");
       SUMA_RETURN(0);
@@ -1530,9 +1517,6 @@ int SUMA_SwitchColPlaneThreshold_one(
 
    SUMA_ENTRY;
    
-   fprintf(stderr, "%s\n", FuncName);
-
-
    SurfCont = SUMA_ADO_Cont(ado);
    curColPlane = SUMA_ADO_CurColPlane(ado);
    if (!ado || !SurfCont || !curColPlane ||
@@ -1629,8 +1613,6 @@ void SUMA_cb_SwitchThreshold(Widget w, XtPointer client_data, XtPointer call)
 
     SUMA_ENTRY;
 
-   // fprintf(stderr, "1: colp->OptScl->UseThr = %d\n", colp->OptScl->UseThr);
-
    /* get the surface object that the setting belongs to */
    datap = (SUMA_MenuCallBackData *)client_data;
    /* ado = (SUMA_ALL_DO *)datap->ContID;*/
@@ -1671,8 +1653,6 @@ int SUMA_SwitchColPlaneBrightness(
 
    SUMA_ENTRY;
    
-   fprintf(stderr, "%s\n", FuncName);
-
    if (LocalHead) {
       fprintf(SUMA_STDERR,
          "%s:\n request to switch brightness to col. %d\n", FuncName, ind);
@@ -1807,8 +1787,6 @@ void SUMA_cb_SwitchBrightness(Widget w, XtPointer client_data, XtPointer call)
 
    SUMA_ENTRY;
    
-   fprintf(stderr, "%s\n", FuncName);
-
    /* get the surface object that the setting belongs to */
    datap = (SUMA_MenuCallBackData *)client_data;
    ado = (SUMA_ALL_DO *)datap->ContID;
