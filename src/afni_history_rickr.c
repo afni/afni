0759f1c5876791e9d59243dab636e60560c4d7e3
rick reynolds <reynoldr@mail.nih.gov>
2025-07-22 15:48:31 -0400

    uab: add -binary_source

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -53,9 +53,19 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 22, Jul, 2025, RCR, "@update.afni.binaries", MICRO, TYPE_NEW_OPT,
+   "add option -binary_source, to supersede -build_afni and -overwrite_build",
+   NULL
+ } ,
+
  {  3, Jul, 2025, RCR, "afni-general", MINOR, TYPE_BUG_FIX,
    "fix test of atr_flt2 when setting TAXIS_FLOATS",
    "Thanks to C Rorden for noting the problem."
+ } ,
+
+ { 18, Jun, 2025, RCR, "build_afni.py", MICRO, TYPE_ENHANCE,
+   "check that build_root is not at or under install dir",
+   NULL
  } ,
 
  { 17, Jun, 2025, RCR, "afni_proc.py", MICRO, TYPE_ENHANCE,
