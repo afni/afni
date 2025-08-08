9d330beeeb84d3bb02c4d5d8da805a467d8a9259
afni-laurenpd <peterdlauren@yahoo.com>
2025-08-08 17:24:52 -0400

    In finding threshold outlines, clicking the "A" button casues variable opacity and threshold outlines (if required) to appear on both surfaces.  This applies to DriveSuma as well as the control panel

--- src/SUMA/SUMA_Color.c
+++ src/SUMA/SUMA_Color.c
@@ -3620,7 +3620,7 @@ SUMA_Boolean SUMA_MakeThresholdOutlines (   SUMA_OVERLAYS *Sover )
 
    ado = SUMA_Overlay_OwnerADO(Sover);
    SO = (SUMA_SurfaceObject *)ado;
-
+// #if 0
    if (!Sover) { SUMA_SL_Err("NULL Sover"); SUMA_RETURN(NOPE); }
    if (!Sover->cmapname) {
       SUMA_SL_Err("NULL Colormap name"); SUMA_RETURN(NOPE);
@@ -3661,68 +3661,25 @@ SUMA_Boolean SUMA_MakeThresholdOutlines (   SUMA_OVERLAYS *Sover )
    } else {
       Opt->ColsContMode = 0;
    }
+
+   // This is totally necessary.  Program crashes w/o it.   
    SV = SUMA_Create_ColorScaledVect(SDSET_VECFILLED(Sover->dset_link),
                                     Opt->ColsContMode);
-
-#if 0   
-
-#endif
 
    SUMA_LH("Fetching vectors from dset");
 
    B = NULL;
    /* Thresholding ? */
-   if (Opt->tind >= 0 && Opt->UseThr) {
-      SUMA_LH("Fetching Threshold column");
-      if (  !SUMA_SetOverlay_Vecs(Sover, 'T', Opt->tind, "update", 0) ||
-            !Sover->T ) {
-         SUMA_S_Err("Failed to get T");
-         SUMA_RETURN(NOPE);
-      }
-      /* setting SV->isMasked[i] means the node overlay is not shown */
-      /* (if Alpha, nothing gets masked out here) */
-      switch (Opt->ThrMode) {
-         case SUMA_NO_THRESH:
-            break;
-         case SUMA_LESS_THAN:
-            for (i=0; i<SDSET_VECFILLED(Sover->dset_link); ++i) {
-               if (Sover->T[i] < Opt->ThreshRange[0]) {
-                  if (!Sover->AlphaOpacityFalloff) SV->isMasked[i] = YUP; /* Mask */
-               }
-            }
-            break;
-         case SUMA_ABS_LESS_THAN:
-            for (i=0; i<SDSET_VECFILLED(Sover->dset_link); ++i) {
-               if (Sover->T[i] < Opt->ThreshRange[0] &&
-                   Sover->T[i] > -Opt->ThreshRange[0]) {
-                  if (!Sover->AlphaOpacityFalloff ||
-                     SO->SurfCont->BoxOutlineThresh) 
-                         SV->isMasked[i] = YUP; /* Mask */
-               }
-            }
-            break;
-         case SUMA_THRESH_OUTSIDE_RANGE:
-            for (i=0; i<SDSET_VECFILLED(Sover->dset_link); ++i) {
-               if (Sover->T[i] >= Opt->ThreshRange[0] &&
-                   Sover->T[i] <= Opt->ThreshRange[1]) {
-                  if (!Sover->AlphaOpacityFalloff) SV->isMasked[i] = YUP; /* Mask */
-               }
-            }
-            break;
-         case SUMA_THRESH_INSIDE_RANGE:
-            for (i=0; i<SDSET_VECFILLED(Sover->dset_link); ++i) {
-               if (Sover->T[i] < Opt->ThreshRange[0] ||
-                   Sover->T[i] > Opt->ThreshRange[1]) {
-                  if (!Sover->AlphaOpacityFalloff) SV->isMasked[i] = YUP; /* Mask */
-               }
-            }
-            break;
-         default:
-            SUMA_SL_Err("Wrond threshold mode");
-            SUMA_RETURN(NOPE);
-            break;
-      }
+   for (i=0; i<SDSET_VECFILLED(Sover->dset_link); ++i) {
+       if (Sover->T[i] < Opt->ThreshRange[0] &&
+           Sover->T[i] > -Opt->ThreshRange[0]) {
+          if (!Sover->AlphaOpacityFalloff ||
+             SO->SurfCont->BoxOutlineThresh) 
+                 SV->isMasked[i] = YUP; /* Mask */
+       }
    }
+   
+   // #if 0
 
    /* setup nodedef so that it can be used along with Sover->V
       for clusterinzing. Sover->V will get modified in subsequent
@@ -3735,6 +3692,7 @@ SUMA_Boolean SUMA_MakeThresholdOutlines (   SUMA_OVERLAYS *Sover )
       }
    }
    nd = SUMA_GetNodeDef(Sover->dset_link);
+    // #if 0
    if (nd) {
       cnt = 0;
       for (i=0; i<SDSET_VECFILLED(Sover->dset_link); ++i) {
@@ -3754,41 +3712,21 @@ SUMA_Boolean SUMA_MakeThresholdOutlines (   SUMA_OVERLAYS *Sover )
          }
       }
    }
+   #if 0
    Sover->N_NodeDef = cnt;
    Sover->FullList = NOPE;
+#endif   
+   // #if 0
 
    /* colorizing */
-   if ( (Opt->interpmode == SUMA_DIRECT)&&
-        (SUMA_is_Label_dset(Sover->dset_link,NULL) ||
-         SUMA_is_Label_dset_col(Sover->dset_link, Opt->find)) ) {
-      SUMA_LH("Scaling a la HASH");
-      if (!SUMA_ScaleToMap_alaHASH (Sover->V, SDSET_VECFILLED(Sover->dset_link),
-                                    ColMap, Opt,
-                                    SV) ) {
-         SUMA_SL_Err("Failed in SUMA_ScaleToMap_alaHASH");
-         SUMA_RETURN(NOPE);
-      }
-   } else if (Opt->alaAFNI) {
-      /* a la AFNI */
-      SUMA_LH("Scaling a la AFNI");
-      if (!SUMA_ScaleToMap_alaAFNI (Sover->V, SDSET_VECFILLED(Sover->dset_link),
-                              SUMA_LARG_ABS(Opt->IntRange[0], Opt->IntRange[1]),
-                                    ColMap, Opt,
-                                    SV) ) {
-         SUMA_SL_Err("Failed in SUMA_ScaleToMap_alaAFNI");
-         SUMA_RETURN(NOPE);
-      }
-   } else {
-      /* a la SUMA */
-      SUMA_LHv("Scaling a la SUMA %f %f\n", Opt->IntRange[0], Opt->IntRange[1]);
-      if (!SUMA_ScaleToMap( Sover->V, SDSET_VECFILLED(Sover->dset_link),
-                            Opt->IntRange[0], Opt->IntRange[1],
-                            ColMap, Opt,
-                            SV) ){
-         SUMA_SL_Err("Failed in  SUMA_ScaleToMap");
-         SUMA_RETURN(NOPE);
-      }
-   }
+  SUMA_LHv("Scaling a la SUMA %f %f\n", Opt->IntRange[0], Opt->IntRange[1]);
+  if (!SUMA_ScaleToMap( Sover->V, SDSET_VECFILLED(Sover->dset_link),
+                        Opt->IntRange[0], Opt->IntRange[1],
+                        ColMap, Opt,
+                        SV) ){
+     SUMA_SL_Err("Failed in  SUMA_ScaleToMap");
+     SUMA_RETURN(NOPE);
+  }
 
    /* Do we need to create contours */
    if (Opt->ColsContMode) {
@@ -3805,6 +3743,8 @@ SUMA_Boolean SUMA_MakeThresholdOutlines (   SUMA_OVERLAYS *Sover )
    SUMA_LH("Cleanup, cleanup, everybody cleanup");
    if (B) SUMA_free(B); B = NULL;
    if (SV)  SUMA_Free_ColorScaledVect (SV); SV = NULL;
+   
+   // #endif
 
    SUMA_RETURN(YUP);
 }
