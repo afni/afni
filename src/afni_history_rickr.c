edfef131255222575bba7ec98c6dcc3fc2228fae
rick reynolds <reynoldr@mail.nih.gov>
2025-12-04 12:06:21 -0500

    plug_3dsvm: cast function pointers

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -53,6 +53,11 @@
 
 afni_history_struct rickr_history[] = {
 
+ {  4, Dec, 2025, RCR, "plug_3dsvm", MICRO, TYPE_MODIFY,
+   "cast function pointers to expected types in plug_3dsvm.c",
+   NULL
+ } ,
+
  { 18, Nov, 2025, RCR, "suma", MICRO, TYPE_BUG_FIX,
    "fix volume ray-trace voxel selection thresholding",
    "Thanks to Z Saad for bringing up the issue and cause :\n"
