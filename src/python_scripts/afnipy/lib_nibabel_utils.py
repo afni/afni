e6bfe8fbad56d6c53622776f8251f5f928e831c1
Paul Taylor <neon.taylor@gmail.com>
2025-12-10 19:06:34 -0500

    lib_nibabel_utils.py: add a function to write an array out, but need to stabilize header part more

--- src/python_scripts/afnipy/lib_nibabel_utils.py
+++ src/python_scripts/afnipy/lib_nibabel_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import numpy   as     np
 import nibabel as     nib
 
@@ -10,14 +11,9 @@ from   afnipy  import afni_base     as ab
 # assorted I/O and other tools for going back and forth from
 # volumetric datasets, via nibabel
 
-
-# NOTE
-# *** probably want to control types differently!  
-# *** not sure nibabel always picks a good type
-
-
 # ===============================================================================
 
+# read in: dset vol -> python arr
 class BabelObj:
     """Object for reading in volumetric dataset (either NIFTI or
 BRIK/HEAD) into the Python realm, using nibabel plus enhancements with
@@ -139,9 +135,127 @@ bo : BabelObj
     @property
     def nb_data_arr(self):
         """shortcut to getting a copy of the nibabel data array (no parentheses)"""
-        # *** probably want to control types differently!  
-        # *** not sure nibabel always picks a good type
         return np.asanyarray(self.nibobj.dataobj).astype(self.nb_data_dtype)
+
+# ------------------------------------------------------------------------------
+
+# write out: python arr -> dset vol
+
+# *** this function is a work in progress, still needing more thought
+# *** about getting all header info safe and correct.
+def write_arr_to_nifti_simple(A, prefix, head=None, overwrite=True,
+                              verb=1):
+    """Take an array A and write it out to a NIFTI dataset, hopefully (if
+not necessarily) guided by having a header from an associated (e.g.,
+input or upstream) dataset.
+
+A key part of this program when providing a header is to ensure that
+fields that *might* have changed during processing (e.g., number of
+volumes, data_type, cal_min, cal_max, etc.). This function works for
+the 'simple' class of cases where voxel and/or matrix dimensions will
+*not* need to be adjusted.
+
+We have hardwired in *some* data consistency checks, as much as
+possible (like, do spatial dims of A match with those of head?).  We
+also hardwire in replacing elements of the final dataset header that
+might require being updated, based on the array A (like data_type).
+
+If the prefix (=output dset name) does not end with '.nii' or
+'.nii.gz', such a prefix will be automatically added.
+
+Parameters
+----------
+A : numpy array (likely 3D or 4D)
+    data to be written out as a NIFTI dataset
+prefix : str
+    output dset filename
+head : nibabel NIFTI header
+    header obtained earlier or from some other related dset using nibabel
+overwrite : bool
+    flag to overwrite data with pre-existing name
+verb : int
+    verbosity level
+
+Returns
+-------
+ecode : int
+    exit code, 0=success and 1=failure
+
+    """
+
+    # header info is required
+    if head is None :
+        msg = "At present, this function *requires* user to provide "
+        msg+= "header info via the head kwarg"
+        ab.EP(msg)
+
+    # output dset must be nifti
+    if not(prefix.endswith('.nii')) and not(prefix.endswith('.nii.gz')) :
+        prefix+= '.nii.gz'
+
+    # check shape
+    SA = np.shape(A)
+    if len(SA) not in [3, 4] :
+        msg = "Array does not have 3 or 4 dims, "
+        msg+= "instead {}: {}".format(len(SA), ', '.join(SA))
+        ab.EP(msg)
+
+    # ... and shape consistency between first 3 dims of A and head info
+    SH = head.get_data_shape()
+    for ii in range(3) :
+        if SA[ii] != SH[ii] :
+            msg = "First 3 dims are not consistent for array and header:\n"
+            msg+= "array dims : {}\n".format(', '.join(SA[:3]))
+            msg+= "head dims  : {}\n".format(', '.join(SH[:3]))
+            ab.EP(msg)
+
+    # overwriting or not
+    if os.path.isfile(prefix) :
+        if not(overwrite) :
+            msg = "Cannot write this dataset, since"
+            msg+= "it exists already and overwrite was not used:\n"
+            msg+= prefix
+            ab.EP(msg)
+        else:
+            msg = "Writing this dataset, which overwrites an existing file:\n"
+            msg+= prefix
+            ab.WP(msg)
+
+    # get affine from header: different method whether AFNI or NIFTI header
+    # Q: how to get all header info right, bc AFNIheader and NIFTIheader have
+    #    some different info; here is one example of navigating differences
+    #    need to think about sform_code and qform_code carefully, bc nibabel
+    #    just defaults to qform_code=0 and sform_code=1; 
+    # NB0 : I wonder if 3dinfo should output qform_code and sform_code values
+    #       directly, so that we can get those from a single source---the C code
+    #       rather than re-use rules here for them.
+    try: 
+        # AFNI header method
+        Haff = head.get_affine()
+    except:
+        # NIFTI header method
+        Haff = head.get_best_affine()
+
+    # this should work whether head is AFNIheader or Nifti?Header type;
+    # get element datatype info from array itself
+    # NB1: using header=head helps qform_code and sform_code be correct for
+    #      NIFTI is input, but not when AFNI is input
+    # NB2: using header=head helps xyzt_units be correct; without it, it is 0
+    # NB3: using header=head helps pixdim[5:] be more like input values, which
+    #      were uniformly 0.0; without it, they appeared to each be 1.0
+    # NB4: when using header=head, the regular field value was 'r'; without it,
+    #      the field value was empty
+    # NB5: if we use header=head, we should remember to purge the extensions
+    # NB6: still need to check consequences of using header=head if we have 
+    #      changed the number of volumes in A vs what the dset of head had
+    nifti_img = nib.Nifti1Image(A, Haff, header=head,
+                                dtype=A.dtype)
+    # *** might still need to set_qform() and set_sform()
+
+    nifti_img.to_filename(prefix)
+    ###nib.save(ovol, prefix)
+
+    return 0
 
 # ============================================================================
 
@@ -158,7 +272,14 @@ if __name__ == "__main__" :
     dset2 = '~/AFNI_data6/afni/mask.auto.nii.gz'
     bo2   = BabelObj(dset2)
 
+    A2  = bo2.nb_data_arr
+    H2  = bo2.nb_header
+    ok2 = write_arr_to_nifti_simple(A2, 'ex2.nii', head=H2, overwrite=True)
+
     # float
-    dset3 ='~/AFNI_data6/afni/mask.auto.nii.gz'
+    dset3 ='~/AFNI_data6/afni/func_slim+orig.HEAD'
     bo3   = BabelObj(dset3)
 
+    A3  = bo3.nb_data_arr
+    H3  = bo3.nb_header
+    ok3 = write_arr_to_nifti_simple(A3, 'ex3.nii.gz', head=H3, overwrite=True)
