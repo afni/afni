204988b36ce162537e3a56131054b5ede53dd336
rick reynolds <reynoldr@mail.nih.gov>
2025-12-16 12:29:18 -0500

    afni_history

--- src/afni_history_rickr.c
+++ src/afni_history_rickr.c
@@ -53,6 +53,17 @@
 
 afni_history_struct rickr_history[] = {
 
+ { 16, Dec, 2025, RCR, "3dresample", MICRO, TYPE_MODIFY,
+   "allow resampling to stay on current voxel grid, as much as possible",
+   "Add -bound_type CENT/CENT_ORIG, add -up/downsample, -delta_scale.\n"
+   "Done for PT."
+ } ,
+
+ {  5, Dec, 2025, RCR, "afni_system_check.py", MICRO, TYPE_ENHANCE,
+   "warn if the 'phia' R library has version 0.3.1",
+   NULL
+ } ,
+
  {  4, Dec, 2025, RCR, "plug_3dsvm", MICRO, TYPE_MODIFY,
    "cast function pointers to expected types in plug_3dsvm.c",
    NULL
