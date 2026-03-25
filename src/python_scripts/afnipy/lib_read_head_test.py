603712960f2a24229e0e449c30bd635d8c0f07f1
mrneont <neon.taylor@gmail.com>
2026-03-25 10:39:28 -0400

    lib_read_head_test.py: single comparison function

--- src/python_scripts/afnipy/lib_read_head_test.py
+++ src/python_scripts/afnipy/lib_read_head_test.py
@@ -16,6 +16,58 @@ from afnipy import afni_base     as AB
 
 # ============================================================================
 
+def verify_lib_read_head_attr_file(inset, prefix_lrh=None, prefix_3dA=None,
+                                   prefix_diff=None, verb=1):
+    """Provide the name of a BRIK/HEAD file called inset, and then use
+'3dAttribute -all ...' to create a report file of attributes, which is
+saved as the name prefix.
+
+Parameters
+----------
+inset : str
+    name of input BRIK/HEAD file
+prefix_lrh : str
+    name of output text report file from lib_read_head.py functions
+prefix_3dA : str
+    name of output text report file from '3dAttribute -all ...'
+prefix_diff : str
+    name of output diff file of reports
+verb : int
+    verbosity level
+
+Returns
+-------
+is_fail : int
+    0 for success, nonzero for failure
+
+"""
+
+    BAD_RETURN = -1
+
+    inset_full     = os.path.expanduser(inset)
+    pref_lrh_full  = os.path.expanduser(prefix_lrh)
+    pref_3dA_full  = os.path.expanduser(prefix_3dA)
+    pref_diff_full = os.path.expanduser(prefix_diff)
+
+    # ----- make reports
+
+    is_fail = create_report_3dAttribute(inset_full, pref_lrh_full)
+    if is_fail :
+        return BAD_RETURN
+    
+    is_fail = create_report_read_head(inset_full, pref_3dA_full)
+    if is_fail :
+        return BAD_RETURN
+
+    # ----- make differences
+
+    is_fail = compare_report_files(pref_lrh_full, pref_3dA_full,
+                                      prefix_diff=pref_diff_full)
+    if is_fail :
+        return BAD_RETURN
+
+    return 0    
+
 def compare_report_files(reportA, reportB, prefix_diff=None, verb=1):
     """Compare two attribute report files, reportA and reportB, presumably
 made by different functionality (like 3dAttribute and lib_read_head.py). 
@@ -139,17 +191,13 @@ is_fail : int
 
 if __name__ == "__main__" :
 
-    inset = '~/AFNI_data6/FT_analysis/FT.results/FT_anat+orig.HEAD'
-    rep00 =  '~/AFNI_data6/FT_analysis/FT.results/report_00_3dA.txt'
-    rep01 =  '~/AFNI_data6/FT_analysis/FT.results/report_01_lrh.txt'
-    diff1 =  '~/AFNI_data6/FT_analysis/FT.results/DIFF_00_01.txt'
+    inset   = '~/AFNI_data6/FT_analysis/FT.results/FT_anat+orig.HEAD'
+    rep_3dA = '~/AFNI_data6/FT_analysis/FT.results/report_01_3dA.txt'
+    rep_lrh = '~/AFNI_data6/FT_analysis/FT.results/report_01_lrh.txt'
+    diff_01 = '~/AFNI_data6/FT_analysis/FT.results/DIFF_01.txt'
     
+    is_fail = verify_lib_read_head_attr_file(inset,
+                                             prefix_lrh=rep_lrh, 
+                                             prefix_3dA=rep_3dA,
+                                             prefix_diff=diff_01)
 
-    is_fail_00 = create_report_3dAttribute(inset, rep00)
-    is_fail_01 = create_report_read_head(inset, rep01)
-
-    is_fail_10 = compare_report_files(rep00, rep01, prefix_diff=diff1)
-
-    print("++ Check out these text files:")
-    print("   reports   : {} {}".format(rep00, rep01))
-    print("   diff file : {}".format(diff1))
